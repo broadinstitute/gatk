9e42ae6567d085c7418b4f2f2a200a67b4db9bfe
Louis Bergelson <louisb@broadinstitute.org>
2015-03-19 14:26:48 -0400

    wip

--- src/main/java/org/broadinstitute/hellbender/tools/dataflow/FlagStatDataflow.java
+++ src/main/java/org/broadinstitute/hellbender/tools/dataflow/FlagStatDataflow.java
@@ -1,13 +1,17 @@
 package org.broadinstitute.hellbender.tools.dataflow;
 
 
+import com.google.api.services.genomics.model.Read;
 import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.coders.CoderRegistry;
+import com.google.cloud.dataflow.sdk.coders.SerializableCoder;
 import com.google.cloud.dataflow.sdk.io.TextIO;
 import com.google.cloud.dataflow.sdk.transforms.Combine;
 import com.google.cloud.dataflow.sdk.transforms.Combine.AccumulatingCombineFn.Accumulator;
 import com.google.cloud.dataflow.sdk.transforms.Create;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.values.PCollection;
+import com.google.cloud.genomics.dataflow.readers.bam.ReadConverter;
 import htsjdk.samtools.SAMRecord;
 import org.broadinstitute.hellbender.cmdline.Argument;
 import org.broadinstitute.hellbender.cmdline.ArgumentCollection;
@@ -21,6 +25,8 @@ import org.broadinstitute.hellbender.engine.dataflow.DataflowTool;
 import org.broadinstitute.hellbender.tools.FlagStat;
 
 import java.util.logging.Logger;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @CommandLineProgramProperties(usage="runs FlagStat on dataflow", usageShort = "FlagStat", programGroup = DataFlowProgramGroup.class)
 public class FlagStatDataflow extends DataflowTool {
@@ -38,12 +44,19 @@ public class FlagStatDataflow extends DataflowTool {
     @Argument
     private String outputFile;
 
+
+
     @Override
     protected void setupPipeline(Pipeline pipeline) {
+        CoderRegistry cr = pipeline.getCoderRegistry();
+        cr.registerCoder(SAMRecord.class, SerializableCoder.class);
 
-        ReadsDataSource reads = new ReadsDataSource(readsArguments.readFiles);
+        ReadsDataSource samReads = new ReadsDataSource(readsArguments.readFiles);
+        Stream<Read> reads = StreamSupport.stream(samReads.spliterator(), false)
+                .map(ReadConverter::makeRead);
 
-        PCollection<SAMRecord> preads = pipeline.apply(Create.of(reads));
+
+        PCollection< Read > preads = pipeline.apply(Create.of(reads.iterator()));
         //preads.apply(ParDo.of(new readsToCount()));
         preads.apply(Combine.globally(new CombineCounts())).apply(TextIO.Write.to(outputFile));
 
