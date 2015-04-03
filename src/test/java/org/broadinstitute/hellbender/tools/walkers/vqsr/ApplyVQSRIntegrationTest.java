f8496dcb49cbda00216535a1297845e9f06fb0c4
Adam Kiezun <akiezun@gmail.com>
2015-04-03 17:03:42 -0400

    apply vqsr

---
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/ApplyVQSRIntegrationTest.java
@@ -0,0 +1,49 @@
+package org.broadinstitute.hellbender.tools.walkers.vqsr;
+
+import org.broadinstitute.hellbender.CommandLineProgramTest;
+import org.broadinstitute.hellbender.tools.IntegrationTestSpec;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class ApplyVQSRIntegrationTest extends CommandLineProgramTest{
+
+    @Test
+    public void testApplyRecalibrationSnpAndIndelTogether() throws IOException {
+        IntegrationTestSpec spec = new IntegrationTestSpec(
+                        " -L 20:1000100-1000500" +
+                        " -mode BOTH" +
+                        " --no_cmdline_in_header" +
+                        " -input " + getToolTestDataDir() + "VQSR.mixedTest.input" +
+                        " -o %s" +
+                        " -tranchesFile " + getToolTestDataDir() + "VQSR.mixedTest.tranches" +
+                        " -recalFile " + getToolTestDataDir() + "VQSR.mixedTest.recal",
+                Arrays.asList("expected.txt")
+                );
+        spec.executeTest("testApplyRecalibrationSnpAndIndelTogether", this);
+    }
+//
+//    @Test(enabled = true)
+//    public void testApplyRecalibrationSnpAndIndelTogetherExcludeFiltered() throws Exception {
+//        final String base = "-R " + b37KGReference +
+//                " -T ApplyRecalibration" +
+//                " -L 20:1000100-1000500" +
+//                " -mode BOTH" +
+//                " --excludeFiltered -ts_filter_level 90.0" +
+//                " --no_cmdline_in_header" +
+//                " -input " + privateTestDir + "VQSR.mixedTest.input" +
+//                " -o %s" +
+//                " -tranchesFile " + privateTestDir + "VQSR.mixedTest.tranches" +
+//                " -recalFile " + privateTestDir + "VQSR.mixedTest.recal";
+//
+//        final WalkerTestSpec spec = new WalkerTestSpec(base, 1, Arrays.asList(""));
+//        final List<File> outputFiles = executeTest("testApplyRecalibrationSnpAndIndelTogether", spec).getFirst();
+//        final File VCF = outputFiles.get(0);
+//        for( final VariantContext VC : VCIterable.readAllVCs(VCF, new VCFCodec()).getSecond() ) {
+//            if( VC != null ) {
+//                Assert.assertTrue(VC.isNotFiltered()); // there should only be unfiltered records in the output VCF file
+//            }
+//        }
+//    }
+}
