bce58731b67230daf69d5605dd58c2db53a57836
David Adams <david.adams.phd@gmail.com>
2015-05-12 08:52:04 -0400

    Adding a Variant interface and a skeleton implementation.

---
+++ src/main/java/org/broadinstitute/hellbender/utils/variant/Variant.java
@@ -0,0 +1,14 @@
+package org.broadinstitute.hellbender.utils.variant;
+
+import htsjdk.samtools.util.Locatable;
+
+/**
+ * Variant is (currently) a minimal variant interface needed by the Hellbender pipeline.
+ * This will be expanded as more methods are needed.
+ * NOTE: getStart() and getEnd() are 1-base inclusive (which matches the current GATK tools).
+ * This does not match the GA4GH spec.
+ */
+public interface Variant extends Locatable {
+    boolean isSnp();
+    boolean isIndel();
+}
