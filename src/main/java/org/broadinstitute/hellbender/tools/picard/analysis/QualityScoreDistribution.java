fcc6d53bef935773b8df00d66dc1974626d0b9eb
Louis Bergelson <louisb@broadinstitute.org>
2015-05-12 14:41:59 -0400

    parameterizing R plotting

--- src/main/java/org/broadinstitute/hellbender/tools/picard/analysis/QualityScoreDistribution.java
+++ src/main/java/org/broadinstitute/hellbender/tools/picard/analysis/QualityScoreDistribution.java
@@ -44,6 +44,9 @@ public final class QualityScoreDistribution extends SinglePassSamProgram {
     @Argument(doc="If set to true, include quality for no-call bases in the distribution.")
     public boolean INCLUDE_NO_CALLS = false;
 
+    @Argument(doc = "Should an output plot be created")
+    public boolean PRODUCE_PLOT = false;
+
     private final long[] qCounts  = new long[128];
     private final long[] oqCounts = new long[128];
 
@@ -107,7 +110,7 @@ public final class QualityScoreDistribution extends SinglePassSamProgram {
         if (qHisto.isEmpty() && oqHisto.isEmpty()) {
             log.warn("No valid bases found in input file. No plot will be produced.");
         }
-        else {
+        else if(PRODUCE_PLOT){
             // Now run R to generate a chart
             final RScriptExecutor executor = new RScriptExecutor();
             executor.addScript(new Resource(R_SCRIPT, QualityScoreDistribution.class));
