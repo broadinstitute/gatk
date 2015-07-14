lessc0de/hellbender

Adding coders and switching to our UUID solution for determinism.
More UUID stuff.

Adding more tests and Mockito.

Roughly finished up testing and a little cleanup.

A few more changes to the overall pipeline.

Adding something for DavidR :)

Test clean up.

Migrate code to new read interface

Cleaning up tests after rebase.

Source and test clean up.

A few more changes..

Conflicts:
	build.gradle
	src/main/java/org/broadinstitute/hellbender/utils/dataflow/DataflowUtils.java

src/test/java/org/broadinstitute/hellbender/engine/dataflow/datasources/ReferenceShardTest.java
@@ -1,0 +1,66 @@
+package org.broadinstitute.hellbender.engine.dataflow.datasources;
+
+import com.google.appengine.repackaged.com.google.common.collect.Lists;
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.testing.DataflowAssert;
+import com.google.cloud.dataflow.sdk.testing.TestPipeline;
+import com.google.cloud.dataflow.sdk.transforms.Create;
+import com.google.cloud.dataflow.sdk.values.KV;
+import com.google.cloud.dataflow.sdk.values.PCollection;
+import com.google.cloud.genomics.dataflow.readers.bam.ReadConverter;
+import org.broadinstitute.hellbender.utils.read.ArtificialReadUtils;
+import org.broadinstitute.hellbender.utils.read.GoogleGenomicsReadToGATKReadAdapter;
+import org.broadinstitute.hellbender.utils.read.MutableGATKRead;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+public final class ReferenceShardTest {
+
+    @DataProvider(name = "reads")
+    public Object[][] reads(){
+        List<MutableGATKRead> reads = Lists.newArrayList(makeRead(1, 300, 1), makeRead(100000, 10, 2), makeRead(299999, 2, 3));
+        List<ReferenceShard> referenceShards = Lists.newArrayList(new ReferenceShard(0, "1"), new ReferenceShard(1, "1"), new ReferenceShard(2, "1"));
+        return new Object[][]{
+                {reads, referenceShards},
+        };
+    }
+
+    @DataProvider(name = "refShards")
+    public Object[][] refShards(){
+        List<ReferenceShard> shards = Lists.newArrayList(new ReferenceShard(0, "1"), new ReferenceShard(1, "1"), new ReferenceShard(2, "1"));
+        return new Object[][]{
+                {shards},
+        };
+    }
+
+    public MutableGATKRead makeRead(int start, int length, int i) {
+        return ArtificialReadUtils.createRandomRead(start, length, i);
+    }
+
+    @Test(dataProvider = "reads")
+    public void getVariantShardsFromIntervalTest(List<MutableGATKRead> reads, List<ReferenceShard> shards) {
+        for (int i = 0; i < reads.size(); ++i) {
+            MutableGATKRead r = reads.get(i);
+            ReferenceShard expectedShard = shards.get(i);
+            ReferenceShard foundShard = ReferenceShard.getShardNumberFromInterval(r);
+            Assert.assertEquals(foundShard, expectedShard);
+        }
+    }
+
+    @Test(dataProvider = "refShards")
+    public void createRefPCollectionTest(List<ReferenceShard> shards) {
+        Pipeline p = TestPipeline.create();
+        p.getCoderRegistry().registerCoder(ReferenceShard.class, ReferenceShard.CODER);
+
+        List<ReferenceShard> shards1 = Lists.newArrayList(shards.iterator());
+        Assert.assertEquals(shards, shards1);
+        PCollection<ReferenceShard> pVariants = p.apply(Create.of(shards));
+        DataflowAssert.that(pVariants).containsInAnyOrder(shards1);
+
+        p.run();
+    }
+}

src/test/java/org/broadinstitute/hellbender/engine/dataflow/datasources/VariantShardTest.java
@@ -1,0 +1,44 @@
+package org.broadinstitute.hellbender.engine.dataflow.datasources;
+
+import com.google.appengine.repackaged.com.google.common.collect.Lists;
+import com.google.cloud.dataflow.sdk.values.KV;
+import com.google.cloud.genomics.dataflow.readers.bam.ReadConverter;
+import org.broadinstitute.hellbender.utils.read.ArtificialReadUtils;
+import org.broadinstitute.hellbender.utils.read.GoogleGenomicsReadToGATKReadAdapter;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.broadinstitute.hellbender.utils.read.MutableGATKRead;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+public final class VariantShardTest {
+
+    @DataProvider(name = "variantShards")
+    public Object[][] variantShards(){
+        List<GATKRead> reads = Lists.newArrayList(makeRead(1, 300, 1), makeRead(100000, 10, 2), makeRead(299999, 2, 3));
+        List<Iterable<VariantShard>> variantShards = Lists.newArrayList();
+        variantShards.add(Lists.newArrayList(new VariantShard(0, "1")));
+        variantShards.add(Lists.newArrayList(new VariantShard(1, "1")));
+        variantShards.add(Lists.newArrayList(new VariantShard(2, "1"), new VariantShard(3, "1")));
+        return new Object[][]{
+                {reads, variantShards},
+        };
+    }
+
+    public MutableGATKRead makeRead(int start, int length, int i) {
+        return ArtificialReadUtils.createRandomRead(start, length, i);
+    }
+
+    @Test(dataProvider = "variantShards")
+    public void getVariantShardsFromIntervalTest(List<GATKRead> reads, List<Iterable<VariantShard>> shards) {
+        for (int i = 0; i < reads.size(); ++i) {
+            GATKRead r = reads.get(i);
+            Iterable<VariantShard> expectedShards = shards.get(i);
+            List<VariantShard> foundShards = VariantShard.getVariantShardsFromInterval(r);
+            Assert.assertEquals(foundShards, expectedShards);
+        }
+    }
+}

broadinstitute/gatk

	src/main/java/org/broadinstitute/hellbender/dev/tools/walkers/bqsr/BaseRecalibratorDataflow.java
	src/main/java/org/broadinstitute/hellbender/engine/dataflow/coders/VariantCoder.java
	src/main/java/org/broadinstitute/hellbender/engine/dataflow/transforms/GoogleGenomicsReadToGATKRead.java
	src/main/java/org/broadinstitute/hellbender/tools/dataflow/pipelines/DataflowReadsPipeline.java
	src/main/java/org/broadinstitute/hellbender/utils/read/ArtificialReadUtils.java
	src/test/java/org/broadinstitute/hellbender/engine/dataflow/ReadsDataflowSourceTest.java
	src/test/java/org/broadinstitute/hellbender/tools/dataflow/transforms/CountBasesTransformUnitTest.java
	src/test/java/org/broadinstitute/hellbender/tools/dataflow/transforms/PrintReadsTransformUnitTest.java
	src/test/java/org/broadinstitute/hellbender/utils/dataflow/DataflowUtilsUnitTest.java

src/test/java/org/broadinstitute/hellbender/engine/dataflow/datasources/ReferenceShardTest.java
@@ -1,0 +1,67 @@
+package org.broadinstitute.hellbender.engine.dataflow.datasources;
+
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.testing.DataflowAssert;
+import com.google.cloud.dataflow.sdk.testing.TestPipeline;
+import com.google.cloud.dataflow.sdk.transforms.Create;
+import com.google.cloud.dataflow.sdk.values.KV;
+import com.google.cloud.dataflow.sdk.values.PCollection;
+import com.google.cloud.genomics.dataflow.readers.bam.ReadConverter;
+import com.google.common.collect.Lists;
+import org.broadinstitute.hellbender.utils.read.ArtificialReadUtils;
+import org.broadinstitute.hellbender.utils.read.GoogleGenomicsReadToGATKReadAdapter;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public final class ReferenceShardTest {
+
+    @DataProvider(name = "reads")
+    public Object[][] reads(){
+        List<GATKRead> reads = Arrays.asList(makeRead(1, 300, 1), makeRead(100000, 10, 2), makeRead(299999, 2, 3));
+        List<ReferenceShard> referenceShards = Arrays.asList(new ReferenceShard(0, "1"), new ReferenceShard(1, "1"), new ReferenceShard(2, "1"));
+        return new Object[][]{
+                {reads, referenceShards},
+        };
+    }
+
+    @DataProvider(name = "refShards")
+    public Object[][] refShards(){
+        List<ReferenceShard> shards = Arrays.asList(new ReferenceShard(0, "1"), new ReferenceShard(1, "1"), new ReferenceShard(2, "1"));
+        return new Object[][]{
+                {shards},
+        };
+    }
+
+    public GATKRead makeRead(int start, int length, int i) {
+        return ArtificialReadUtils.createRandomRead(start, length, i);
+    }
+
+    @Test(dataProvider = "reads")
+    public void getVariantShardsFromIntervalTest(List<GATKRead> reads, List<ReferenceShard> shards) {
+        for (int i = 0; i < reads.size(); ++i) {
+            GATKRead r = reads.get(i);
+            ReferenceShard expectedShard = shards.get(i);
+            ReferenceShard foundShard = ReferenceShard.getShardNumberFromInterval(r);
+            Assert.assertEquals(foundShard, expectedShard);
+        }
+    }
+
+    @Test(dataProvider = "refShards")
+    public void createRefPCollectionTest(List<ReferenceShard> shards) {
+        Pipeline p = TestPipeline.create();
+        p.getCoderRegistry().registerCoder(ReferenceShard.class, ReferenceShard.CODER);
+
+        List<ReferenceShard> shards1 = Lists.newArrayList(shards.iterator());
+        Assert.assertEquals(shards, shards1);
+        PCollection<ReferenceShard> pVariants = p.apply(Create.of(shards));
+        DataflowAssert.that(pVariants).containsInAnyOrder(shards1);
+
+        p.run();
+    }
+}

src/test/java/org/broadinstitute/hellbender/engine/dataflow/datasources/VariantShardTest.java
@@ -1,0 +1,44 @@
+package org.broadinstitute.hellbender.engine.dataflow.datasources;
+
+import com.google.cloud.dataflow.sdk.values.KV;
+import com.google.cloud.genomics.dataflow.readers.bam.ReadConverter;
+import org.broadinstitute.hellbender.utils.read.ArtificialReadUtils;
+import org.broadinstitute.hellbender.utils.read.GoogleGenomicsReadToGATKReadAdapter;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public final class VariantShardTest {
+
+    @DataProvider(name = "variantShards")
+    public Object[][] variantShards(){
+        List<GATKRead> reads = Arrays.asList(makeRead(1, 300, 1), makeRead(100000, 10, 2), makeRead(299999, 2, 3));
+        List<Iterable<VariantShard>> variantShards = Arrays.asList();
+        variantShards.add(Arrays.asList(new VariantShard(0, "1")));
+        variantShards.add(Arrays.asList(new VariantShard(1, "1")));
+        variantShards.add(Arrays.asList(new VariantShard(2, "1"), new VariantShard(3, "1")));
+        return new Object[][]{
+                {reads, variantShards},
+        };
+    }
+
+    public GATKRead makeRead(int start, int length, int i) {
+        return ArtificialReadUtils.createRandomRead(start, length, i);
+    }
+
+    @Test(dataProvider = "variantShards")
+    public void getVariantShardsFromIntervalTest(List<GATKRead> reads, List<Iterable<VariantShard>> shards) {
+        for (int i = 0; i < reads.size(); ++i) {
+            GATKRead r = reads.get(i);
+            Iterable<VariantShard> expectedShards = shards.get(i);
+            List<VariantShard> foundShards = VariantShard.getVariantShardsFromInterval(r);
+            Assert.assertEquals(foundShards, expectedShards);
+        }
+    }
+}