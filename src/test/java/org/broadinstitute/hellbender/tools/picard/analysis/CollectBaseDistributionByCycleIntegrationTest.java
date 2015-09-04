e66f7c8e7e179f9ef90be19517a813d570881a9b
David E. Kling <dkling@broadinstitute.org>
2015-09-04 08:43:21 -0400

    Fixed file path for CollectBaseDistributionByCycleIntegrationTest

--- src/test/java/org/broadinstitute/hellbender/tools/picard/analysis/CollectBaseDistributionByCycleIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/picard/analysis/CollectBaseDistributionByCycleIntegrationTest.java
@@ -13,7 +13,7 @@ import htsjdk.samtools.metrics.MetricsFile;
 public final class CollectBaseDistributionByCycleIntegrationTest extends CommandLineProgramTest {
     private static final File TEST_DATA_DIR = new File(getTestDataDir(), "picard/analysis/CollectBaseDistributionByCycle");
 
-    @Test(groups = {"R"})
+    @Test
     public void test() throws IOException {
         final File input = new File(TEST_DATA_DIR, "first5000a.bam");
         final File expectedFile = new File(TEST_DATA_DIR, "CollectBaseDistributionByCycle.txt");
