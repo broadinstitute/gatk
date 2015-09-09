905560a133942a7a794c1c6bd83ee7ac7ceef0e4
David E. Kling <dkling@broadinstitute.org>
2015-09-09 14:50:35 -0400

    Tested additional parameters including METRIC_ACCUMULATION_LEVEL {ALL_READS, SAMPLE, LIBRARY, READ_GROUP}, and used a smaller interval file.

--- src/test/java/org/broadinstitute/hellbender/tools/picard/analysis/directed/CollectTargetedPcrMetricsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/picard/analysis/directed/CollectTargetedPcrMetricsIntegrationTest.java
@@ -19,18 +19,22 @@ public final class CollectTargetedPcrMetricsIntegrationTest extends CommandLineP
     public void testCollect () throws IOException {
 
         final File input = new File(TEST_DATA_PATH, "first5000a.bam");
-        final File amplicon_intervals = new File(TEST_DATA_PATH, "intervallist.vcf");
-        final File target_intervals = new File(TEST_DATA_PATH, "intervallist.vcf");
-        final File expectedFile = new File(TEST_DATA_PATH, "PCRMetrics_2.txt");
+        final File amplicon_intervals = new File(TEST_DATA_PATH, "micro_interval_list.vcf");
+        final File target_intervals = new File(TEST_DATA_PATH, "micro_interval_list.vcf");
+        final File expectedFile = new File(TEST_DATA_PATH, "pcr_Metrics_micro.txt");
         final File outfile = BaseTest.createTempFile("PCRMetrics", ".txt");
         final File reference = new File(TEST_DATA_PATH, "human_b37_20.fasta");
+        final File pertargetcoverage = new File(TEST_DATA_PATH, "pcr_metrics_pertarg_micro.txt");
 
         final String[] args = {
                 "--INPUT", input.getAbsolutePath(),
                 "--AI", amplicon_intervals.getAbsolutePath(),
                 "--TI", target_intervals.getAbsolutePath(),
                 "--OUTPUT", outfile.getAbsolutePath(),
-                "--R", reference.getAbsolutePath() ,
+                "--R", reference.getAbsolutePath(),
+                "--N", "Test_amplicons",
+                "--LEVEL", "ALL_READS",
+                "--PER_TARGET_COVERAGE", pertargetcoverage.getAbsolutePath(),
         };
 
         runCommandLine(args);
