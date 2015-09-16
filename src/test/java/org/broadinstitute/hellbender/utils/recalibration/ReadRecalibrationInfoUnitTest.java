839141871c4ab557e2516c20e473a9aba9ef4c10
David Roazen <droazen@broadinstitute.org>
2015-09-16 17:35:20 -0400

    Move a stray test into the right package

--- src/test/java/org/broadinstitute/hellbender/utils/recalibration/ReadRecalibrationInfoUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/utils/recalibration/ReadRecalibrationInfoUnitTest.java
@@ -1,12 +1,9 @@
-package org.broadinstitute.hellbender.tools.walkers.bqsr;
+package org.broadinstitute.hellbender.utils.recalibration;
 
 import htsjdk.samtools.SAMUtils;
-import org.broadinstitute.hellbender.utils.recalibration.ReadCovariates;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
-import org.broadinstitute.hellbender.utils.recalibration.EventType;
 import org.broadinstitute.hellbender.utils.read.ArtificialReadUtils;
 import org.broadinstitute.hellbender.utils.read.ReadUtils;
-import org.broadinstitute.hellbender.utils.recalibration.ReadRecalibrationInfo;
 import org.broadinstitute.hellbender.utils.test.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
