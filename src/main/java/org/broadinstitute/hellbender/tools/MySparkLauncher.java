ff54f49a863602ad140c56bf97944a9d9be7ab96
David Adams <davidada@google.com>
2015-09-20 08:49:31 -0400

    Finally got it running with reasonable path ordering.

--- src/main/java/org/broadinstitute/hellbender/tools/MySparkLauncher.java
+++ src/main/java/org/broadinstitute/hellbender/tools/MySparkLauncher.java
@@ -9,11 +9,10 @@ import java.io.InputStreamReader;
 public class MySparkLauncher {
     // You must set SPARK_HOME in the environment.
     public static void main(String[] args) throws Exception {
-        String path = "/Users/davidada/apps/spark-1.4.1-bin-hadoop2.6/lib/*:" + new File("build/classes/main").getCanonicalPath();
+        String path = new File("build/classes/main").getCanonicalPath();
         String jarLibPath = new File("build/lib/*").getCanonicalPath();
         System.out.println("starting... " + path);
 
-        //String jarPath = new File("build/libs/hellbender-all-GATK.4.alpha-624-g1c89045-SNAPSHOT-spark.jar").getCanonicalPath();
         String inputBam = new File("src/test/resources/org/broadinstitute/hellbender/tools/BQSR/CEUTrio.HiSeq.WGS.b37.ch20.1m-1m1k.NA12878.bam").getCanonicalPath();
         String inputVcf = new File("src/test/resources/org/broadinstitute/hellbender/tools/BQSR/dbsnp_132.b37.excluding_sites_after_129.chr17_69k_70k.vcf").getCanonicalPath();
         String outputBam = "file:///" + new File("output.bam").getCanonicalPath();
