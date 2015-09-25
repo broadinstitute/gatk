8b7a163b7bb23cd964e5c3bcf307d39fa2186663
Adam Kiezun <akiezun@gmail.com>
2015-09-25 12:30:41 -0400

    print reads on spark and tests

---
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/pipelines/PrintReadsSpark.java
@@ -0,0 +1,84 @@
+package org.broadinstitute.hellbender.tools.spark.pipelines;
+
+import htsjdk.samtools.SAMFileHeader;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.broadinstitute.hellbender.cmdline.Argument;
+import org.broadinstitute.hellbender.cmdline.ArgumentCollection;
+import org.broadinstitute.hellbender.cmdline.CommandLineProgramProperties;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.argumentcollections.IntervalArgumentCollection;
+import org.broadinstitute.hellbender.cmdline.argumentcollections.OptionalIntervalArgumentCollection;
+import org.broadinstitute.hellbender.cmdline.argumentcollections.ReadInputArgumentCollection;
+import org.broadinstitute.hellbender.cmdline.argumentcollections.RequiredReadInputArgumentCollection;
+import org.broadinstitute.hellbender.cmdline.programgroups.SparkProgramGroup;
+import org.broadinstitute.hellbender.engine.spark.SparkCommandLineProgram;
+import org.broadinstitute.hellbender.engine.spark.datasources.ReadsSparkSink;
+import org.broadinstitute.hellbender.engine.spark.datasources.ReadsSparkSource;
+import org.broadinstitute.hellbender.exceptions.GATKException;
+import org.broadinstitute.hellbender.exceptions.UserException;
+import org.broadinstitute.hellbender.utils.IntervalUtils;
+import org.broadinstitute.hellbender.utils.SimpleInterval;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.broadinstitute.hellbender.utils.read.ReadsWriteFormat;
+
+import java.io.IOException;
+import java.util.List;
+
+@CommandLineProgramProperties(summary = "Print reads from the input BAM", oneLineSummary = "Print reads from the input BAM", programGroup = SparkProgramGroup.class)
+public final class PrintReadsSpark extends SparkCommandLineProgram {
+
+    public static final String SHARDED_OUTPUT_LONG_ARG = "shardedOutput";
+    public static final String SHARDED_OUTPUT_SHORT_ARG = "shardedOutput";
+
+    private static final long serialVersionUID = 1L;
+
+    @ArgumentCollection
+    public ReadInputArgumentCollection readArguments= new RequiredReadInputArgumentCollection();
+
+    @ArgumentCollection
+    public IntervalArgumentCollection intervalArgumentCollection = new OptionalIntervalArgumentCollection();
+
+    @Argument(doc = "uri for the output file: a local file path",
+            shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME, fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME,
+            optional = false)
+    public String output;
+
+    @Argument(doc = "If specified, shard the output bam", shortName = SHARDED_OUTPUT_SHORT_ARG, fullName = SHARDED_OUTPUT_LONG_ARG, optional = true)
+    public boolean shardedOutput = false;
+
+    @Override
+    protected void runPipeline(final JavaSparkContext ctx) {
+        if ( readArguments.getReadFilesNames().size() != 1 ) {
+            throw new UserException("This tool only accepts a single bam/sam/cram as input");
+        }
+        final String bam = readArguments.getReadFilesNames().get(0);
+
+        final ReadsSparkSource readSource = new ReadsSparkSource(ctx);
+        final SAMFileHeader readsHeader = ReadsSparkSource.getHeader(ctx, bam);
+
+        if (readsHeader.getSortOrder() != SAMFileHeader.SortOrder.coordinate){
+            //https://github.com/broadinstitute/hellbender/issues/929
+            throw new UserException("PrintReadsSpark: Only coordinate-sorted files are currently supported");
+        }
+
+        /*
+         * If no intervals are given, we want all reads, mapped and unmapped.
+         */
+        final List<SimpleInterval> intervals =  intervalArgumentCollection.intervalsSpecified() ?
+                intervalArgumentCollection.getIntervals(readsHeader.getSequenceDictionary()) :
+                null;
+        final JavaRDD<GATKRead> reads= readSource.getParallelReads(bam, intervals);
+
+        try {
+            ReadsSparkSink.writeReads(ctx, output, reads, readsHeader, shardedOutput ? ReadsWriteFormat.SHARDED : ReadsWriteFormat.SINGLE);
+        } catch (final IOException e) {
+            throw new GATKException("unable to write bam: " + e);
+        }
+    }
+
+    @Override
+    protected String getProgramName() {
+        return getClass().getSimpleName();
+    }
+}
---
+++ src/test/java/org/broadinstitute/hellbender/tools/spark/pipelines/PrintReadsSparkIntegrationTest.java
@@ -0,0 +1,66 @@
+package org.broadinstitute.hellbender.tools.spark.pipelines;
+
+import org.broadinstitute.hellbender.CommandLineProgramTest;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.exceptions.UserException;
+import org.broadinstitute.hellbender.utils.read.SamAssertionUtils;
+import org.broadinstitute.hellbender.utils.test.ArgumentsBuilder;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public final class PrintReadsSparkIntegrationTest extends CommandLineProgramTest {
+
+    @Override
+    public String getTestedClassName() {
+        return PrintReadsSpark.class.getSimpleName();
+    }
+
+    @Test
+    public void testCoordinateSorted() throws Exception {
+        final File inBam = new File(getTestDataDir(), "print_reads.sorted.bam");
+        final File outBam = createTempFile("print_reads_spark", ".bam");
+        ArgumentsBuilder args = new ArgumentsBuilder();
+        args.add("--" + StandardArgumentDefinitions.INPUT_LONG_NAME);
+        args.add(inBam.getCanonicalPath());
+        args.add("--" + StandardArgumentDefinitions.OUTPUT_LONG_NAME);
+        args.add(outBam.getCanonicalPath());
+
+        this.runCommandLine(args.getArgsArray());
+
+        SamAssertionUtils.assertSamsEqual(outBam, inBam);
+    }
+
+    @Test
+    public void testCoordinateSortedInRegion() throws Exception {
+        final File inBam = new File(getTestDataDir(), "print_reads.sorted.bam");
+        final File expectedBam = new File(getTestDataDir(), "print_reads.sorted.chr1_1.bam");
+        final File outBam = createTempFile("print_reads_spark", ".bam");
+        ArgumentsBuilder args = new ArgumentsBuilder();
+        args.add("--" + StandardArgumentDefinitions.INPUT_LONG_NAME);
+        args.add(inBam.getCanonicalPath());
+        args.add("--" + StandardArgumentDefinitions.OUTPUT_LONG_NAME);
+        args.add(outBam.getCanonicalPath());
+        args.add("-L chr7:1-100 -XL chr7:2-100");
+
+        this.runCommandLine(args.getArgsArray());
+
+        SamAssertionUtils.assertSamsEqual(outBam, expectedBam);
+    }
+
+    @Test(expectedExceptions = UserException.class)
+    public void testNameSorted() throws Exception {
+        final File inBam = new File(getTestDataDir(), "print_reads.bam");
+        final File outBam = createTempFile("print_reads", ".bam");
+        ArgumentsBuilder args = new ArgumentsBuilder();
+        args.add("--" + StandardArgumentDefinitions.INPUT_LONG_NAME);
+        args.add(inBam.getCanonicalPath());
+        args.add("--" + StandardArgumentDefinitions.OUTPUT_LONG_NAME);
+        args.add(outBam.getCanonicalPath());
+
+        this.runCommandLine(args.getArgsArray());
+
+        SamAssertionUtils.assertSamsEqual(outBam, inBam);
+    }
+
+}
