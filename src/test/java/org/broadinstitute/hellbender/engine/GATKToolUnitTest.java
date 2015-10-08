25976d755f25d70b8eea69a88e99a5941672637b
Adam Kiezun <akiezun@gmail.com>
2015-10-08 11:46:26 -0400

    tests for feature headers in GATKTool

---
+++ src/test/java/org/broadinstitute/hellbender/engine/GATKToolUnitTest.java
@@ -0,0 +1,95 @@
+package org.broadinstitute.hellbender.engine;
+
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.SamReaderFactory;
+import htsjdk.samtools.ValidationStringency;
+import htsjdk.tribble.Feature;
+import htsjdk.variant.vcf.VCFFileReader;
+import htsjdk.variant.vcf.VCFHeader;
+import org.broadinstitute.hellbender.cmdline.Argument;
+import org.broadinstitute.hellbender.cmdline.CommandLineParser;
+import org.broadinstitute.hellbender.utils.test.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public final class GATKToolUnitTest extends BaseTest{
+
+    private static final class TestGATKToolWithReads extends GATKTool{
+
+        @Override
+        public boolean requiresReads() {
+            return true;
+        }
+
+        @Override
+        public void traverse() {
+            //no op
+        }
+    }
+
+    private static final class TestGATKToolWithFeatures extends GATKTool{
+
+        @Argument(fullName="mask", shortName="mask", doc="Input mask", optional=true)
+        public FeatureInput<Feature> mask;
+
+        @Override
+        public boolean requiresFeatures() {
+            return true;
+        }
+
+        @Override
+        public void traverse() {
+            //no op
+        }
+    }
+
+
+    @Test
+    public void testReadsHeader() throws Exception {
+        final GATKTool tool = new TestGATKToolWithReads();
+        final CommandLineParser clp = new CommandLineParser(tool);
+        final File bamFile = new File(publicTestDir + "org/broadinstitute/hellbender/engine/reads_data_source_test1.bam");
+        final String[] args = {"-I", bamFile.getCanonicalPath()};
+        clp.parseArguments(System.out, args);
+        tool.onStartup();
+        final SAMFileHeader headerForReads = tool.getHeaderForReads();
+
+        final SamReaderFactory factory = SamReaderFactory.makeDefault()    //read the file directly and compare headers
+                .validationStringency(ValidationStringency.SILENT);
+        try(SamReader samReader = factory.open(bamFile)) {
+            final SAMFileHeader samFileHeader = samReader.getFileHeader();
+            Assert.assertEquals(headerForReads, samFileHeader);
+        }
+        tool.doWork();
+        tool.onShutdown();
+    }
+
+    @Test
+    public void testFeaturesHeader() throws Exception {
+        final TestGATKToolWithFeatures tool = new TestGATKToolWithFeatures();
+        final CommandLineParser clp = new CommandLineParser(tool);
+        final File vcfFile = new File(publicTestDir + "org/broadinstitute/hellbender/engine/feature_data_source_test_with_bigHeader.vcf");
+        final String[] args = {"--mask", vcfFile.getCanonicalPath()};
+        clp.parseArguments(System.out, args);
+        tool.onStartup();
+        final Object headerForFeatures = tool.getHeaderForFeatures(tool.mask);
+        Assert.assertTrue(headerForFeatures instanceof VCFHeader);
+        final VCFHeader  vcfheaderForFeatures = (VCFHeader) headerForFeatures;
+
+        try(final VCFFileReader vcfReader = new VCFFileReader(vcfFile, false)){  //read the file directly and compare headers
+            final VCFHeader vcfFileHeader= vcfReader.getFileHeader();
+            Assert.assertEquals(vcfheaderForFeatures.getGenotypeSamples(), vcfFileHeader.getGenotypeSamples());
+            Assert.assertEquals(vcfheaderForFeatures.getInfoHeaderLines(), vcfFileHeader.getInfoHeaderLines());
+            Assert.assertEquals(vcfheaderForFeatures.getFormatHeaderLines(), vcfFileHeader.getFormatHeaderLines());
+            Assert.assertEquals(vcfheaderForFeatures.getFilterLines(), vcfFileHeader.getFilterLines());
+            Assert.assertEquals(vcfheaderForFeatures.getContigLines(), vcfFileHeader.getContigLines());
+            Assert.assertEquals(vcfheaderForFeatures.getOtherHeaderLines(), vcfFileHeader.getOtherHeaderLines());
+        }
+        tool.doWork();
+        tool.onShutdown();
+    }
+
+}
