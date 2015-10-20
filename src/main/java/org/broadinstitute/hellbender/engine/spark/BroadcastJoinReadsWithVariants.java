c1333ad4cfb016b1ebf1dafe600692330be575f3
David Roazen <droazen@broadinstitute.org>
2015-10-20 12:55:19 -0400

    BroadcastJoinReadsWithVariants: Broadcast an IntervalsSkipList<Variant> instead of a List<Variant>

--- src/main/java/org/broadinstitute/hellbender/engine/spark/BroadcastJoinReadsWithVariants.java
+++ src/main/java/org/broadinstitute/hellbender/engine/spark/BroadcastJoinReadsWithVariants.java
@@ -1,16 +1,14 @@
 package org.broadinstitute.hellbender.engine.spark;
 
-import com.google.common.collect.Lists;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
+import org.broadinstitute.hellbender.utils.collections.IntervalsSkipList;
 import org.broadinstitute.hellbender.utils.SimpleInterval;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
 import org.broadinstitute.hellbender.utils.variant.Variant;
 import scala.Tuple2;
-
-import java.util.List;
 
 /**
  * Joins an RDD of GATKReads to variant data using a broadcast strategy.
@@ -22,16 +20,12 @@ public class BroadcastJoinReadsWithVariants {
     public static JavaPairRDD<GATKRead, Iterable<Variant>> join(
             final JavaRDD<GATKRead> reads, final JavaRDD<Variant> variants) {
         JavaSparkContext ctx = new JavaSparkContext(reads.context());
-        Broadcast<List<Variant>> variantsBroadcast = ctx.broadcast(variants.collect());
+        final IntervalsSkipList<Variant> variantSkipList = new IntervalsSkipList<>(variants.collect());
+        Broadcast<IntervalsSkipList<Variant>> variantsBroadcast = ctx.broadcast(variantSkipList);
+
         return reads.mapToPair(r -> {
-            List<Variant> overlappingVariants = Lists.newArrayList();
-            SimpleInterval interval = new SimpleInterval(r);
-            for (Variant v : variantsBroadcast.getValue()) {
-                if (interval.overlaps(v)) {
-                    overlappingVariants.add(v);
-                }
-            }
-            return new Tuple2<>(r, overlappingVariants);
+            IntervalsSkipList<Variant> intervalsSkipList = variantsBroadcast.getValue();
+            return new Tuple2<>(r, intervalsSkipList.getOverlapping(new SimpleInterval(r)));
         });
     }
 }
