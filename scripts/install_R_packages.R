e7381f909228564cded968c32ae8dbf94e5b5894
Samuel Lee <lee.samuel.k@gmail.com>
2015-11-13 17:36:53 -0500

    Fixed minor typos in install_R_packages.R

--- scripts/install_R_packages.R
+++ scripts/install_R_packages.R
@@ -2,11 +2,11 @@ source("http://bioconductor.org/biocLite.R")
 biocLite("DNAcopy")
 #Make sure to use http not https as this will give an "unsupported URL scheme" error
 getoptUrl="http://cran.r-project.org/src/contrib/getopt_1.20.0.tar.gz"
-if (!("getopt" %in% rownames(installed.packages))) {
+if (!("getopt" %in% rownames(installed.packages()))) {
   install.packages(getoptUrl, repos=NULL, type="source")
 }
 optparseUrl="http://cran.r-project.org/src/contrib/optparse_1.3.2.tar.gz"
-if (!("optparse" %in% rownames(installed.packages))) {
+if (!("optparse" %in% rownames(installed.packages()))) {
   install.packages(optparseUrl, repos=NULL, type="source")
 }
 dependencies = c("naturalsort")
