2b45f31178333be1b41747a768d3c93235d5a974
vruano <valentin.ruano@gmail.com>
2015-11-17 17:35:58 -0500

    Modified CalcualteTargetCovarage:
    * Added a maximum coverage limit.
    * Added a min MQ read filter argument.

--- src/test/java/org/broadinstitute/hellbender/tools/exome/CalculateTargetCoverageIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/exome/CalculateTargetCoverageIntegrationTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Integration tests for {@link CalculateTargetCoverage}.
@@ -23,6 +24,8 @@ public class CalculateTargetCoverageIntegrationTest extends CommandLineProgramTe
     private static final File TEST_BAM_NA12878 = new File(TEST_DATA_DIR, "exome-read-counts-NA12878.bam");
     private static final File TARGETS_FILE = new File(TEST_DATA_DIR, "exome-read-counts-test-targets.tsv");
     private static final File EXPECTED_COUNTS_FILE = new File(TEST_DATA_DIR, "exome-read-counts.output");
+    private static final File EXPECTED_COUNTS_MAX_OF_9_FILE = new File(TEST_DATA_DIR, "exome-read-counts-max-of-9.output");
+    private static final File EXPECTED_COUNTS_MIN_MQ_30_FILE = new File(TEST_DATA_DIR, "exome-read-counts-min-MQ-30.output");
     private static final File TARGETS_FILE_WITHOUT_COORDINATES = new File(TEST_DATA_DIR, "exome-read-counts-test-targets-wo-coords.tsv");
     private static final File INEXISTENT_TARGETS_FILE = new File(TEST_DATA_DIR, "fantasy-exome-read-counts-test-targets.tsv");
 
@@ -53,6 +56,58 @@ public class CalculateTargetCoverageIntegrationTest extends CommandLineProgramTe
 
         Assert.assertEquals(outputCounts.columnNames(), expectedCounts.columnNames());
         Assert.assertEquals(outputCounts.counts().getData(), expectedCounts.counts().getData());
+    }
+
+    @Test
+    public void testSimpleRunWithMaximumCoverage() throws IOException {
+        final File outputFile = createTempFile("ctc-test-",".tsv");
+        runCommandLine(
+                new String[]{
+                        "-" + TargetArgumentCollection.TARGET_FILE_SHORT_NAME,
+                        TARGETS_FILE.toString(),
+                        "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME,
+                        TEST_BAM_NA12878.toString(),
+                        "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME,
+                        TEST_BAM_NA12778.toString(),
+                        "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME,
+                        TEST_BAM_NA12872.toString(),
+                        "-" + CalculateTargetCoverage.MAXIMUM_COVERAGE_SHORT_NAME,
+                        "9",
+                        "-" + StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
+                        outputFile.toString()
+                });
+        Assert.assertTrue(outputFile.canRead());
+        final ReadCountCollection outputCounts = ReadCountCollectionUtils.parse(outputFile);
+        final ReadCountCollection expectedCounts = ReadCountCollectionUtils.parse(EXPECTED_COUNTS_MAX_OF_9_FILE);
+
+        Assert.assertEquals(outputCounts.columnNames(), expectedCounts.columnNames());
+        Assert.assertEquals(outputCounts.counts().getData(), expectedCounts.counts().getData());
+    }
+
+    @Test
+    public void testSimpleRunWithMinimumMQ() throws IOException {
+        final File outputFile = createTempFile("ctc-test-",".tsv");
+        runCommandLine(
+                new String[]{
+                        "-" + TargetArgumentCollection.TARGET_FILE_SHORT_NAME,
+                        TARGETS_FILE.toString(),
+                        "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME,
+                        TEST_BAM_NA12878.toString(),
+                        "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME,
+                        TEST_BAM_NA12778.toString(),
+                        "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME,
+                        TEST_BAM_NA12872.toString(),
+                        "-" + CalculateTargetCoverage.MINIMUM_MAPPING_QUALITY_SHORT_NAME,
+                        "30",
+                        "-" + StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
+                        outputFile.toString()
+                });
+        Assert.assertTrue(outputFile.canRead());
+        final ReadCountCollection outputCounts = ReadCountCollectionUtils.parse(outputFile);
+        final ReadCountCollection expectedCounts = ReadCountCollectionUtils.parse(EXPECTED_COUNTS_MIN_MQ_30_FILE);
+
+        Assert.assertEquals(outputCounts.columnNames(), expectedCounts.columnNames());
+        Assert.assertEquals(outputCounts.counts().getData(), expectedCounts.counts().getData(), Arrays.toString(outputCounts.counts().getData()[0]));
     }
 
     @Test(expectedExceptions = UserException.class)
