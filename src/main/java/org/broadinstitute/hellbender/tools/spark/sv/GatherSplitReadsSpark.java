4ac50405eec4605047231d452a6233a311551cfe
tsharpe <tsharpe@broadinstitute.org>
2015-11-24 15:05:22 -0500

    use GATKRead and ReadsSparkSink

--- src/main/java/org/broadinstitute/hellbender/tools/spark/sv/GatherSplitReadsSpark.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/sv/GatherSplitReadsSpark.java
@@ -2,6 +2,7 @@ package org.broadinstitute.hellbender.tools.spark.sv;
 
 import htsjdk.samtools.*;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 import org.broadinstitute.hellbender.cmdline.Argument;
@@ -9,7 +10,11 @@ import org.broadinstitute.hellbender.cmdline.CommandLineProgramProperties;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.cmdline.programgroups.SparkProgramGroup;
 import org.broadinstitute.hellbender.engine.spark.GATKSparkTool;
+import org.broadinstitute.hellbender.engine.spark.datasources.ReadsSparkSink;
+import org.broadinstitute.hellbender.exceptions.GATKException;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
+import org.broadinstitute.hellbender.utils.read.ReadConstants;
+import org.broadinstitute.hellbender.utils.read.ReadsWriteFormat;
 import org.seqdoop.hadoop_bam.BAMRecordWriter;
 import org.seqdoop.hadoop_bam.SAMRecordWritable;
 
@@ -23,7 +28,7 @@ import java.util.*;
         programGroup = SparkProgramGroup.class)
 public class GatherSplitReadsSpark extends GATKSparkTool
 {
-    private static final long serialVersionUID = 1l;
+    private static final long serialVersionUID = 1L;
 
     @Argument(doc = "the output bam", shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
             fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME, optional = false)
@@ -35,64 +40,36 @@ public class GatherSplitReadsSpark extends GATKSparkTool
         return true;
     }
 
-    // A shim to allow writing a stream of SAMRecords as a BAM.
-    private static final class BAMWriter extends BAMRecordWriter<NullWritable> {
-        public BAMWriter( OutputStream output, SAMFileHeader header ) throws IOException
-        { super(output, header, true); }
-
-        public void write( SAMRecord rec ) { writeAlignment(rec); }
-        public void close() throws IOException { close(null); }
-
-        @Override public void write( NullWritable ignored, SAMRecordWritable rec )
-        { throw new UnsupportedOperationException(); }
-    }
-
-    // A shim to transform an iterator over GATKReads into an iterator over SAMRecords
-    private static final class GATKRead2SAMRecordIterator implements Iterator<SAMRecord>
-    {
-        GATKRead2SAMRecordIterator( Iterator<GATKRead> someIterator ) { iterator = someIterator; }
-
-        @Override public boolean hasNext() { return iterator.hasNext(); }
-        @Override public SAMRecord next() { return iterator.next().convertToSAMRecord(null); }
-
-        private final Iterator<GATKRead> iterator;
-    }
-
     private static final class EventEvidence implements Comparable<EventEvidence>
     {
-        public EventEvidence( final int someContigIdx, final int someLocus )
+        public EventEvidence( final int someLocus )
         {
-            contigIdx = someContigIdx;
             locus = someLocus;
             counter = 0;
         }
 
-        public EventEvidence( final int someContigIdx, final int someLocus, final long someCounter )
+        public EventEvidence( final int someLocus, final long someCounter )
         {
-            contigIdx = someContigIdx;
             locus = someLocus;
             counter = someCounter;
         }
 
-        public int getContigIdx() { return contigIdx; }
         public int getLocus() { return locus; }
 
         public int compareTo( final EventEvidence ee )
         {
-            int result = Integer.compare(contigIdx,ee.contigIdx);
-            if ( result == 0 ) result = Integer.compare(locus,ee.locus);
+            int result = Integer.compare(locus,ee.locus);
             if ( result == 0 ) result = Long.compare(counter,ee.counter);
             return result;
         }
 
-        private final int contigIdx;
         private final int locus;
         private final long counter; // used as a uniquifier
     }
 
-    private static final class SplitReadClusterer implements Iterator<SAMRecord>
+    private static final class SplitReadClusterer implements Iterator<GATKRead>
     {
-        public SplitReadClusterer( final Iterator<SAMRecord> someInputIterator )
+        public SplitReadClusterer( final Iterator<GATKRead> someInputIterator )
         {
             inputIterator = someInputIterator;
         }
@@ -110,19 +87,25 @@ public class GatherSplitReadsSpark extends GATKSparkTool
         }
 
         @Override
-        public SAMRecord next()
+        public GATKRead next()
         {
             if ( !hasNext() )
                 throw new NoSuchElementException("Iterator<GATKRead> is exhausted.");
             return outputIterator.next();
         }
 
-        private Iterator<SAMRecord> processRead( final SAMRecord read )
+        private Iterator<GATKRead> processRead( final GATKRead read )
         {
-            if ( !read.getReadFailsVendorQualityCheckFlag() && !read.getReadUnmappedFlag() &&
-                    !read.getDuplicateReadFlag() && read.getMappingQuality() > 0 &&
-                    read.getReferenceIndex() != SAMRecord.NO_ALIGNMENT_REFERENCE_INDEX )
+            if ( !read.failsVendorQualityCheck() && !read.isUnmapped() &&
+                    !read.isDuplicate() && read.getMappingQuality() > 0 &&
+                    read.getStart() != ReadConstants.UNSET_POSITION )
             {
+                if ( partitionContig == null )
+                    partitionContig = read.getContig();
+                else if ( partitionContig != read.getContig() )
+                    throw new GATKException("This tool cannot handle partitions that cross contig boundaries.  "+
+                            "Please reindex your input to provide single-contig partitions.");
+
                 final List<CigarElement> cigarElements = read.getCigar().getCigarElements();
                 int matchLen = 0;
                 for ( CigarElement ele : cigarElements )
@@ -149,7 +132,7 @@ public class GatherSplitReadsSpark extends GATKSparkTool
                             lastEle = itrN.previous();
                         if ( lastEle.getOperator() == CigarOperator.SOFT_CLIP &&
                                 lastEle.getLength() >= MIN_SOFT_CLIP_LEN &&
-                                highQualityRegion(read.getBaseQualities(), read.getReadLength()-lastEle.getLength()) )
+                                highQualityRegion(read.getBaseQualities(), read.getLength()-lastEle.getLength()) )
                             return cluster(read, read.getEnd()); // NON-STRUCTURED return
                     }
                 }
@@ -157,30 +140,29 @@ public class GatherSplitReadsSpark extends GATKSparkTool
             return Collections.emptyIterator();
         }
 
-        private Iterator<SAMRecord> cluster( final SAMRecord read, final int locus )
+        private Iterator<GATKRead> cluster( final GATKRead read, final int locus )
         {
-            final EventEvidence newEvidence = new EventEvidence(read.getReferenceIndex(),locus,++counter);
+            final EventEvidence newEvidence = new EventEvidence(locus,++counter);
             locMap.put(newEvidence,read);
 
             // clean out old stuff that can't possibly be interesting anymore
-            final Iterator<Map.Entry<EventEvidence,SAMRecord>> itr = locMap.entrySet().iterator();
+            final Iterator<Map.Entry<EventEvidence,GATKRead>> itr = locMap.entrySet().iterator();
             while ( itr.hasNext() )
             {
                 final EventEvidence oldEvidence = itr.next().getKey();
-                if ( oldEvidence.getContigIdx() == newEvidence.getContigIdx() &&
-                        oldEvidence.getLocus() + MAX_LOCUS_DIST > newEvidence.getLocus() )
+                if ( oldEvidence.getLocus() + MAX_LOCUS_DIST > newEvidence.getLocus() )
                     break;
                 itr.remove();
             }
 
             // find all the evidence in a window surrounding the locus of the new evidence
-            final SortedMap<EventEvidence,SAMRecord> windowMap = locMap
-                    .tailMap(new EventEvidence(newEvidence.getContigIdx(), newEvidence.getLocus()-CLUSTER_WINDOW))
-                    .headMap(new EventEvidence(newEvidence.getContigIdx(), newEvidence.getLocus()+CLUSTER_WINDOW + 1));
+            final SortedMap<EventEvidence,GATKRead> windowMap = locMap
+                    .tailMap(new EventEvidence(newEvidence.getLocus()-CLUSTER_WINDOW))
+                    .headMap(new EventEvidence(newEvidence.getLocus()+CLUSTER_WINDOW + 1));
             if ( windowMap.size() >= MIN_EVIDENCE )
             {
-                final List<SAMRecord> list = new LinkedList<>();
-                for ( Map.Entry<EventEvidence,SAMRecord> entry : windowMap.entrySet() )
+                final List<GATKRead> list = new LinkedList<>();
+                for ( Map.Entry<EventEvidence,GATKRead> entry : windowMap.entrySet() )
                 {
                     if (entry.getValue() != null)
                     {
@@ -201,10 +183,11 @@ public class GatherSplitReadsSpark extends GATKSparkTool
             return true;
         }
 
+        private String partitionContig = null;
         private long counter = 0;
-        private final Iterator<SAMRecord> inputIterator;
-        private final SortedMap<EventEvidence,SAMRecord> locMap = new TreeMap<>();
-        private Iterator<SAMRecord> outputIterator = Collections.emptyIterator();
+        private final Iterator<GATKRead> inputIterator;
+        private final SortedMap<EventEvidence,GATKRead> locMap = new TreeMap<>();
+        private Iterator<GATKRead> outputIterator = Collections.emptyIterator();
 
         private static final int MIN_MATCH_LEN = 45; // minimum length of matched portion of an interesting alignment
         private static final int MIN_SOFT_CLIP_LEN = 30; // minimum length of an interesting soft clip
@@ -214,9 +197,9 @@ public class GatherSplitReadsSpark extends GATKSparkTool
         private static final int MIN_EVIDENCE = 3; // minimum evidence count in a cluster
     }
 
-    private static final class SAMWindowSorter implements Iterator<SAMRecord>, Iterable<SAMRecord>
+    private static final class SAMWindowSorter implements Iterator<GATKRead>, Iterable<GATKRead>
     {
-        public SAMWindowSorter( final Iterator<SAMRecord> someInputIterator, final int someWindowSize )
+        public SAMWindowSorter( final Iterator<GATKRead> someInputIterator, final int someWindowSize )
         {
             inputIterator = someInputIterator;
             windowSize = someWindowSize;
@@ -230,17 +213,17 @@ public class GatherSplitReadsSpark extends GATKSparkTool
         }
 
         @Override
-        public SAMRecord next()
+        public GATKRead next()
         {
-            final Iterator<SAMRecord> itr = recordSet.iterator();
-            final SAMRecord result = itr.next();
+            final Iterator<GATKRead> itr = recordSet.iterator();
+            final GATKRead result = itr.next();
             itr.remove();
             fillBuffer();
             return result;
         }
 
         @Override
-        public Iterator<SAMRecord> iterator() { return this; }
+        public Iterator<GATKRead> iterator() { return this; }
 
         private void fillBuffer()
         {
@@ -252,28 +235,25 @@ public class GatherSplitReadsSpark extends GATKSparkTool
         {
             if ( recordSet.isEmpty() )
                 return 0;
-            final SAMRecord first = recordSet.first();
-            final SAMRecord last = recordSet.last();
-            if ( !first.getReferenceIndex().equals(last.getReferenceIndex()) )
-                return Integer.MAX_VALUE;
-            return last.getAlignmentStart() - first.getAlignmentStart();
+            final GATKRead first = recordSet.first();
+            final GATKRead last = recordSet.last();
+            return last.getStart() - first.getStart();
         }
 
-        private static final class SAMComparator implements Comparator<SAMRecord>
+        private static final class ReadComparator implements Comparator<GATKRead>
         {
             @Override
-            public int compare( final SAMRecord rec1, final SAMRecord rec2 )
+            public int compare( final GATKRead rec1, final GATKRead rec2 )
             {
-                int result = rec1.getReferenceIndex().compareTo(rec2.getReferenceIndex());
-                if ( result == 0 ) result = Integer.compare(rec1.getAlignmentStart(), rec2.getAlignmentStart());
-                if ( result == 0 ) result = rec1.getReadName().compareTo(rec2.getReadName());
-                if ( result == 0 ) result = Integer.compare(rec1.getFlags(),rec2.getFlags());
+                int result = Integer.compare(rec1.getStart(), rec2.getStart());
+                if ( result == 0 ) result = rec1.getName().compareTo(rec2.getName());
+                if ( result == 0 ) result = -Boolean.compare(rec1.isFirstOfPair(),rec2.isFirstOfPair());
                 return result;
             }
         }
 
-        private final SortedSet<SAMRecord> recordSet = new TreeSet<SAMRecord>(new SAMComparator());
-        private final Iterator<SAMRecord> inputIterator;
+        private final SortedSet<GATKRead> recordSet = new TreeSet<>(new ReadComparator());
+        private final Iterator<GATKRead> inputIterator;
         private final int windowSize;
     }
 
@@ -284,19 +264,14 @@ public class GatherSplitReadsSpark extends GATKSparkTool
         if ( header.value().getSortOrder() != SAMFileHeader.SortOrder.coordinate )
             throw new IllegalStateException("The BAM must be coordinate sorted.");
 
-        getReads()
-            .mapPartitions(readItr ->
-            {
-                final SplitReadClusterer itr = new SplitReadClusterer(new GATKRead2SAMRecordIterator(readItr));
-                return new SAMWindowSorter(itr,SAM_WINDOW_SIZE);
-            }, true)
-            .coalesce(1)
-            .foreachPartition(readItr ->
-            {
-                final BAMWriter writer = new BAMWriter(new FileOutputStream(output), header.value());
-                while (readItr.hasNext()) writer.write(readItr.next());
-                writer.close();
-            });
+        final JavaRDD<GATKRead> clusteredReads =
+            getReads().mapPartitions(readItr ->
+                { return new SAMWindowSorter(new SplitReadClusterer(readItr),SAM_WINDOW_SIZE); }, true);
+
+        try
+        { ReadsSparkSink.writeReads(ctx, output, clusteredReads, header.value(), ReadsWriteFormat.SINGLE); }
+        catch ( IOException e )
+        { throw new GATKException("Unable to write BAM" + output, e); }
     }
 
     private static final int SAM_WINDOW_SIZE = 500;
