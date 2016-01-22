e767e1ab88ac9ef8ee51ec3c63ae523860b422cf
achevali <aaronc@broadinstitute.org>
2016-01-22 13:16:54 -0500

    Drop X/Y/MT regions if specified

--- src/test/java/org/broadinstitute/hellbender/tools/genome/SparkGenomeReadCountsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/genome/SparkGenomeReadCountsIntegrationTest.java
@@ -1,15 +1,21 @@
 package org.broadinstitute.hellbender.tools.genome;
 
+import htsjdk.samtools.SAMSequenceDictionary;
 import htsjdk.tribble.bed.BEDCodec;
 import htsjdk.tribble.bed.BEDFeature;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.engine.datasources.ReferenceFileSource;
 import org.broadinstitute.hellbender.tools.exome.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SparkGenomeReadCountsIntegrationTest extends CommandLineProgramTest {
     private static final File TEST_FILE_DIR = new File("src/test/resources/org/broadinstitute/hellbender/tools/genome");
@@ -64,5 +70,17 @@ public class SparkGenomeReadCountsIntegrationTest extends CommandLineProgramTest
         Assert.assertEquals(bedFeatureCollection.target(1).getEnd(), 16000);
         Assert.assertEquals(bedFeatureCollection.target(2).getName(), "target_3_1_16000");
         Assert.assertEquals(targetCoverages.size(), bedFeatureCollection.targetCount());
+    }
+
+    @Test
+    public void testDropContigsFromSequence() throws IOException{
+        final Set<String> testContigsToDrop = new HashSet<>(Arrays.asList("3"));
+        ReferenceFileSource testFasta = new ReferenceFileSource(REFERENCE_FILE.getAbsolutePath());
+        final SAMSequenceDictionary originalSequenceDictionary = testFasta.getReferenceSequenceDictionary(null);
+        SAMSequenceDictionary modifiedSequenceDictionary = originalSequenceDictionary;
+        modifiedSequenceDictionary = SparkGenomeReadCounts.dropContigsFromSequence(originalSequenceDictionary, testContigsToDrop);
+        SAMSequenceDictionary finalSequenceDictionary = modifiedSequenceDictionary;
+        Assert.assertEquals(finalSequenceDictionary.size(), 3);
+
     }
 }
