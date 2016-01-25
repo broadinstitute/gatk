4baa2f8e0d5fdc1b2f40a8ab7e6d2bef58c4f565
lichtens <lichtens@broadinstitute.org>
2016-01-25 15:13:12 -0500

    Addressing PR comments and a few warnings.

--- src/main/java/org/broadinstitute/hellbender/utils/GATKProtectedMathUtils.java
+++ src/main/java/org/broadinstitute/hellbender/utils/GATKProtectedMathUtils.java
@@ -4,9 +4,14 @@ package org.broadinstitute.hellbender.utils;
  * Extra MathUtils that should be moved to gatk-public
  * Created by davidben on 1/22/16.
  */
-public class ExtraMathUtils {
+public class GATKProtectedMathUtils {
 
-    //compute log(e^a + e^b + e^c. . .) = log(e^M [e^(a-M) + e^(b-M) + e^(c-M). . .]) where M = max(a,b,c. . .)
+    /**
+     * Compute log(e^a + e^b + e^c. . .) = log(e^M [e^(a-M) + e^(b-M) + e^(c-M). . .]) where M = max(a,b,c. . .)
+     *
+     * @return
+     */
+
     public static double naturalLogSumNaturalLog(double[] values) {
         double max = MathUtils.arrayMax(values);
         double sum = 0.0;
--- src/test/java/org/broadinstitute/hellbender/utils/GATKProtectedMathUtilsTest.java
+++ src/test/java/org/broadinstitute/hellbender/utils/GATKProtectedMathUtilsTest.java
@@ -5,19 +5,17 @@ import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
-import static org.testng.Assert.*;
-
 /**
  * Created by davidben on 1/22/16.
  */
-public class ExtraMathUtilsTest {
+public class GATKProtectedMathUtilsTest {
     @Test
     public void naturalLogSumNaturalLogTest() {
         final double[] realSpaceValues = {0.1, 0.2, 1.4, 5.9};
         final double[] logSpaceValues= Arrays.stream(realSpaceValues).map(Math::log).toArray();
 
         final double expected = Math.log(Arrays.stream(realSpaceValues).sum());
-        final double actual = ExtraMathUtils.naturalLogSumNaturalLog(logSpaceValues);
+        final double actual = GATKProtectedMathUtils.naturalLogSumNaturalLog(logSpaceValues);
         Assert.assertEquals(actual, expected, 1e-10);
     }
 
