ce13957d965e383198e9e194b4e13807cef86fc9
Adam Kiezun <akiezun@gmail.com>
2016-03-06 21:41:38 -0500

    callrate

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/CollectVariantQCMetrics.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/CollectVariantQCMetrics.java
@@ -2,7 +2,9 @@ package org.broadinstitute.hellbender.tools.walkers.variantutils;
 
 import htsjdk.variant.variantcontext.VariantContext;
 import org.broadinstitute.hellbender.cmdline.Argument;
+import org.broadinstitute.hellbender.cmdline.CommandLineProgramProperties;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.programgroups.VariantProgramGroup;
 import org.broadinstitute.hellbender.engine.FeatureContext;
 import org.broadinstitute.hellbender.engine.ReadsContext;
 import org.broadinstitute.hellbender.engine.ReferenceContext;
@@ -12,9 +14,16 @@ import org.broadinstitute.hellbender.exceptions.UserException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
+@CommandLineProgramProperties(
+        summary = "VariantQC",
+        oneLineSummary = "VariantQC",
+        programGroup = VariantProgramGroup.class
+)
 public final class CollectVariantQCMetrics extends VariantWalker{
 
     @Argument(fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME,
@@ -24,23 +33,59 @@ public final class CollectVariantQCMetrics extends VariantWalker{
 
     private PrintStream outputStream = null;
 
-    private List<Function<VariantContext, VariantQCMetric>> metricComputers;//Note: it's a list because the order is defined
+    private Map<String, Function<VariantContext, String>> metricComputers;//Note: it's a list because the order is defined
 
     @Override
     public void onTraversalStart() {
+        outputStream = initializeOutputStream();
+        metricComputers = initializeMetrics();
+        printHeader();
+    }
+
+    private void printHeader() {
+        final String header = String.join("\t", metricComputers.keySet());
+        outputStream.println(
+                "contig" + "\t" +
+                "start" + "\t" +
+                "end" + "\t" +
+                "ref" + "\t" +
+                "alt" + "\t" +
+
+                header);
+    }
+
+    private Map<String, Function<VariantContext, String>> initializeMetrics() {
+        final Map<String, Function<VariantContext, String>> results = new LinkedHashMap<>();
+        results.put("callRate", vc -> formatDouble(callRate(vc)));
+        return results;
+    }
+
+    private double callRate(final VariantContext vc) {
+        final int nGenotypes = vc.getGenotypes().size();
+        return (double) vc.getGenotypes().stream().filter(gt -> gt.isCalled()).count() / nGenotypes;
+    }
+
+    private static String formatDouble(final double d){
+        return String.format("%.3f", d);
+    }
+
+    private PrintStream initializeOutputStream() {
         try {
-            outputStream = new PrintStream(out);
+            return new PrintStream(out);
         } catch ( final FileNotFoundException e ) {
             throw new UserException.CouldNotCreateOutputFile(out, e);
         }
     }
 
-    interface VariantQCMetric{
-
-    }
     @Override
     public void apply(final VariantContext variant, final ReadsContext readsContext, final ReferenceContext referenceContext, final FeatureContext featureContext) {
-         metricComputers.stream().map(computer -> computer.apply())
+        final String line = metricComputers.values().stream().map(computer -> computer.apply(variant)).collect(Collectors.joining("\t"));
+        outputStream.println(variant.getContig() + "\t" +
+                             variant.getStart() + "\t" +
+                             variant.getEnd()  + "\t" +
+                             variant.getReference()  + "\t" +
+                             variant.getAlternateAlleles()  + "\t" +
+                             line);
     }
 
     /**
---
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/variantutils/CollectVariantQCMetricsIntegrationTest.java
@@ -0,0 +1,30 @@
+package org.broadinstitute.hellbender.tools.walkers.variantutils;
+
+import org.broadinstitute.hellbender.CommandLineProgramTest;
+import org.broadinstitute.hellbender.utils.test.IntegrationTestSpec;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ */
+public class CollectVariantQCMetricsIntegrationTest extends CommandLineProgramTest {
+    @Test
+    public void test1() throws IOException {
+        final IntegrationTestSpec spec = new IntegrationTestSpec(
+                " --variant " + getToolTestDataDir() + "CEUtrioTest.vcf" +
+                        " -O %s",
+                Arrays.asList(getToolTestDataDir() + "expected.CEUtrioTest.variantQC.table"));
+        spec.executeTest("test1", this);
+    }
+
+    @Test
+    public void testLarge() throws IOException {
+        final IntegrationTestSpec spec = new IntegrationTestSpec(
+                " --variant " + largeFileTestDir + "1000G.phase3.broad.withGenotypes.chr20.10100000.vcf" +
+                        " -O %s",
+                Arrays.asList(getToolTestDataDir() + "expected.CEUtrioTest.variantQC.table"));
+        spec.executeTest("test1", this);
+    }
+}
