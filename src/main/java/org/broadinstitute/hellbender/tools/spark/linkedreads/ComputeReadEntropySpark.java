7ffb0eae3b535e75cba0cfda6a4718c8446453cf
Chris Whelan <cwhelan@broadinstitute.org>
2016-03-09 15:44:16 -0500

    tool to commit read entropy

---
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/linkedreads/ComputeReadEntropySpark.java
@@ -0,0 +1,55 @@
+package org.broadinstitute.hellbender.tools.spark.linkedreads;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.broadinstitute.hellbender.cmdline.Argument;
+import org.broadinstitute.hellbender.cmdline.CommandLineProgramProperties;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.programgroups.SparkProgramGroup;
+import org.broadinstitute.hellbender.engine.spark.GATKSparkTool;
+import org.broadinstitute.hellbender.tools.spark.sv.SVKmer;
+import org.broadinstitute.hellbender.tools.spark.sv.SVKmerizer;
+import org.broadinstitute.hellbender.utils.read.GATKRead;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@CommandLineProgramProperties(
+        summary = "Computes read entropy",
+        oneLineSummary = "ComputeReadEntropy on Spark",
+        programGroup = SparkProgramGroup.class
+)
+public class ComputeReadEntropySpark extends GATKSparkTool {
+    private static final long serialVersionUID = 1L;
+
+    @Argument(doc = "uri for the output file",
+            shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME, fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME,
+            optional = true)
+    public String out;
+
+    @Override
+    public boolean requiresReads() {
+        return true;
+    }
+
+    @Override
+    protected void runTool(final JavaSparkContext ctx) {
+
+        final JavaRDD<GATKRead> reads = getReads();
+
+        reads.map(r -> computeEntropy(r.getBasesString())).saveAsTextFile(out);
+
+    }
+
+    public static double computeEntropy(String sequence) {
+        final Map<SVKmer, Long> frequencies = SVKmerizer.stream(sequence, 3).collect(Collectors.groupingBy(k -> k, Collectors.counting()));
+        final int numKmers = sequence.length() - 2;
+        final Double entropy = -1 * frequencies.entrySet().stream().collect(Collectors.summingDouble(e -> {
+            final double p = (double) e.getValue() / numKmers;
+            return p * Math.log(p) / Math.log(2.0d);
+        }));
+        return entropy;
+    }
+}
