d87819054cab084ad393a004a200a989ec04b756
Chris Whelan <cwhelan@broadinstitute.org>
2016-03-15 11:54:45 -0400

    don't create so many new sets

--- src/main/java/org/broadinstitute/hellbender/tools/spark/linkedreads/ComputeBarcodeOverlapSpark.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/linkedreads/ComputeBarcodeOverlapSpark.java
@@ -59,14 +59,12 @@ public class ComputeBarcodeOverlapSpark extends GATKSparkTool {
         final JavaPairRDD<SimpleInterval, Set<String>> barcodesByWindow = reads.mapToPair(read -> new Tuple2<>(new SimpleInterval(read.getContig(), read.getStart() - read.getStart() % windowsSize + 1, read.getStart() + (windowsSize - read.getStart() % windowsSize)), read)).aggregateByKey(
                 new HashSet<>(),
                 (barcodeSet, read) -> {
-                    final Set<String> newBarcodeSet = new HashSet<>(barcodeSet);
-                    newBarcodeSet.add(read.getAttributeAsString("BX"));
-                    return newBarcodeSet;
+                    barcodeSet.add(read.getAttributeAsString("BX"));
+                    return barcodeSet;
                 },
                 (barcodeSet1, barcodeSet2) -> {
-                    final Set<String> newBarcodeSet = new HashSet<>(barcodeSet1);
-                    newBarcodeSet.addAll(barcodeSet2);
-                    return newBarcodeSet;
+                    barcodeSet1.addAll(barcodeSet2);
+                    return barcodeSet1;
                 });
         final JavaPairRDD<Tuple2<SimpleInterval, SimpleInterval>, Integer> barcodeOverlapsByWindowPairs = barcodesByWindow.cartesian(barcodesByWindow).filter(windowPair -> {
             // only keep windows where the first window comes before the second
@@ -84,7 +82,7 @@ public class ComputeBarcodeOverlapSpark extends GATKSparkTool {
             return false;
         }).mapToPair(windowPair -> {
             final Tuple2<SimpleInterval, SimpleInterval> newkey = new Tuple2<>(windowPair._1._1, windowPair._2._1);
-            final Set<String> combinedSet = new HashSet<>(windowPair._1()._2());
+            final Set<String> combinedSet = windowPair._1()._2();
             combinedSet.retainAll(windowPair._2._2);
             return new Tuple2<>(newkey, combinedSet.size());
         });
