c1c53a8fdcee0de8f91350e772eba7db37bff50b
JP Martin <jpmartin@google.com>
2016-04-01 12:28:57 -0700

    More detailed examples

--- src/test/java/org/broadinstitute/hellbender/utils/nio/NioTests.java
+++ src/test/java/org/broadinstitute/hellbender/utils/nio/NioTests.java
@@ -2,6 +2,7 @@ package org.broadinstitute.hellbender.utils.nio;
 
 import com.google.gcloud.AuthCredentials;
 import com.google.gcloud.RetryParams;
+import com.google.gcloud.storage.StorageException;
 import com.google.gcloud.storage.StorageOptions;
 import com.google.gcloud.storage.contrib.nio.CloudStorageConfiguration;
 import com.google.gcloud.storage.contrib.nio.CloudStorageFileSystem;
@@ -17,6 +18,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 /**
  * Test GCS access via the NIO APIs.
@@ -34,14 +36,59 @@ public final class NioTests extends BaseTest {
     int firstByte = inputStream.read();
   }
 
+
+  /**
+   * When we give no explicit credentials, then the system will still work if either
+   * - the GOOGLE_APPLICATION_CREDENTIALS environment variable is set
+   * - the user ran "gcloud auth login" first
+   * - the code is running on a Google Cloud Compute machine.
+   * (see http://gcloud-python.readthedocs.org/en/latest/gcloud-auth.html)
+   */
   @Test
-  public void OpenPrivateFile() throws IOException {
+  public void openPrivateFileUsingDefaultCredentials() throws IOException {
     // this file, potentially unlike the others in the set, is not marked as "Public link".
     final String privateFile = getGCPTestInputPath() + "org/broadinstitute/hellbender/utils/private_file.txt";
-    final String BUCKET = privateFile.split("/")[2];
-    FileSystem fs = getAuthenticatedGcs(BUCKET);
-    Path path = fs.getPath(privateFile);
+
+    Path path = Paths.get(URI.create((privateFile)));
     int firstByte = Files.newInputStream(path).read();
+  }
+
+  /**
+   * Opening the private file even when the user is not logged in on gcloud should work
+   * when we provide explicit credentials.
+   */
+  @Test
+  public void openPrivateFileWithExplicitCredentials() throws IOException {
+    // this file, potentially unlike the others in the set, is not marked as "Public link".
+    final String privateFile = getGCPTestInputPath() + "org/broadinstitute/hellbender/utils/private_file.txt";
+    final String[] split = privateFile.split("/");
+    final String BUCKET = split[2];
+    final String pathWithoutBucket = String.join("/", Arrays.copyOfRange(split,3, split.length));
+
+    FileSystem fs = getAuthenticatedGcs(BUCKET);
+    Path path = fs.getPath(pathWithoutBucket);
+    int firstByte = Files.newInputStream(path).read();
+  }
+
+  /**
+   * Using explicit credentials only works on that access, they are not kept.
+   */
+  @Test(expectedExceptions = { StorageException.class })
+  public void explicitCredentialsAreNotKept() throws IOException {
+    // this file, potentially unlike the others in the set, is not marked as "Public link".
+    final String privateFile = getGCPTestInputPath() + "org/broadinstitute/hellbender/utils/private_file.txt";
+    final String privateFile2 = getGCPTestInputPath() + "org/broadinstitute/hellbender/utils/private_file_2.txt";
+    final String[] split = privateFile.split("/");
+    final String BUCKET = split[2];
+    final String pathWithoutBucket = String.join("/", Arrays.copyOfRange(split,3, split.length));
+
+    FileSystem fs = getAuthenticatedGcs(BUCKET);
+    Path path = fs.getPath(pathWithoutBucket);
+    int firstByte = Files.newInputStream(path).read();
+
+    // now let's open another private file. It shouldn't work.
+    path = Paths.get(URI.create((privateFile2)));
+    firstByte = Files.newInputStream(path).read();
   }
 
   private FileSystem getAuthenticatedGcs(String bucket) throws IOException {
