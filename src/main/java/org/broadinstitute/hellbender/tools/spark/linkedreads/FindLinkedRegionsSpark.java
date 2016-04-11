b3ad93965ed4b3f8bb8099e1bc35b5a36352cbac
Chris Whelan <cwhelan@broadinstitute.org>
2016-04-11 14:09:15 -0400

    compute coverage of linked reads by bin

--- src/main/java/org/broadinstitute/hellbender/tools/spark/linkedreads/FindLinkedRegionsSpark.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/linkedreads/FindLinkedRegionsSpark.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.hellbender.tools.spark.linkedreads;
 
 import htsjdk.samtools.util.IntervalTree;
+import org.apache.spark.HashPartitioner;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -25,6 +26,7 @@ import java.util.stream.Collectors;
 )
 public class FindLinkedRegionsSpark extends GATKSparkTool {
     private static final long serialVersionUID = 1L;
+    public static final int COVERAGE_BIN_WIDTH = 1000;
 
     @Argument(doc = "Linked reads file created by CollectLinkedReadCoverage tool",
             shortName = "linkedReadsFile", fullName = "linkedReadsFile",
@@ -71,9 +73,29 @@ public class FindLinkedRegionsSpark extends GATKSparkTool {
 
         final JavaPairRDD<String, String> barcodeLinkedReads = linkedReadsBedFile.mapToPair(line -> new Tuple2<>(line.split("\t")[3], line));
 
-        final JavaPairRDD<SimpleInterval, Iterable<String>> intervalsToLinkedReads = barcodeIntervals.join(barcodeLinkedReads).values().mapToPair(value -> value).groupByKey();
+        final JavaPairRDD<String, Tuple2<SimpleInterval, String>> intervalsAndLinkedReadJoinedByBarcodes = barcodeIntervals.join(barcodeLinkedReads);
 
-        intervalsToLinkedReads.saveAsTextFile(out);
+        final JavaPairRDD<Tuple2<SimpleInterval, SimpleInterval>, Integer> coverageCountsByIntervalAndBin = intervalsAndLinkedReadJoinedByBarcodes.flatMapToPair(join -> {
+            final Tuple2<SimpleInterval, String> intervalLinkedReadPair = join._2();
+            final List<Tuple2<Tuple2<SimpleInterval, SimpleInterval>, Integer>> results = new ArrayList<>();
+            final SimpleInterval linkedReadInterval = getLinkedReadInterval(intervalLinkedReadPair._2());
+            for (int binStart = linkedReadInterval.getStart() - linkedReadInterval.getStart() % COVERAGE_BIN_WIDTH; binStart < linkedReadInterval.getEnd() + COVERAGE_BIN_WIDTH; binStart += COVERAGE_BIN_WIDTH) {
+                final SimpleInterval bin = new SimpleInterval(linkedReadInterval.getContig(), binStart, binStart + COVERAGE_BIN_WIDTH - 1);
+                results.add(new Tuple2<>(new Tuple2<>(intervalLinkedReadPair._1(), bin), 1));
+            }
+            return results;
+        }).reduceByKey((count1, count2) -> count1 + count2);
+
+        final JavaPairRDD<SimpleInterval, Tuple2<SimpleInterval, Integer>> intervalBinCoverages =
+                coverageCountsByIntervalAndBin.mapToPair(intervalBinCovCount -> new Tuple2<>(intervalBinCovCount._1._1, new Tuple2<>(intervalBinCovCount._1._2, intervalBinCovCount._2)))
+                .partitionBy(new HashPartitioner(intervalList.size()));
+
+        intervalBinCoverages.saveAsTextFile(out);
+    }
+
+    private SimpleInterval getLinkedReadInterval(final String linkedReadString) {
+        final String[] fields = linkedReadString.split("\t");
+        return new SimpleInterval(fields[0], Integer.valueOf(fields[1]), Integer.valueOf(fields[2]));
     }
 
     private static List<SimpleInterval> getIntervalsForRead(final IntervalsSkipList<SimpleInterval> intervalsSkipList, final GATKRead read) {
