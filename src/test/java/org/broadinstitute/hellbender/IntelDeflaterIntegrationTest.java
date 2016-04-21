5be05d449e8ccbadcb5c82149776268efad1c23d
Louis Bergelson <louisb@broadinstitute.org>
2016-04-21 12:33:26 -0400

    fix lib intel deflater extraction (#1742)
    libIntelDeflater.so wasn't being placed in the right location, causing it to not be used
    
    it now is placed in build/libIntelDeflater.so, where it was intended to be
      fixes #1739
    
    adding IntelDeflaterTest that checks if the IntelDeflater is loaded at runtime
      this will be skipped if the OS is not some linux variant
      it runs in travis which should prevent us from accidentally breaking this in the future
      making test dependsOn extractIntelDeflater
      fixes #1739
    
    improving test logging so that tests run in travis and on the command line will output a totals section
      this will clearly show any skipped tests
    
    adding an explicit PrintReads call to travis.  this checks that gatk-launch will run PrintReads and exit without failing
      it runs quickly and outputs some useful log messages

---
+++ src/test/java/org/broadinstitute/hellbender/IntelDeflaterIntegrationTest.java
@@ -0,0 +1,25 @@
+package org.broadinstitute.hellbender;
+
+import htsjdk.samtools.util.zip.DeflaterFactory;
+import org.apache.commons.lang3.SystemUtils;
+import org.broadinstitute.hellbender.utils.test.BaseTest;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+/**
+ * Test that it's possible to load libIntelDeflater
+ */
+public class IntelDeflaterIntegrationTest extends BaseTest {
+
+    @Test
+    public void testIntelDeflaterIsAvailable(){
+        if ( !SystemUtils.IS_OS_LINUX ) {
+            throw new SkipException("IntelDeflater not available on this platform");
+        }
+
+        Assert.assertTrue(DeflaterFactory.usingIntelDeflater(), "libIntelDeflater.so was not loaded. " +
+                "This could be due to a configuration error, or your system might not support it.");
+    }
+
+}
