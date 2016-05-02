8b4df00fb822b6fad2b9816c202ddfc77776e5b0
Adam Kiezun <akiezun@gmail.com>
2016-05-02 16:21:06 -0400

    remove pull lfs

--- scripts/install_git_lfs.sh
+++ scripts/install_git_lfs.sh
@@ -21,9 +21,6 @@ GIT_TRACE=1 $GIT_LFS fetch
 echo "checkout"
 GIT_TRACE=1 $GIT_LFS checkout
 
-echo "and pull once again to work around https://github.com/github/git-lfs/issues/904"
-git-lfs/bin/git-lfs pull
-
 echo "ls-files"
 GIT_TRACE=1 $GIT_LFS ls-files
 ls -lah src/test/resources/large/
