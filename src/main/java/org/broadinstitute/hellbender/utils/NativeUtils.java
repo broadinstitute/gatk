2119492a62ddcc682f1ef2a9c539d05b7e118acb
David Roazen <droazen@broadinstitute.org>
2016-05-20 14:54:35 -0400

    Add dependency on jbwa snapshot, script to release future jbwa snapshots, and method to load the library at runtime
    -Published a jbwa snapshot to the Broad artifactory, which we now depend on
     via gradle. This snapshot contains builds of the native jbwa code for both
     Mac and Linux.
    
    -Added utility methods to NativeUtils to load this library at runtime,
     and a test proving that it can be loaded successfully. Also switched
     to the new NativeUtils methods for loading the PairHMM, and confirmed
     that it loads successfully with the HaplotypeCaller in protected
    
    -Included a gradle script to publish a new jbwa snapshot, should it
     become necessary, along with instructions on how to do so.
    
    Resolves #1838

--- src/main/java/org/broadinstitute/hellbender/utils/NativeUtils.java
+++ src/main/java/org/broadinstitute/hellbender/utils/NativeUtils.java
@@ -1,6 +1,11 @@
 package org.broadinstitute.hellbender.utils;
 
 import org.apache.commons.lang3.SystemUtils;
+import org.broadinstitute.hellbender.exceptions.UserException;
+import org.broadinstitute.hellbender.utils.io.IOUtils;
+import org.broadinstitute.hellbender.utils.io.Resource;
+
+import java.io.File;
 
 /**
  * Utilities to provide architecture-dependent native functions
@@ -12,7 +17,55 @@ public final class NativeUtils {
     /**
      * boolean : returns whether the current platform is PowerPC, LE only
      */
-    public static boolean isPPCArchitecture() {
-	return SystemUtils.OS_ARCH.equals("ppc64le");
+    public static boolean runningOnPPCArchitecture() {
+        return runningOnArchitecture("ppc64le");
+    }
+
+    /**
+     * @param architecture Architecture to look for
+     * @return true if we're running on the specified architecture, otherwise false
+     */
+    public static boolean runningOnArchitecture( final String architecture ) {
+        Utils.nonNull(architecture);
+        final String archtectureString = SystemUtils.OS_ARCH;
+
+        return archtectureString != null && archtectureString.equals(architecture);
+    }
+
+    /**
+     * @return true if we're running on a Mac operating system, otherwise false
+     */
+    public static boolean runningOnMac() {
+        return SystemUtils.IS_OS_MAC;
+    }
+
+    /**
+     * @return true if we're running on a Linux operating system, otherwise false
+     */
+    public static boolean runningOnLinux() {
+        return SystemUtils.IS_OS_LINUX;
+    }
+
+    /**
+     * Loads a native library stored on our classpath by extracting it to a temporary location and invoking
+     * {@link System#load} on it
+     *
+     * @param libraryPathInJar absolute path to the library file on the classpath
+     * @return true if the library was extracted and loaded successfully, otherwise false
+     */
+    public static boolean loadLibraryFromClasspath( final String libraryPathInJar ) {
+        Utils.nonNull(libraryPathInJar);
+        Utils.validateArg(libraryPathInJar.startsWith("/"), "library path in jar must be absolute");
+
+        try {
+            final File extractedLibrary = IOUtils.writeTempResource(new Resource(libraryPathInJar, NativeUtils.class));
+            extractedLibrary.deleteOnExit();
+            System.load(extractedLibrary.getAbsolutePath());
+        }
+        catch ( UnsatisfiedLinkError | UserException e ) {
+            return false;
+        }
+
+        return true;
     }
 }
