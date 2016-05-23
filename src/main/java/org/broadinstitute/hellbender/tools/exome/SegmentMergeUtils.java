frank-y-liu/gatk-protected

Eliminated TargetCoverage in favor of ReadCountCollecction everywhere

src/main/java/org/broadinstitute/hellbender/tools/exome/SegmentMergeUtils.java
@@ -560,8 +560,8 @@ public final class SegmentMergeUtils {
          * @return          list of the coverages for targets in the segment
          */
         private static List<Double> makeCoverageList(final SimpleInterval segment,
-                                                     final TargetCollection<TargetCoverage> targets) {
-            return targets.targets(segment).stream().map(TargetCoverage::getCoverage).collect(Collectors.toList());
+                                                     final TargetCollection<ReadCountRecord.SingletonRecord> targets) {
+            return targets.targets(segment).stream().map(ReadCountRecord.SingletonRecord::getCount).collect(Collectors.toList());
         }
 
         /**
@@ -575,7 +575,7 @@ public final class SegmentMergeUtils {
          * @return          scores for adjacent segments based on Hodges-Lehmann estimators
          */
         private static Pair<Double, Double> calculateTargetScores(final List<SimpleInterval> segments,
-                                                                  final TargetCollection<TargetCoverage> targets,
+                                                                  final TargetCollection<ReadCountRecord.SingletonRecord> targets,
                                                                   final int index) {
             final SimpleInterval leftSegment = segments.get(index - 1);
             final SimpleInterval centerSegment = segments.get(index);
@@ -625,7 +625,7 @@ public final class SegmentMergeUtils {
 
             //if any of the three segments is missing SNPs or the SNP scores are too similar,
             //try to use target scores based on Hodges-Lehmann estimator
-            final TargetCollection<TargetCoverage> targets = genome.getTargets();
+            final TargetCollection<ReadCountRecord.SingletonRecord> targets = genome.getTargets();
             final Pair<Double, Double> targetScores = calculateTargetScores(segments, targets, index);
             if (Math.max(targetScores.getLeft(), targetScores.getRight()) > TARGET_SCORE_THRESHOLD) {
                 return targetScores;

src/main/java/org/broadinstitute/hellbender/tools/exome/SegmentUtils.java
@@ -34,22 +34,6 @@ public final class SegmentUtils {
     private SegmentUtils() {}
 
     /**
-     * Returns the mean of coverages for targets in a given collection that overlap a given interval.
-     */
-    public static double meanTargetCoverage(final Locatable interval,
-                                            final TargetCollection<TargetCoverage> targets) {
-        Utils.nonNull(interval, "Can't get mean coverage of null interval.");
-        Utils.nonNull(targets, "The collection of target coverages cannot be null.");
-
-        final List<TargetCoverage> myTargets = targets.targets(interval);
-
-        if (myTargets.size() == 0) {
-            throw new IllegalStateException("Empty segment -- no overlapping targets.");
-        }
-        return myTargets.stream().mapToDouble(TargetCoverage::getCoverage).average().getAsDouble();
-    }
-
-    /**
      * Get difference between a segment mean and the overlapping targets.
      * This will select the overlapping targets from the input collection.
      *
@@ -58,13 +42,12 @@ public final class SegmentUtils {
      * @return          never {@code null}.  List of doubles of the difference, possibly empty.  Modifiable.
      */
     public static List<Double> segmentMeanTargetDifference(final ModeledSegment segment,
-                                                           final TargetCollection<TargetCoverage> targets) {
+                                                           final TargetCollection<ReadCountRecord.SingletonRecord> targets) {
         Utils.nonNull(segment, "Can't count targets of null segment.");
         Utils.nonNull(targets, "Counting targets requires non-null targets collection.");
 
         final double segMean = segment.getSegmentMeanInCRSpace();
-        final List<TargetCoverage> myTargets = targets.targets(segment);
-        return myTargets.stream().map(t -> (Math.pow(2, t.getCoverage()) - segMean)).collect(Collectors.toList());
+        return targets.targets(segment).stream().map(t -> (Math.pow(2, t.getCount()) - segMean)).collect(Collectors.toList());
     }
 
     /*===============================================================================================================*
@@ -314,7 +297,7 @@ public final class SegmentUtils {
                                                                 final Genome genome) {
         Utils.nonNull(genome, "The genome cannot be null.");
 
-        final TargetCollection<TargetCoverage> targets = genome.getTargets();
+        final TargetCollection<ReadCountRecord.SingletonRecord> targets = genome.getTargets();
         final TargetCollection<AllelicCount> snps = genome.getSNPs();
         final String sampleName = genome.getSampleName();
 
@@ -337,7 +320,7 @@ public final class SegmentUtils {
                                                    final Genome genome) {
         Utils.nonNull(genome, "The genome cannot be null.");
 
-        final TargetCollection<TargetCoverage> targets = genome.getTargets();
+        final TargetCollection<ReadCountRecord.SingletonRecord> targets = genome.getTargets();
         final TargetCollection<AllelicCount> snps = genome.getSNPs();
         final String sampleName = genome.getSampleName();
 

src/main/java/org/broadinstitute/hellbender/tools/exome/detectcoveragedropout/CoverageDropoutDetector.java
@@ -11,10 +11,7 @@ import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.commons.math3.random.RandomGeneratorFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.broadinstitute.hellbender.tools.exome.ModeledSegment;
-import org.broadinstitute.hellbender.tools.exome.SegmentUtils;
-import org.broadinstitute.hellbender.tools.exome.TargetCollection;
-import org.broadinstitute.hellbender.tools.exome.TargetCoverage;
+import org.broadinstitute.hellbender.tools.exome.*;
 import org.broadinstitute.hellbender.utils.Utils;
 import org.broadinstitute.hellbender.utils.param.ParamUtils;
 
@@ -79,7 +76,8 @@ public final class CoverageDropoutDetector {
      *
      * @return never {@code null}.
      */
-    private MixtureMultivariateNormalFitResult retrieveGaussianMixtureModelForFilteredTargets(final ModeledSegment segment, final TargetCollection<TargetCoverage> targets, final double minProportion, final int numComponents){
+    private MixtureMultivariateNormalFitResult retrieveGaussianMixtureModelForFilteredTargets(final ModeledSegment segment,
+        final TargetCollection<ReadCountRecord.SingletonRecord> targets, final double minProportion, final int numComponents){
         final List<ModeledSegment> segmentSingleton = Collections.singletonList(segment);
         return retrieveGaussianMixtureModelForFilteredTargets(segmentSingleton, targets, minProportion, numComponents);
     }
@@ -96,7 +94,8 @@ public final class CoverageDropoutDetector {
      * @param numComponents -- number of components to use in the GMM.  Usually, this is 2.
      * @return  never {@code null}.  Fitting result with indications whether it converged or was even attempted.
      */
-    private MixtureMultivariateNormalFitResult retrieveGaussianMixtureModelForFilteredTargets(final List<ModeledSegment> segments, final TargetCollection<TargetCoverage> targets, double minProportion, int numComponents){
+    private MixtureMultivariateNormalFitResult retrieveGaussianMixtureModelForFilteredTargets(final List<ModeledSegment> segments,
+        final TargetCollection<ReadCountRecord.SingletonRecord> targets, double minProportion, int numComponents){
 
         // For each target in a segment that contains enough targets, normalize the difference against the segment mean
         //  and collapse the filtered targets into the copy ratio estimates.
@@ -141,7 +140,8 @@ public final class CoverageDropoutDetector {
      * @param minProportion -- minimum proportion (0, 1) of targets in the segment in order for the targets to be considered.
      * @return never {@code null}.  If no targets remain after filtering, an empty list will be returned.
      */
-    private List<Double> getNumProbeFilteredTargetList(final List<ModeledSegment> segments, final TargetCollection<TargetCoverage> targets, final double minProportion) {
+    private List<Double> getNumProbeFilteredTargetList(final List<ModeledSegment> segments,
+        final TargetCollection<ReadCountRecord.SingletonRecord> targets, final double minProportion) {
         final int allTargetCount = targets.targetCount();
         final double minProportionCount = Math.max(Math.ceil(allTargetCount * minProportion), ABSOLUTE_MIN_NUM_TARGETS);
 
@@ -169,7 +169,8 @@ public final class CoverageDropoutDetector {
      *                             an actual two component GMM.
      * @return never {@code null}.  Whether this sample should be treated as a failure and additional data for analysis.
      */
-    public CoverageDropoutResult determineCoverageDropoutDetected(final List<ModeledSegment> segments, final TargetCollection<TargetCoverage> targets, final double minTargetProportion, final double thresholdDistancePerSegment, final double minProportionGoodSegments, final double minWeightRestriction) {
+    public CoverageDropoutResult determineCoverageDropoutDetected(final List<ModeledSegment> segments, final TargetCollection<ReadCountRecord.SingletonRecord> targets,
+        final double minTargetProportion, final double thresholdDistancePerSegment, final double minProportionGoodSegments, final double minWeightRestriction) {
 
         Utils.nonNull(segments, "Segments cannot be null");
         ParamUtils.inRange(minTargetProportion, 0, 1, "minimum target proportion must be in the range (0,1).");

src/test/java/org/broadinstitute/hellbender/tools/exome/acnvconversion/ACNVModeledSegmentConversionUtilsUnitTest.java
@@ -18,10 +18,10 @@ public class ACNVModeledSegmentConversionUtilsUnitTest extends BaseTest {
         final ACNVModeledSegment acnvModeledSegment = new ACNVModeledSegment(new SimpleInterval("1", 1000, 1500),
                 new PosteriorSummary(-4000, -4001, -4002),
                 new PosteriorSummary(-4000, -4001, -4002));
-        final List<TargetCoverage> targets = new ArrayList<>();
-        targets.add(new TargetCoverage("test", new SimpleInterval("1", 1300, 1302), 40));
+        final List<ReadCountRecord.SingletonRecord> targets = new ArrayList<>();
+        targets.add(new ReadCountRecord.SingletonRecord(new Target("test", new SimpleInterval("1", 1300, 1302)), 40));
 
-        final TargetCollection<TargetCoverage> targetCollection = new HashedListTargetCollection<>(targets);
+        final TargetCollection<ReadCountRecord.SingletonRecord> targetCollection = new HashedListTargetCollection<>(targets);
         final ModeledSegment guess = ACNVModeledSegmentConversionUtils.convertACNVModeledSegmentToModeledSegment(acnvModeledSegment, targetCollection);
         Assert.assertTrue(guess.getSegmentMeanInCRSpace() > 0);
         Assert.assertEquals(guess.getSegmentMean(), ParamUtils.log2(TangentNormalizer.EPSILON), 1e-10);

broadinstitute/gatk

src/main/java/org/broadinstitute/hellbender/tools/exome/SegmentMergeUtils.java
@@ -560,8 +560,8 @@ public final class SegmentMergeUtils {
          * @return          list of the coverages for targets in the segment
          */
         private static List<Double> makeCoverageList(final SimpleInterval segment,
-                                                     final TargetCollection<TargetCoverage> targets) {
-            return targets.targets(segment).stream().map(TargetCoverage::getCoverage).collect(Collectors.toList());
+                                                     final TargetCollection<ReadCountRecord.SingleSampleRecord> targets) {
+            return targets.targets(segment).stream().map(ReadCountRecord.SingleSampleRecord::getCount).collect(Collectors.toList());
         }
 
         /**
@@ -575,7 +575,7 @@ public final class SegmentMergeUtils {
          * @return          scores for adjacent segments based on Hodges-Lehmann estimators
          */
         private static Pair<Double, Double> calculateTargetScores(final List<SimpleInterval> segments,
-                                                                  final TargetCollection<TargetCoverage> targets,
+                                                                  final TargetCollection<ReadCountRecord.SingleSampleRecord> targets,
                                                                   final int index) {
             final SimpleInterval leftSegment = segments.get(index - 1);
             final SimpleInterval centerSegment = segments.get(index);
@@ -625,7 +625,7 @@ public final class SegmentMergeUtils {
 
             //if any of the three segments is missing SNPs or the SNP scores are too similar,
             //try to use target scores based on Hodges-Lehmann estimator
-            final TargetCollection<TargetCoverage> targets = genome.getTargets();
+            final TargetCollection<ReadCountRecord.SingleSampleRecord> targets = genome.getTargets();
             final Pair<Double, Double> targetScores = calculateTargetScores(segments, targets, index);
             if (Math.max(targetScores.getLeft(), targetScores.getRight()) > TARGET_SCORE_THRESHOLD) {
                 return targetScores;

src/main/java/org/broadinstitute/hellbender/tools/exome/SegmentUtils.java
@@ -34,22 +34,6 @@ public final class SegmentUtils {
     private SegmentUtils() {}
 
     /**
-     * Returns the mean of coverages for targets in a given collection that overlap a given interval.
-     */
-    public static double meanTargetCoverage(final Locatable interval,
-                                            final TargetCollection<TargetCoverage> targets) {
-        Utils.nonNull(interval, "Can't get mean coverage of null interval.");
-        Utils.nonNull(targets, "The collection of target coverages cannot be null.");
-
-        final List<TargetCoverage> myTargets = targets.targets(interval);
-
-        if (myTargets.size() == 0) {
-            throw new IllegalStateException("Empty segment -- no overlapping targets.");
-        }
-        return myTargets.stream().mapToDouble(TargetCoverage::getCoverage).average().getAsDouble();
-    }
-
-    /**
      * Get difference between a segment mean and the overlapping targets.
      * This will select the overlapping targets from the input collection.
      *
@@ -58,13 +42,12 @@ public final class SegmentUtils {
      * @return          never {@code null}.  List of doubles of the difference, possibly empty.  Modifiable.
      */
     public static List<Double> segmentMeanTargetDifference(final ModeledSegment segment,
-                                                           final TargetCollection<TargetCoverage> targets) {
+                                                           final TargetCollection<ReadCountRecord.SingleSampleRecord> targets) {
         Utils.nonNull(segment, "Can't count targets of null segment.");
         Utils.nonNull(targets, "Counting targets requires non-null targets collection.");
 
         final double segMean = segment.getSegmentMeanInCRSpace();
-        final List<TargetCoverage> myTargets = targets.targets(segment);
-        return myTargets.stream().map(t -> (Math.pow(2, t.getCoverage()) - segMean)).collect(Collectors.toList());
+        return targets.targets(segment).stream().map(t -> (Math.pow(2, t.getCount()) - segMean)).collect(Collectors.toList());
     }
 
     /*===============================================================================================================*
@@ -314,7 +297,7 @@ public final class SegmentUtils {
                                                                 final Genome genome) {
         Utils.nonNull(genome, "The genome cannot be null.");
 
-        final TargetCollection<TargetCoverage> targets = genome.getTargets();
+        final TargetCollection<ReadCountRecord.SingleSampleRecord> targets = genome.getTargets();
         final TargetCollection<AllelicCount> snps = genome.getSNPs();
         final String sampleName = genome.getSampleName();
 
@@ -337,7 +320,7 @@ public final class SegmentUtils {
                                                    final Genome genome) {
         Utils.nonNull(genome, "The genome cannot be null.");
 
-        final TargetCollection<TargetCoverage> targets = genome.getTargets();
+        final TargetCollection<ReadCountRecord.SingleSampleRecord> targets = genome.getTargets();
         final TargetCollection<AllelicCount> snps = genome.getSNPs();
         final String sampleName = genome.getSampleName();
 

src/main/java/org/broadinstitute/hellbender/tools/exome/detectcoveragedropout/CoverageDropoutDetector.java
@@ -11,10 +11,7 @@ import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.commons.math3.random.RandomGeneratorFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.broadinstitute.hellbender.tools.exome.ModeledSegment;
-import org.broadinstitute.hellbender.tools.exome.SegmentUtils;
-import org.broadinstitute.hellbender.tools.exome.TargetCollection;
-import org.broadinstitute.hellbender.tools.exome.TargetCoverage;
+import org.broadinstitute.hellbender.tools.exome.*;
 import org.broadinstitute.hellbender.utils.Utils;
 import org.broadinstitute.hellbender.utils.param.ParamUtils;
 
@@ -79,7 +76,8 @@ public final class CoverageDropoutDetector {
      *
      * @return never {@code null}.
      */
-    private MixtureMultivariateNormalFitResult retrieveGaussianMixtureModelForFilteredTargets(final ModeledSegment segment, final TargetCollection<TargetCoverage> targets, final double minProportion, final int numComponents){
+    private MixtureMultivariateNormalFitResult retrieveGaussianMixtureModelForFilteredTargets(final ModeledSegment segment,
+                                                                                              final TargetCollection<ReadCountRecord.SingleSampleRecord> targets, final double minProportion, final int numComponents){
         final List<ModeledSegment> segmentSingleton = Collections.singletonList(segment);
         return retrieveGaussianMixtureModelForFilteredTargets(segmentSingleton, targets, minProportion, numComponents);
     }
@@ -96,7 +94,8 @@ public final class CoverageDropoutDetector {
      * @param numComponents -- number of components to use in the GMM.  Usually, this is 2.
      * @return  never {@code null}.  Fitting result with indications whether it converged or was even attempted.
      */
-    private MixtureMultivariateNormalFitResult retrieveGaussianMixtureModelForFilteredTargets(final List<ModeledSegment> segments, final TargetCollection<TargetCoverage> targets, double minProportion, int numComponents){
+    private MixtureMultivariateNormalFitResult retrieveGaussianMixtureModelForFilteredTargets(final List<ModeledSegment> segments,
+                                                                                              final TargetCollection<ReadCountRecord.SingleSampleRecord> targets, double minProportion, int numComponents){
 
         // For each target in a segment that contains enough targets, normalize the difference against the segment mean
         //  and collapse the filtered targets into the copy ratio estimates.
@@ -141,7 +140,8 @@ public final class CoverageDropoutDetector {
      * @param minProportion -- minimum proportion (0, 1) of targets in the segment in order for the targets to be considered.
      * @return never {@code null}.  If no targets remain after filtering, an empty list will be returned.
      */
-    private List<Double> getNumProbeFilteredTargetList(final List<ModeledSegment> segments, final TargetCollection<TargetCoverage> targets, final double minProportion) {
+    private List<Double> getNumProbeFilteredTargetList(final List<ModeledSegment> segments,
+                                                       final TargetCollection<ReadCountRecord.SingleSampleRecord> targets, final double minProportion) {
         final int allTargetCount = targets.targetCount();
         final double minProportionCount = Math.max(Math.ceil(allTargetCount * minProportion), ABSOLUTE_MIN_NUM_TARGETS);
 
@@ -169,7 +169,8 @@ public final class CoverageDropoutDetector {
      *                             an actual two component GMM.
      * @return never {@code null}.  Whether this sample should be treated as a failure and additional data for analysis.
      */
-    public CoverageDropoutResult determineCoverageDropoutDetected(final List<ModeledSegment> segments, final TargetCollection<TargetCoverage> targets, final double minTargetProportion, final double thresholdDistancePerSegment, final double minProportionGoodSegments, final double minWeightRestriction) {
+    public CoverageDropoutResult determineCoverageDropoutDetected(final List<ModeledSegment> segments, final TargetCollection<ReadCountRecord.SingleSampleRecord> targets,
+        final double minTargetProportion, final double thresholdDistancePerSegment, final double minProportionGoodSegments, final double minWeightRestriction) {
 
         Utils.nonNull(segments, "Segments cannot be null");
         ParamUtils.inRange(minTargetProportion, 0, 1, "minimum target proportion must be in the range (0,1).");

src/test/java/org/broadinstitute/hellbender/tools/exome/acnvconversion/ACNVModeledSegmentConversionUtilsUnitTest.java
@@ -18,10 +18,10 @@ public class ACNVModeledSegmentConversionUtilsUnitTest extends BaseTest {
         final ACNVModeledSegment acnvModeledSegment = new ACNVModeledSegment(new SimpleInterval("1", 1000, 1500),
                 new PosteriorSummary(-4000, -4001, -4002),
                 new PosteriorSummary(-4000, -4001, -4002));
-        final List<TargetCoverage> targets = new ArrayList<>();
-        targets.add(new TargetCoverage("test", new SimpleInterval("1", 1300, 1302), 40));
+        final List<ReadCountRecord.SingleSampleRecord> targets = new ArrayList<>();
+        targets.add(new ReadCountRecord.SingleSampleRecord(new Target("test", new SimpleInterval("1", 1300, 1302)), 40));
 
-        final TargetCollection<TargetCoverage> targetCollection = new HashedListTargetCollection<>(targets);
+        final TargetCollection<ReadCountRecord.SingleSampleRecord> targetCollection = new HashedListTargetCollection<>(targets);
         final ModeledSegment guess = ACNVModeledSegmentConversionUtils.convertACNVModeledSegmentToModeledSegment(acnvModeledSegment, targetCollection);
         Assert.assertTrue(guess.getSegmentMeanInCRSpace() > 0);
         Assert.assertEquals(guess.getSegmentMean(), ParamUtils.log2(TangentNormalizer.EPSILON), 1e-10);