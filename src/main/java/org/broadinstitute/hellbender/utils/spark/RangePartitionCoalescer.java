c750ced721c804acb0e07a32dddd6bbc9b7b4afb
Tom White <tom@cloudera.com>
2016-07-26 15:44:42 +0100

    Handle overlapping partitions.

---
+++ src/main/java/org/broadinstitute/hellbender/utils/spark/RangePartitionCoalescer.java
@@ -0,0 +1,44 @@
+package org.broadinstitute.hellbender.utils.spark;
+
+import org.apache.spark.Partition;
+import org.apache.spark.rdd.PartitionGroup;
+import org.apache.spark.rdd.RDD;
+import scala.collection.JavaConversions;
+import scala.collection.Seq;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+class RangePartitionCoalescer implements PartitionCoalescer, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<Integer> maxEndPartitionIndexes;
+
+    public RangePartitionCoalescer(List<Integer> maxEndPartitionIndexes) {
+        this.maxEndPartitionIndexes = maxEndPartitionIndexes;
+    }
+
+    @Override
+    public PartitionGroup[] coalesce(int maxPartitions, RDD<?> parent) {
+        if (maxPartitions != parent.getNumPartitions()) {
+            throw new IllegalArgumentException("Cannot use " + getClass().getSimpleName() +
+                    " with a different number of partitions to the parent RDD.");
+        }
+        List<Partition> partitions = Arrays.asList(parent.getPartitions());
+        PartitionGroup[] groups = new PartitionGroup[partitions.size()];
+
+        for (int i = 0; i < partitions.size(); i++) {
+            Seq<String> preferredLocations = parent.getPreferredLocations(partitions.get(i));
+            scala.Option<String> preferredLocation = scala.Option.apply
+                    (preferredLocations.isEmpty() ? null : preferredLocations.apply(0));
+            PartitionGroup group = new PartitionGroup(preferredLocation);
+            List<Partition> partitionsInGroup =
+                    partitions.subList(i, maxEndPartitionIndexes.get(i) + 1);
+            group.arr().append(JavaConversions.asScalaBuffer(partitionsInGroup));
+            groups[i] = group;
+        }
+        return groups;
+    }
+}
