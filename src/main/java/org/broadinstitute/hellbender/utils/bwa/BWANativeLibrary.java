49a0fc11f11935744f6ea05ffae9d97eb008be56
Louis Bergelson <louisb@broadinstitute.org>
2016-08-16 14:22:39 -0400

    adding support for jBWA on power (#2078)
    * adding support for jBWA on power
    
    moving from jbwa 1.0.0 -> 1.0.0_ppc64
    updating the BWANativeLibrary loader to be ppc aware

--- src/main/java/org/broadinstitute/hellbender/utils/bwa/BWANativeLibrary.java
+++ src/main/java/org/broadinstitute/hellbender/utils/bwa/BWANativeLibrary.java
@@ -8,11 +8,25 @@ import org.broadinstitute.hellbender.utils.NativeUtils;
  * BWANativeLibrary.loadNativeJBWALibrary() first.
  */
 public class BWANativeLibrary {
+    /**
+     * load the native JBWA Library, this should be called before using any methods from {@link com.github.lindenb.jbwa.jni}
+     * @throws org.broadinstitute.hellbender.exceptions.UserException.HardwareFeatureException if the library could not be loaded
+     */
     public static void load() {
         // Load native library in each task VM
-        final String libraryPath = NativeUtils.runningOnLinux() ? "/libbwajni.so" : "/libbwajni.jnilib";
-        if (! NativeUtils.loadLibraryFromClasspath(libraryPath)){
-            throw new UserException( "jbwa library was not loaded. This could be due to a configuration error, or your system might not support it.");
+        final String libraryPath = getLibraryNameForSystem();
+        if (!NativeUtils.loadLibraryFromClasspath(libraryPath)) {
+            throw new UserException.HardwareFeatureException("jbwa library was not loaded. This could be due to a configuration error, or your system might not support it.");
+        }
+    }
+
+    private static String getLibraryNameForSystem() {
+        if (NativeUtils.runningOnPPCArchitecture()) {
+            return "/libbwajni_ppc64.so";
+        } else if (NativeUtils.runningOnLinux()) {
+            return "/libbwajni.so";
+        } else {
+            return "/libbwajni.jnilib";
         }
     }
 }
