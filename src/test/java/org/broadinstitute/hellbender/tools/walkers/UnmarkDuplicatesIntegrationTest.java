839234bf395b2302080d8e724473cde7d1da5b71
Mark Fleharty <fleharty@users.noreply.github.com>
2016-09-20 15:07:59 -0400

    Adding cram test to UnmarkDuplicatesIntergrationTest (#2174)
    * Adding cram test to UnmarkDuplicatesIntergrationTest

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/UnmarkDuplicatesIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/UnmarkDuplicatesIntegrationTest.java
@@ -5,7 +5,9 @@ import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.SamReader;
 import htsjdk.samtools.SamReaderFactory;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
+import org.broadinstitute.hellbender.utils.test.ArgumentsBuilder;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -13,24 +15,45 @@ import java.io.IOException;
 
 public class UnmarkDuplicatesIntegrationTest extends CommandLineProgramTest {
 
-    @Test
-    public void testUnmarkDuplicates() throws IOException {
-        final File inputBam = new File(getTestDataDir(), "walkers/UnmarkDuplicates/allDuplicates.bam");
-        Assert.assertEquals(getDuplicateCountForBam(inputBam), 11, "Wrong number of duplicates in original input file");
-
-        final File outputBam = createTempFile("testUnmarkDuplicates", ".bam");
-        final String[] args = new String[] {
-                "--input" , inputBam.getAbsolutePath(),
-                "--output", outputBam.getAbsolutePath()
-        };
-        runCommandLine(args);
-
-        Assert.assertEquals(getDuplicateCountForBam(outputBam), 0, "Duplicates still present after UnmarkDuplicates");
+    @DataProvider(name = "testUnmarkDuplicatesProvider")
+    private Object[][] testUmiSetsDataProvider() {
+        return new Object[][]{{
+                "walkers/UnmarkDuplicates/allDuplicates.bam",
+                11,
+                null
+        }, {
+                "walkers/UnmarkDuplicates/allDuplicates.cram",
+                11,
+                new File("src/test/resources/hg19mini.fasta")
+        } };
     }
 
-    private static int getDuplicateCountForBam( final File bam ) throws IOException {
+    @Test(dataProvider = "testUnmarkDuplicatesProvider")
+    public void testUnmarkDuplicates(String samFile, int duplicateCount, File referenceFile) throws IOException {
+        final File inputBam = new File(getTestDataDir(), samFile);
+        Assert.assertEquals(getDuplicateCountForBam(inputBam, referenceFile), duplicateCount, "Wrong number of duplicates in original input file");
+
+        final File outputBam = createTempFile("testUnmarkDuplicates", ".bam");
+
+        ArgumentsBuilder args = new ArgumentsBuilder();
+        args.addInput(inputBam).addOutput(outputBam);
+
+        if(referenceFile != null) {
+            args.addReference(referenceFile);
+        }
+
+        runCommandLine(args);
+
+        Assert.assertEquals(getDuplicateCountForBam(outputBam, referenceFile), 0, "Duplicates still present after UnmarkDuplicates");
+    }
+
+    private static int getDuplicateCountForBam(final File bam, final File referenceFile) throws IOException {
         int duplicateCount = 0;
-        try ( final SamReader reader = SamReaderFactory.makeDefault().open(bam) ) {
+        final SamReaderFactory factory = SamReaderFactory.makeDefault();
+        if(referenceFile != null) {
+            factory.referenceSequence(referenceFile);
+        }
+        try ( final SamReader reader = factory.open(bam) ) {
             for ( SAMRecord read : reader ) {
                 if ( read.getDuplicateReadFlag() ) {
                     ++duplicateCount;
