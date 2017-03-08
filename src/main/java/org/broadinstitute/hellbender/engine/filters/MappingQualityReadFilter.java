179121d0a89053a4805d43f677d18326e84c2e36
Daniel Gómez-Sánchez <magicDGS@users.noreply.github.com>
2017-03-08 19:02:00 +0100

    Add --maximumMappingQuality to MappingQualityReadFilter (#2433)
    * Add --maximumMappingQuality to MappingQualityReadFilter
    
    * Addressed comments

--- src/main/java/org/broadinstitute/hellbender/engine/filters/MappingQualityReadFilter.java
+++ src/main/java/org/broadinstitute/hellbender/engine/filters/MappingQualityReadFilter.java
@@ -8,14 +8,20 @@ import org.broadinstitute.hellbender.utils.read.GATKRead;
 import java.io.Serializable;
 
 /**
- * Keep reads with mapping qualities above a specified threshold.
+ * Keep reads with mapping qualities within a specified range.
+ *
+ * Note: this filter does not handle specially the unavailable mapping quality ({@link org.broadinstitute.hellbender.utils.QualityUtils#MAPPING_QUALITY_UNAVAILABLE}).
+ * Use {@link org.broadinstitute.hellbender.engine.filters.ReadFilterLibrary.MappingQualityAvailableReadFilter} to explicitly filter out reads with unavailable quality.
  */
 @DocumentedFeature(groupName= HelpConstants.DOC_CAT_READFILTERS, groupSummary=HelpConstants.DOC_CAT_READFILTERS_SUMMARY)
 public final class MappingQualityReadFilter extends ReadFilter implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Argument(fullName="minimumMappingQuality", shortName="minimumMappingQuality", optional=true)
+    @Argument(fullName="minimumMappingQuality", shortName="minimumMappingQuality", doc = "Minimum mapping quality to keep (inclusive)", optional=true)
     public int minMappingQualityScore = 10;
+
+    @Argument(fullName="maximumMappingQuality", shortName="maximumMappingQuality", doc = "Maximum mapping quality to keep (inclusive)", optional=true)
+    public Integer maxMappingQualityScore = null;
 
     // Command line parser requires a no-arg constructor
     public MappingQualityReadFilter() {}
@@ -24,8 +30,15 @@ public final class MappingQualityReadFilter extends ReadFilter implements Serial
         this.minMappingQualityScore = minMappingQualityScore;
     }
 
+    public MappingQualityReadFilter( final int minMappingQualityScore, final Integer maxMappingQualityScore) {
+        this.minMappingQualityScore = minMappingQualityScore;
+        this.maxMappingQualityScore = maxMappingQualityScore;
+    }
+
     @Override
     public boolean test( final GATKRead read ) {
-        return read.getMappingQuality() >= minMappingQualityScore;
+        final int mq = read.getMappingQuality();
+        return  mq >= minMappingQualityScore
+                && (maxMappingQualityScore == null || mq <= maxMappingQualityScore);
     }
 }
