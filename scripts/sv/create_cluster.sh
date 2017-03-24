d40ccc263f673f4d5a11457528d1f30e2b5ec797
Steve Huang <shuang@broadinstitute.org>
2017-03-23 21:09:50 -0400

    updated sv-pipeline cluster creation, initialization and execution script (#2468)

--- scripts/sv/create_cluster.sh
+++ scripts/sv/create_cluster.sh
@@ -1,27 +1,70 @@
 #!/bin/bash
 
-# This script deletes a Google Dataproc cluster used for running the GATK-SV pipeline.
+# This script creates a Google Dataproc cluster used for running the GATK-SV pipeline.
 
 set -eu
-if [[ "$#" -ne 3 ]]; then
-    echo "Please provide the project, the name of cluster, and the bucket where the data lives"
+
+if [[ "$#" -lt 5 ]]; then
+    echo -e "Please provide:"
+    echo -e "  [1] project name (required)"
+    echo -e "  [2] cluster name (required)"
+    echo -e "  [3] GCS directory containing the correct reference & indices (required)"
+    echo -e "  [4] GCS directory containing the BAM and its index (required), and"
+    echo -e "either when a local initialization script is to be uploaded to a bucket:"
+    echo -e "  [5] local initialization script"
+    echo -e "  [6] GCS path to upload initialization script to (required as per Google)"
+    echo -e "or when the initialization script is already in the cloud: "
+    echo -e "  [5] path to initialization script on GCS"
+    echo -e "\nExample when you have a local custom initialization script:"
+    echo -e "  bash create_cluster.sh \\"
+    echo -e "       <PROJECT-NAME> \\"
+    echo -e "       <CLUSTER-NAME> \\"
+    echo -e "       <BUCKET-DIR-STORING-REFERENCE> \\"
+    echo -e "       <BUCKET-DIR-STORING-BAM> \\"
+    echo -e "       <LOCAL-CUSTOM-INIT-ACTION-SCRIPT> \\"
+    echo -e "       <BUCKET-DIR-TO-STORE-INIT-SCRIPT>"
+    echo -e "A default initialization script \"default_init.sh\" is distributed with this script."
+    echo -e "If you don't need any custom initialization actions, use that."
+    echo -e "\nExample 2 when a initialization script lives in a GCS bucket"
+    echo -e "  bash create_cluster.sh \\"
+    echo -e "       <PROJECT-NAME> \\"
+    echo -e "       <CLUSTER-NAME> \\"
+    echo -e "       <BUCKET-DIR-STORING-REFERENCE> \\"
+    echo -e "       <BUCKET-DIR-STORING-BAM> \\"
+    echo -e "       <PATH-TO-INIT-SCRIPT-ON-GCS>"
     exit 1
 fi
 
 PROJECT=$1
 CLUSTER_NAME=$2
-INITIALIZATION_BUCKET=$3
+REF_DIR=$3
+SAMP_DIR=$4
+
+INIT_ACTION=""
+if [[ "$#" -eq 5 ]]; then 
+    INIT_ACTION=$5
+else
+    gsutil cp $5 $6
+    INIT_ACTION=$6
+    if [[ "$INIT_ACTION" =~ .+/ ]]; then
+        INIT_ACTION+=$(basename $5)
+    else
+        INIT_ACTION+="/"
+        INIT_ACTION+=$(basename $5)
+    fi
+fi
 
 gcloud dataproc clusters create ${CLUSTER_NAME} \
     --zone us-central1-a \
     --master-machine-type n1-highmem-8 \
-    --num-worker-local-ssds 1 \
     --worker-machine-type n1-highmem-16 \
     --master-boot-disk-size 500 \
     --worker-boot-disk-size 500 \
     --num-workers 10 \
+    --num-worker-local-ssds 1 \
+    --metadata "reference=$REF_DIR" \
+    --metadata "sample=$SAMP_DIR" \
     --image-version preview \
     --project ${PROJECT} \
-    --initialization-actions gs://${INITIALIZATION_BUCKET}/initialization_scripts/dataproc_initialization_new_bucket_copy_ref_img.sh \
+    --initialization-actions ${INIT_ACTION} \
     --initialization-action-timeout 60m
-
---
+++ scripts/sv/discoverVariants.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+# either have the required variables set in the running environment, or provide them 
+#  in command line
+if [[ -z ${GATK_DIR+x} || -z ${CLUSTER_NAME+x} || -z ${PROJECT_OUTPUT_DIR+x} || -z ${REF_FASTA+x} || -z ${REF_2BIT+x} ]]; then 
+    if [[ "$#" -ne 4 ]]; then
+        echo -e "Please provide:"
+        echo -e "  [1] local directory of GATK build (required)"
+        echo -e "  [2] cluster name (required)"
+        echo -e "  [3] output directory on the cluster (required)"
+        echo -e "  [4] absolute path to the reference fasta on the cluster (required)"
+    exit 1
+    fi
+    GATK_DIR=$1
+    CLUSTER_NAME=$2
+    MASTER_NODE="hdfs://""$CLUSTER_NAME""-m:8020"
+    PROJECT_OUTPUT_DIR="$MASTER_NODE"/$3
+    REF_FASTA="$MASTER_NODE"/$4
+    REF_2BIT=$(echo $REF_FASTA | sed 's/.fasta$/.2bit/')
+fi
+
+cd "$GATK_DIR" 
+
+./gatk-launch CallVariantsFromAlignedContigsSpark \
+    --inputAlignments "$PROJECT_OUTPUT_DIR"/aligned_assemblies \
+    --inputAssemblies "$PROJECT_OUTPUT_DIR"/assembly_0 \
+    --outputPath "$PROJECT_OUTPUT_DIR"/variants \
+    -R "$REF_2BIT" \
+    -fastaReference "$REF_FASTA" \
+    -- \
+    --sparkRunner GCS \
+    --cluster "$CLUSTER_NAME" \
+    --driver-memory 30G \
+    --executor-memory 8G \
+    --conf spark.yarn.executor.memoryOverhead=5000
