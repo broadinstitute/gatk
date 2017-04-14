267581d18b1dc439a932f2a87c3295a4c366ee21
kdatta <kushal.datta@intel.com>
2017-04-14 08:45:16 -0700

    required/optional variant argument collection reverted back as they are not used in genomicsdb import tool

--- src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/RequiredVariantInputArgumentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/RequiredVariantInputArgumentCollection.java
@@ -5,50 +5,18 @@ import htsjdk.variant.variantcontext.VariantContext;
 import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.engine.FeatureInput;
-import org.broadinstitute.hellbender.utils.io.IOUtils;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * An argument collection for use with tools that accept one or more input files containing VariantContext records
  * (eg., VCF files), and require at least one such input.
  */
-public final class RequiredVariantInputArgumentCollection extends VariantInputArgumentCollection {
+public final class RequiredVariantInputArgumentCollection implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Argument(fullName = StandardArgumentDefinitions.VARIANT_LONG_NAME,
-      shortName = StandardArgumentDefinitions.VARIANT_SHORT_NAME,
-      doc = "One or more files containing variants", optional = false)
+    @Argument(fullName = StandardArgumentDefinitions.VARIANT_LONG_NAME, shortName = StandardArgumentDefinitions.VARIANT_SHORT_NAME, doc = "One or more files containing variants", optional = false)
     public List<FeatureInput<VariantContext>> variantFiles;
 
-    public List<String> variantFileNames;
-
-    /**
-     * Get the list of BAM/SAM/CRAM files specified at the command line.
-     * Paths are the preferred format, as this can handle both local
-     * disk and NIO direct access to cloud storage.
-     */
-    @Override
-    public List<Path> getVariantPaths() {
-        ArrayList<Path> ret = new ArrayList<>();
-        for (String fn : variantFileNames) {
-            ret.add(IOUtils.getPath(fn));
-        }
-        return ret;
-    }
-
-    /**
-     * Get the list of BAM/SAM/CRAM files specified at the command line
-     */
-    @Override
-    public List<File> getVariantFiles() {
-      ArrayList<File> ret = new ArrayList<>();
-      for (String fn : variantFileNames) {
-        ret.add(new File(fn));
-      }
-      return ret;
-    }
 }
