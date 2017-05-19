0457084ceea9e5c5c10a46fae9c5415355f5f531
Louis Bergelson <louisb@broadinstitute.org>
2017-05-19 17:40:08 -0400

    fix unit test

--- src/test/java/org/broadinstitute/hellbender/tools/picard/vcf/GatherVcfsUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/picard/vcf/GatherVcfsUnitTest.java
@@ -5,13 +5,16 @@ import htsjdk.samtools.util.CloseableIterator;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.variantcontext.VariantContextComparator;
 import htsjdk.variant.vcf.VCFFileReader;
+import org.broadinstitute.hellbender.utils.io.IOUtils;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public final class GatherVcfsUnitTest {
     //tests for GatherVcfs, specifically areAllBlockCompressed()
@@ -23,16 +26,17 @@ public final class GatherVcfsUnitTest {
 
         return new Object[][]{
                 {Lists.newArrayList(nullFile), false},
-                {Lists.newArrayList(new File("test.vcf")), false},
-                {Lists.newArrayList(new File("test.bcf")), false},
-                {Lists.newArrayList(new File("test.vcf.gz")), true},
-                {Lists.newArrayList(new File("test1.vcf"), new File("test2.vcf.gz")), false}
+                {Lists.newArrayList("test.vcf"), false},
+                {Lists.newArrayList("test.bcf"), false},
+                {Lists.newArrayList("test.vcf.gz"), true},
+                {Lists.newArrayList("test1.vcf", "test2.vcf.gz", false}
         };
     }
 
     @Test(dataProvider = "files")
-    public void testareAllBlockCompressed(List<File> files, boolean expected) throws IOException {
-        Assert.assertEquals(GatherVcfs.areAllBlockCompressed(files), expected);
+    public void testareAllBlockCompressed(List<String> files, boolean expected) throws IOException {
+        List<Path> paths = files.stream().map(IOUtils::getPath).collect(Collectors.toList());
+        Assert.assertEquals(GatherVcfs.areAllBlockCompressed(paths), expected);
     }
 
 }
