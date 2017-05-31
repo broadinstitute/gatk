73c4c0903c2fba8118cfcbef183a584df75fd71d
sooheelee <shlee@broadinstitute.org>
2017-05-31 07:52:04 -0400

    CNV GetBayesianHetCoverage: tag doc, give example command and interval_list, massage summary, prioritize modes (#1125)

--- src/main/java/org/broadinstitute/hellbender/tools/exome/GetBayesianHetCoverage.java
+++ src/main/java/org/broadinstitute/hellbender/tools/exome/GetBayesianHetCoverage.java
@@ -24,35 +24,111 @@ import org.broadinstitute.hellbender.utils.read.ReadConstants;
 import java.io.File;
 
 /**
- * Calls heterozygous sites and outputs information about the called sites.
+ * Calls heterozygous SNPs, counts ref and alt alleles and gives genotype likelihoods.
  *
- * The tool has three modes. The mode is automatically selected based on the provided arguments:
+ * <p>The tool has three modes. The mode is automatically selected based on the provided arguments as follows. </p>
  *
  * <dl>
- *     <dt>NORMAL_ONLY</dt><dd>This mode is chosen if just the normal BAM file is provided. The balanced prior
- *     is used for estimating the likelihood of het sites (see {@link BalancedHeterozygousPileupPriorModel}).</dd>
- *
- *      <dt>TUMOR_ONLY</dt><dd>This mode is chosen if just the tumor BAM file is provided. The heterogeneous prior
- *      is used for estimating the likelihood of het sites (see {@link HeterogeneousHeterozygousPileupPriorModel}).</dd>
- *
- *      <dt>MATCHED_NORMAL_TUMOR</dt><dd>This mode is chosen if both normal and tumor BAM files are provided.
- *      The balanced prior (see {@link BalancedHeterozygousPileupPriorModel}) is used to estimate the likelihood of
- *      Het sites on the normal reads. The base counts on the same het sites are collected and reported from the
+ *      <dt>MATCHED_NORMAL_TUMOR</dt><dd>Mode when given both matched normal and tumor BAM files.
+ *      Estimates the likelihood of heterozygous genotypes on the normal reads using a balanced prior
+ *      (see {@link BalancedHeterozygousPileupPriorModel}). Counts bases for the same sites for the
  *      tumor reads.</dd>
+ *
+ *      <dt>TUMOR_ONLY</dt><dd>For instances lacking a matched normal.
+ *      Mode when given just the tumor BAM file with the --tumor argument. Estimates the likelihood of heterozygous genotypes
+ *      using the heterogeneous prior (see {@link HeterogeneousHeterozygousPileupPriorModel}).
+ *      This mode has only been tested for low purity tumors, i.e. with high normal contamination.
+ *      For high purity tumors, both sensitivity and precision may decrease.</dd>
+ *
+ *     <dt>NORMAL_ONLY</dt><dd>Extended use-case for germline heterozygous site coverage.
+ *     Mode when given just the normal BAM file with the --normal argument.
+ *     Estimates the likelihood of heterozygous genotypes using the balanced prior
+ *     (see {@link BalancedHeterozygousPileupPriorModel}).</dd>
  * </dl>
+ *
+ * <p>
+ *     The tool writes a table with the following columns:
+ *
+ *     <pre>
+ *     CONTIG,
+ *     POSITION,
+ *     REF_COUNT,
+ *     ALT_COUNT,
+ *     REF_NUCLEOTIDE,
+ *     ALT_NUCLEOTIDE,
+ *     READ_DEPTH,
+ *     HET_LOG_ODDS
+ *     READ_DEPTH = total pileup depth at the SNP site
+ *     HET_LOG_ODDS = log odds of the SNP site being heterozygous
+ *     </pre>
+ * </p>
+ *
+ * <p>
+ *     The required --snpIntervals file gives the genomic positions of common population Single Nucleotide Polymorphisms (SNPs)
+ *     in Picard-style interval list format. See https://broadinstitute.github.io/picard/command-line-overview.html#BedToIntervalList for details.
+ *     Here is an example file:
+ * </p>
+ *
+ * <pre>
+ *     @HD	VN:1.4	SO:unsorted
+ *     @SQ	SN:1	LN:16000	M5:8c0c38e352d8f3309eabe4845456f274
+ *     @SQ	SN:2	LN:16000	M5:5f8388fe3fb34aa38375ae6cf5e45b89
+ *     1	10736	10736	+	normal
+ *     1	11522	11522	+	normal
+ *     1	12098	12098	+	normal
+ *     1	12444	12444	+	normal
+ *      1	13059	13059	+	normal
+ *      1	14630	14630	+	normal
+ *     1	15204	15204	+	normal
+ *     2	14689	14689	+	normal
+ *      2	14982	14982	+	normal
+ *     2	15110	15110	+	normal
+ *     2	15629	15629	+	normal
+ * </pre>
+ *
+ * <h3>Examples</h3>
+ *
+ * <p>For MATCHED_NORMAL_TUMOR mode:</p>
+ *
+ * <pre>
+ * java -Xmx4g -jar $gatk_jar GetBayesianHetCoverage \
+ *   --reference ref_fasta.fa \
+ *   --snpIntervals common_sites.interval_list \
+ *   --tumor tumor.bam \
+ *   --normal normal.bam \
+ *   --tumorHets tumor.hets.tsv
+ * </pre>
+ *
+ * <p>
+ *     Detects het sites in the normal, and gives ref and alt counts for corresponding sites in the tumor sample.
+ *     The tool will not include the log odds of heterozygosity for the tumor
+ *     since the inference is made from normal sample's sites.
+ * </p>
+ *
+ * <p>For TUMOR_ONLY mode:</p>
+ *
+ * <pre>
+ * java -Xmx4g -jar $gatk_jar GetBayesianHetCoverage \
+ *   --reference ref_fasta.fa \
+ *   --snpIntervals common_sites.interval_list \
+ *   --tumor tumor.bam \
+ *   --tumorHets tumor.hets.tsv
+ * </pre>
+ *
  **
  * @author Mehrtash Babadi &lt;mehrtash@brgoadinstitute.org&gt;
  */
 @CommandLineProgramProperties(
-        summary = "Calls heterozygous sites and output the reference and alternate base counts at the called sites. A list" +
-                " of SNP sites must be provided. If only the normal BAM file is provided, the job will be run in the NORMAL_ONLY" +
-                " mode. The output will be detailed (genomic position of het sites, alt and ref counts and nucleotides," +
-                " read depth, and log odds of heterzygosity). If only the tumor BAM file is provided, the job will be run in" +
-                " the TUMOR_ONLY mode. The output will be detailed. If both the normal and tumor BAM files are provided, the" +
-                " Het sites are inferred from the normal reads and the counts on the same sites will be collected from the" +
-                " tumor reads. The output for the normal reads will be detailed. The outout for the tumor reads will not" +
-                " include the log odds of heterozygosity since the inference is made from the normal reads.",
-        oneLineSummary = "Call heterozygous sites and output information about the called sites",
+        summary = "Calls sites of heterozygous single nucleotide polymorphisms (SNPs) and counts reads with reference " +
+                "and alternate bases at the called sites. A Picard-style interval_list of expected or common population " +
+                "variant sites is required. In the first mode, given both matched normal and tumor BAMs, the tool infers heterozygous " +
+                "sites from the normal and produces counts and genotype likelihoods for the corresponding sites in the " +
+                "tumor. The tool will not include the log odds of heterozygosity for the tumor since the inference is " +
+                "made from the normal reads. \n" +
+                "Two other modes are available. Given only the tumor BAM, the tool estimates the likelihood of " +
+                "heterozygous genotypes using the heterogeneous prior. In contrast, given only a normal BAM, the tool " +
+                "estimates the likelihood of heterozygous genotypes using the balanced prior.",
+        oneLineSummary = "Calls heterozygous SNPs, counts ref and alt alleles and gives genotype likelihoods",
         programGroup = CopyNumberProgramGroup.class
 )
 @DocumentedFeature
