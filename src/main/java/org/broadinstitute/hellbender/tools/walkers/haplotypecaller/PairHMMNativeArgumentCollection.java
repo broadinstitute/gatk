84fbda69bf9528059777496a415be8eb6db63e61
Ernesto Brau <gooneto@gmail.com>
2017-06-05 18:50:05 -0400

    Changed default cores (threads) to 4 in OpenMP PairHMM; addresses #2717 (#2838)

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/PairHMMNativeArgumentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/PairHMMNativeArgumentCollection.java
@@ -9,7 +9,7 @@ import org.broadinstitute.barclay.argparser.Argument;
 public class PairHMMNativeArgumentCollection {
 
     @Argument(fullName = "nativePairHmmThreads", shortName = "threads", doc="How many threads should a native pairHMM implementation use", optional = true)
-    private int pairHmmNativeThreads = 1;
+    private int pairHmmNativeThreads = 4;
 
     @Argument(fullName = "useDoublePrecision", shortName = "useDoublePrecision", doc="use double precision in the native pairHmm. " +
             "This is slower but matches the java implementation better", optional = true)
