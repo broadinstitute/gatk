e5f6826de60a879fa984ca45ff8f9811b9e657a6
Takuto Sato <tsato@broadinstitute.org>
2017-06-30 16:45:24 -0400

    gc content annotation

---
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/annotator/GCcontent.java
@@ -0,0 +1,48 @@
+package org.broadinstitute.hellbender.tools.walkers.annotator;
+
+import htsjdk.variant.variantcontext.Allele;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFInfoHeaderLine;
+import htsjdk.variant.vcf.VCFStandardHeaderLines;
+import org.broadinstitute.hellbender.engine.ReferenceContext;
+import org.broadinstitute.hellbender.utils.SimpleInterval;
+import org.broadinstitute.hellbender.utils.genotyper.ReadLikelihoods;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+/**
+ * Created by tsato on 6/30/17.
+ */
+public class GCcontent extends InfoFieldAnnotation {
+    private static final String KEY = "GC";
+    private static final int k = 100;
+
+    @Override
+    public List<String> getKeyNames() {
+        return Collections.singletonList(KEY);
+    }
+
+    @Override
+    public List<VCFInfoHeaderLine> getDescriptions() {
+        // TODO: is this right?
+        return Collections.singletonList(VCFStandardHeaderLines.getInfoLine(getKeyNames().get(0)));
+    }
+
+    @Override
+    public Map<String, Object> annotate(ReferenceContext ref, VariantContext vc, ReadLikelihoods<Allele> likelihoods) {
+        SimpleInterval oldWindow = ref.getWindow();
+        // TODO: make interval precise
+        ref.setWindow(k/2, k/2);
+        final int C = 67;
+        final int G = 71;
+        final byte[] bases = ref.getBases();
+        final long gccount = IntStream.range(0, bases.length).map(i -> bases[i]).filter(j -> j == C || j == G).count();
+        final double gcfraction = (double) gccount / bases.length;
+        // TODO: restore window?
+        return Collections.singletonMap(getKeyNames().get(0), String.format("%.4f", gcfraction));
+    }
+}
---
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/annotator/GCcontentUnitTest.java
@@ -0,0 +1,44 @@
+package org.broadinstitute.hellbender.tools.walkers.annotator;
+
+import htsjdk.variant.variantcontext.Allele;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.VariantContextBuilder;
+import org.broadinstitute.hellbender.CommandLineProgramTest;
+import org.broadinstitute.hellbender.engine.ReferenceContext;
+import org.broadinstitute.hellbender.engine.ReferenceDataSource;
+import org.broadinstitute.hellbender.engine.ReferenceFileSource;
+import org.broadinstitute.hellbender.utils.SimpleInterval;
+import org.broadinstitute.hellbender.utils.test.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by tsato on 6/30/17.
+ */
+public class GCcontentUnitTest extends BaseTest {
+    @Test
+    public void testBasic() throws Exception {
+        ReferenceDataSource refSource = new ReferenceFileSource(new File(hg19_chr1_1M_Reference));
+        String chr = "1";
+        int start = 800000;
+        SimpleInterval interval = new SimpleInterval("1", start, start);
+        ReferenceContext refContext = new ReferenceContext(refSource, interval);
+
+        // an A -> C SNP at position 50
+        VariantContextBuilder variantContextBuilder = new VariantContextBuilder("Megan", "1", start / 2, start / 2,
+                Arrays.asList(Allele.create("A", true), Allele.create("C")));
+        VariantContext variantContext = variantContextBuilder.make();
+
+        final GCcontent gccontentAnnotation = new GCcontent();
+        Map<String, Object> annotation = gccontentAnnotation.annotate(refContext, variantContext, null);
+
+        final double observed = Double.valueOf(String.valueOf(annotation.get(gccontentAnnotation.getKeyNames().get(0))));
+        Assert.assertEquals(observed, 0.405, 1e-3);
+    }
+}
