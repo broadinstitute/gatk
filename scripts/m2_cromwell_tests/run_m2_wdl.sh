c951f6a5c448a6a0a02cfa0fe40c836b4414326f
Louis Bergelson <louisb@broadinstitute.org>
2017-07-06 17:21:16 -0400

    update

--- scripts/m2_cromwell_tests/run_m2_wdl.sh
+++ scripts/m2_cromwell_tests/run_m2_wdl.sh
@@ -14,7 +14,7 @@ HASH_TO_USE=$TRAVIS_COMMIT
 echo "Building docker without running unit tests... ========="
 cd $WORKING_DIR/gatk
 
-if [ ${TRAVIS_PULL_REQUEST} ]; then
+if [ ${TRAVIS_PULL_REQUEST} != false ]; then
   sudo bash build_docker.sh  -e FETCH_HEAD -s -u -d $PWD/temp_staging/ -t ${TRAVIS_PULL_REQUEST};
 else
   sudo bash build_docker.sh  -e ${HASH_TO_USE} -s -u -d $PWD/temp_staging/;
