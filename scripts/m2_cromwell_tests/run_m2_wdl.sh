0ab481179da00da976a9983bd2294f79592fd96e
Louis Bergelson <louisb@broadinstitute.org>
2017-07-07 17:43:03 -0400

    yet another

--- scripts/m2_cromwell_tests/run_m2_wdl.sh
+++ scripts/m2_cromwell_tests/run_m2_wdl.sh
@@ -8,15 +8,17 @@ WORKING_DIR=/home/travis/build/broadinstitute
 
 set -e
 echo "Building docker image for M2 WDL tests (skipping unit tests)..."
-HASH_TO_USE=$TRAVIS_COMMIT
+
 #cd $WORKING_DIR/gatk/scripts/docker/
 #assume Dockerfile is in root
 echo "Building docker without running unit tests... ========="
 cd $WORKING_DIR/gatk
 
 if [ ${TRAVIS_PULL_REQUEST} != false ]; then
+  HASH_TO_USE=FETCH_HEAD
   sudo bash build_docker.sh  -e FETCH_HEAD -s -u -d $PWD/temp_staging/ -t ${TRAVIS_PULL_REQUEST};
 else
+  HASH_TO_USE=${TRAVIS_COMMIT}
   sudo bash build_docker.sh  -e ${HASH_TO_USE} -s -u -d $PWD/temp_staging/;
 fi
 echo "Docker build done =========="
