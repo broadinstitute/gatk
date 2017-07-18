98240e33498a4f8ac19929960b7902e523073174
James <emeryj@broadinstitute.org>
2017-07-18 16:54:42 -0400

    more work for testing the travis issue

--- src/test/java/org/broadinstitute/hellbender/BwaMemIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/BwaMemIntegrationTest.java
@@ -2,6 +2,7 @@ package org.broadinstitute.hellbender;
 
 import org.broadinstitute.hellbender.utils.bwa.BwaMemIndex;
 import org.broadinstitute.hellbender.utils.test.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -31,6 +32,11 @@ public class BwaMemIntegrationTest extends BaseTest {
     @Test
     public void testAlignChimericContig() throws Exception {
         BwaMemTestUtils.assertCorrectChimericContigAlignment(index);
+    }
+
+    @Test
+    public void dummy() throws Exception {
+        Assert.assertEquals(1,0);
     }
 
 }
