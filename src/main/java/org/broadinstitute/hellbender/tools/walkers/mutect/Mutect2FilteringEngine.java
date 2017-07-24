86093987d4063bff346ff8f9dd15d5556f6e178f
byoo <byunggil.yoo@gmail.com>
2017-07-24 13:23:59 -0500

    correct filter names (#3333)
    MEDIAN_BASE_QUALITY_DIFFERENCE_FILTER and MEDIAN_MAPPING_QUALITY_DIFFERENCE_FILTER may be reported as MEDIAN_CLIPPING_DIFFERENCE_FILTER.

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/Mutect2FilteringEngine.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/Mutect2FilteringEngine.java
@@ -74,14 +74,14 @@ public class Mutect2FilteringEngine {
     private void applyMedianBaseQualityDifferenceFilter(final M2FiltersArgumentCollection MTFAC, final VariantContext vc, final Collection<String> filters) {
         final int[] baseQualityByAllele = getIntArrayTumorField(vc, BaseQuality.KEY);
         if (baseQualityByAllele != null && baseQualityByAllele[0] - baseQualityByAllele[1] > MTFAC.maxMedianBaseQualityDifference) {
-            filters.add(GATKVCFConstants.MEDIAN_CLIPPING_DIFFERENCE_FILTER_NAME);
+            filters.add(GATKVCFConstants.MEDIAN_BASE_QUALITY_DIFFERENCE_FILTER_NAME);
         }
     }
 
     private void applyMedianMappingQualityDifferenceFilter(final M2FiltersArgumentCollection MTFAC, final VariantContext vc, final Collection<String> filters) {
         final int[] mappingQualityByAllele = getIntArrayTumorField(vc, MappingQuality.KEY);
         if (mappingQualityByAllele != null && mappingQualityByAllele[0] - mappingQualityByAllele[1] > MTFAC.maxMedianMappingQualityDifference) {
-            filters.add(GATKVCFConstants.MEDIAN_CLIPPING_DIFFERENCE_FILTER_NAME);
+            filters.add(GATKVCFConstants.MEDIAN_MAPPING_QUALITY_DIFFERENCE_FILTER_NAME);
         }
     }
 
