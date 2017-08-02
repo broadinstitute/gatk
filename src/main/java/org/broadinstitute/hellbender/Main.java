c54bb7f40bbec0e1407fefa8af2dbb32f7642f2d
Louis Bergelson <louisb@broadinstitute.org>
2017-08-02 13:28:11 -0400

    adding comment

--- src/main/java/org/broadinstitute/hellbender/Main.java
+++ src/main/java/org/broadinstitute/hellbender/Main.java
@@ -151,6 +151,7 @@ public class Main {
         try {
             final Object result = runCommandLineProgram(program, args);
             handleResult(result);
+            //no explicit System.exit(0) since that causes issues when running in Yarn containers
         } catch (final CommandLineException e){
             System.err.println(program.getUsage());
             handleUserException(e);
