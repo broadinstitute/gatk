07b9f4662b1fce52a3f65c30db2b5b350e9680b7
Mark Walker <markw@broadinstitute.org>
2017-08-02 17:45:47 -0400

    Use ReadClipper in BaseQualityClipReadTransformer (#3388)
    * Use ReadClipper in BaseQualityClipReadTransformer
    
    * Fixes bug when hard clipping unmapped reads using ClippingOp

--- src/test/java/org/broadinstitute/hellbender/transformers/BaseQualityClipReadTransformerTest.java
+++ src/test/java/org/broadinstitute/hellbender/transformers/BaseQualityClipReadTransformerTest.java
@@ -1,5 +1,8 @@
 package org.broadinstitute.hellbender.transformers;
 
+import htsjdk.samtools.Cigar;
+import htsjdk.samtools.CigarElement;
+import htsjdk.samtools.CigarOperator;
 import htsjdk.samtools.SAMUtils;
 import org.broadinstitute.hellbender.utils.read.ArtificialReadUtils;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
@@ -14,30 +17,37 @@ public class BaseQualityClipReadTransformerTest {
         return new Object[][] {
             {"IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
             "CTCAAGTACAAGCTGATCCAGACCTACAGGGTGATGTCATTAGAGGCACTGATAACACACACACTATGGGGTGGGGGTGGACAGTTCCCCACTGCAATCC",
-            "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"},
+            "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+            "100M"},
 
             {"####################################################################################################",
             "",
-            ""},
+            "",
+            "*"},
 
             {"############################IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
             "GGGTGATGTCATTAGAGGCACTGATAACACACACACTATGGGGTGGGGGTGGACAGTTCCCCACTGCAATCC",
-            "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"},
+            "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+            "28H72M"},
 
             {"IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII######",
             "CTCAAGTACAAGCTGATCCAGACCTACAGGGTGATGTCATTAGAGGCACTGATAACACACACACTATGGGGTGGGGGTGGACAGTTCCCCACTG",
-            "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"}
+            "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+            "94M6H"}
         };
     }
 
     @Test(dataProvider = "sequenceStrings")
-    public void testApply(final String quals_in, final String bases_out, final String quals_out) throws Exception {
-        final String bases_in = "CTCAAGTACAAGCTGATCCAGACCTACAGGGTGATGTCATTAGAGGCACTGATAACACACACACTATGGGGTGGGGGTGGACAGTTCCCCACTGCAATCC";
+    public void testApply(final String quals_in, final String basesOut, final String qualsOut, final String cigarOut) throws Exception {
+        final String basesIn = "CTCAAGTACAAGCTGATCCAGACCTACAGGGTGATGTCATTAGAGGCACTGATAACACACACACTATGGGGTGGGGGTGGACAGTTCCCCACTGCAATCC";
         final BaseQualityClipReadTransformer trans = new BaseQualityClipReadTransformer(15);
-        final GATKRead read_in = ArtificialReadUtils.createArtificialRead(bases_in.getBytes(),SAMUtils.fastqToPhred(quals_in),"*");
-        final GATKRead read_out = trans.apply(read_in);
-        Assert.assertEquals(read_out.getBases(),bases_out.getBytes());
-        Assert.assertEquals(read_out.getBaseQualities(),SAMUtils.fastqToPhred(quals_out));
+        final Cigar cigar = new Cigar();
+        cigar.add(new CigarElement(basesIn.length(), CigarOperator.MATCH_OR_MISMATCH));
+        final GATKRead readIn = ArtificialReadUtils.createArtificialRead(basesIn.getBytes(),SAMUtils.fastqToPhred(quals_in), cigar.toString());
+        final GATKRead readOut = trans.apply(readIn);
+        Assert.assertEquals(readOut.getBases(),basesOut.getBytes());
+        Assert.assertEquals(readOut.getBaseQualities(),SAMUtils.fastqToPhred(qualsOut));
+        Assert.assertEquals(readOut.getCigar().toString(), cigarOut);
     }
 
 }
