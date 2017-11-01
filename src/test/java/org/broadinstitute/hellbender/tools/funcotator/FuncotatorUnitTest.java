07d19eda066595873ec6e00af965f6741c35d669
Jonn Smith <jonn@broadinstitute.org>
2017-11-01 17:12:41 -0400

    Rebased on master, now tests are GATKBaseTests.

--- src/test/java/org/broadinstitute/hellbender/tools/funcotator/FuncotatorUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/funcotator/FuncotatorUnitTest.java
@@ -1,12 +1,13 @@
 package org.broadinstitute.hellbender.tools.funcotator;
 
+import org.broadinstitute.hellbender.GATKBaseTest;
 import org.broadinstitute.hellbender.utils.test.BaseTest;
 
 /**
  * Unit tests for the {@link org.broadinstitute.hellbender.tools.funcotator}.
  * Created by jonn on 8/22/17.
  */
-public class FuncotatorUnitTest extends BaseTest {
+public class FuncotatorUnitTest extends GATKBaseTest {
 
     //==================================================================================================================
     // Static Variables:
--- src/test/java/org/broadinstitute/hellbender/tools/funcotator/vcfOutput/VcfOutputRendererUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/funcotator/vcfOutput/VcfOutputRendererUnitTest.java
@@ -1,12 +1,12 @@
 package org.broadinstitute.hellbender.tools.funcotator.vcfOutput;
 
-import org.broadinstitute.hellbender.utils.test.BaseTest;
+import org.broadinstitute.hellbender.GATKBaseTest;
 
 /**
  * Unit test class for the {@link VcfOutputRenderer} class.
  * Created by jonn on 9/1/17.
  */
-public class VcfOutputRendererUnitTest extends BaseTest {
+public class VcfOutputRendererUnitTest extends GATKBaseTest {
     //==================================================================================================================
     // Static Variables:
 
