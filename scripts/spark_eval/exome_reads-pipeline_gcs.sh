80fee339b1635d8bab68bb5f19eed5d59d4f4a4a
Tom White <tom.e.white@gmail.com>
2017-11-03 14:24:10 +0000

    Tune some parameters for Spark evaluation scripts. (#3790)

--- scripts/spark_eval/exome_reads-pipeline_gcs.sh
+++ scripts/spark_eval/exome_reads-pipeline_gcs.sh
@@ -4,4 +4,4 @@
 
 . utils.sh
 
-time_gatk "ReadsPipelineSpark -I gs://gatk-tom-testdata-exome/NA12878.ga2.exome.maq.raw.bam -O gs://gatk-tom-testdata-exome/NA12878.ga2.exome.maq.raw.vcf -R gs://gatk-tom-testdata-exome/Homo_sapiens_assembly18.2bit --knownSites gs://gatk-tom-testdata-exome/dbsnp_138.hg18.vcf -pairHMM AVX_LOGLESS_CACHING -maxReadsPerAlignmentStart 10" 8 8 32g 4g
+time_gatk "ReadsPipelineSpark -I gs://broad-spark-eval-test-data/exome/NA12878.ga2.exome.maq.raw.bam -O gs://broad-spark-eval-test-data/exome/NA12878.ga2.exome.maq.raw.vcf -R gs://broad-spark-eval-test-data/exome/Homo_sapiens_assembly18.2bit --knownSites gs://broad-spark-eval-test-data/exome/dbsnp_138.hg18.vcf -pairHMM AVX_LOGLESS_CACHING -maxReadsPerAlignmentStart 10" 8 8 32g 4g
--- scripts/spark_eval/genome_reads-pipeline_hdfs.sh
+++ scripts/spark_eval/genome_reads-pipeline_hdfs.sh
@@ -4,4 +4,4 @@
 
 . utils.sh
 
-time_gatk "ReadsPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878-no-NC_007605.bam -O hdfs://${HDFS_HOST_PORT}/user/$USER/q4_spark_eval/out/WGS-G94982-NA12878.vcf -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs://${HDFS_HOST_PORT}/user/$USER/q4_spark_eval/dbsnp_138.b37.vcf -pairHMM AVX_LOGLESS_CACHING -maxReadsPerAlignmentStart 10" 8 4 46g 8g
+time_gatk "ReadsPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878-no-NC_007605.bam -O hdfs://${HDFS_HOST_PORT}/user/$USER/q4_spark_eval/out/WGS-G94982-NA12878.vcf -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs://${HDFS_HOST_PORT}/user/$USER/q4_spark_eval/dbsnp_138.b37.vcf -pairHMM AVX_LOGLESS_CACHING -maxReadsPerAlignmentStart 10" 20 8 46g 8g
