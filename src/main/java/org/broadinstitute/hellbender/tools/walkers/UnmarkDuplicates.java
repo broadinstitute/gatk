887d4ad249fb8814017d690e0b39e7bc6883867e
Madeleine Duran <mduran@broadinstitute.org>
2018-01-02 09:12:08 -0500

    initial documentation update

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/UnmarkDuplicates.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/UnmarkDuplicates.java
@@ -17,14 +17,43 @@ import java.io.File;
 import java.util.Collections;
 import java.util.List;
 
-@DocumentedFeature
+/**
+ * Removes duplicate read tags.
+ *
+ * <p> This tool "unmarks" duplicate reads in a SAM/BAM/CRAM file. Duplicate reads are defined as originated from a
+ * single fragment of DNA and can arise during sample preparation. </p>
+ *
+ *
+ * <h3>Input</h3>
+ * <p>
+ * A SAM/BAM/CRAM file that has been marked for duplicates.
+ * </p>
+ * <h3>Output</h3>
+ * <p>
+ * A SAM/BAM/CRAM file without isDuplicate on reads.
+ * </p>
+ *
+ * <h3>Usage example: </h3>
+ * <pre>
+ *     gatk UnmarkDuplicates \
+ *     -I marked_duplicates.bam \
+ *     -O unmarked_duplicates.bam
+ * </pre>
+ *
+ */
+
+
 @CommandLineProgramProperties(
-        summary = "Simple tool to \"unmark\" duplicates in a SAM/BAM/CRAM file. Clears the isDuplicate bit on all reads.",
-        oneLineSummary = "Unmark duplicates in a SAM/BAM/CRAM file",
-        usageExample = "hellbender UnmarkDuplicates -I input.bam -O output.bam",
-        programGroup = ReadProgramGroup.class
-)
+        summary = UnmarkDuplicates.USAGE_DETAILS,
+        oneLineSummary = UnmarkDuplicates.USAGE_SUMMARY,
+        usageExample = "gatk UnmarkDuplicates -I marked_duplicates.bam -O unmarked_duplicates.bam",
+        programGroup = ReadProgramGroup.class)
+@DocumentedFeature
 public class UnmarkDuplicates extends ReadWalker {
+
+    static final String USAGE_SUMMARY = "Unmark duplicates in a SAM/BAM/CRAM file";
+    static final String USAGE_DETAILS = "Simple tool to \"unmark\" duplicates in a SAM/BAM/CRAM file."+
+            "Clears the isDuplicate bit on all reads.";
 
     @Argument(fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME, shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME, doc="Write output to this file")
     public File OUTPUT;
