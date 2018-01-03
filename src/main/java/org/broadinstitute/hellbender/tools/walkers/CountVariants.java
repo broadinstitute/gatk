6882a57281b5fd97f4359e69038bd98969e9b848
Sarah Walker <skwalker@broadinstitute.org>
2018-01-03 15:47:17 -0500

    first draft documentation update

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/CountVariants.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/CountVariants.java
@@ -9,14 +9,32 @@ import org.broadinstitute.hellbender.engine.ReadsContext;
 import org.broadinstitute.hellbender.engine.ReferenceContext;
 import org.broadinstitute.hellbender.engine.VariantWalker;
 
+/**
+ *
+ * Count variants in a VCF file.
+ *
+ * <h3> Input </h3>
+ * <ul>
+ *     <li> A single VCF file. </li>
+ * </ul>
+ *
+ * <h3> Example: </h3>
+ * <pre>
+ *  gatk CountVariants \
+ *      -V input_variants.vcf
+ * </pre>
+ */
 @DocumentedFeature
 @CommandLineProgramProperties(
-        summary = "Walks over the input data set, calculating the number of variants seen.",
-        oneLineSummary = "Count variants in a VCF file",
+        summary = CountVariants.USAGE_SUMMARY,
+        oneLineSummary = CountVariants.USAGE_ONE_LINE_SUMMARY,
         programGroup = VariantProgramGroup.class
 )
 public final class CountVariants extends VariantWalker{
     private long count = 0;
+
+    static final String USAGE_ONE_LINE_SUMMARY = "Count variants in a VCF file";
+    static final String USAGE_SUMMARY = "Walks over the input data set, calculating the number of variants seen.";
 
     @Override
     public void apply( VariantContext variant, ReadsContext readsContext, ReferenceContext referenceContext, FeatureContext featureContext ) {
