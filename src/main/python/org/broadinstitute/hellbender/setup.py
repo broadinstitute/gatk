a14e85db4e491d4bd0f03593cbf2e3a4bbc72967
Chris Norman <cnorman@broadinstitute.org>
2018-01-03 16:28:44 -0500

    Install GATK Python packages. (#3964)

---
+++ src/main/python/org/broadinstitute/hellbender/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gatkpython',
+      version='0.1',
+      description='Testing install from archive',
+      author='GATK Team',
+      packages=['gatkpython'],
+      zip_safe=False)
+
