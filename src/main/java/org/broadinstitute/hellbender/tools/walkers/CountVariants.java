88375f7a3b2a0ebb8699ed59e80a5b01f76f0237
Sarah Walker <skwalker@broadinstitute.org>
2018-01-04 13:07:13 -0500

    responding to comments

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/CountVariants.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/CountVariants.java
@@ -3,7 +3,7 @@ package org.broadinstitute.hellbender.tools.walkers;
 import htsjdk.variant.variantcontext.VariantContext;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.help.DocumentedFeature;
-import org.broadinstitute.hellbender.cmdline.programgroups.VariantProgramGroup;
+import org.broadinstitute.hellbender.cmdline.programgroups.VariantEvaluationProgramGroup;
 import org.broadinstitute.hellbender.engine.FeatureContext;
 import org.broadinstitute.hellbender.engine.ReadsContext;
 import org.broadinstitute.hellbender.engine.ReferenceContext;
@@ -11,14 +11,14 @@ import org.broadinstitute.hellbender.engine.VariantWalker;
 
 /**
  *
- * Count variants in a VCF file.
+ * Count variant records in a VCF file, regardless of filter status. The tool gives the count at end of the standard out.
  *
  * <h3> Input </h3>
  * <ul>
  *     <li> A single VCF file. </li>
  * </ul>
  *
- * <h3> Example: </h3>
+ * <h3> Usage example: </h3>
  * <pre>
  *  gatk CountVariants \
  *      -V input_variants.vcf
@@ -28,13 +28,15 @@ import org.broadinstitute.hellbender.engine.VariantWalker;
 @CommandLineProgramProperties(
         summary = CountVariants.USAGE_SUMMARY,
         oneLineSummary = CountVariants.USAGE_ONE_LINE_SUMMARY,
-        programGroup = VariantProgramGroup.class
+        programGroup = VariantEvaluationProgramGroup.class
 )
 public final class CountVariants extends VariantWalker{
     private long count = 0;
 
-    static final String USAGE_ONE_LINE_SUMMARY = "Count variants in a VCF file";
-    static final String USAGE_SUMMARY = "Walks over the input data set, calculating the number of variants seen.";
+    static final String USAGE_ONE_LINE_SUMMARY = "Counts variant records in a VCF file, regardless of filter status.";
+    static final String USAGE_SUMMARY = "This tool counts the variant records in a VCF file, regardless of filter status. " +
+            "Because it counts the number of rows in the VCF, it does not necessarily reflect the number of variant " +
+            "alleles. The count is returned at the end of the standard out.";
 
     @Override
     public void apply( VariantContext variant, ReadsContext readsContext, ReferenceContext referenceContext, FeatureContext featureContext ) {
