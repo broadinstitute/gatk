7338cd036d6cbbd42719d4024639522d5295a43e
Samuel Lee <lee.samuel.k@gmail.com>
2018-01-05 16:07:46 -0500

    Fixed a screwup in the python integration tests.

--- src/test/java/org/broadinstitute/hellbender/utils/python/PythonEnvironmentIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/utils/python/PythonEnvironmentIntegrationTest.java
@@ -25,7 +25,7 @@ public class PythonEnvironmentIntegrationTest {
                 { "keras" },
                 { "pymc3" },
                 { "argparse" },
-                { "gatkpythonpackages" }
+                { "gcnvkernel" }
         };
     }
 
