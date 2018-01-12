6c18f1378e3543ac74f40aba92a4aa64913f1937
Steve Huang <shuang@broadinstitute.org>
2018-01-12 16:15:27 -0500

    temporarily turn off experimental SV event interpretation

--- scripts/sv/runWholePipeline.sh
+++ scripts/sv/runWholePipeline.sh
@@ -64,7 +64,6 @@ NUM_EXECUTORS=$((2 * ${NUM_WORKERS}))
     --fastq-dir "${PROJECT_OUTPUT_DIR}/fastq" \
     --contig-sam-file "${PROJECT_OUTPUT_DIR}/assemblies.sam" \
     --target-link-file ${PROJECT_OUTPUT_DIR}/target_links.bedpe \
-    --exp-variants-out-dir "${PROJECT_OUTPUT_DIR}/experimentalVariantInterpretations" \
     ${SV_ARGS} \
     -- \
     --spark-runner GCS \
