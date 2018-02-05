dadc085ead0702e389d82ba8a10bdf2d7391fdfb
James <emeryj@broadinstitute.org>
2018-02-05 17:24:40 -0500

    More -yes

--- build_docker.sh
+++ build_docker.sh
@@ -112,7 +112,7 @@ unzip -j ${ZIPPATHGATK} -d ./unzippedJar
 unzip -o -j ${ZIPPATHPYTHON} -d ./unzippedJar/scripts
 
 echo "Building image to tag ${REPO_PRJ}:${GITHUB_TAG}..."
-docker build -t ${REPO_PRJ}:${GITHUB_TAG} --build-arg ZIPPATH=./unzippedJar .
+docker build -t ${REPO_PRJ}:${GITHUB_TAG} --squash --build-arg ZIPPATH=./unzippedJar .
 
 if [ -z "${IS_NOT_RUN_UNIT_TESTS}" ] ; then
 
