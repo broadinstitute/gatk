0d079a28dc0334beda736e9c12d4ec12e02f7519
James <emeryj@broadinstitute.org>
2018-02-05 15:42:17 -0500

    maybe this will work on travis now?

--- build_docker.sh
+++ build_docker.sh
@@ -109,7 +109,7 @@ fi
 ZIPPATHGATK=$( find ./build -name "gatk-*.zip" )
 ZIPPATHPYTHON=$( find ./build -name "gatkPython*.zip" )
 unzip -j ${ZIPPATHGATK} -d ./unzippedJar
-unzip -j ${ZIPPATHPYTHON} -d ./unzippedJar/scripts
+unzip -o -j ${ZIPPATHPYTHON} -d ./unzippedJar/scripts
 
 echo "Building image to tag ${REPO_PRJ}:${GITHUB_TAG}..."
 docker build -t ${REPO_PRJ}:${GITHUB_TAG} --build-arg ZIPPATH=./unzippedJar .
@@ -126,8 +126,8 @@ if [ -z "${IS_NOT_RUN_UNIT_TESTS}" ] ; then
 	git lfs pull
     chmod -R a+w ${STAGING_ABSOLUTE_PATH}/src/test/resources
 
-	echo docker run ${REMOVE_CONTAINER_STRING} -v ${STAGING_ABSOLUTE_PATH}/src/test/resources:/testdata -t ${REPO_PRJ}:${GITHUB_TAG} bash /root/run_unit_tests.sh
-	docker run ${REMOVE_CONTAINER_STRING} -v ${STAGING_ABSOLUTE_PATH}/src/test/resources:/testdata -t ${REPO_PRJ}:${GITHUB_TAG} bash /root/run_unit_tests.sh
+	echo docker run ${REMOVE_CONTAINER_STRING} -v ${STAGING_ABSOLUTE_PATH}:/gatksrc -t ${REPO_PRJ}:${GITHUB_TAG} bash /root/run_unit_tests.sh
+	docker run ${REMOVE_CONTAINER_STRING} -v ${STAGING_ABSOLUTE_PATH}:/gatksrc -v ~/.gradle:/root/.gradle -t ${REPO_PRJ}:${GITHUB_TAG} bash /root/run_unit_tests.sh
 	echo " Unit tests passed..."
 fi
 
