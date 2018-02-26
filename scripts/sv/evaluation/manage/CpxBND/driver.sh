f379bed92789f50fb7be48fb431c8761abe42e1d
Steve Huang <shuang@broadinstitute.org>
2018-02-26 16:47:15 -0500

    Implementing suggested changes from PR review: * optionally run analysis on complex SV vcf's

--- scripts/sv/evaluation/manage/CpxBND/driver.sh
+++ scripts/sv/evaluation/manage/CpxBND/driver.sh
@@ -8,26 +8,25 @@ echo "BEGIN TO WORK ON COMPLEX VARIANTS"
 echo -e '\033[0;35m#################################################\033[0m'
 echo -e '\033[0;35m#################################################\033[0m'
 
-####################
+if [[ ! -z "$GATK_CPX_VCF_MASTER" ]]; then
+	ANALYSIS_DIR_MASTER="$ANALYSIS_DIR_MASTER/CPX"
+	mkdir -p "$ANALYSIS_DIR_MASTER"
+	bash "$SCRIPT_DIR""/parseCpx.sh" \
+		"$GATK_CPX_VCF_MASTER" \
+		"$PACBIO_VCF_CHM1" \
+		"$PACBIO_VCF_CHM13" \
+		"$ANALYSIS_DIR_MASTER"
+fi
 
-ANALYSIS_DIR_MASTER="$ANALYSIS_DIR_MASTER/CPX"
-mkdir -p "$ANALYSIS_DIR_MASTER"
-
-ANALYSIS_DIR_FEATURE="$ANALYSIS_DIR_FEATURE/CPX"
-mkdir -p "$ANALYSIS_DIR_FEATURE"
-
-# bash "$SCRIPT_DIR""/parseCpx.sh" \
-# 	"$GATK_CPX_VCF_MASTER" \
-# 	"$PACBIO_VCF_CHM1" \
-# 	"$PACBIO_VCF_CHM13" \
-# 	"$ANALYSIS_DIR_MASTER"
-
-
-bash "$SCRIPT_DIR""/parseCpx.sh" \
-	"$GATK_CPX_VCF_FEATURE" \
-	"$PACBIO_VCF_CHM1" \
-	"$PACBIO_VCF_CHM13" \
-	"$ANALYSIS_DIR_FEATURE"
+if [[ ! -z "$GATK_CPX_VCF_FEATURE" ]]; then
+	ANALYSIS_DIR_FEATURE="$ANALYSIS_DIR_FEATURE/CPX"
+	mkdir -p "$ANALYSIS_DIR_FEATURE"
+	bash "$SCRIPT_DIR""/parseCpx.sh" \
+		"$GATK_CPX_VCF_FEATURE" \
+		"$PACBIO_VCF_CHM1" \
+		"$PACBIO_VCF_CHM13" \
+		"$ANALYSIS_DIR_FEATURE"
+fi
 
 echo -e '\033[0;35m#################################################\033[0m'
 echo -e '\033[0;35m#################################################\033[0m'
