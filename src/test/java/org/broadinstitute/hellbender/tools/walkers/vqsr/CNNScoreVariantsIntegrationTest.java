c4b68f639261d4af68f66b6982464dbe929e8cd4
Samuel Friedman <sam@broadinstitute.org>
2018-03-27 09:13:56 -0400

    rebased and re enable tests

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNScoreVariantsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNScoreVariantsIntegrationTest.java
@@ -23,16 +23,22 @@ public class CNNScoreVariantsIntegrationTest extends CommandLineProgramTest {
      */
     @Test(groups = {"python"})
     public void testInference() throws IOException{
+        final boolean newExpectations = false;
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
         argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, inputVCF)
-                .addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, "%s")
                 .addArgument(StandardArgumentDefinitions.REFERENCE_LONG_NAME, b37_reference_20_21)
                 .addArgument("architecture", architecture1D)
                 .addArgument(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
 
-        final IntegrationTestSpec spec = new IntegrationTestSpec(argsBuilder.toString(),
-                Arrays.asList(largeFileTestDir + "VQSR/expected/cnn_1d_chr20_subset_expected.vcf"));
-        spec.executeTest("testInference", this);
+        if (newExpectations) {
+            argsBuilder.addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, largeFileTestDir + "VQSR/expected/cnn_1d_chr20_subset_expected.vcf");
+            runCommandLine(argsBuilder);
+        } else {
+            argsBuilder.addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, "%s");
+            final IntegrationTestSpec spec = new IntegrationTestSpec(argsBuilder.toString(),
+                    Arrays.asList(largeFileTestDir + "VQSR/expected/cnn_1d_chr20_subset_expected.vcf"));
+            spec.executeTest("testInference", this);
+        }
     }
 
     @Test(groups = {"python"})
@@ -64,7 +70,7 @@ public class CNNScoreVariantsIntegrationTest extends CommandLineProgramTest {
         spec.executeTest("testInference", this);
     }
 
-    @Test(groups = {"python"}, enabled = false)
+    @Test(groups = {"python"}, enabled = true)
     public void testInferenceResourceModel() throws IOException{
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
         argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, inputVCF)
@@ -76,20 +82,6 @@ public class CNNScoreVariantsIntegrationTest extends CommandLineProgramTest {
                 Arrays.asList(largeFileTestDir + "VQSR/expected/cnn_1d_chr20_subset_expected.vcf"));
         spec.executeTest("testInference", this);
     }
-
-//    @Test(groups = {"python"})
-//    public void generateTestExample() throws IOException{
-//        final String inputVCF = largeFileTestDir + "VQSR/recalibrated_chr20_start.vcf";
-//        final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
-//        argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, inputVCF)
-//                .addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, largeFileTestDir + "VQSR/expected/cnn_1d_chr20_subset_expected.vcf")
-//                .addArgument(StandardArgumentDefinitions.REFERENCE_LONG_NAME, b37_reference_20_21)
-//                .addArgument("architecture", architecture1D)
-//                .addArgument(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
-//
-//        runCommandLine(argsBuilder);
-//    }
-
 
     @Test(groups = {"python"})
     public void testSmallBatchInference()throws IOException {
@@ -106,7 +98,6 @@ public class CNNScoreVariantsIntegrationTest extends CommandLineProgramTest {
                 Arrays.asList(largeFileTestDir + "VQSR/expected/cnn_1d_chr20_subset_expected.vcf"));
         spec.executeTest("testInference", this);
     }
-
 
     /**
      * Run the 2D Model on a small test VCF.
@@ -140,7 +131,7 @@ public class CNNScoreVariantsIntegrationTest extends CommandLineProgramTest {
     /**
      * Run the 2D Model on a small test VCF with the resource loaded architecture.
      */
-    @Test(groups = {"python"}, enabled = false)
+    @Test(groups = {"python"}, enabled = true)
     public void testInference2dResourceModel() throws IOException{
         TensorType tt = TensorType.read_tensor;
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
