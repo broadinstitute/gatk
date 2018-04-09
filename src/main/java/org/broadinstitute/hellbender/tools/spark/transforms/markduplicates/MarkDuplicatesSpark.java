88e08674c306e2fbce2a84a3c7448b7685a5d8ef
James <emeryj@broadinstitute.org>
2018-04-09 13:10:01 -0400

    fixing a stupid error

--- src/main/java/org/broadinstitute/hellbender/tools/spark/transforms/markduplicates/MarkDuplicatesSpark.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/transforms/markduplicates/MarkDuplicatesSpark.java
@@ -74,7 +74,7 @@ public final class MarkDuplicatesSpark extends GATKSparkTool {
                 .values();
         
         return reads.zipPartitions(repartitionedReadNames, (readsIter, readNamesIter)  -> {
-            final Map<String,Integer> namesOfNonDuplicateReadsAndOpticalCounts = Utils.stream(readNamesIter).collect(Collectors.toMap(Tuple2::_1,Tuple2::_2); //TODO there is a bug here with value 0 being overloaded with entries, this is a stopgap for profiling
+            final Map<String,Integer> namesOfNonDuplicateReadsAndOpticalCounts = Utils.stream(readNamesIter).collect(Collectors.toMap(Tuple2::_1,Tuple2::_2)); //TODO there is a bug here with value 0 being overloaded with entries, this is a stopgap for profiling
             return Utils.stream(readsIter).peek(read -> {
                 // Handle read that aren't duplicates (and thus may be marked as containing opticalDuplicates)
                 if( namesOfNonDuplicateReadsAndOpticalCounts.containsKey(read.getName())) { //todo figure out if we should be marking the unmapped mates of duplicate reads as duplicates
