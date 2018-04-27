5ed226003fd5fd2874c40cd92c74b272c28d283a
MartonKN <kanasz.nagy.marton@gmail.com>
2018-04-27 17:24:43 -0400

    Changed the headers of automatically generated files to the new standard.

--- src/test/java/org/broadinstitute/hellbender/tools/copynumber/CallModeledSegmentsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/copynumber/CallModeledSegmentsIntegrationTest.java
@@ -3,6 +3,7 @@ package org.broadinstitute.hellbender.tools.copynumber;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.tools.copynumber.formats.collections.CalledModeledSegmentCollection;
+import org.broadinstitute.hellbender.tools.copynumber.formats.records.CalledModeledSegment;
 import org.broadinstitute.hellbender.utils.test.ArgumentsBuilder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,35 +12,54 @@ import java.io.File;
 import java.util.Scanner;
 
 public class CallModeledSegmentsIntegrationTest extends CommandLineProgramTest {
-    private static final File INPUT_NORMAL = new File(
-            "/Volumes/dsde_working/slee/archived/2017/ModelSegments-pipeline-test-cfc/run/cromwell-executions/CNVSomaticPairsWorkflow/99e77cf5-1aaf-4517-8926-8e0cb38c3222/call-CNVSomaticPairWorkflow/shard-12/CNVSomaticPairWorkflow/3adbf26d-78e0-4da0-96a7-ac48bf5b8648/call-ModelSegmentsNormal/execution/TCGA-44-6147-10A-01D-1753-08.modelFinal.seg");
-    private static final File INPUT_TUMOR = new File(
-            "/Volumes/dsde_working/slee/archived/2017/ModelSegments-pipeline-test-cfc/run/cromwell-executions/CNVSomaticPairsWorkflow/99e77cf5-1aaf-4517-8926-8e0cb38c3222/call-CNVSomaticPairWorkflow/shard-14/CNVSomaticPairWorkflow/d612e80d-bb5f-4db2-bf86-3b3ce296dafd/call-ModelSegmentsTumor/execution/TCGA-44-6774-01A-21D-1855-08.modelFinal.seg");
-    private static final File INPUT_CELL_LINE_50_PER_CENT_PURITY = new File(
-            "/Users/mkanaszn/Broad_Institute/Code/Tumor_segmentation/Testing_Lee_Lischtenstein_data/SM-74P3M.modelFinal.seg");
+    // Simulated samples
+    private static final File SIMULATED_DATA_DIR = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/");
+    private static final File INPUT_SIMULATED_NORMAL_DATA = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/normal_sample_data.seg");
+    private static final File INPUT_SIMULATED_NORMAL_TRUTH = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/normal_sample_truth.seg");
+    private static final File INPUT_SIMULATED_NORMAL_NOISY_DATA = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/normal_noisy_sample_data.seg");
+    private static final File INPUT_SIMULATED_NORMAL_NOISY_TRUTH = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/normal_noisy_sample_truth.seg");
+    private static final File INPUT_SIMULATED_CANCER_40P_PURITY_DATA = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/cancer_0p40_purity_data.seg");
+    private static final File INPUT_SIMULATED_CANCER_40P_PURITY_TRUTH = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/cancer_0p40_purity_truth.seg");
+    private static final File INPUT_SIMULATED_CANCER_60P_PURITY_DATA = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/cancer_0p60_purity_data.seg");
+    private static final File INPUT_SIMULATED_CANCER_60P_PURITY_TRUTH = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/cancer_0p60_purity_truth.seg");
+    private static final File INPUT_SIMULATED_CANCER_100P_PURITY_DATA = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/cancer_1p00_purity_data.seg");
+    private static final File INPUT_SIMULATED_CANCER_100P_PURITY_TRUTH = new File("/Users/mkanaszn/Broad_Institute/Code/gatk_ssh/gatk/src/test/resources/org/broadinstitute/hellbender/tools/copynumber/modeled-segments-caller-sim-data/cancer_1p00_purity_truth.seg");
+
+    // Real samples
+    // private static final File INPUT_NORMAL = new File(
+    //         "/Volumes/dsde_working/slee/archived/2017/ModelSegments-pipeline-test-cfc/run/cromwell-executions/CNVSomaticPairsWorkflow/99e77cf5-1aaf-4517-8926-8e0cb38c3222/call-CNVSomaticPairWorkflow/shard-12/CNVSomaticPairWorkflow/3adbf26d-78e0-4da0-96a7-ac48bf5b8648/call-ModelSegmentsNormal/execution/TCGA-44-6147-10A-01D-1753-08.modelFinal.seg");
+    // private static final File INPUT_TUMOR = new File(
+    //         "/Volumes/dsde_working/slee/archived/2017/ModelSegments-pipeline-test-cfc/run/cromwell-executions/CNVSomaticPairsWorkflow/99e77cf5-1aaf-4517-8926-8e0cb38c3222/call-CNVSomaticPairWorkflow/shard-14/CNVSomaticPairWorkflow/d612e80d-bb5f-4db2-bf86-3b3ce296dafd/call-ModelSegmentsTumor/execution/TCGA-44-6774-01A-21D-1855-08.modelFinal.seg");
+    // private static final File INPUT_CELL_LINE_50_PER_CENT_PURITY = new File(
+    //         "/Users/mkanaszn/Broad_Institute/Code/Tumor_segmentation/Testing_Lee_Lischtenstein_data/SM-74P3M.modelFinal.seg");
 
     @Test
     public void testTmp() {
-        final File FigOutputDir = createTempDir("/Users/mkanaszn/Desktop/test/figs");
-        final File FileOutputDir = createTempDir("/Users/mkanaszn/Desktop/test/calls");
+        final File FigOutputDir = createTempDir(SIMULATED_DATA_DIR.getAbsolutePath() + "test/figs");
+        final File FileOutputDir = createTempDir(SIMULATED_DATA_DIR.getAbsolutePath() + "test/calls");
+        final File LogOutputDir =  createTempDir(SIMULATED_DATA_DIR.getAbsolutePath() + "test/logs");
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder()
-                .addArgument(StandardArgumentDefinitions.INPUT_LONG_NAME, INPUT_NORMAL.getAbsolutePath())
+                .addArgument(StandardArgumentDefinitions.INPUT_LONG_NAME, INPUT_SIMULATED_NORMAL_DATA.getAbsolutePath())
                 //.addArgument(CallModeledSegments.OUTPUT_IMAGE_DIR_LONG_NAME, FigOutputDir.getAbsolutePath())
                 //.addArgument(CallModeledSegments.OUTPUT_CALLS_DIR_LONG_NAME, FileOutputDir.getAbsolutePath())
+                //.addArgument(CallModeledSegments.LOG_FILENAME_PREFIX_LONG_NAME, LogOutputDir.getAbsolutePath())
                 .addArgument(CallModeledSegments.OUTPUT_IMAGE_DIR_LONG_NAME, "/Users/mkanaszn/Desktop/tmp/figs")
                 .addArgument(CallModeledSegments.OUTPUT_CALLS_DIR_LONG_NAME, "/Users/mkanaszn/Desktop/tmp/calls")
+                .addArgument(CallModeledSegments.LOG_FILENAME_PREFIX_LONG_NAME, "/Users/mkanaszn/Desktop/tmp/logging/test.log")
                 .addArgument(CallModeledSegments.OUTPUT_IMAGE_PREFIX_LONG_NAME, "testFig")
                 .addArgument(CallModeledSegments.OUTPUT_CALLS_PREFIX_LONG_NAME, "testCalls")
                 .addArgument(CallModeledSegments.LOAD_COPY_RATIO_LONG_NAME, "true")
                 .addArgument(CallModeledSegments.LOAD_ALLELE_FRACTION_LONG_NAME, "true")
-                .addArgument(CallModeledSegments.INTERACTIVE_RUN_LONG_NAME, "true")
-                .addArgument(CallModeledSegments.LOG_FILENAME_PREFIX_LONG_NAME, "/Users/mkanaszn/Desktop/tmp/logging/test.log");
+                .addArgument(CallModeledSegments.INTERACTIVE_RUN_LONG_NAME, "true");
         List<String> args = argsBuilder.getArgsList();
         for (String ar:args) {
             System.out.println(ar);
         }
         runCommandLine(argsBuilder);
-        //assertOutputFiles(outputDir);
+        // assertOutputFiles(outputDir);
+        File outputFile = new File("/Users/mkanaszn/Desktop/testing/results/normal_data.called.seg");
+        File truthFile = new File("/Users/mkanaszn/Desktop/testing/normal_truth.seg");
+        compareCallFiles(outputFile, truthFile);
 
 
         File calls_file = new File("/Users/mkanaszn/Desktop/tmp/calls/testCalls.called.seg");
@@ -58,7 +78,7 @@ public class CallModeledSegmentsIntegrationTest extends CommandLineProgramTest {
         final File FigOutputDir = createTempDir("test/figs");
         final File FileOutputDir = createTempDir("test/calls");
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder()
-                .addInput(INPUT_NORMAL)
+                .addInput(INPUT_SIMULATED_NORMAL_DATA)
                 .addArgument(CallModeledSegments.OUTPUT_IMAGE_DIR_LONG_NAME, FigOutputDir.getAbsolutePath())
                 .addArgument(CallModeledSegments.OUTPUT_CALLS_DIR_LONG_NAME, FileOutputDir.getAbsolutePath())
                 .addArgument(CallModeledSegments.LOAD_COPY_RATIO_LONG_NAME, "true")
@@ -67,33 +87,6 @@ public class CallModeledSegmentsIntegrationTest extends CommandLineProgramTest {
         //assertOutputFiles(outputDir);
     }
 
-    @Test
-    public void testTumor() {
-        final File FigOutputDir = createTempDir("test/figs");
-        final File FileOutputDir = createTempDir("test/calls");
-        final ArgumentsBuilder argsBuilder = new ArgumentsBuilder()
-                .addInput(INPUT_TUMOR)
-                .addArgument(CallModeledSegments.OUTPUT_IMAGE_DIR_LONG_NAME, FigOutputDir.getAbsolutePath())
-                .addArgument(CallModeledSegments.OUTPUT_CALLS_DIR_LONG_NAME, FileOutputDir.getAbsolutePath())
-                .addArgument(CallModeledSegments.LOAD_COPY_RATIO_LONG_NAME, "true")
-                .addArgument(CallModeledSegments.LOAD_ALLELE_FRACTION_LONG_NAME, "true");
-        runCommandLine(argsBuilder);
-        //assertOutputFiles(outputDir);
-    }
-
-    @Test
-    public void testCellLine() {
-        final File FigOutputDir = createTempDir("test/figs");
-        final File FileOutputDir = createTempDir("test/calls");
-        final ArgumentsBuilder argsBuilder = new ArgumentsBuilder()
-                .addInput(INPUT_CELL_LINE_50_PER_CENT_PURITY)
-                .addArgument(CallModeledSegments.OUTPUT_IMAGE_DIR_LONG_NAME, FigOutputDir.getAbsolutePath())
-                .addArgument(CallModeledSegments.OUTPUT_CALLS_DIR_LONG_NAME, FileOutputDir.getAbsolutePath())
-                .addArgument(CallModeledSegments.LOAD_COPY_RATIO_LONG_NAME, "true")
-                .addArgument(CallModeledSegments.LOAD_ALLELE_FRACTION_LONG_NAME, "true");
-        runCommandLine(argsBuilder);
-        //assertOutputFiles(outputDir);
-    }
 
     private static void assertOutputFiles(final File inputFile,
                                           final File figOutputDir,
@@ -107,7 +100,13 @@ public class CallModeledSegmentsIntegrationTest extends CommandLineProgramTest {
     private static boolean compareCallFiles(final File outputFile, final File truthFile) {
         CalledModeledSegmentCollection outputData = new CalledModeledSegmentCollection(outputFile);
         CalledModeledSegmentCollection truthData = new CalledModeledSegmentCollection(truthFile);
-
+        List<CalledModeledSegment> outputDataRecords = outputData.getRecords();
+        for (int i=0; i<10; i++) {
+            System.out.println(outputDataRecords.get(i).getCallNormal());
+        }
+        //for (CalledModeledSegment o : outputData) {
+        //    System.out.println(o.getCallNormal());
+        //}
 
 
         return true;
