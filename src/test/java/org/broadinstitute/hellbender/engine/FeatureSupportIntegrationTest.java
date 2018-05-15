860b6c1f930a09d4a49804263ccb3900d28f982c
David Roazen <droazen@broadinstitute.org>
2018-05-15 17:40:54 -0400

    Last few fix-ups

--- src/test/java/org/broadinstitute/hellbender/engine/FeatureSupportIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/engine/FeatureSupportIntegrationTest.java
@@ -1,23 +1,13 @@
 package org.broadinstitute.hellbender.engine;
 
-import htsjdk.variant.variantcontext.VariantContext;
-import htsjdk.variant.vcf.VCFHeader;
-import org.apache.commons.lang3.tuple.Pair;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
-import org.broadinstitute.hellbender.Main;
-import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.exceptions.UserException;
-import org.broadinstitute.hellbender.tools.walkers.variantutils.SelectVariants;
 import org.broadinstitute.hellbender.utils.test.IntegrationTestSpec;
 import org.broadinstitute.hellbender.tools.examples.ExampleReadWalkerWithVariants;
-import org.broadinstitute.hellbender.utils.test.VariantContextTestUtils;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 
 public final class FeatureSupportIntegrationTest extends CommandLineProgramTest {
     private static final String FEATURE_INTEGRATION_TEST_DIRECTORY = publicTestDir + "org/broadinstitute/hellbender/engine/";
