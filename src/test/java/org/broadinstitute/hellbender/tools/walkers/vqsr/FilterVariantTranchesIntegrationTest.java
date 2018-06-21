c88dbde5f20e700bcc0594164535f9b29114abdc
Samuel Friedman <sam@broadinstitute.org>
2018-06-21 08:48:26 -0400

    fix tests

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/FilterVariantTranchesIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/FilterVariantTranchesIntegrationTest.java
@@ -18,29 +18,23 @@ public class FilterVariantTranchesIntegrationTest  extends CommandLineProgramTes
      */
     @Test
     public void testTrancheFiltering() throws IOException {
-        final boolean newExpectations = false;
-
         final String trancheVCF = largeFileTestDir + "VQSR/g94982_20_1m_10m_python_2dcnn.vcf.gz";
         final String indelTruthVCF = largeFileTestDir + "VQSR/ALL.wgs.indels_mills_devine_hg19_leftAligned_collapsed_double_hit.sites.20.1M-10M.vcf";
         final String snpTruthVCF = largeFileTestDir + "VQSR/Omni25_sites_1525_samples.b37.20.1M-10M.vcf";
 
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
         argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, trancheVCF)
+                .addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, "%s")
                 .addArgument("resource", snpTruthVCF)
                 .addArgument("resource", indelTruthVCF)
                 .addArgument("tranche", "99.0")
                 .addArgument("info-key", "MIX_SMALL_2D_W_DROPOUT")
                 .addArgument(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
 
-        if(newExpectations){
-            argsBuilder.addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, largeFileTestDir + "VQSR/expected/g94982_20_1m_10m_tranched_99.vcf");
-            runCommandLine(argsBuilder);
-        } else {
-            argsBuilder.addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, "%s");
-            final IntegrationTestSpec spec = new IntegrationTestSpec(argsBuilder.toString(),
+        final IntegrationTestSpec spec = new IntegrationTestSpec(argsBuilder.toString(),
                     Arrays.asList(largeFileTestDir + "VQSR/expected/g94982_20_1m_10m_tranched_99.vcf"));
-            spec.executeTest("testTrancheFiltering", this);
-        }
+        spec.executeTest("testTrancheFiltering", this);
+
     }
 
     @Test
@@ -66,14 +60,13 @@ public class FilterVariantTranchesIntegrationTest  extends CommandLineProgramTes
 
     @Test
     public void testTrancheFilteringTranches() throws IOException {
-        final boolean newExpectations = false;
-
         final String trancheVCF = largeFileTestDir + "VQSR/g94982_20_1m_10m_python_2dcnn.vcf.gz";
         final String indelTruthVCF = largeFileTestDir + "VQSR/ALL.wgs.indels_mills_devine_hg19_leftAligned_collapsed_double_hit.sites.20.1M-10M.vcf";
         final String snpTruthVCF = largeFileTestDir + "VQSR/Omni25_sites_1525_samples.b37.20.1M-10M.vcf";
 
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
         argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, trancheVCF)
+                .addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, "%s")
                 .addArgument("resource", snpTruthVCF)
                 .addArgument("resource", indelTruthVCF)
                 .addArgument("tranche", "95.0")
@@ -82,15 +75,9 @@ public class FilterVariantTranchesIntegrationTest  extends CommandLineProgramTes
                 .addArgument("info-key", "MIX_SMALL_2D_W_DROPOUT")
                 .addArgument(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
 
-        if(newExpectations){
-            argsBuilder.addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, largeFileTestDir + "VQSR/expected/g94982_20_1m_10m_tranched_95_99_99.9.vcf");
-            runCommandLine(argsBuilder);
-        } else {
-            argsBuilder.addArgument(StandardArgumentDefinitions.OUTPUT_LONG_NAME, "%s");
-            final IntegrationTestSpec spec = new IntegrationTestSpec(argsBuilder.toString(),
+        final IntegrationTestSpec spec = new IntegrationTestSpec(argsBuilder.toString(),
                     Arrays.asList(largeFileTestDir + "VQSR/expected/g94982_20_1m_10m_tranched_95_99_99.9.vcf"));
-            spec.executeTest("testTrancheFilteringTranches", this);
-        }
+        spec.executeTest("testTrancheFilteringTranches", this);
     }
 
     @Test
