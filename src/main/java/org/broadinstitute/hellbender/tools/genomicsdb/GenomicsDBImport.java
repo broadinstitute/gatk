313496d3884a2c81e32eb437f2bcdb7d6c8e304a
Jake VanCampen <jvancampen@stowers.org>
2018-07-23 11:13:08 -0500

    recommend --TMP-DIR

--- src/main/java/org/broadinstitute/hellbender/tools/genomicsdb/GenomicsDBImport.java
+++ src/main/java/org/broadinstitute/hellbender/tools/genomicsdb/GenomicsDBImport.java
@@ -86,6 +86,7 @@ import java.util.stream.Collectors;
  *      -V data/gvcfs/father.g.vcf.gz \
  *      -V data/gvcfs/son.g.vcf.gz \
  *      --genomicsdb-workspace-path my_database \
+ *      --TMP-DIR=path/to/other/tmp \
  *      -L 20
  *  </pre>
  *
@@ -98,6 +99,7 @@ import java.util.stream.Collectors;
  *       --batch-size 50 \
  *       -L chr1:1000-10000 \
  *       --sample-name-map cohort.sample_map \
+ *       --TMP-DIR=path/to/other/tmp \
  *       --reader-threads 5
  *  </pre>
  *
@@ -117,6 +119,7 @@ import java.util.stream.Collectors;
  *     <li>At least one interval must be provided</li>
  *     <li>Input GVCFs cannot contain multiple entries for a single genomic position</li>
  *     <li>The --genomicsdb-workspace-path must point to a non-existent or empty directory.</li>
+ *     <li>GenomicsDBImport makes greedy use of /tmp by default, if your /tmp space is limited this will cause errors once /tmp has filled up. This happens quickly if the tools is running many intervals. It is therefore recommended to specify a different `--TMP-DIR`.</li>
  * </ul>
  *
  * <h3>Developer Note</h3>
