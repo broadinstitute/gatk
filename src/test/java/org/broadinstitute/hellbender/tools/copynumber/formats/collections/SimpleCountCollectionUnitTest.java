8fc243cec1f8dda7a28f98b2f7092e69f6e05268
Samuel Lee <lee.samuel.k@gmail.com>
2018-07-25 10:41:51 -0400

    Changed order of HDF5/TSV read attempts for CNV read-count collections to avoid native HDF5 warning.

--- src/test/java/org/broadinstitute/hellbender/tools/copynumber/formats/collections/SimpleCountCollectionUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/copynumber/formats/collections/SimpleCountCollectionUnitTest.java
@@ -22,6 +22,8 @@ import java.util.List;
 public final class SimpleCountCollectionUnitTest extends GATKBaseTest {
     private static final File TEST_SUB_DIR = new File(toolsTestDir, "copynumber/formats/collections");
     private static final File INTEGER_COUNTS_FILE = new File(TEST_SUB_DIR,"simple-count-collection-integer-counts.tsv");
+    private static final File INTEGER_COUNTS_HDF5_FILE = new File(TEST_SUB_DIR,"simple-count-collection-integer-counts.hdf5");
+    private static final File INTEGER_COUNTS_MISSING_HEADER_FILE = new File(TEST_SUB_DIR,"simple-count-collection-integer-counts-missing-header.tsv");
     private static final File DOUBLE_COUNTS_FILE = new File(TEST_SUB_DIR, "simple-count-collection-double-counts.tsv");
 
     private static final SampleLocatableMetadata METADATA_EXPECTED = new SimpleSampleLocatableMetadata(
@@ -59,6 +61,23 @@ public final class SimpleCountCollectionUnitTest extends GATKBaseTest {
         Assert.assertEquals(metadata, METADATA_EXPECTED);
         Assert.assertEquals(intervals, INTERVALS_EXPECTED);
         Assert.assertEquals(readCounts, READ_COUNTS_EXPECTED);
+    }
+
+    @Test
+    public void testReadIntegerCountsHDF5() {
+        final SimpleCountCollection scc = SimpleCountCollection.read(INTEGER_COUNTS_HDF5_FILE);
+        final SampleLocatableMetadata metadata = scc.getMetadata();
+        final List<SimpleInterval> intervals = scc.getIntervals();
+        final RealMatrix readCounts = new Array2DRowRealMatrix(new double[][]{scc.getRecords().stream().mapToDouble(SimpleCount::getCount).toArray()});
+
+        Assert.assertEquals(metadata, METADATA_EXPECTED);
+        Assert.assertEquals(intervals, INTERVALS_EXPECTED);
+        Assert.assertEquals(readCounts, READ_COUNTS_EXPECTED);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testReadIntegerCountsMissingHeader() {
+        SimpleCountCollection.read(INTEGER_COUNTS_MISSING_HEADER_FILE);
     }
 
     @Test(expectedExceptions = UserException.BadInput.class)
