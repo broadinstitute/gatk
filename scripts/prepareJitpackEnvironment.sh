7e577da6a955787c8e272fe730e0cf8924063318
magicDGS <daniel.gomez.sanchez@hotmail.es>
2018-08-15 08:04:32 +0200

    Fix git-lfs .tar.gz name with new version

--- scripts/prepareJitpackEnvironment.sh
+++ scripts/prepareJitpackEnvironment.sh
@@ -4,7 +4,7 @@
 # This script downloads git-lfs and pull needed sources to build GATK in the jitpack environment
 
 GIT_LFS_VERSION="2.5.1"
-GIT_LFS_LINK=https://github.com/github/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-linux-amd64-${GIT_LFS_VERSION}.tar.gz
+GIT_LFS_LINK=https://github.com/github/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-linux-amd64-v${GIT_LFS_VERSION}.tar.gz
 GIT_LFS="git-lfs-${GIT_LFS_VERSION}/git-lfs"
 echo "Downloading and untarring git-lfs binary"
 wget -qO- $GIT_LFS_LINK | tar xvz
