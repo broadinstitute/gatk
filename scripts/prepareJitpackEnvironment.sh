9021d2807b1c15f2eb770332799ae92be10da992
magicDGS <daniel.gomez.sanchez@hotmail.es>
2018-08-15 07:53:33 +0200

    Address comments

--- scripts/prepareJitpackEnvironment.sh
+++ scripts/prepareJitpackEnvironment.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-GIT_LFS_VERSION="1.2.1"
+# This script's purpose is for use with jitpack.io - a repository to publish snapshot automatically
+# This script downloads git-lfs and pull needed sources to build GATK in the jitpack environment
+
+GIT_LFS_VERSION="2.5.1"
 GIT_LFS_LINK=https://github.com/github/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-linux-amd64-${GIT_LFS_VERSION}.tar.gz
 GIT_LFS="git-lfs-${GIT_LFS_VERSION}/git-lfs"
 echo "Downloading and untarring git-lfs binary"
@@ -8,4 +11,4 @@ wget -qO- $GIT_LFS_LINK | tar xvz
 
 echo "Fetching LFS files."
 $GIT_LFS install
-$GIT_LFS pull
+$GIT_LFS pull --include src/main/resources/large
