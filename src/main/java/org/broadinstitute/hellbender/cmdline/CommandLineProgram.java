8da197ef9bd3519dda5e7e1f666a950d62c2383b
David Roazen <droazen@broadinstitute.org>
2018-08-24 15:54:39 -0400

    google-cloud-java: update to the official 0.59.0 release, and move off of our custom fork
    The google-cloud-java maintainers have merged a fix for the longstanding issue
    https://github.com/GoogleCloudPlatform/google-cloud-java/issues/2453 that prevented us
    from running on a modern version of the library, and forced us to run off of a fork.
    This PR updates us to the latest release, which incorporates the fix.
    
    Resolves #3591
    Resolves #3500
    Resolves #4986

--- src/main/java/org/broadinstitute/hellbender/cmdline/CommandLineProgram.java
+++ src/main/java/org/broadinstitute/hellbender/cmdline/CommandLineProgram.java
@@ -436,7 +436,6 @@ public abstract class CommandLineProgram implements CommandLinePluginProvider {
         logger.info("Inflater: " + (usingIntelInflater ? "IntelInflater": "JdkInflater"));
 
         logger.info("GCS max retries/reopens: " + BucketUtils.getCloudStorageConfiguration(NIO_MAX_REOPENS).maxChannelReopens());
-        logger.info("Using google-cloud-java fork https://github.com/broadinstitute/google-cloud-java/releases/tag/0.20.5-alpha-GCS-RETRY-FIX");
     }
 
     /**
