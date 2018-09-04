ef0094c26735229ee2bdafb771d8bc20b7910e62
DonFreed <donfreed12@gmail.com>
2018-09-04 12:03:44 -0700

    Remove `@BeforeClass` and add some explanatory comments

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/AssemblyBasedCallerUtilsUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/AssemblyBasedCallerUtilsUnitTest.java
@@ -22,16 +22,19 @@ import org.testng.annotations.Test;
 public class AssemblyBasedCallerUtilsUnitTest extends GATKBaseTest {
     private SAMFileHeader header;
 
-    @BeforeClass
-    public void init() {
+    // In finalizeRegion(), the base qualities of overlapped read clips pairs are adjusted.
+    // Most of read clips are clipped/copy from original reads, and the base qualities of original reads are not affected.
+    // However, GATK 4.0.0.x has a bug : in some cases, the clipping procedure directly returns the original reads.
+    // So the base qualities of original reads are changed.
+    // This test is added to make sure the unexpected behavior is fixed.
+    @Test
+    public void testfinalizeRegion() {
+        SAMFileHeader header;
         header = ArtificialReadUtils.createArtificialSamHeader(1, 1, 100000000);
         SAMReadGroupRecord rg = new SAMReadGroupRecord("tumor");
         rg.setSample("tumor");
         header.addReadGroup(rg);
-    }
 
-    @Test
-    public void testfinalizeRegion() {
         Assert.assertEquals(header.getSequenceIndex("1"), 0);
         final AssemblyRegion activeRegion = new AssemblyRegion(new SimpleInterval("1",42596728,42598843), 100, header);
         Assert.assertTrue(activeRegion.isActive());
@@ -42,14 +45,17 @@ public class AssemblyBasedCallerUtilsUnitTest extends GATKBaseTest {
         SAMRecord orgRead0 = parser.parseLine("HWI-ST807:461:C2P0JACXX:4:2204:18080:5857\t83\t1\t42596803\t39\t1S95M5S\t=\t42596891\t-7\tGAATCATCATCAAATGGAATCTAATGGAATCATTGAACAGAATTGAATGGAATCGTCATCGAATGAATTGAATGCAATCATCGAATGGTCTCGAATAGAAT\tDAAAEDCFCCGEEDDBEDDDGCCDEDECDDFDCEECCFEECDCEDBCDBDBCC>DCECC>DBCDDBCBDDBCDDEBCCECC>DBCDBDBGC?FCCBDB>>?\tRG:Z:tumor");
         SAMRecord orgRead1 = parser.parseLine("HWI-ST807:461:C2P0JACXX:4:2204:18080:5857\t163\t1\t42596891\t39\t101M\t=\t42596803\t7\tCTCGAATGGAATCATTTTCTACTGGAAAGGAATGGAATCATCGCATAGAATCGAATGGAATTAACATGGAATGGAATCGAATGTAATCATCATCAAATGGA\t>@>:ABCDECCCEDCBBBDDBDDEBCCBEBBCBEBCBCDDCD>DECBGCDCF>CCCFCDDCBABDEDFCDCDFFDDDG?DDEGDDFDHFEGDDGECB@BAA\tRG:Z:tumor");
 
+        // use a copy of the original reads so the original reads are not touched
         reads.add(new SAMRecordToGATKReadAdapter(orgRead0.deepCopy()));
         reads.add(new SAMRecordToGATKReadAdapter(orgRead1.deepCopy()));
-        activeRegion.addAll(reads);
 
+        // add reads into active region and call finalizeRegion
+        activeRegion.addAll(reads);
         SampleList sampleList = SampleList.singletonSampleList("tumor");
         Byte minbq = 9;
         AssemblyBasedCallerUtils.finalizeRegion(activeRegion, false, false, minbq, header, sampleList);
 
+        // make sure reads are not changed due to finalizeRegion()
         Assert.assertTrue(reads.get(0).convertToSAMRecord(header).equals(orgRead0));
         Assert.assertTrue(reads.get(1).convertToSAMRecord(header).equals(orgRead1));
     }
