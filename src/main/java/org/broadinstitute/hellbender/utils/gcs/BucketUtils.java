e6f1c6ba2e2b85797cc147beb0d9ccb246b6df81
JP Martin <jean-philippe-martin@users.noreply.github.com>
2018-09-04 11:04:59 -0700

    Enable Requester Pays support (#5140)
    * Enable Requester Pays support
    
    Google Cloud Storage has "requester pays" buckets. When reading from
    those buckets, the requester is billed for the bandwidth (normally, it's
    the bucket owner who is billed).
    
    This pull request enables this feature with GATK. By default it is
    turned off (so there are no unexpected charges). To turn it on, use the
    command line argument: "--gcs-project-for-requester-pays"
    
    Example:
    
    $ ./gatk PrintReads --input $INPUT --output /tmp/reads.bam
    fails with: com.google.cloud.storage.StorageException: Bucket is requester pays bucket but no user project provided.
    
    $ ./gatk PrintReads --input $INPUT --output /tmp/reads.bam --gcs-project-for-requester-pays $PROJECT
    works
    
    This PR also removes the argumentless version of
    setGlobalNIODefaultOptions() because it was confusing (it uses the
    default values, not those indicated by the user - usually we'd expect
    the user's values to be taken into account).

--- src/main/java/org/broadinstitute/hellbender/utils/gcs/BucketUtils.java
+++ src/main/java/org/broadinstitute/hellbender/utils/gcs/BucketUtils.java
@@ -3,12 +3,13 @@ package org.broadinstitute.hellbender.utils.gcs;
 import com.google.cloud.http.HttpTransportOptions;
 import com.google.cloud.storage.StorageOptions;
 import com.google.cloud.storage.contrib.nio.CloudStorageConfiguration;
+import com.google.cloud.storage.contrib.nio.CloudStorageConfiguration.Builder;
 import com.google.cloud.storage.contrib.nio.CloudStorageFileSystem;
 import com.google.cloud.storage.contrib.nio.CloudStorageFileSystemProvider;
+import com.google.common.base.Strings;
 import com.google.common.io.ByteStreams;
 import htsjdk.samtools.util.IOUtil;
 import htsjdk.samtools.util.RuntimeIOException;
-import htsjdk.tribble.AbstractFeatureReader;
 import htsjdk.tribble.Tribble;
 import htsjdk.tribble.util.TabixUtils;
 import org.apache.hadoop.conf.Configuration;
@@ -19,7 +20,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.utils.Utils;
-import org.broadinstitute.hellbender.utils.config.ConfigFactory;
 import org.broadinstitute.hellbender.utils.io.IOUtils;
 import shaded.cloud_nio.com.google.api.gax.retrying.RetrySettings;
 import shaded.cloud_nio.com.google.auth.oauth2.GoogleCredentials;
@@ -343,20 +343,20 @@ public final class BucketUtils {
      */
     public static String getPathWithoutBucket(String path) {
         final String[] split = path.split("/");
-        final String BUCKET = split[2];
         return String.join("/", Arrays.copyOfRange(split, 3, split.length));
-
     }
 
     /**
-     * Sets NIO_MAX_REOPENS and generous timeouts as the global default.
+     * Sets max_reopens, requester_pays, and generous timeouts as the global default.
      * These will apply even to library code that creates its own paths to access with NIO.
+     *
+     * @param maxReopens If the GCS bucket channel errors out, how many times it will attempt to
+     *                   re-initiate the connection.
+     * @param requesterProject Project to bill when accessing "requester pays" buckets. If unset,
+     *                         these buckets cannot be accessed.
      */
-    public static void setGlobalNIODefaultOptions() {
-        setGlobalNIODefaultOptions(ConfigFactory.getInstance().getGATKConfig().gcsMaxRetries());
-    }
-    public static void setGlobalNIODefaultOptions(int maxReopens) {
-        CloudStorageFileSystemProvider.setDefaultCloudStorageConfiguration(getCloudStorageConfiguration(maxReopens));
+    public static void setGlobalNIODefaultOptions(int maxReopens, String requesterProject) {
+        CloudStorageFileSystemProvider.setDefaultCloudStorageConfiguration(getCloudStorageConfiguration(maxReopens, requesterProject));
         CloudStorageFileSystemProvider.setStorageOptions(setGenerousTimeouts(StorageOptions.newBuilder()).build());
     }
 
@@ -373,12 +373,24 @@ public final class BucketUtils {
         return CloudStorageFileSystem.forBucket(BUCKET).getPath(pathWithoutBucket);
     }
 
-    /** The config we want to use. **/
-    public static CloudStorageConfiguration getCloudStorageConfiguration(int maxReopens) {
-        return CloudStorageConfiguration.builder()
+    /**
+     * The config we want to use.
+     *
+     * @param maxReopens If the GCS bucket channel errors out, how many times it will attempt to
+     *                   re-initiate the connection.
+     * @param requesterProject Project to bill when accessing "requester pays" buckets. If unset,
+     *                         these buckets cannot be accessed.
+     *
+     **/
+    public static CloudStorageConfiguration getCloudStorageConfiguration(int maxReopens, String requesterProject) {
+        Builder builder = CloudStorageConfiguration.builder()
             // if the channel errors out, re-open up to this many times
-            .maxChannelReopens(maxReopens)
-            .build();
+            .maxChannelReopens(maxReopens);
+        if (!Strings.isNullOrEmpty(requesterProject)) {
+            // enable requester pays and indicate who pays
+            builder = builder.autoDetectRequesterPays(true).userProject(requesterProject);
+        }
+        return builder.build();
     }
 
     private static StorageOptions.Builder setGenerousTimeouts(StorageOptions.Builder builder) {
