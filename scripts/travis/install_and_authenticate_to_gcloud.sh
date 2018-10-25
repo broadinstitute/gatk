0bbb225042bd39a979d1bb3d93efb0dc388f695d
Louis Bergelson <louisb@broadinstitute.org>
2018-10-25 11:49:05 -0400

    responding to comments

--- scripts/travis/install_and_authenticate_to_gcloud.sh
+++ scripts/travis/install_and_authenticate_to_gcloud.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-# Install and Gcloud and authenticate on travis
+# Install and Gcloud and authenticate on travis.
+# This is expected to be run from the travis root directory.
 
-export BOTO_CONFIG=/dev/null;
+export BOTO_CONFIG=/dev/null; # see for more information https://github.com/broadinstitute/gatk/pull/3350
 openssl aes-256-cbc -K $encrypted_703d76169d63_key -iv $encrypted_703d76169d63_iv -in resources_for_CI/servicekey.json.enc -out servicekey.json -d;
-scripts/install_gcloud.sh;
+scripts/travis/install_gcloud.sh;
 printf 'y\n' | $GCLOUD_HOME/gcloud components update;
 if [[ $TEST_TYPE == cloud ]]; then
    printf 'y\n' | $GCLOUD_HOME/gcloud components install beta;
