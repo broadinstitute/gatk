e2da682dc9599a63563f259965a76d03c077b9d9
Jonn Smith <jonn@broadinstitute.org>
2019-01-11 14:14:05 -0500

    Adding in test cases for empty interval lists.

--- src/test/java/org/broadinstitute/hellbender/tools/CompareIntervalListsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/CompareIntervalListsIntegrationTest.java
@@ -9,22 +9,26 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 
-public class CompareIntervalListsIntegrationTest extends CommandLineProgramTest{
+public class CompareIntervalListsIntegrationTest extends CommandLineProgramTest {
 
     private static final String INTERVALS_1 = "intervals1.intervals";
-    private static final String INTERVLS_2  = "intervals2.intervals";
-    private static final String INTERVALS_3 = "intervals3.intervals"; //same territory as
+    private static final String INTERVALS_2 = "intervals2.intervals";
+    private static final String INTERVALS_3 = "intervals3.intervals"; // same territory as
+    private static final String INTERVALS_4 = "intervals4.intervals"; // empty file
 
     @DataProvider
     public Object[][] getIntervalFiles(){
         return new Object[][]{
                 {getTestFile(INTERVALS_1), getTestFile(INTERVALS_1), true},
-                {getTestFile(INTERVLS_2), getTestFile(INTERVALS_1), false},
-                {getTestFile(INTERVALS_1), getTestFile(INTERVLS_2), false},
-                {getTestFile(INTERVLS_2), getTestFile(INTERVLS_2), true},
+                {getTestFile(INTERVALS_2), getTestFile(INTERVALS_1), false},
+                {getTestFile(INTERVALS_1), getTestFile(INTERVALS_2), false},
+                {getTestFile(INTERVALS_2), getTestFile(INTERVALS_2), true},
                 {getTestFile(INTERVALS_3), getTestFile(INTERVALS_1), true},
-                {getTestFile(INTERVALS_3), getTestFile(INTERVLS_2), false},
+                {getTestFile(INTERVALS_3), getTestFile(INTERVALS_2), false},
                 {getTestFile(INTERVALS_1), getTestFile(INTERVALS_3), true},
+                {getTestFile(INTERVALS_1), getTestFile(INTERVALS_4), false},
+                {getTestFile(INTERVALS_4), getTestFile(INTERVALS_1), false},
+                {getTestFile(INTERVALS_4), getTestFile(INTERVALS_4), false},
         };
     }
 
