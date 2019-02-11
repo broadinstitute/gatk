bdb6913da40940394ef1a1f26c6b867a08de176b
James <emeryj@broadinstitute.org>
2019-02-11 13:08:14 -0500

    responding to last comments

--- src/main/java/org/broadinstitute/hellbender/tools/spark/bwa/BwaSpark.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/bwa/BwaSpark.java
@@ -73,6 +73,8 @@ public final class BwaSpark extends GATKSparkTool {
                 reads = bwaEngine.alignPaired(getUnfilteredReads()).filter(filter::test);
             }
             try {
+                // NOTE, we must include 'shardedOutput' as the input to `sortReadsToMatchHeader` to preserve the old default behavior for writing BwaSpark output
+                // where we would not sort the bam if outputting to a sharded output.
                 ReadsSparkSink.writeReads(ctx, output, null, reads, bwaEngine.getHeader(),
                                             shardedOutput ? ReadsWriteFormat.SHARDED : ReadsWriteFormat.SINGLE, getRecommendedNumReducers(), shardedPartsDir, shardedOutput);
             } catch (final IOException e) {
