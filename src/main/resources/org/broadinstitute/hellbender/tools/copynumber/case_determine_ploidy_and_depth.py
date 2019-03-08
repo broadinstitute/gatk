3c2ace4b178f6190a1c7cbe8ad0d317db346178c
Samuel Lee <lee.samuel.k@gmail.com>
2019-03-08 10:28:34 -0500

    mkl theano 1.0.4

--- src/main/resources/org/broadinstitute/hellbender/tools/copynumber/case_determine_ploidy_and_depth.py
+++ src/main/resources/org/broadinstitute/hellbender/tools/copynumber/case_determine_ploidy_and_depth.py
@@ -1,7 +1,7 @@
 import os
 
 # set theano flags
-os.environ["THEANO_FLAGS"] = "device=cpu,floatX=float64,optimizer=fast_run,compute_test_value=ignore,openmp=true"
+os.environ["THEANO_FLAGS"] = "device=cpu,floatX=float64,optimizer=fast_run,compute_test_value=ignore,openmp=true,blas.ldflags=-lmkl_rt"
 
 import argparse
 import gcnvkernel
