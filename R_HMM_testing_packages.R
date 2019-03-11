cd9ee67f1a374ca08c96db28a29574d7b8991a94
James <emeryj@broadinstitute.org>
2019-03-11 16:33:25 -0400

    more chiping away at the little pieces of garbage that litter the beach

--- R_HMM_testing_packages.R
+++ R_HMM_testing_packages.R
@@ -15,17 +15,6 @@ InstallPackageFromArchive = function(packageName, packageURL) {
     }
 }
 
-dependencies = c("gplots",
-                 "digest", "gtable", "MASS", "plyr", "reshape2", "scales", "tibble", "lazyeval")    # for ggplot2
-repos <- c("http://cran.cnr.Berkeley.edu", "http://cran.mtu.edu")
-install.packages(dependencies, repos = repos, clean = TRUE)
-
-InstallPackageFromArchive("getopt", "http://cran.r-project.org/src/contrib/Archive/getopt/getopt_1.20.0.tar.gz")
-InstallPackageFromArchive("optparse", "http://cran.r-project.org/src/contrib/Archive/optparse/optparse_1.3.2.tar.gz")
-InstallPackageFromArchive("data.table", "http://cran.r-project.org/src/contrib/Archive/data.table/data.table_1.10.4-2.tar.gz")
-InstallPackageFromArchive("gsalib", "http://cran.r-project.org/src/contrib/gsalib_2.1.tar.gz")
-InstallPackageFromArchive("ggplot2", "http://cran.r-project.org/src/contrib/Archive/ggplot2/ggplot2_2.2.1.tar.gz")
-
 # HMM is only required for testing and not in production:
 InstallPackageFromArchive("HMM", "http://cran.r-project.org/src/contrib/HMM_1.0.tar.gz")
 
