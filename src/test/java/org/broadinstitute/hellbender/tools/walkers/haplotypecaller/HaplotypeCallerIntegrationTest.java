9a771a76ff33eb410ce35d6c7400363f117c9cda
James <emeryj@broadinstitute.org>
2019-03-13 16:50:26 -0400

    right branch this time

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.hellbender.tools.walkers.haplotypecaller;
 
 import com.google.common.collect.ImmutableMap;
+import com.intel.gkl.NativeLibraryLoader;
 import htsjdk.samtools.SamFiles;
 import htsjdk.tribble.Tribble;
 import htsjdk.variant.variantcontext.Allele;
@@ -10,7 +11,11 @@ import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFConstants;
 import htsjdk.variant.vcf.VCFHeader;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.broadinstitute.barclay.argparser.CommandLineException;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
@@ -38,6 +43,7 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -53,6 +59,76 @@ public class HaplotypeCallerIntegrationTest extends CommandLineProgramTest {
     public static final boolean UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS = false;
 
     public static final String TEST_FILES_DIR = toolsTestDir + "haplotypecaller/";
+
+    /*
+     * Make sure that someone didn't leave the UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS toggle turned on
+     */
+    @Test
+    public void testGKLLoad() throws Exception {
+        load(null, "gkl_utils");
+    }
+
+    private static final Logger logger = LogManager.getLogger(NativeLibraryLoader.class);
+    private static final String USE_LIBRARY_PATH = "USE_LIBRARY_PATH";
+    private static final Set<String> loadedLibraries = new HashSet<String>();
+    /**
+     * Tries to load the native library from the classpath, usually from a jar file. <p>
+     *
+     * If the USE_LIBRARY_PATH environment variable is defined, the native library will be loaded from the
+     * java.library.path instead of the classpath.
+     *
+     * @param tempDir  directory where the native library is extracted or null to use the system temp directory
+     * @param libraryName  name of the shared library without system dependent modifications
+     * @return true if the library was loaded successfully, false otherwise
+     */
+    public static synchronized boolean load(File tempDir, String libraryName) throws Exception {
+        if (loadedLibraries.contains(libraryName)) {
+            return true;
+        }
+
+        final String systemLibraryName = System.mapLibraryName(libraryName);
+
+        // load from the java.library.path
+        if (System.getenv(USE_LIBRARY_PATH) != null) {
+            final String javaLibraryPath = System.getProperty("java.library.path");
+            try {
+                logger.error(String.format("OVERRIDE DEFAULT: Loading %s from %s", systemLibraryName, javaLibraryPath));
+                logger.error(String.format("LD_LIBRARY_PATH = %s", System.getenv("LD_LIBRARY_PATH")));
+                System.loadLibrary(libraryName);
+                return true;
+            } catch (Exception|Error e) {
+                logger.error(String.format("OVERRIDE DEFAULT: Unable to load %s from %s", systemLibraryName, javaLibraryPath));
+                throw new Exception(e.getCause());
+//                return false;
+            }
+        }
+
+        // load from the java classpath
+        final String resourcePath = "native/" +  systemLibraryName;
+        final URL inputUrl = NativeLibraryLoader.class.getResource(resourcePath);
+        if (inputUrl == null) {
+            logger.error("Unable to find native library: " + resourcePath);
+            throw new Exception("Unable to find native library: " + resourcePath);
+//            return false;
+        }
+        logger.error(String.format("Loading %s from %s", systemLibraryName, inputUrl.toString()));
+
+        try {
+            final File temp = File.createTempFile(FilenameUtils.getBaseName(resourcePath),
+                    "." + FilenameUtils.getExtension(resourcePath), tempDir);
+            FileUtils.copyURLToFile(inputUrl, temp);
+            temp.deleteOnExit();
+            logger.error(String.format("Extracting %s to %s", systemLibraryName, temp.getAbsolutePath()));
+            System.load(temp.getAbsolutePath());
+        } catch (Exception|Error e) {
+            logger.error(String.format("Unable to load %s from %s (%s)", systemLibraryName, resourcePath, e.getMessage()));
+            throw new Exception(String.format("Unable to load %s from %s (%s)", systemLibraryName, resourcePath, e.getMessage()));
+            //            return false;
+        }
+
+        loadedLibraries.add(libraryName);
+        return true;
+    }
 
     /*
      * Make sure that someone didn't leave the UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS toggle turned on
