bb37fae68350d58505d902291eb46d6dba43df5d
benjamincarlin <bcarlin@broadinstitute.org>
2019-03-20 11:37:10 -0400

    updated methylation walker class documentation

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/MethylationTypeCaller.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/MethylationTypeCaller.java
@@ -23,9 +23,27 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * Tool which identifies methylated bases from bisulfite sequencing data
- * printing methylation-based coverage from supplied read to the specified output vcf file
- * walker that prints methylation-based coverage with contextual data
+ * Outputs Methylation-based coverage in VCF format
+ *
+ * <p>This tool identifies methylated bases from bisulfite sequencing data. Given a bisulfite BAM
+ * and a reference, it outputs methylation-site coverage to a specified output vcf file.
+ * </p>
+ *
+ * <p>
+ * Tool that prints methylation-based coverage from supplied bisulfite BAM to the specified output vcf file
+ * </p>
+ *
+ * <p>
+ * The walker that prints methylation-based coverage with contextual data
+ * </p>
+ *
+ * <h>Usage example</h>
+ * <pre>
+ *     gatk MethylationTypeCaller \
+ *     -R Homo_sapiens_assembly38.fasta \
+ *     -I bisulfite_input.bam \
+ *     -O output.vcf
+ * </pre>
  *
  * @author Benjamin Carlin
  */
