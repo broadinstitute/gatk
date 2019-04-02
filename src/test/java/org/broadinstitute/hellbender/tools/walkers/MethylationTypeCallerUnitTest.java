f70e1c3adb1830be28377c0dad807d8c3e53e3b3
benjamincarlin <bcarlin@broadinstitute.org>
2019-04-02 17:09:06 -0400

    changed test file location, addded data provider, assert by variant context rather than as txt files

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/MethylationTypeCallerUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/MethylationTypeCallerUnitTest.java
@@ -1,11 +1,16 @@
 package org.broadinstitute.hellbender.tools.walkers;
 
+import htsjdk.variant.variantcontext.VariantContext;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.testutils.IntegrationTestSpec;
+import org.broadinstitute.hellbender.testutils.VariantContextTestUtils;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.broadinstitute.hellbender.utils.Utils;
 
 import java.io.File;
+import java.util.List;
 
 public class MethylationTypeCallerUnitTest extends CommandLineProgramTest {
 
@@ -16,24 +21,23 @@ public class MethylationTypeCallerUnitTest extends CommandLineProgramTest {
     public static final boolean UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS = false;
 
     public static final String TEST_FILES_DIR = toolsTestDir + "MethylationTypeCaller/";
+    public static final String TEST_FILES_INPUT_REFERENCE_DIR = largeFileTestDir + "GRCm38_primary_assembly_genome/";
 
     /*
      * Test that in VCF mode we're consistent with past  results
      */
-    @Test
+    @Test(dataProvider="getMethylationTypeCallerTestInput")
     public void testVCFIsConsistentWithPastResults(final String inputFileName, final String referenceFileName) throws Exception {
         Utils.resetRandomGenerator();
 
         final File outputVCF = createTempFile("testVCFIsConsistentWithPastResults", ".vcf");
-        final File outputVCFIndex = createTempFile("testVCFIndexIsConsistentWithPastResults", ".vcf.idx");
         final File expectedVCF = new File(TEST_FILES_DIR, "chr14.unique_reads.methylC_seq.vcf");
-        final File expectedVCFIndex = new File(TEST_FILES_DIR, "chr14.unique_reads.methylC_seq.vcf.idx");
 
         final String outputPath = UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS ? expectedVCF.getAbsolutePath() : outputVCF.getAbsolutePath();
 
         final String[] inputArgs = {
-                "-I", TEST_FILES_DIR + inputFileName,
-                "-R", toolsTestDir +  referenceFileName,
+                "-I", inputFileName,
+                "-R", referenceFileName,
                 "-O", outputPath
         };
 
@@ -41,8 +45,20 @@ public class MethylationTypeCallerUnitTest extends CommandLineProgramTest {
 
         // Test for an exact match against past results
         if (!UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS) {
-            IntegrationTestSpec.assertEqualTextFiles(outputVCF, expectedVCF);
-            IntegrationTestSpec.assertEqualTextFiles(outputVCFIndex, expectedVCFIndex);
+            final List<VariantContext> outputVariants = VariantContextTestUtils.readEntireVCFIntoMemory(outputVCF.toString()).getRight();
+            final List<VariantContext> expectedVariants = VariantContextTestUtils.readEntireVCFIntoMemory(expectedVCF.toString()).getRight();
+            Assert.assertEquals(expectedVariants.toString(), outputVariants.toString());
         }
+
+    }
+
+    @DataProvider
+    public Object[][] getMethylationTypeCallerTestInput() {
+        final String inputFileName = TEST_FILES_DIR + "chr14.unique_reads.methylC_seq.bam";
+        final String referenceFileName = TEST_FILES_INPUT_REFERENCE_DIR + "chr14.GRCm38.primary_assembly.genome.fa.gz";
+        return new Object[][] {
+                {inputFileName, referenceFileName}
+        };
     }
 }
+
