ee79539e63d1bbceed7b213f5bb0f6c4b47b62eb
David Roazen <droazen@broadinstitute.org>
2019-05-08 09:10:44 -0400

    Add BigQuery dependency, move to unshaded google-cloud-java, and newer guava version
    Add BigQuery as a GATK dependency. In order to add this dependency, we have to move
    to an unshaded version of google-cloud-java, as the shaded version causes breakage
    in BigQuery, as well as a newer version of guava.

--- src/main/java/org/broadinstitute/hellbender/utils/nio/SeekableByteChannelPrefetcher.java
+++ src/main/java/org/broadinstitute/hellbender/utils/nio/SeekableByteChannelPrefetcher.java
@@ -12,6 +12,8 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.NonWritableChannelException;
 import java.nio.channels.SeekableByteChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UnknownFormatConversionException;
@@ -21,7 +23,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import shaded.cloud_nio.com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
  * SeekableByteChannelPrefetcher wraps an existing SeekableByteChannel to add prefetching.
