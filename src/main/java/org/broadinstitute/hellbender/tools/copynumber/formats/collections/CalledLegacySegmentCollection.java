7b6e0d5024c1105984f4743310afc024a0b15548
Samuel Lee <lee.samuel.k@gmail.com>
2019-05-30 09:49:16 -0400

    Changed LegacySegmentCollection and CalledLegacySegmentCollection writers to overwrite instead of append.

--- src/main/java/org/broadinstitute/hellbender/tools/copynumber/formats/collections/CalledLegacySegmentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/tools/copynumber/formats/collections/CalledLegacySegmentCollection.java
@@ -95,7 +95,7 @@ public final class CalledLegacySegmentCollection extends AbstractSampleLocatable
     @Override
     public void write(final File outputFile) {
         Utils.nonNull(outputFile);
-        try (final RecordWriter recordWriter = new RecordWriter(new FileWriter(outputFile, true))) {
+        try (final RecordWriter recordWriter = new RecordWriter(new FileWriter(outputFile))) {
             recordWriter.writeAllRecords(getRecords());
         } catch (final IOException e) {
             throw new UserException.CouldNotCreateOutputFile(outputFile, e);
