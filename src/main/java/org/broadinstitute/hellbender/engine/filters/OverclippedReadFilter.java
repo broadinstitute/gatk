8a5a3708bc3faf7dac5a1525dd798603c90d8ad3
Jonn Smith <jonn@broadinstitute.org>
2019-06-10 15:51:15 -0400

    Added in filter for leading/trailing soft clips only.
    Added in tests.

--- src/main/java/org/broadinstitute/hellbender/engine/filters/OverclippedReadFilter.java
+++ src/main/java/org/broadinstitute/hellbender/engine/filters/OverclippedReadFilter.java
@@ -1,5 +1,6 @@
 package org.broadinstitute.hellbender.engine.filters;
 
+import com.google.common.annotations.VisibleForTesting;
 import htsjdk.samtools.CigarElement;
 import htsjdk.samtools.CigarOperator;
 import org.apache.logging.log4j.LogManager;
@@ -22,7 +23,7 @@ import org.broadinstitute.hellbender.utils.read.GATKRead;
  * <p>Note: Consecutive soft-clipped blocks are treated as a single block. For example, 1S2S10M1S2S is treated as 3S10M3S</p>
  */
 @DocumentedFeature(groupName= HelpConstants.DOC_CAT_READFILTERS, groupSummary=HelpConstants.DOC_CAT_READFILTERS_SUMMARY, summary = "Filter out reads that are over-soft-clipped")
-public final class OverclippedReadFilter extends ReadFilter{
+public final class OverclippedReadFilter extends ReadFilter {
     static final long serialVersionUID = 1L;
     private final Logger logger = LogManager.getLogger(this.getClass());
 
@@ -30,35 +31,50 @@ public final class OverclippedReadFilter extends ReadFilter{
 
     @Argument(fullName = ReadFilterArgumentDefinitions.FILTER_TOO_SHORT_NAME,
             doc = "Minimum number of aligned bases",
-            optional = true,
-            mutex = {"soft-clipped-ratio-threshold"})
-    public int minimumSequenceLength = 30;
+            optional = true)
+    private int minAlignedBases = 30;
 
+    @VisibleForTesting
     @Argument(fullName = "soft-clipped-ratio-threshold",
-            doc = "Minimum ratio of soft clipped bases to total bases in read for read to be included.",
+            doc = "Minimum ratio of soft clipped bases (anywhere in the cigar string) to total bases in read for read to be included.",
             optional = true,
-            mutex = {ReadFilterArgumentDefinitions.FILTER_TOO_SHORT_NAME}
+            mutex = {ReadFilterArgumentDefinitions.FILTER_TOO_SHORT_NAME, "leading-trailing-soft-clipped-ratio"}
     )
-    public double minimumSoftClippedRatio = DEFAULT_MIN_SOFT_CLIPPED_RATIO;
+    double minimumSoftClippedRatio = DEFAULT_MIN_SOFT_CLIPPED_RATIO;
+
+    @VisibleForTesting
+    @Argument(fullName = "leading-trailing-soft-clipped-ratio",
+            doc = "Minimum ratio of soft clipped bases (leading / trailing the cigar string) to total bases in read for read to be included.",
+            optional = true,
+            mutex = {ReadFilterArgumentDefinitions.FILTER_TOO_SHORT_NAME, "soft-clipped-ratio-threshold"}
+    )
+    double minimumLeadingTrailingSoftClippedRatio = DEFAULT_MIN_SOFT_CLIPPED_RATIO;
 
     @Argument(fullName = ReadFilterArgumentDefinitions.DONT_REQUIRE_SOFT_CLIPS_BOTH_ENDS_NAME,
             doc = "Allow a read to be filtered out based on having only 1 soft-clipped block. By default, both ends must " +
                     "have a soft-clipped block, setting this flag requires only 1 soft-clipped block",
-            optional = true)
-    public boolean doNotRequireSoftClipsOnBothEnds;
+            optional = true,
+            mutex = {"soft-clipped-ratio-threshold", "leading-trailing-soft-clipped-ratio"})
+    private boolean doNotRequireSoftClipsOnBothEnds;
 
     // Command line parser requires a no-arg constructor
     public OverclippedReadFilter() {}
 
-    public OverclippedReadFilter(final int minimumSequenceLength, final boolean doNotRequireSoftClipsOnBothEnds) {
-        this.minimumSequenceLength = minimumSequenceLength;
+    @VisibleForTesting
+    OverclippedReadFilter(final int minAlignedBases) {
+        this.minAlignedBases = minAlignedBases;
+    }
+
+    @VisibleForTesting
+    OverclippedReadFilter(final int minAlignedBases, final boolean doNotRequireSoftClipsOnBothEnds) {
+        this.minAlignedBases = minAlignedBases;
         this.doNotRequireSoftClipsOnBothEnds = doNotRequireSoftClipsOnBothEnds;
     }
 
     private boolean testMinSequenceLength(final GATKRead read) {
         int alignedLength = 0;
         int softClipBlocks = 0;
-        int minSoftClipBlocks = doNotRequireSoftClipsOnBothEnds ? 1 : 2;
+        final int minSoftClipBlocks = doNotRequireSoftClipsOnBothEnds ? 1 : 2;
         CigarOperator prevOperator = null;
 
         for ( final CigarElement element : read.getCigarElements() ) {
@@ -74,36 +90,92 @@ public final class OverclippedReadFilter extends ReadFilter{
             prevOperator = element.getOperator();
         }
 
-        return(alignedLength >= minimumSequenceLength || softClipBlocks < minSoftClipBlocks);
+        return (alignedLength >= minAlignedBases || softClipBlocks < minSoftClipBlocks);
     }
 
     private boolean testMinSoftClippedRatio(final GATKRead read) {
+        int totalLength = 0;
+        int alignedLength = 0;
         int numSoftClippedBases = 0;
         for ( final CigarElement element : read.getCigarElements() ) {
             if (element.getOperator() == CigarOperator.S) {
                 numSoftClippedBases += element.getLength();
+            } else if ( element.getOperator().consumesReadBases() ) {   // M, I, X, and EQ (S was already accounted for above)
+                alignedLength += element.getLength();
             }
+
+            totalLength += element.getLength();
         }
 
-        final double softClipRatio = ((double)numSoftClippedBases / (double)read.getLength());
+        final double softClipRatio = ((double)numSoftClippedBases / (double)totalLength);
 
         logger.debug( "  Num Soft Clipped Bases: " + numSoftClippedBases );
         logger.debug( "  Read Length: " + read.getLength() );
         logger.debug( "  Read Filter status: (" + softClipRatio + " > " + minimumSoftClippedRatio + ")" );
         logger.debug( "  Read Filter status: " + (softClipRatio > minimumSoftClippedRatio) );
+        logger.debug( "  Aligned Length: " + alignedLength );
 
-        return softClipRatio > minimumSoftClippedRatio;
+        return (alignedLength >= minAlignedBases) && (softClipRatio > minimumSoftClippedRatio);
+    }
+
+    private boolean testMinLeadingTrailingSoftClippedRatio(final GATKRead read) {
+
+        if ( read.getCigarElements().size() < 1 ) {
+            return false;
+        }
+
+        // Get the index of the last cigar element:
+        final int lastCigarOpIndex = read.getCigarElements().size() - 1;
+
+        // Calculate the number of bases here:
+        // Note: we don't want to double-count if the read has only 1 cigar operator.
+        final int numLeadingTrailingSoftClippedBases =
+                (read.getCigarElement(0).getOperator() == CigarOperator.S ? read.getCigarElement(0).getLength() : 0)
+                        +
+                ((lastCigarOpIndex != 0) && (read.getCigarElement(lastCigarOpIndex).getOperator() == CigarOperator.S)
+                        ? read.getCigarElement(lastCigarOpIndex).getLength()
+                        : 0);
+
+        // Determine lengths:
+        int totalLength = 0;
+        int alignedLength = 0;
+        for ( final CigarElement element : read.getCigarElements() ) {
+            if ( (element.getOperator() != CigarOperator.S) && element.getOperator().consumesReadBases() ) {   // M, I, X, and EQ (S was already accounted for above)
+                alignedLength += element.getLength();
+            }
+            totalLength += element.getLength();
+        }
+
+        // Calculate the ratio:
+        final double softClipRatio = ((double)numLeadingTrailingSoftClippedBases / (double)totalLength);
+
+        logger.debug( "  Num Leading / Trailing Soft Clipped Bases: " + numLeadingTrailingSoftClippedBases );
+        logger.debug( "  Read Length: " + read.getLength() );
+        logger.debug( "  Read Filter status: (" + softClipRatio + " > " + minimumLeadingTrailingSoftClippedRatio + ")" );
+        logger.debug( "  Read Filter status: " + (softClipRatio > minimumLeadingTrailingSoftClippedRatio) );
+        logger.debug( "  Aligned Length: " + alignedLength );
+
+        return (alignedLength >= minAlignedBases) && (softClipRatio > minimumLeadingTrailingSoftClippedRatio);
     }
 
     @Override
     public boolean test(final GATKRead read) {
         logger.debug( "About to test read: " + read.toString());
-        // If we didn't specify the clipping ratio, we use the min sequence length test:
-        if ( minimumSoftClippedRatio == DEFAULT_MIN_SOFT_CLIPPED_RATIO ) {
-            return testMinSequenceLength(read);
-        }
-        else {
+
+        // NOTE: Since we have mutex'd the args for the clipping ratios, we only need to see if they
+        //       have been specified.  If they have, that's the filter logic we're using.
+
+        // If we specified the clipping ratio, we use the min sequence length test:
+        if ( minimumSoftClippedRatio != DEFAULT_MIN_SOFT_CLIPPED_RATIO ) {
             return testMinSoftClippedRatio(read);
+        }
+        // If we specified the leading/trailing clipping ratio, we use the min sequence length test:
+        if ( minimumLeadingTrailingSoftClippedRatio != DEFAULT_MIN_SOFT_CLIPPED_RATIO ) {
+            return testMinLeadingTrailingSoftClippedRatio(read);
+        }
+        // Default condition is to test against sequence length:
+        else {
+            return testMinSequenceLength(read);
         }
     }
 }
--- src/test/java/org/broadinstitute/hellbender/engine/filters/OverclippedReadFilterUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/engine/filters/OverclippedReadFilterUnitTest.java
@@ -24,22 +24,131 @@ public class OverclippedReadFilterUnitTest extends GATKBaseTest {
 
     private final SAMFileHeader header= ArtificialReadUtils.createArtificialSamHeaderWithGroups(CHR_COUNT, CHR_START, CHR_SIZE, GROUP_COUNT);
 
-
-    @Test(dataProvider= "OverclippedDataProvider")
-    public void testOverclippedFilter(final String cigarString, boolean doNotRequireSoftClipsOnBothEnds, final boolean expectedResult) {
-
-        final OverclippedReadFilter filter = new OverclippedReadFilter(30, false);
-        filter.doNotRequireSoftClipsOnBothEnds = doNotRequireSoftClipsOnBothEnds;
-        final GATKRead read = buildSAMRead(cigarString);
-        Assert.assertEquals(filter.test(read), expectedResult, cigarString);
-    }
-
     private GATKRead buildSAMRead(final String cigarString) {
         final Cigar cigar = TextCigarCodec.decode(cigarString);
         return ArtificialReadUtils.createArtificialRead(header, cigar);
     }
 
-    @DataProvider(name= "OverclippedDataProvider")
+    @Test(dataProvider= "OverclippedDataProvider")
+    public void testOverclippedFilter(final String cigarString,
+                                      final boolean doNotRequireSoftClipsOnBothEnds,
+                                      final boolean expectedResult) {
+
+        final OverclippedReadFilter filter = new OverclippedReadFilter(30, doNotRequireSoftClipsOnBothEnds);
+        final GATKRead read = buildSAMRead(cigarString);
+        Assert.assertEquals(filter.test(read), expectedResult, cigarString);
+    }
+
+    @Test(dataProvider= "OverclippedSoftClipRatioDataProvider")
+    public void testOverclippedSoftClipRatioFilter(final String cigarString,
+                                                   final double clipRatio,
+                                                   final boolean expectedResult) {
+
+        final OverclippedReadFilter filter = new OverclippedReadFilter(10);
+        filter.minimumSoftClippedRatio = clipRatio;
+
+        final GATKRead read = buildSAMRead(cigarString);
+        Assert.assertEquals(filter.test(read), expectedResult, cigarString);
+    }
+
+    @Test(dataProvider= "OverclippedLeadingTrailingSoftClipRatioDataProvider")
+    public void testOverclippedLeadingTrailingSoftClipRatioFilter(final String cigarString,
+                                                   final double clipRatio,
+                                                   final boolean expectedResult) {
+
+        final OverclippedReadFilter filter = new OverclippedReadFilter(10);
+        filter.minimumLeadingTrailingSoftClippedRatio = clipRatio;
+
+        final GATKRead read = buildSAMRead(cigarString);
+        Assert.assertEquals(filter.test(read), expectedResult, cigarString);
+    }
+
+    @DataProvider(name = "OverclippedSoftClipRatioDataProvider")
+    public Iterator<Object[]> overclippedSoftClipRatioDataProvider() {
+        final List<Object[]> testData = new LinkedList<>();
+
+        // ---------------------------------------
+        // Null / trivial cases:
+        testData.add(new Object[] { "", 0.1, false });
+        testData.add(new Object[] { "10H", 0.1, false });
+
+        // ---------------------------------------
+        // Min bases test:
+        testData.add(new Object[] { "20S5M", 0.2, false }); // 20/25 = .8
+        testData.add(new Object[] { "20S20M", 0.2, true }); // 20/40 = .5
+
+        // ---------------------------------------
+        // Soft clip ratio test:
+
+        testData.add(new Object[] { "1S1M1S17M", 0.2, false }); // 2/20 = .100
+        testData.add(new Object[] { "1S1M2S17M", 0.2, false }); // 3/21 = .143
+        testData.add(new Object[] { "1S1M3S17M", 0.2, false }); // 4/22 = .182
+        testData.add(new Object[] { "1S1M4S17M", 0.2, true });  // 5/23 = .217
+        testData.add(new Object[] { "1S1M5S17M", 0.2, true });  // 6/24 = .250
+        testData.add(new Object[] { "1S1M6S17M", 0.2, true });  // 7/25 = .280
+
+        // ---------------------------------------
+        // Soft clip placement:
+
+        testData.add(new Object[] { "101S100M", 0.5, true });
+        testData.add(new Object[] { "100M101S", 0.5, true });
+        testData.add(new Object[] { "25H20S10M20S10M20S10M20S10M20S10M20S25H", 0.5, true });
+
+        return testData.iterator();
+    }
+
+    @DataProvider(name = "OverclippedLeadingTrailingSoftClipRatioDataProvider")
+    public Iterator<Object[]> overclippedLeadingTrailingSoftClipRatioDataProvider() {
+        final List<Object[]> testData = new LinkedList<>();
+
+        // ---------------------------------------
+        // Null / trivial cases:
+        testData.add(new Object[] { "", 0.1, false });
+        testData.add(new Object[] { "10H", 0.1, false });
+
+        // ---------------------------------------
+        // Min bases test:
+        testData.add(new Object[] { "20S5M", 0.2, false }); // 20/25 = .8
+        testData.add(new Object[] { "20S20M", 0.2, true }); // 20/40 = .5
+
+        // ---------------------------------------
+        // Soft clip ratio test:
+
+        // Non-leading/-trailing
+        testData.add(new Object[] { "1S1M1S17M", 0.2, false }); // 2/20 = .100
+        testData.add(new Object[] { "1S1M2S17M", 0.2, false }); // 3/21 = .143
+        testData.add(new Object[] { "1S1M3S17M", 0.2, false }); // 4/22 = .182
+        testData.add(new Object[] { "1S1M4S17M", 0.2, false });  // 5/23 = .217
+        testData.add(new Object[] { "1S1M5S17M", 0.2, false });  // 6/24 = .250
+        testData.add(new Object[] { "1S1M6S17M", 0.2, false });  // 7/25 = .280
+
+        // Leading:
+        testData.add(new Object[] { "2S1S1S16M", 0.2, false });  // 2/20 = .100
+        testData.add(new Object[] { "3S1S1S16M", 0.2, false });  // 3/21 = .143
+        testData.add(new Object[] { "4S1S1S16M", 0.2, false });  // 4/22 = .182
+        testData.add(new Object[] { "5S1S1S16M", 0.2, true });   // 5/23 = .217
+        testData.add(new Object[] { "6S1S1S16M", 0.2, true });   // 6/24 = .250
+        testData.add(new Object[] { "7S1S1S16M", 0.2, true });   // 7/25 = .280
+
+        // Trailing:
+        testData.add(new Object[] { "1M1S16M2S", 0.2, false });  // 2/20 = .100
+        testData.add(new Object[] { "1M1S16M3S", 0.2, false });  // 3/21 = .143
+        testData.add(new Object[] { "1M1S16M4S", 0.2, false });  // 4/22 = .182
+        testData.add(new Object[] { "1M1S16M5S", 0.2, true });   // 5/23 = .217
+        testData.add(new Object[] { "1M1S16M6S", 0.2, true });   // 6/24 = .250
+        testData.add(new Object[] { "1M1S16M7S", 0.2, true });   // 7/25 = .280
+
+        // ---------------------------------------
+        // Soft clip placement:
+
+        testData.add(new Object[] { "101S100M", 0.5, true });
+        testData.add(new Object[] { "100M101S", 0.5, true });
+        testData.add(new Object[] { "25H20S10M20S10M20S10M20S10M20S10M20S25H", 0.5, false });
+
+        return testData.iterator();
+    }
+
+    @DataProvider(name = "OverclippedDataProvider")
     public Iterator<Object[]> overclippedDataProvider() {
         final List<Object[]> result = new LinkedList<>();
 
