1e5edfaa5b43a1f005c1b66a4d1561fe579c1ee0
James Pirruccello <james@carbocation.com>
2019-06-11 10:30:07 -0400

    Adds a bunch of comments to explain the meaning of various parts of the code and its rationale.

--- analysis/phenotyping/cvdi/disease/tabfile.go
+++ analysis/phenotyping/cvdi/disease/tabfile.go
@@ -39,6 +39,8 @@ func (t TabEntry) FormattedValues() []string {
 	return out
 }
 
+// A tabfile can be represented as a simple list of inclusion and exclusion
+// criteria
 type TabFile struct {
 	Include struct {
 		Hesin    []TabEntry
@@ -53,6 +55,8 @@ type TabFile struct {
 	}
 }
 
+// Safely initialize all of the nullable properties of the tabfile
+// representation so you don't get nil pointer errors.
 func NewTabFile() *TabFile {
 	t := &TabFile{}
 
@@ -67,6 +71,9 @@ func NewTabFile() *TabFile {
 	return t
 }
 
+// Read over the data in this tab file and retrieve the FieldIDs for any
+// 'standard' data type, which is neither HESIN data nor the special fields that
+// are paired with dates.
 func (t *TabFile) AllStandardFields() []int {
 	all := make([]int, 0)
 
@@ -83,16 +90,27 @@ func (t *TabFile) AllStandardFields() []int {
 	return all
 }
 
+// Get every inclusion criterion from this tabfile
 func (t *TabFile) AllIncluded() []TabEntry {
 	all := append(t.Include.Hesin, t.Include.Special...)
 	return append(all, t.Include.Standard...)
 }
 
+// Get every exclusion criterion from this tabfile
 func (t *TabFile) AllExcluded() []TabEntry {
 	all := append(t.Exclude.Hesin, t.Exclude.Special...)
 	return append(all, t.Exclude.Standard...)
 }
 
+// ICD9, ICD10, and OPCS codes in the UK Biobank are distributed across many
+// FieldIDs. For example, there is a FieldID for "primary ICD10 code" as well as
+// a separate FieldID for "secondary ICD10 code". For our purposes, we almost
+// always want to consider them equally. Therefore, if a tabfile includes one of
+// those fields but not the other, that's usually in error. (It's not *always*
+// in error or else we could just override it: there may be special
+// circumstances where an analyist only wants to review primary diagnoses.) So,
+// this method looks over all of the fields in a tabfile and lets you know
+// whether it thinks there are additional FieldIDs you should be adding.
 func (t *TabFile) CheckSensibility() ([]string, error) {
 	batchnames := []string{"Inclusion", "Exclusion"}
 	batches := [][]TabEntry{t.AllIncluded(), t.AllExcluded()}
@@ -100,12 +118,23 @@ func (t *TabFile) CheckSensibility() ([]string, error) {
 	missingFields := make(map[int]struct{})
 	errs := make([]string, 0)
 
+	// Iterate over all the fields you're including and excluding separately.
+	// Rationale: if you successfully listed all the primary and secondary ICD10
+	// fields for inclusion, but only listed one of them for exclusion and
+	// forgot to list the other, that's probably unintentional and should be
+	// flagged.
 	for batchID, batch := range batches {
 		icd9 := make(map[int]struct{})
 		icd10 := make(map[int]struct{})
 		opcs := make(map[int]struct{})
 
 		for _, v := range batch {
+			// Capitalized ICD9 (etc) here refer to the manual global maps that
+			// we create in special_fields.go which list all possible FieldIDs
+			// for ICD9 codes. Lower case icd9 represents the local map that
+			// will track which of those FieldIDs actually appeared in the
+			// tabfile. Basically, this is a lookup to see whether the field we
+			// are currently looking at is an ICD9 field, etc.
 			if _, exists := ICD9[v.FieldID]; exists {
 				icd9[v.FieldID] = struct{}{}
 			}
@@ -117,6 +146,8 @@ func (t *TabFile) CheckSensibility() ([]string, error) {
 			}
 		}
 
+		// So, you included at least one ICD9 field, but you failed to include
+		// all of them. Probably an error.
 		if len(icd9) > 0 && len(icd9) < len(ICD9) {
 			for known := range ICD9 {
 				if _, exists := icd9[known]; !exists {
@@ -145,6 +176,8 @@ func (t *TabFile) CheckSensibility() ([]string, error) {
 		}
 	}
 
+	// If we think you forgot one or more FieldIDs, we'll let you know about all
+	// of them.
 	if len(errs) > 0 {
 		missingSlice := make([]string, 0, len(missingFields))
 		for missing := range missingFields {
@@ -157,6 +190,8 @@ func (t *TabFile) CheckSensibility() ([]string, error) {
 	return nil, nil
 }
 
+// ParseTabFile consumes a tabfile and returns our machine representation of
+// that file
 func ParseTabFile(tabPath string) (*TabFile, error) {
 	f, err := os.Open(tabPath)
 	if err != nil {
