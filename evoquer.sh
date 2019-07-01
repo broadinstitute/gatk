797e9f6f1a692474757602736d33286e95a419ee
David Roazen <droazen@broadinstitute.org>
2019-07-01 14:48:02 -0400

    evoquer.sh improvements

--- evoquer.sh
+++ evoquer.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 #
 
-if [ $# -ne 1 ]
+if [ $# -ne 1 -a $# -ne 2 ]
 then
-    echo "Usage: ./evoquer.sh interval"
+    echo "Usage: ./evoquer.sh interval [\"extra args\"]"
     exit 1
 fi
 
-./gatk Evoquer --project-id broad-dsp-spec-ops -L "$1" -R ~/src/references/Homo_sapiens_assembly38_v0/Homo_sapiens_assembly38.fasta --dataset-map evoquer_dataset_map -O evoquer.vcf 
+./gatk Evoquer \
+--project-id broad-dsp-spec-ops \
+-L "$1" \
+-R "src/test/resources/large/Homo_sapiens_assembly38.fasta.gz" \
+--dataset-map evoquer_dataset_map \
+-O evoquer.vcf \
+$2
+
 exit 0
