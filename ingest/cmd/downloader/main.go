cea4fac3dadd01531ed510e05500c68c38f1b37d
James Pirruccello <jamesp@broadinstitute.org>
2019-08-22 10:07:07 -0400

    Updates the ukbb bulk file downloader tool (#37)
    * Updates the downloader tool to default 10 conconcurrent connections,
    which is the max that the UK Biobank now permits per project.
    
    Also changes the sort order to be more GCSFuse friendly - rather than
    checking every file, once it finds a file that does not yet exist, it
    will assume that every subsequent file also needs to be downloaded.
    
    * Actually implement the error retry that the code claims it does.

--- ingest/cmd/downloader/main.go
+++ ingest/cmd/downloader/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func main() {
@@ -14,12 +15,21 @@ func main() {
 	// Download all data with ukbfetch
 
 	var bulkPath, ukbKey, ukbFetch string
+	var concurrency int
 
 	flag.StringVar(&bulkPath, "bulk", "", "Path to *.bulk file, as specified by UKBB.")
 	flag.StringVar(&ukbFetch, "ukbfetch", "ukbfetch", "Path to the ukbfetch utility (if not already in your PATH as ukbfetch).")
 	flag.StringVar(&ukbKey, "ukbkey", ".ukbkey", "Path to the .ukbkey file with the app ID and special key.")
+	flag.IntVar(&concurrency, "concurrency", 10, "Number of simultaneous connections to UK Biobank servers.")
 
 	flag.Parse()
+
+	log.Println("Note: This tool only checks for pre-existing files in the order specified by the bulk file.")
+
+	if bulkPath == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	f, err := os.Open(bulkPath)
 	if err != nil {
@@ -34,17 +44,41 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	concurrency := 16
+	// Note: The UK Biobank updated their rules to permit only 10 simultaneous
+	// downloads per application in 3/2019.
+	log.Println("Using up to", concurrency, "simultaneous downloads")
+
+	// Make it 1-based
+	concurrency = concurrency - 1
+
 	sem := make(chan bool, concurrency)
 
+	finishedCheckingExisting := false
 	for i, row := range entries {
-		zipFile := fmt.Sprintf("%s_%s.zip", row[0], row[1])
+		exists := false
+		zipFile := ""
 
-		// If we already downloaded this file, skip it
-		if _, err := os.Stat(zipFile); !os.IsNotExist(err) {
-			log.Println(i, len(entries), "Already downloaded", zipFile)
+		if !finishedCheckingExisting {
+			// Since statting on a GCSFuse filesystem is slow, we assume sorted
+			// order. If that is true, then once we stop finding files we have
+			// already downloaded, we can stop checking.
+			for _, suffix := range []string{"zip", "cram", "cram.crai"} {
+				zipFile = fmt.Sprintf("%s_%s.%s", row[0], row[1], suffix)
+
+				// If we already downloaded this file, skip it
+				if _, err := os.Stat(zipFile); !os.IsNotExist(err) {
+					log.Println(i, len(entries), "Already downloaded", zipFile)
+					exists = true
+					break
+				}
+			}
+		}
+
+		if exists {
 			continue
 		}
+
+		finishedCheckingExisting = true
 
 		log.Println(i, len(entries), "Downloading", zipFile)
 
@@ -52,8 +86,18 @@ func main() {
 		go func(row []string) {
 			defer func() { <-sem }()
 
-			if out, err := exec.Command(ukbFetch, fmt.Sprintf("-a%s", ukbKey), fmt.Sprintf("-e%s", row[0]), fmt.Sprintf("-d%s", row[1])).CombinedOutput(); err != nil {
-				log.Fatalln(fmt.Errorf("Output: %s | Error: %s", string(out), err.Error()))
+			nErrors := 0
+			for {
+				if out, err := exec.Command(ukbFetch, fmt.Sprintf("-a%s", ukbKey), fmt.Sprintf("-e%s", row[0]), fmt.Sprintf("-d%s", row[1])).CombinedOutput(); err != nil && nErrors < 3 {
+					nErrors++
+					log.Println(fmt.Errorf("Output: %s | Error: %s", string(out), err.Error()))
+					log.Println("Sleeping 30 seconds and retrying")
+					time.Sleep(30 * time.Second)
+					continue
+				}
+
+				// If we already errored 3x or we had no error, break the loop
+				break
 			}
 		}(append([]string{}, row...))
 
