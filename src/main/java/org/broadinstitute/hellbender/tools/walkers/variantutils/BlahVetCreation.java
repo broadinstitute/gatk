1e12387942d1852ca1a86e362c6359ccf647dcc1
rori <ro@sigmasurgical.com>
2019-09-06 21:15:47 -0400

    review vet trim with Laura

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/BlahVetCreation.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/BlahVetCreation.java
@@ -10,6 +10,7 @@ import org.broadinstitute.hellbender.utils.variant.GATKVariantContextUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,7 +25,6 @@ public final class BlahVetCreation {
      * alternate_bases, // req ** concat all alt bases with '|' delimiter // TODO--wait, why is this here? looks like its in v1, but not v2
      * alternate_bases.alt, // req
      * alternate_bases.AS_RAW_MQ, // req
-     * alternate_bases.AS_MQ_DP, // req // TODO this doesn't exist
      * alternate_bases.AS_RAW_MQRankSum,
      * alternate_bases.AS_QUALapprox, // req
      * alternate_bases.AS_RAW_ReadPosRankSum,
@@ -34,7 +34,7 @@ public final class BlahVetCreation {
      * call_name, // req
      * call_genotype, // req
      * call_AD,
-     * call_DP,
+     * call_DP, // remove for now--so similar to AS_VarDP
      * call_GQ, // req
      * call_PGT,
      * call_PID,
@@ -43,7 +43,6 @@ public final class BlahVetCreation {
     public enum HeaderFieldEnum {
         // TODO is this where the validation step (required vs not) lives  -- fail if there is missing data for a required field
         // and just leave it empty if not required
-        // TODO whenever we strip out data, if the data is not as expected, throw an error
 
         position { // Required
              public String getColumnValue(final VariantContext variant) {
@@ -76,7 +75,7 @@ public final class BlahVetCreation {
 
         AS_RAW_MQ {
             // Required
-            // TODO these are string (floats) -- make them ints --- Louis will know where the helper method is?
+            // can strip off the first one?
             // TODO sci notation?
             public String getColumnValue(final VariantContext variant) {
                 String out = getAttribute(variant, GATKVCFConstants.AS_RAW_RMS_MAPPING_QUALITY_KEY, null);
@@ -85,6 +84,11 @@ public final class BlahVetCreation {
                 }
                 if (out.endsWith("|0.00")) {
                     out = out.substring(0, out.length() - 5);
+                    String[] outValues = out.split("\\|");
+                    out = Arrays
+                            .stream(outValues)
+                            .map(val -> val.endsWith(".00") ? val.substring(0, val.length() - 3) : val)
+                            .collect(Collectors.joining(VCFConstants.PHASED));
                 } else {
                     throw new UserException("Expected AS_RAW_MQ value to end in |0.00");
                 }
@@ -121,19 +125,21 @@ public final class BlahVetCreation {
                 if (out == null) {
                     throw new IllegalArgumentException("Cannot be missing required value for alternate_bases.AS_QUALapprox");
                 }
-                if (out.contentEquals("||") || out.contentEquals("|||") ) {
-                    out = " "; //  TODO is this better than null? WAIT WHY IS THIS EVER A ||| VAL IF ITS REQUIRED?
-                    return out;
-                }
                 if (out.startsWith("|")) {
                     out = out.substring(1);
                 } else {
                     throw new UserException("Expected AS_RAW_MQRankSum value to begin with a |");
                 }
-                if (out.endsWith("|0")) {
-                    out = out.substring(0, out.length() - 2);
+                // check to see if there are two or three values, make sure last is smallest, throw out last
+                List<String> outList = Arrays.asList(out.split("\\|"));
+                if (outList.size() == 2 | outList.size() == 3) { // check length of array -- needs to be 2 or 3
+                    if (outList.lastIndexOf(Collections.min(outList)) == outList.size() - 1) { // this should be the smallest value
+                        out = StringUtils.join(outList.subList(0, outList.size() - 1), VCFConstants.PHASED);
+                    } else {
+                        throw new UserException(String.format("Expected the final value of AS_QUALapprox to be the smallest at %d", variant.getStart()));
+                    }
                 } else {
-                    throw new UserException("Expected AS_QUALapprox value to be ||, ||| or to end in |0");
+                    throw new UserException("Expected AS_QUALapprox to have two or three values");
                 }
                 return out;
             }
@@ -160,7 +166,7 @@ public final class BlahVetCreation {
             }
         },
 
-        AS_SB_TABLE { // Required // TODO -- we could remove the 0,0 | if the call_GT is 1/1
+        AS_SB_TABLE { // Required
             public String getColumnValue(final VariantContext variant) {
                 String out = getAttribute(variant, GATKVCFConstants.AS_SB_TABLE_KEY, null);
                 if (out == null) {
@@ -220,11 +226,12 @@ public final class BlahVetCreation {
             }
         },
 
-        call_DP { // TODO ask Laura if we can drop whole column since it looks similar to AS_VarDP
-            public String getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).hasDP() ? String.valueOf(variant.getGenotype(0).getDP()): "";
-            }
-        },
+        // call_DP { // TODO we can drop whole column since it is similar to AS_VarDP
+            // TODO come up with a check-- looks like we can drop this whole one!!!!!
+            // public String getColumnValue(final VariantContext variant) {
+                // return variant.getGenotype(0).hasDP() ? String.valueOf(variant.getGenotype(0).getDP()): "";
+            // }
+        // },
 
         call_GQ { // Required
             public String getColumnValue(final VariantContext variant) {
@@ -235,7 +242,7 @@ public final class BlahVetCreation {
             }
         },
 
-        CALL_PGT {
+        call_PGT {
             public String getColumnValue(final VariantContext variant) {
                 return variant.getGenotype(0).hasAnyAttribute(GATKVCFConstants.HAPLOTYPE_CALLER_PHASING_GT_KEY) ? String.valueOf(variant.getGenotype(0).getAnyAttribute(GATKVCFConstants.HAPLOTYPE_CALLER_PHASING_GT_KEY)) : "";
             }
