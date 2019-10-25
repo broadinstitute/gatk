f74017761ae523b8ec89dc082a600c6849cb5787
Louis Bergelson <louisb@broadinstitute.org>
2019-10-25 12:14:25 -0400

    doing test stuff

--- src/main/java/org/broadinstitute/hellbender/utils/variant/LocalAlleler.java
+++ src/main/java/org/broadinstitute/hellbender/utils/variant/LocalAlleler.java
@@ -4,6 +4,7 @@ import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.Genotype;
 import htsjdk.variant.variantcontext.GenotypeBuilder;
 import htsjdk.variant.variantcontext.VariantContext;
+import shaded.cloud_nio.com.google.errorprone.annotations.Var;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -18,29 +19,55 @@ public class LocalAlleler {
         // GT -> LGT
         // PL -> LPL
         // AD -> LAD
-
+        //todo handle originalGenotype has no GT field
         final Map<String, Object> localAttributes = new LinkedHashMap<>();
 
         //construct LAA
-        final LinkedHashSet<Allele> localAlleles = getLocalAlleles(originalGenotype, vc);
-        final List<Integer> localAlleleIndexes = vc.getAlleleIndices(localAlleles);
-        //todo this is stupidly inefficient probably
-        localAlleleIndexes.remove(0);
+        final LinkedHashSet<Allele> localAllelesIncludingRef = getLocalAlleles(originalGenotype, vc);
+        final List<Integer> localAlleleIndexes = convertToVCFRepresentationOfLocalAlleles(vc, localAllelesIncludingRef);
         localAttributes.put(LAA, localAlleleIndexes);
 
+        //construct LGT
+        List<Integer> localGenotypes = getLocalGenotypeList(originalGenotype, localAllelesIncludingRef);
+        String localGenotypesString = createLocalGenotypeString(localGenotypes, originalGenotype.isPhased());
+        localAttributes.put(LGT, localGenotypesString);
+
+        //construct LAD
+        if( originalGenotype.hasAD()){
+            ArrayList<Integer> localAlleleDepth = new ArrayList<>(localAlleleIndexes.size());
+            // all alleles A*, C,T, AAT, NON_REF  local A*,T,NON_REF  AD 10,11,12,13,14  LAD 10,12,14
+            int[] originalAlleleDepth = originalGenotype.getAD();
+            for( int index : localAlleleIndexes){
+                localAlleleDepth.add(originalAlleleDepth[index]);
+            }
+            localAttributes.put("LAD", localAlleleDepth);
+        }
+
+        GenotypeBuilder genotypeBuilder = new GenotypeBuilder(originalGenotype);
+        //add the new attributes to the builder
+        localAttributes.forEach(genotypeBuilder::attribute);
+        return genotypeBuilder.make();
+    }
+
+    private static List<Integer> convertToVCFRepresentationOfLocalAlleles(VariantContext vc, LinkedHashSet<Allele> localAllelesIncludingRef) {
+        final List<Integer> localAlleleIndexes = vc.getAlleleIndices(localAllelesIncludingRef);
+        //todo this is stupidly inefficient probably
+        localAlleleIndexes.remove(0);
+        return localAlleleIndexes;
+    }
+
+    private static List<Integer> getLocalGenotypeList(Genotype originalGenotype, LinkedHashSet<Allele> localAlleles) {
         List<Integer> localGenotypes = new ArrayList<>(localAlleles.size());
         List<Allele> localAlleleList = new ArrayList<>(localAlleles);
         for(final Allele allele: originalGenotype.getAlleles()){
             localGenotypes.add(localAlleleList.indexOf(allele));
         }
+        return localGenotypes;
+    }
 
-        String delimiter = originalGenotype.isPhased() ? Genotype.PHASED_ALLELE_SEPARATOR : Genotype.UNPHASED_ALLELE_SEPARATOR;
-        String localGenotypesString = localGenotypes.stream().map(String::valueOf).collect(Collectors.joining(delimiter));
-        localAttributes.put(LGT, localGenotypesString);
-
-        GenotypeBuilder genotypeBuilder = new GenotypeBuilder(originalGenotype);
-        localAttributes.forEach(genotypeBuilder::attribute);
-        return genotypeBuilder.make();
+    private static String createLocalGenotypeString(List<Integer> localGenotypes, boolean phased) {
+        String delimiter = phased ? Genotype.PHASED_ALLELE_SEPARATOR : Genotype.UNPHASED_ALLELE_SEPARATOR;
+        return localGenotypes.stream().map(String::valueOf).collect(Collectors.joining(delimiter));
     }
 
     private static LinkedHashSet<Allele> getLocalAlleles(Genotype originalGenotype, VariantContext vc) {
@@ -55,6 +82,13 @@ public class LocalAlleler {
             localAlleles.add(Allele.NON_REF_ALLELE);
         }
         return localAlleles;
+    }
+
+    private static class LocalAlleleMapper {
+        public LocalAlleleMapper(Genotype originalGenotype, VariantContext variantContext){
+            LinkedHashSet<Allele> localAlleles = getLocalAlleles(originalGenotype, variantContext);
+
+        }
     }
 
 }
--- src/test/java/org/broadinstitute/hellbender/utils/variant/LocalAllelerTest.java
+++ src/test/java/org/broadinstitute/hellbender/utils/variant/LocalAllelerTest.java
@@ -6,7 +6,6 @@ import org.broadinstitute.hellbender.testutils.VariantContextTestUtils;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -14,42 +13,64 @@ import java.util.List;
 public class LocalAllelerTest extends BaseTest {
 
     @DataProvider
-    public Object[][] getTestCases(){
-        VariantContextBuilder vcBuilder = new VariantContextBuilder("handmade", "chr1", 100, 100, Collections.emptyList());
+    public Object[][] getTestCasesLAAandLGT(){
         final Allele ALT_AAT = Allele.create("AAT");
-        List<Allele> alleles = Arrays.asList(Allele.REF_A, Allele.ALT_C, Allele.ALT_T, ALT_AAT);
-        vcBuilder.alleles(alleles);
-        GenotypeBuilder genotypeBuilder = new GenotypeBuilder("s1");
+        final VariantContextBuilder vcbNoNonRef = getVcBuilder().alleles(Arrays.asList(Allele.REF_A, Allele.ALT_C, Allele.ALT_T, ALT_AAT));
+        final VariantContextBuilder vcbWithNonRef = getVcBuilder().alleles(Arrays.asList(Allele.REF_A, Allele.ALT_C, Allele.ALT_T, ALT_AAT, Allele.NON_REF_ALLELE));
+
         // GT 0/2 LAA [2]   LGT 0/1
-        Genotype het0_2 = genotypeBuilder.alleles( Arrays.asList(Allele.REF_A, Allele.ALT_T)).make();
+        Genotype het0_2 = makeGenotypeWithAlleles(Arrays.asList(Allele.REF_A, Allele.ALT_T));
 
         // GT 0/0 LAA []    LGT 0/0
-        Genotype hom0_0 = genotypeBuilder.alleles( Arrays.asList(Allele.REF_A, Allele.REF_A)).make();
+        Genotype hom0_0 = makeGenotypeWithAlleles(Arrays.asList(Allele.REF_A, Allele.REF_A));
 
         // GT 1/2 LAA [1,2] LGT 1/2
-        Genotype het1_2 = genotypeBuilder.alleles( Arrays.asList(Allele.ALT_C, Allele.ALT_T)).make();
+        Genotype het1_2 = makeGenotypeWithAlleles(Arrays.asList(Allele.ALT_C, Allele.ALT_T));
 
         // GT 2/2 LAA [2]   LGT 1/1
-        Genotype hom2_2 = genotypeBuilder.alleles( Arrays.asList(Allele.ALT_T, Allele.ALT_T)).make();
+        Genotype hom2_2 = makeGenotypeWithAlleles(Arrays.asList(Allele.ALT_T, Allele.ALT_T));
 
         // GT 1/3 LAA [1,3] LGT 1/2
-        Genotype het1_3 = genotypeBuilder.alleles( Arrays.asList(Allele.ALT_C, ALT_AAT)).make();
+        Genotype het1_3 = makeGenotypeWithAlleles(Arrays.asList(Allele.ALT_C, ALT_AAT));
+
+
         return new Object[][]{
-                makeGenotypes(vcBuilder, het0_2, Arrays.asList(2), "0/1"),
-                makeGenotypes(vcBuilder, hom0_0, Arrays.asList(), "0/0"),
-                makeGenotypes(vcBuilder, het1_2, Arrays.asList(1,2), "1/2" ),
-                makeGenotypes(vcBuilder, hom2_2, Arrays.asList(2), "1/1"),
-                makeGenotypes(vcBuilder, het1_3, Arrays.asList(1,3), "1/2")
+                makeGenotypes(vcbNoNonRef, het0_2, Arrays.asList(2), "0/1"),
+                makeGenotypes(vcbNoNonRef, hom0_0, Arrays.asList(), "0/0"),
+                makeGenotypes(vcbNoNonRef, het1_2, Arrays.asList(1,2), "1/2"),
+                makeGenotypes(vcbNoNonRef, hom2_2, Arrays.asList(2), "1/1"),
+                makeGenotypes(vcbNoNonRef, het1_3, Arrays.asList(1,3), "1/2"),
+
+                makeGenotypes(vcbWithNonRef, het0_2, Arrays.asList(2,4), "0/1"),
+                makeGenotypes(vcbWithNonRef, hom0_0, Arrays.asList(4), "0/0"),
+                makeGenotypes(vcbWithNonRef, het1_2, Arrays.asList(1,2,4), "1/2"),
+                makeGenotypes(vcbWithNonRef, hom2_2, Arrays.asList(2,4), "1/1"),
+                makeGenotypes(vcbWithNonRef, het1_3, Arrays.asList(1,3,4), "1/2")
         };
     }
 
-    private static Object[] makeGenotypes(VariantContextBuilder vcb, Genotype het0_2, List<Integer> LAA, String LGT) {
-        return new Object[]{vcb.genotypes(het0_2).make(), het0_2, new GenotypeBuilder(het0_2).attribute("LAA", LAA).attribute("LGT", LGT).make()};
+    private VariantContextBuilder getVcBuilder() {
+        return new VariantContextBuilder("handmade", "chr1", 100, 100, Collections.emptyList());
     }
 
-    @Test(dataProvider = "getTestCases")
+    private Genotype makeGenotypeWithAlleles(List<Allele> alleles) {
+        GenotypeBuilder gb = new GenotypeBuilder("sample");
+        return gb.alleles(alleles).make();
+    }
+
+    private static Object[] makeGenotypes(VariantContextBuilder vcb, Genotype het0_2, List<Integer> LAA, String LGT) {
+        return new Object[]{vcb.genotypes(het0_2).make(), het0_2, new GenotypeBuilder(het0_2)
+                .attribute("LAA", LAA).attribute("LGT", LGT).make()};
+    }
+
+    @Test(dataProvider = "getTestCasesLAAandLGT")
     public void testAddLocalFields(VariantContext vc, Genotype original, Genotype expected) {
         Genotype actual = LocalAlleler.addLocalFields(original, vc);
         VariantContextTestUtils.assertGenotypesAreEqual(actual, expected);
+    }
+
+    @Test
+    public void testADToLAD(){
+
     }
 }
