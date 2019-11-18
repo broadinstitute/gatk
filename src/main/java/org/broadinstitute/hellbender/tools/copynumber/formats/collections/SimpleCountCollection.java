0d9d9da3d02d961ee5dd8de60172d7055a6d7d5e
Samuel Lee <lee.samuel.k@gmail.com>
2019-11-18 11:20:57 -0500

    Added Tribble codec for SimpleCount counts.tsv/counts.tsv.gz files and methods for streaming and subsetting counts.

--- src/main/java/org/broadinstitute/hellbender/tools/copynumber/formats/collections/SimpleCountCollection.java
+++ src/main/java/org/broadinstitute/hellbender/tools/copynumber/formats/collections/SimpleCountCollection.java
@@ -1,16 +1,24 @@
 package org.broadinstitute.hellbender.tools.copynumber.formats.collections;
 
+import com.google.common.collect.ImmutableList;
 import org.broadinstitute.hdf5.HDF5File;
+import org.broadinstitute.hellbender.engine.FeatureDataSource;
+import org.broadinstitute.hellbender.tools.copynumber.arguments.CopyNumberArgumentValidationUtils;
 import org.broadinstitute.hellbender.tools.copynumber.formats.metadata.SampleLocatableMetadata;
 import org.broadinstitute.hellbender.tools.copynumber.formats.records.SimpleCount;
+import org.broadinstitute.hellbender.utils.IntervalUtils;
 import org.broadinstitute.hellbender.utils.SimpleInterval;
 import org.broadinstitute.hellbender.utils.Utils;
+import org.broadinstitute.hellbender.utils.codecs.copynumber.SimpleCountCodec;
+import org.broadinstitute.hellbender.utils.config.ConfigFactory;
+import org.broadinstitute.hellbender.utils.gcs.BucketUtils;
 import org.broadinstitute.hellbender.utils.io.IOUtils;
 import org.broadinstitute.hellbender.utils.tsv.DataLine;
 import org.broadinstitute.hellbender.utils.tsv.TableColumnCollection;
 
 import java.io.File;
 import java.util.List;
+import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -23,17 +31,23 @@ import java.util.stream.IntStream;
  * @author Samuel Lee &lt;slee@broadinstitute.org&gt;
  */
 public final class SimpleCountCollection extends AbstractSampleLocatableCollection<SimpleCount> {
+    private static final int DEFAULT_FEATURE_QUERY_LOOKAHEAD_IN_BP = 1_000_000;
+
     //note to developers: repeat the column headers in Javadoc so that they are viewable when linked
     /**
      * CONTIG, START, END, COUNT
+     *
+     * Note: Unlike the package-private enums in other collection classes, this enum and its
+     * {@link TableColumnCollection} are public so that they can be accessed by {@link SimpleCountCodec},
+     * which must be in org.broadinstitute.hellbender.utils.codecs to be discovered as a codec.
      */
-    enum SimpleCountTableColumn {
+    public enum SimpleCountTableColumn {
         CONTIG,
         START,
         END,
         COUNT;
 
-        static final TableColumnCollection COLUMNS = new TableColumnCollection((Object[]) values());
+        public static final TableColumnCollection COLUMNS = new TableColumnCollection((Object[]) values());
     }
     
     private static final Function<DataLine, SimpleCount> SIMPLE_COUNT_RECORD_FROM_DATA_LINE_DECODER = dataLine -> {
@@ -60,13 +74,37 @@ public final class SimpleCountCollection extends AbstractSampleLocatableCollecti
         super(metadata, simpleCounts, SimpleCountCollection.SimpleCountTableColumn.COLUMNS, SIMPLE_COUNT_RECORD_FROM_DATA_LINE_DECODER, SIMPLE_COUNT_RECORD_TO_DATA_LINE_ENCODER);
     }
 
+    /**
+     * Read all counts from a file (HDF5 or TSV).
+     */
     public static SimpleCountCollection read(final File file) {
         IOUtils.canReadFile(file);
-        if (IOUtils.isHDF5File(file.toPath())) {
-            return readHDF5(new HDF5File(file));
-        } else {
-            return readTSV(file);
+        return readAndSubset(file, null);  //specifying a null intervalSubset returns all counts
+    }
+
+    /**
+     * From a file (HDF5 or TSV), subset only the counts with intervals coinciding with intervals from a given list.
+     * The list may contain intervals that do not coincide with any count intervals.
+     * Unlike {@link #readOverlappingSubsetFromGCS(String, List)}, this method first reads and constructs a {@link SimpleCountCollection}
+     * using the entire file, and then creates and returns a second {@link SimpleCountCollection} containing only the
+     * requested subset.  This method also does not subset count intervals that overlap but do not strictly coincide
+     * with intervals in the given list, and does not require that intervals in the given list do not overlap each other.
+     * @param intervalSubset    if {@code null} or empty, all counts will be returned
+     */
+    public static SimpleCountCollection readAndSubset(final File file,
+                                                      final Set<SimpleInterval> intervalSubset) {
+        IOUtils.canReadFile(file);
+        final SimpleCountCollection simpleCounts = IOUtils.isHDF5File(file.toPath())
+                ? readHDF5(new HDF5File(file))
+                : readTSV(file);
+        if (intervalSubset == null || intervalSubset.isEmpty()) {
+            return simpleCounts;
         }
+        return new SimpleCountCollection(
+                simpleCounts.getMetadata(),
+                simpleCounts.getRecords().stream()
+                        .filter(c -> intervalSubset.contains(c.getInterval()))
+                        .collect(Collectors.toList()));
     }
 
     private static SimpleCountCollection readTSV(final File file) {
@@ -83,6 +121,51 @@ public final class SimpleCountCollection extends AbstractSampleLocatableCollecti
         final List<SimpleCount> simpleCounts = IntStream.range(0, intervals.size())
                 .mapToObj(i -> new SimpleCount(intervals.get(i), (int) counts[i]))
                 .collect(Collectors.toList());
+        return new SimpleCountCollection(metadata, simpleCounts);
+    }
+
+    /**
+     * Read all counts from a Google Cloud Storage URL.
+     * A corresponding index for the counts file must also be present.
+     */
+    public static SimpleCountCollection readFromGCS(final String path) {
+        IOUtils.assertFileIsReadable(IOUtils.getPath(path));
+        Utils.validate(BucketUtils.isCloudStorageUrl(path), "Read-count path must be a Google Cloud Storage URL.");
+        Utils.validate(new SimpleCountCodec().canDecode(path), String.format(
+                "Read-count file extension must be one of the following: [%s]",
+                String.join(",", SimpleCountCodec.SIMPLE_COUNT_CODEC_EXTENSIONS)));
+        return readOverlappingSubsetFromGCS(path, null);  //specifying a null intervalSubset returns all counts
+    }
+
+    /**
+     * From a Google Cloud Storage URL, subset only the counts with intervals overlapping with intervals from a given list.
+     * A corresponding index for the counts file must also be present.
+     * The given list may contain intervals that do not overlap with any count intervals.
+     * Unlike {@link #readAndSubset(File, Set)}, this method checks for overlaps, rather than requiring the intervals
+     * to be strictly coincident.  Calling code can use {@link IntervalUtils#getIntervalsWithFlanks} to create a merged
+     * list of the original intervals desired to be strictly coincident; this merged list can then be used with this method.
+     * @param overlapIntervals    if {@code null} or empty, all counts will be returned; must be sorted and non-overlapping otherwise
+     */
+    public static SimpleCountCollection readOverlappingSubsetFromGCS(final String path,
+                                                                     final List<SimpleInterval> overlapIntervals) {
+        IOUtils.assertFileIsReadable(IOUtils.getPath(path));
+        Utils.validate(BucketUtils.isCloudStorageUrl(path), "Read-count path must be a Google Cloud Storage URL.");
+        Utils.validate(new SimpleCountCodec().canDecode(path), String.format(
+                "Read-count file extension must be one of the following: [%s]",
+                String.join(",", SimpleCountCodec.SIMPLE_COUNT_CODEC_EXTENSIONS)));
+        final FeatureDataSource<SimpleCount> simpleCountsFeatureDataSource = new FeatureDataSource<>(
+                path,
+                path,
+                DEFAULT_FEATURE_QUERY_LOOKAHEAD_IN_BP,
+                SimpleCount.class,
+                ConfigFactory.getInstance().getGATKConfig().cloudPrefetchBuffer(),
+                ConfigFactory.getInstance().getGATKConfig().cloudIndexPrefetchBuffer());
+        final SampleLocatableMetadata metadata = (SampleLocatableMetadata) simpleCountsFeatureDataSource.getHeader();
+        if (overlapIntervals != null) {
+            CopyNumberArgumentValidationUtils.validateIntervals(overlapIntervals, metadata.getSequenceDictionary());
+        }
+        simpleCountsFeatureDataSource.setIntervalsForTraversal(overlapIntervals);
+        final List<SimpleCount> simpleCounts = ImmutableList.copyOf(simpleCountsFeatureDataSource.iterator());
         return new SimpleCountCollection(metadata, simpleCounts);
     }
 
---
+++ src/main/java/org/broadinstitute/hellbender/utils/codecs/copynumber/SimpleCountCodec.java
@@ -0,0 +1,102 @@
+package org.broadinstitute.hellbender.utils.codecs.copynumber;
+
+import com.google.common.collect.ImmutableList;
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMTextHeaderCodec;
+import htsjdk.samtools.util.BufferedLineReader;
+import htsjdk.tribble.AsciiFeatureCodec;
+import htsjdk.tribble.index.tabix.TabixFormat;
+import htsjdk.tribble.readers.LineIterator;
+import org.apache.commons.lang3.StringUtils;
+import org.broadinstitute.hellbender.exceptions.UserException;
+import org.broadinstitute.hellbender.tools.copynumber.formats.CopyNumberFormatsUtils;
+import org.broadinstitute.hellbender.tools.copynumber.formats.collections.SimpleCountCollection;
+import org.broadinstitute.hellbender.tools.copynumber.formats.metadata.Metadata;
+import org.broadinstitute.hellbender.tools.copynumber.formats.metadata.MetadataUtils;
+import org.broadinstitute.hellbender.tools.copynumber.formats.metadata.SampleLocatableMetadata;
+import org.broadinstitute.hellbender.tools.copynumber.formats.records.SimpleCount;
+import org.broadinstitute.hellbender.utils.SimpleInterval;
+import org.broadinstitute.hellbender.utils.tsv.TableUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public final class SimpleCountCodec extends AsciiFeatureCodec<SimpleCount> {
+
+    private static final int SAM_HEADER_LINES_INITIAL_CAPACITY = 10_000;
+
+    private static final String COLUMN_HEADER_STRING = String.join(
+            TableUtils.COLUMN_SEPARATOR_STRING,
+            SimpleCountCollection.SimpleCountTableColumn.COLUMNS.names());
+
+    private static final int TABIX_FORMAT_SEQUENCE_COLUMN = 1;
+    private static final int TABIX_FORMAT_START_POSITION_COLUMN = 2;
+    private static final int TABIX_FORMAT_END_POSITION_COLUMN = 3;
+    private static final char TABIX_FORMAT_META_CHARACTER = CopyNumberFormatsUtils.COMMENT_PREFIX.charAt(0);
+
+    public static final List<String> SIMPLE_COUNT_CODEC_EXTENSIONS = ImmutableList.of(".counts.tsv", ".counts.tsv.gz");
+
+    public SimpleCountCodec() {
+        super(SimpleCount.class);
+    }
+
+    @Override
+    public SimpleCount decode(final String line) {
+        if (line.startsWith(CopyNumberFormatsUtils.COMMENT_PREFIX) || line.startsWith(COLUMN_HEADER_STRING)) {
+            return null;
+        } else {
+            final String[] split = line.split(TableUtils.COLUMN_SEPARATOR_STRING);
+            try {
+                return new SimpleCount(new SimpleInterval(split[0], Integer.parseInt(split[1]), Integer.parseInt(split[2])), Integer.parseInt(split[3]));
+            } catch (final NumberFormatException e) {
+                throw new UserException.MalformedFile("Line = " + line + " is not formatted correctly.");
+            }
+        }
+    }
+
+    @Override
+    public SampleLocatableMetadata readActualHeader(final LineIterator reader) {
+        final List<String> samHeaderLines = new ArrayList<>(SAM_HEADER_LINES_INITIAL_CAPACITY);
+        //we check that the SAM header lines and the column header line are present in the correct order, then return the mandatory column header
+        boolean isSAMHeaderPresent = false;
+        while (reader.hasNext()) {
+            final String line = reader.peek();
+            if (line.startsWith(CopyNumberFormatsUtils.COMMENT_PREFIX)) {
+                isSAMHeaderPresent = true;
+                samHeaderLines.add(line);
+                reader.next();
+            } else {
+                if (!isSAMHeaderPresent) {
+                    throw new UserException.MalformedFile("SAM header lines must be at the beginning of the file.");
+                } else if (!line.startsWith(COLUMN_HEADER_STRING)) {
+                    throw new UserException.MalformedFile("File does not have a column header.");
+                } else {
+                    //we just peeked at the column header line, so we need to advance past it
+                    reader.next();
+                    break;
+                }
+            }
+        }
+        final SAMFileHeader samFileHeader = new SAMTextHeaderCodec()
+                .decode(BufferedLineReader.fromString(StringUtils.join(samHeaderLines, System.lineSeparator())), null);
+        return MetadataUtils.fromHeader(samFileHeader, Metadata.Type.SAMPLE_LOCATABLE);
+    }
+
+    @Override
+    public boolean canDecode(final String path) {
+        return SIMPLE_COUNT_CODEC_EXTENSIONS.stream().anyMatch(path::endsWith);
+    }
+
+    @Override
+    public TabixFormat getTabixFormat() {
+        return new TabixFormat(
+                TabixFormat.GENERIC_FLAGS,
+                TABIX_FORMAT_SEQUENCE_COLUMN,
+                TABIX_FORMAT_START_POSITION_COLUMN,
+                TABIX_FORMAT_END_POSITION_COLUMN,
+                TABIX_FORMAT_META_CHARACTER,
+                0);
+    }
+}
+
