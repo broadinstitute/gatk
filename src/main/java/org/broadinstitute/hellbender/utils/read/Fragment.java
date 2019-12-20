2de1246ca2b0a90efbc33a4c2c8abe2bc4e45cff
Kevin Lydon <klydon@broadinstitute.org>
2019-12-20 08:47:54 -0500

    Removed unused import

--- src/main/java/org/broadinstitute/hellbender/utils/read/Fragment.java
+++ src/main/java/org/broadinstitute/hellbender/utils/read/Fragment.java
@@ -11,7 +11,6 @@ import org.broadinstitute.hellbender.utils.Utils;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
