52a782540d854de78f88961bf9a5656959fecbf0
James <emeryj@broadinstitute.org>
2020-01-06 09:53:43 -0500

    10mers instead

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/readthreading/ReadThreadingAssembler.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/readthreading/ReadThreadingAssembler.java
@@ -598,7 +598,7 @@ public final class ReadThreadingAssembler {
 ////            }
 ////        }
 ////        return returnList;
-        return Collections.singletonList(35);
+        return Collections.singletonList(10);
     }
 
     private static int arrayMaxInt(final List<Integer> array) {
