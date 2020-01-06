1568a0e5ba355631ee213d2467754c50aa5a257e
Andrea Haessly <ahaessly@broadinstitute.org>
2020-01-06 13:04:45 -0500

    update read pos filter

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/filtering/ReadPositionFilter.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/filtering/ReadPositionFilter.java
@@ -1,13 +1,14 @@
 package org.broadinstitute.hellbender.tools.walkers.mutect.filtering;
 
 import htsjdk.variant.variantcontext.VariantContext;
-import org.broadinstitute.hellbender.tools.walkers.annotator.ReadPosition;
+import org.broadinstitute.hellbender.engine.ReferenceContext;
 import org.broadinstitute.hellbender.utils.variant.GATKVCFConstants;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ReadPositionFilter extends HardFilter {
+public class ReadPositionFilter extends HardAlleleFilter<Integer> {
     private final double minMedianReadPosition;
 
     public ReadPositionFilter(final double minMedianReadPosition) {
@@ -18,11 +19,11 @@ public class ReadPositionFilter extends HardFilter {
     public ErrorType errorType() { return ErrorType.ARTIFACT; }
 
     @Override
-    public boolean isArtifact(final VariantContext vc, final Mutect2FilteringEngine filteringEngine) {
+    public List<Boolean> areAllelesArtifacts(final VariantContext vc, final Mutect2FilteringEngine filteringEngine, ReferenceContext referenceContext) {
         final List<Integer> readPositionByAllele = vc.getAttributeAsIntList(GATKVCFConstants.MEDIAN_READ_POSITON_KEY, 0);
-
-        // a negative value is possible due to a bug: https://github.com/broadinstitute/gatk/issues/5492
-        return readPositionByAllele.get(0) > -1 && readPositionByAllele.get(0) < minMedianReadPosition;
+        return readPositionByAllele.stream()
+                // a negative value is possible due to a bug: https://github.com/broadinstitute/gatk/issues/5492
+                .map(readPos -> readPos > -1 && readPos < minMedianReadPosition).collect(Collectors.toList());
     }
 
     @Override
