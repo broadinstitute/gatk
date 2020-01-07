666ef1c6694ea19711aae6132e13a6187adb97e0
David Benjamin <davidben@broadinstitute.org>
2020-01-07 12:14:01 -0500

    removed unused code to write likelihoods to file

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/PairHMMLikelihoodCalculationEngineUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/PairHMMLikelihoodCalculationEngineUnitTest.java
@@ -159,49 +159,43 @@ public final class PairHMMLikelihoodCalculationEngineUnitTest extends GATKBaseTe
 
     @Test
     public void testComputeLikelihoods(){
-        try {
-            final LikelihoodEngineArgumentCollection LEAC = new LikelihoodEngineArgumentCollection();
+        final LikelihoodEngineArgumentCollection LEAC = new LikelihoodEngineArgumentCollection();
 
-            PairHMMLikelihoodCalculationEngine.writeLikelihoodsToFile = true;
+        final ReadLikelihoodCalculationEngine lce = new PairHMMLikelihoodCalculationEngine((byte) SAMUtils.MAX_PHRED_SCORE, new PairHMMNativeArguments(),
+                PairHMM.Implementation.LOGLESS_CACHING, MathUtils.logToLog10(QualityUtils.qualToErrorProbLog10(LEAC.phredScaledGlobalReadMismappingRate)),
+                PairHMMLikelihoodCalculationEngine.PCRErrorModel.CONSERVATIVE);
 
-            final ReadLikelihoodCalculationEngine lce = new PairHMMLikelihoodCalculationEngine((byte) SAMUtils.MAX_PHRED_SCORE, new PairHMMNativeArguments(),
-                    PairHMM.Implementation.LOGLESS_CACHING, MathUtils.logToLog10(QualityUtils.qualToErrorProbLog10(LEAC.phredScaledGlobalReadMismappingRate)),
-                    PairHMMLikelihoodCalculationEngine.PCRErrorModel.CONSERVATIVE);
+        final Map<String, List<GATKRead>> perSampleReadList = new HashMap<>();
+        final int n = 10;
+        final GATKRead read1 = ArtificialReadUtils.createArtificialRead(TextCigarCodec.decode(n + "M"));
+        read1.setMappingQuality(60);
+        final String sample1 = "sample1";
+        perSampleReadList.put(sample1, Arrays.asList(read1));
 
-            final Map<String, List<GATKRead>> perSampleReadList = new HashMap<>();
-            final int n = 10;
-            final GATKRead read1 = ArtificialReadUtils.createArtificialRead(TextCigarCodec.decode(n + "M"));
-            read1.setMappingQuality(60);
-            final String sample1 = "sample1";
-            perSampleReadList.put(sample1, Arrays.asList(read1));
+        final SampleList samples = new IndexedSampleList(sample1);
 
-            final SampleList samples = new IndexedSampleList(sample1);
+        final AssemblyResultSet assemblyResultSet = new AssemblyResultSet();
+        final byte[] bases = Strings.repeat("A", n + 1).getBytes();
+        final Haplotype hap1 = new Haplotype(bases, true);
+        hap1.setGenomeLocation(read1);
+        assemblyResultSet.add(hap1);
 
-            final AssemblyResultSet assemblyResultSet = new AssemblyResultSet();
-            final byte[] bases = Strings.repeat("A", n + 1).getBytes();
-            final Haplotype hap1 = new Haplotype(bases, true);
-            hap1.setGenomeLocation(read1);
-            assemblyResultSet.add(hap1);
-
-            final byte[] basesModified = bases;
-            basesModified[5] = 'C';//different bases
-            final Haplotype hap2 = new Haplotype(basesModified, false);
-            hap2.setGenomeLocation(read1);//use same loc
-            assemblyResultSet.add(hap2);
+        final byte[] basesModified = bases;
+        basesModified[5] = 'C';//different bases
+        final Haplotype hap2 = new Haplotype(basesModified, false);
+        hap2.setGenomeLocation(read1);//use same loc
+        assemblyResultSet.add(hap2);
 
 
-            final AlleleLikelihoods<GATKRead, Haplotype> likes = lce.computeReadLikelihoods(assemblyResultSet, samples, perSampleReadList);
-            final LikelihoodMatrix<GATKRead, Haplotype> mtx = likes.sampleMatrix(0);
+        final AlleleLikelihoods<GATKRead, Haplotype> likes = lce.computeReadLikelihoods(assemblyResultSet, samples, perSampleReadList);
+        final LikelihoodMatrix<GATKRead, Haplotype> mtx = likes.sampleMatrix(0);
 
-            Assert.assertEquals(mtx.numberOfAlleles(), 2);
-            Assert.assertEquals(mtx.evidenceCount(), 1);
-            final double v1 = mtx.get(0, 0);
-            final double v2 = mtx.get(1, 0);
+        Assert.assertEquals(mtx.numberOfAlleles(), 2);
+        Assert.assertEquals(mtx.evidenceCount(), 1);
+        final double v1 = mtx.get(0, 0);
+        final double v2 = mtx.get(1, 0);
 
-            Assert.assertTrue(v1 > v2, "matching haplotype should have a higher likelihood");
-            lce.close();
-        } finally {
-            new File(PairHMMLikelihoodCalculationEngine.LIKELIHOODS_FILENAME).delete();
-        }
+        Assert.assertTrue(v1 > v2, "matching haplotype should have a higher likelihood");
+        lce.close();
     }
 }
