4b54c96ed65b889c8d27c17ecbbd6cb67173e7c6
Mark Walker <markw@broadinstitute.org>
2020-02-06 17:42:16 -0500

    Add cnmops support

--- src/main/java/org/broadinstitute/hellbender/tools/sv/MergeSVCalls.java
+++ src/main/java/org/broadinstitute/hellbender/tools/sv/MergeSVCalls.java
@@ -19,13 +19,14 @@ import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.tools.copynumber.PostprocessGermlineCNVCalls;
 import org.broadinstitute.hellbender.utils.*;
 import org.broadinstitute.hellbender.utils.codecs.SVCallRecordCodec;
+import org.broadinstitute.hellbender.utils.io.IOUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -41,6 +42,9 @@ import java.util.stream.StreamSupport;
  *     </li>
  *     <li>
  *         gCNV segments VCFs
+ *     </li>
+ *     <li>
+ *         cnMOPs call tables
  *     </li>
  * </ul>
  *
@@ -76,6 +80,7 @@ public final class MergeSVCalls extends GATKTool {
     public static final String SMALL_CNV_PADDING_LONG_NAME = "small-cnv-padding";
     public static final String SMALL_CNV_OUTPUT_LONG_NAME = "small-cnv-output";
     public static final String IGNORE_DICTIONARY_LONG_NAME = "ignore-dict";
+    public static final String CNMOPS_INPUT_LONG_NAME = "cnmops";
 
     public static final int DEFAULT_SMALL_CNV_SIZE = 5000;
     public static final int DEFAULT_SMALL_CNV_PADDING = 1000;
@@ -86,6 +91,12 @@ public final class MergeSVCalls extends GATKTool {
             shortName = StandardArgumentDefinitions.VARIANT_SHORT_NAME
     )
     private List<String> inputFiles;
+
+    @Argument(
+            doc = "cnMOPS input files in tabular format",
+            fullName = CNMOPS_INPUT_LONG_NAME
+    )
+    private List<String> cnmopsFiles;
 
     @Argument(
             doc = "Output TSV file",
@@ -156,12 +167,58 @@ public final class MergeSVCalls extends GATKTool {
 
     @Override
     public void traverse() {
+        for (final String path : cnmopsFiles) {
+            processCNMOPSFile(path);
+        }
         for (int i = 0; i < inputFiles.size(); i++) {
-            processFile(inputFiles.get(i), i);
+            processVariantFile(inputFiles.get(i), i);
         }
     }
 
-    private final void processFile(final String file, final int index) {
+    private void processCNMOPSFile(final String path) {
+        try {
+            final BufferedReader reader = new BufferedReader(IOUtils.makeReaderMaybeGzipped(Paths.get(path)));
+            final String headerString = reader.readLine();
+            if (headerString == null) {
+                logger.warn("Skipping empty cnMOPS file: " + path);
+                return;
+            }
+            logger.info("Parsing cnMOPS file: " + path);
+            if (!headerString.startsWith("#")) {
+                throw new UserException.BadInput("Expected first line to be a header starting with \"#\" but found: \"" + headerString + "\"");
+            }
+            reader.lines().map(this::cnmopsRecordParser).forEach(r -> {
+                records.add(r);
+                progressMeter.update(r.getStartAsInterval());
+            });
+        } catch (final IOException e) {
+            throw new UserException.CouldNotReadInputFile("Encountered exception while reading cnMOPS file: " + path, e);
+        }
+    }
+
+    private SVCallRecord cnmopsRecordParser(final String line) {
+        final String[] tokens = line.split("\t");
+        final String contig = tokens[0];
+        final int start = Integer.valueOf(tokens[1]);
+        final int end = Integer.valueOf(tokens[2]);
+        final Set<String> samples = Collections.singleton(tokens[4]);
+        final StructuralVariantType type = StructuralVariantType.valueOf(tokens[5]);
+        if (!type.equals(StructuralVariantType.DEL) && !type.equals(StructuralVariantType.DUP)) {
+            throw new UserException.BadInput("Unexpected cnMOPS record type: " + type.name());
+        }
+        final String sources = tokens[6];
+        if (!sources.equals("cnmops")) {
+            throw new UserException.BadInput("Unexpected cnMOPS record source(s): " + sources);
+        }
+        final int length = end - start;
+        final boolean isDel = type.equals(StructuralVariantType.DEL);
+        final boolean startStrand = isDel;
+        final boolean endStrand = !isDel;
+        final List<String> algorithms = Collections.singletonList(SVCluster.DEPTH_ALGORITHM);
+        return new SVCallRecord(contig, start, startStrand, contig, end, endStrand, type, length, algorithms, samples);
+    }
+
+    private void processVariantFile(final String file, final int index) {
         final FeatureDataSource<VariantContext> source = getFeatureDataSource(file, "file_" + index);
         final VCFHeader header = getHeaderFromFeatureSource(source);
         final VCFHeaderLine headerSource = header.getMetaDataLine("source");
@@ -174,9 +231,10 @@ public final class MergeSVCalls extends GATKTool {
         } else {
             inputRecords = inputVariants.map(SVCallRecord::create);
         }
-        final Collection<SVCallRecord> inputRecordCollection = inputRecords.collect(Collectors.toList());
-        inputRecordCollection.stream().forEachOrdered(r -> progressMeter.update(r.getStartAsInterval()));
-        records.addAll(inputRecordCollection);
+        inputRecords.forEachOrdered(r -> {
+            records.add(r);
+            progressMeter.update(r.getStartAsInterval());
+        });
     }
 
     private VCFHeader getHeaderFromFeatureSource(final FeatureDataSource<VariantContext> source) {
