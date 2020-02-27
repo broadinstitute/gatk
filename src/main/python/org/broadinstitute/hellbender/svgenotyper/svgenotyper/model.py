19aee6dda0631bee61db0f543dd8c2274e525f25
Mark Walker <markw@broadinstitute.org>
2020-02-27 17:44:27 -0500

    Try to fix discrete inference memory leak

--- src/main/python/org/broadinstitute/hellbender/svgenotyper/svgenotyper/model.py
+++ src/main/python/org/broadinstitute/hellbender/svgenotyper/svgenotyper/model.py
@@ -214,6 +214,15 @@ class SVGenotyperPyroModel(object):
         logging.info("Inference complete.")
         return {key: sample[key].detach().cpu().numpy() for key in sample}
 
+    def clean_tensors(self, off_limit_tensors):
+        import gc
+        tensors = [o for o in gc.get_objects() if torch.is_tensor(o)]
+        for tensor in tensors:
+            for t in off_limit_tensors:
+                if tensor is t:
+                    continue
+            del tensor
+
     def infer_discrete(self, data: SVGenotyperData, svtype: SVTypes, log_freq: int = 100, n_samples: int = 1000):
         logging.info("Running discrete inference...")
         sites = ['z', 'm_sr1', 'm_sr2']
@@ -232,6 +241,7 @@ class SVGenotyperPyroModel(object):
                                                             data_sr2=data.sr2_t, depth_t=data.depth_t,
                                                             rd_gt_prob_t=data.rd_gt_prob_t)
             posterior_samples.append({site: trace.nodes[site]["value"].detach().cpu() for site in sites})
+            self.clean_tensors([data.pe_t, data.sr1_t, data.sr2_t, data.depth_t, data.rd_gt_prob_t])
             if (i + 1) % log_freq == 0:
                 logging.info("[sample {:d}] discrete latent".format(i + 1))
         posterior_samples = {site: torch.stack([posterior_samples[i][site] for i in range(n_samples)], dim=0).numpy() for site in sites}
