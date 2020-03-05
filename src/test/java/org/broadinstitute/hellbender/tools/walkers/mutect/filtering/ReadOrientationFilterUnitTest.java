cf6ce1b751126f7fd27baf55356af8b9b607b34b
David Benjamin <davidben@broadinstitute.org>
2020-03-05 12:02:50 -0500

    Mutect2 can filter MNVs with orientation bias (#6486)

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/mutect/filtering/ReadOrientationFilterUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/mutect/filtering/ReadOrientationFilterUnitTest.java
@@ -8,6 +8,7 @@ import org.broadinstitute.hellbender.GATKBaseTest;
 import org.broadinstitute.hellbender.engine.ReferenceContext;
 import org.broadinstitute.hellbender.engine.ReferenceFileSource;
 import org.broadinstitute.hellbender.tools.walkers.readorientation.*;
+import org.broadinstitute.hellbender.utils.BaseUtils;
 import org.broadinstitute.hellbender.utils.MathUtils;
 import org.broadinstitute.hellbender.utils.Nucleotide;
 import org.broadinstitute.hellbender.utils.SimpleInterval;
@@ -59,32 +60,6 @@ public class ReadOrientationFilterUnitTest extends GATKBaseTest {
                      final Optional<ReadOrientation> expectedArtifactType) throws IOException {
         final ReferenceContext reference = new ReferenceContext(new ReferenceFileSource(IOUtils.getPath(hg19_chr1_1M_Reference)),
                 new SimpleInterval("1", variantPosition -5, variantPosition +5));
-        final String refContext = reference.getKmerAround(variantPosition, F1R2FilterConstants.REF_CONTEXT_PADDING);
-        final Allele refAllele = Allele.create(F1R2FilterUtils.getMiddleBase(refContext).toString(), true);
-        final Nucleotide altBase = Nucleotide.A;
-        final Allele altAllele = Allele.create(altBase.toString(), false); // C -> A transition
-        final List<Allele> alleles = Arrays.asList(refAllele, altAllele);
-        final VariantContext vc  = new VariantContextBuilder("source", Integer.toString(chromosomeIndex), variantPosition, variantPosition, alleles)
-                .attribute(GATKVCFConstants.TUMOR_LOG_10_ODDS_KEY, new double[]{ 7.0 })
-                .make();
-
-        /**
-         * The prior distribution of the states predicts that most of the time a site is hom ref, and the remaining
-         * probability mass is distributed equally among the remaining states
-         */
-        double[] pi = createReasonablePrior(altBase);
-
-        // Arbitrarily choose the number of ref and alt examples observed to compute the priors. They do not affect inference.
-        int numRefExamples = 1_000_000;
-        int numAltExamples = 1000;
-
-        final ArtifactPriorCollection priors = new ArtifactPriorCollection(sampleName);
-        priors.set(new ArtifactPrior(refContext, pi, numRefExamples, numAltExamples));
-        final File table = File.createTempFile("prior", "table");
-        priors.writeArtifactPriors(table);
-
-        final ReadOrientationFilter filter = new ReadOrientationFilter(Collections.singletonList(table));
-
 
         final int altCount = (int) (depth * alleleFraction);
         final int refCount = depth - altCount;
@@ -92,17 +67,49 @@ public class ReadOrientationFilterUnitTest extends GATKBaseTest {
         final int[] f1r2 = new int[] { refCount/2, altF1R2};
         final int[] f2r1 = new int[] { refCount - refCount/2, altCount - altF1R2};
 
-        final GenotypeBuilder genotypeBuilder = new GenotypeBuilder(sampleName)
-                .attribute(GATKVCFConstants.F1R2_KEY, f1r2)
-                .attribute(GATKVCFConstants.F2R1_KEY, f2r1);
+        // Arbitrarily choose the number of ref and alt examples observed to compute the priors. They do not affect inference.
+        int numRefExamples = 1_000_000;
+        int numAltExamples = 1000;
 
-        genotypeBuilder.alleles(alleles);
-        final double posterior = filter.artifactProbability(reference, vc, genotypeBuilder.make());
 
-        if (expectedArtifactType.isPresent()){
-            Assert.assertEquals(posterior, expectedArtifactProb, epsilon);
-        } else {
-            Assert.assertTrue(posterior < 0.01);
+        final Nucleotide[] altBases = new Nucleotide[] {Nucleotide.A, Nucleotide.C, Nucleotide.A};
+        final StringBuilder altBasesStringBuilder = new StringBuilder();
+        for (final Nucleotide nuc : altBases) {
+            altBasesStringBuilder.append((char) nuc.encodeAsByte());
+        }
+        final String altBasesString = altBasesStringBuilder.toString();
+
+        final ArtifactPriorCollection priors = new ArtifactPriorCollection(sampleName);
+
+        for (int n = 0; n < altBases.length; n++) {
+            // most of the time a site is hom ref, and the remaining probability mass is distributed equally among the remaining states
+            double[] pi = createReasonablePrior(altBases[n]);
+            priors.set(new ArtifactPrior(reference.getKmerAround(variantPosition + n, F1R2FilterConstants.REF_CONTEXT_PADDING), pi, numRefExamples, numAltExamples));
+        }
+        final File table = File.createTempFile("prior", "table");
+        priors.writeArtifactPriors(table);
+
+        final ReadOrientationFilter filter = new ReadOrientationFilter(Collections.singletonList(table));
+
+        for (int mnpLength = 1; mnpLength <= altBases.length; mnpLength++) {
+            final Allele refAllele = Allele.create(reference.getBases(new SimpleInterval("1", variantPosition, variantPosition + mnpLength - 1)), true);
+            final Allele altAllele = Allele.create(altBasesString.substring(0, mnpLength), false); // C -> A transition
+            final List<Allele> alleles = Arrays.asList(refAllele, altAllele);
+            final VariantContext vc = new VariantContextBuilder("source", Integer.toString(chromosomeIndex), variantPosition, variantPosition + mnpLength - 1, alleles)
+                    .attribute(GATKVCFConstants.TUMOR_LOG_10_ODDS_KEY, new double[]{7.0})
+                    .make();
+
+            final GenotypeBuilder genotypeBuilder = new GenotypeBuilder(sampleName)
+                    .attribute(GATKVCFConstants.F1R2_KEY, f1r2)
+                    .attribute(GATKVCFConstants.F2R1_KEY, f2r1);
+            genotypeBuilder.alleles(alleles);
+            final double posterior = filter.artifactProbability(reference, vc, genotypeBuilder.make());
+
+            if (expectedArtifactType.isPresent()) {
+                Assert.assertEquals(posterior, expectedArtifactProb, epsilon);
+            } else {
+                Assert.assertTrue(posterior < 0.01);
+            }
         }
     }
 
