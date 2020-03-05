28558c0a0736145bea03c4f3a79d86277ac944ed
David Benjamin <davidben@broadinstitute.org>
2020-03-05 09:47:47 -0500

    made read orientation filter handle MNVs

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/filtering/ReadOrientationFilter.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/filtering/ReadOrientationFilter.java
@@ -15,6 +15,7 @@ import org.broadinstitute.hellbender.utils.variant.VariantContextGetters;
 
 import java.io.File;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class ReadOrientationFilter extends Mutect2VariantFilter {
     private Map<String, ArtifactPriorCollection> artifactPriorCollections = new HashMap<>();
@@ -83,13 +84,19 @@ public class ReadOrientationFilter extends Mutect2VariantFilter {
         final double[] tumorLods = VariantContextGetters.getAttributeAsDoubleArray(vc, GATKVCFConstants.TUMOR_LOG_10_ODDS_KEY, () -> null, -1);
         final int indexOfMaxTumorLod = MathUtils.maxElementIndex(tumorLods);
         final Allele altAllele = vc.getAlternateAllele(indexOfMaxTumorLod);
-        final Nucleotide altBase = Nucleotide.valueOf(altAllele.toString());
+        final byte[] altBases = altAllele.getBases();
 
-        return artifactProbability(referenceContext, vc, g, indexOfMaxTumorLod, altBase);
+        // for MNVs, treat each base as an independent substitution and take the maximum of all error probabilities
+        return IntStream.range(0, altBases.length).mapToDouble(n -> {
+            final Nucleotide altBase = Nucleotide.valueOf(new String(new byte[] {altBases[n]}));
+
+            return artifactProbability(referenceContext, vc.getStart() + n, g, indexOfMaxTumorLod, altBase);
+        }).max().orElse(0.0);
+
     }
 
-    private double artifactProbability(final ReferenceContext referenceContext, final VariantContext vc, final Genotype g, final int indexOfMaxTumorLod, final Nucleotide altBase) {
-        final String refContext = referenceContext.getKmerAround(vc.getStart(), F1R2FilterConstants.REF_CONTEXT_PADDING);
+    private double artifactProbability(final ReferenceContext referenceContext, final int refPosition, final Genotype g, final int indexOfMaxTumorLod, final Nucleotide altBase) {
+        final String refContext = referenceContext.getKmerAround(refPosition, F1R2FilterConstants.REF_CONTEXT_PADDING);
         if (refContext ==  null || refContext.contains("N")){
             return 0;
         }
@@ -98,9 +105,6 @@ public class ReadOrientationFilter extends Mutect2VariantFilter {
                 String.format("kmer must have length %d but got %d", 2 * F1R2FilterConstants.REF_CONTEXT_PADDING + 1, refContext.length()));
 
         final Nucleotide refAllele = F1R2FilterUtils.getMiddleBase(refContext);
-        Utils.validate(refAllele == Nucleotide.valueOf(vc.getReference().toString().replace("*", "")),
-                String.format("ref allele in the kmer, %s, does not match the ref allele in the variant context, %s",
-                        refAllele, vc.getReference().toString().replace("*", "")));
 
         if (!(g.hasExtendedAttribute(GATKVCFConstants.F1R2_KEY) && g.hasExtendedAttribute(GATKVCFConstants.F2R1_KEY))) {
             return 0;
