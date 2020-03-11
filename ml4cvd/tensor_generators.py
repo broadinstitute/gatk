c919b6d3e7c060d21d3b805cd75c69abcadcd333
Nathaniel Diamant <ndiamant@hmc.edu>
2020-03-11 10:07:31 -0400

    skip paths (#154)
    * better printing for cache, paths skipped consistently
    
    * autoencoder style tmaps only cached once
    
    * keys adjusted for autoencoder tmaps
    
    * unnecessary logging line deleted
    
    * made max_rows optional

--- ml4cvd/tensor_generators.py
+++ ml4cvd/tensor_generators.py
@@ -154,31 +154,39 @@ class TensorMapArrayCache:
     Caches numpy arrays created by tensor maps up to a maximum number of bytes
     """
 
-    def __init__(self, max_size, input_tms: List[TensorMap], output_tms: List[TensorMap], max_rows: int):
+    def __init__(self, max_size, input_tms: List[TensorMap], output_tms: List[TensorMap], max_rows: Optional[int] = np.inf):
+        input_tms = [tm for tm in input_tms if tm.cacheable]
+        output_tms = [tm for tm in output_tms if tm.cacheable]
         self.max_size = max_size
         self.data = {}
-        self.row_size = sum(np.zeros(tm.shape, dtype=np.float32).nbytes for tm in input_tms + output_tms)
+        self.row_size = sum(np.zeros(tm.shape, dtype=np.float32).nbytes for tm in set(input_tms + output_tms))
         self.nrows = min(int(max_size / self.row_size), max_rows)
-        for tm in filter(lambda tm: tm.cacheable, input_tms):
+        self.autoencode_names: Dict[str, str] = {}
+        for tm in input_tms:
             self.data[tm.input_name()] = np.zeros((self.nrows,) + tm.shape, dtype=np.float32)
-        for tm in filter(lambda tm: tm.cacheable, output_tms):
+        for tm in output_tms:
             if tm in input_tms:  # Useful for autoencoders
-                self.data[tm.output_name()] = self.data[tm.input_name()]
-            self.data[tm.output_name()] = np.zeros((self.nrows,) + tm.shape, dtype=np.float32)
+                self.autoencode_names[tm.output_name()] = tm.input_name()
+            else:
+                self.data[tm.output_name()] = np.zeros((self.nrows,) + tm.shape, dtype=np.float32)
         self.files_seen = Counter()  # name -> max position filled in cache
         self.key_to_index = {}  # file_path, name -> position in self.data
         self.hits = 0
         self.failed_paths: Set[str] = set()
 
-    def __setitem__(self, key: Tuple[str, str], value):
+    def _fix_key(self, key: Tuple[str, str]) -> Tuple[str, str]:
+        file_path, name = key
+        return file_path, self.autoencode_names.get(name, name)
+
+    def __setitem__(self, key: Tuple[str, str], value) -> bool:
         """
         :param key: should be a tuple file_path, name
         """
-        file_path, name = key
-        if key in self.key_to_index:
+        file_path, name = self._fix_key(key)
+        if key in self.key_to_index:  # replace existing value
             self.data[name][self.key_to_index[key]] = value
             return True
-        if self.files_seen[name] >= self.nrows:
+        if self.files_seen[name] >= self.nrows:  # cache already full
             return False
         self.key_to_index[key] = self.files_seen[name]
         self.data[name][self.key_to_index[key]] = value
@@ -189,19 +197,24 @@ class TensorMapArrayCache:
         """
         :param key: should be a tuple file_path, name
         """
-        file_path, name = key
+        file_path, name = self._fix_key(key)
         val = self.data[name][self.key_to_index[file_path, name]]
         self.hits += 1
         return val
 
     def __contains__(self, key: Tuple[str, str]):
-        return key in self.key_to_index
+        return self._fix_key(key) in self.key_to_index
 
     def __len__(self):
         return sum(self.files_seen.values())
 
     def average_fill(self):
         return np.mean(list(self.files_seen.values()) or [0]) / self.nrows
+
+    def __str__(self):
+        hits = f"The cache has had {self.hits} hits."
+        fullness = ' - '.join(f"{name} has {count} / {self.nrows} tensors" for name, count in self.files_seen.items())
+        return f'{hits} {fullness}.'
 
 
 class _MultiModalMultiTaskWorker:
@@ -238,8 +251,6 @@ class _MultiModalMultiTaskWorker:
         self.cache = TensorMapArrayCache(cache_size, input_maps, output_maps, true_epoch_len)
         logging.info(f'{name} initialized cache of size {self.cache.row_size * self.cache.nrows / 1e9:.3f} GB.')
 
-        self.all_cacheable = all((tm.cacheable for tm in input_maps + output_maps))
-
         self.dependents = {}
         self.idx = 0
 
@@ -265,7 +276,7 @@ class _MultiModalMultiTaskWorker:
 
     def _handle_tensor_path(self, path: Path) -> None:
         hd5 = None
-        if path in self.cache.failed_paths and self.all_cacheable:
+        if path in self.cache.failed_paths:
             self.epoch_stats['skipped_paths'] += 1
             return
         try:
@@ -298,9 +309,8 @@ class _MultiModalMultiTaskWorker:
             f"The following errors occurred:\n\t\t{error_info}",
             f"Generator looped & shuffled over {self.true_epoch_len} paths.",
             f"{int(self.stats['Tensors presented']/self.stats['epochs'])} tensors were presented.",
-            f"The cache holds {len(self.cache)} out of a possible {self.true_epoch_len * (len(self.input_maps) + len(self.output_maps))} tensors and is {100 * self.cache.average_fill():.0f}% full.",
-            f"So far there have been {self.cache.hits} cache hits.",
             f"{self.epoch_stats['skipped_paths']} paths were skipped because they previously failed.",
+            str(self.cache),
             f"{(time.time() - self.start):.2f} seconds elapsed.",
         ])
         logging.info(f"Worker {self.name} - In true epoch {self.stats['epochs']}:\n\t{info_string}")
