ec62ccacbb0d7f02cb7a77e7fc9c56ff8c8b4e55
Andrea Haessly <ahaessly@broadinstitute.org>
2020-04-23 12:31:19 -0400

    initial cohort extract

--- src/main/java/org/broadinstitute/hellbender/tools/variantdb/ExtractCohort.java
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/ExtractCohort.java
@@ -1,6 +1,7 @@
-package org.broadinstitute.hellbender.tools.VariantStore;
+package org.broadinstitute.hellbender.tools.variantdb;
 
 import htsjdk.variant.variantcontext.writer.VariantContextWriter;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.broadinstitute.barclay.argparser.Argument;
@@ -9,13 +10,19 @@ import org.broadinstitute.barclay.help.DocumentedFeature;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.cmdline.programgroups.ShortVariantDiscoveryProgramGroup;
 import org.broadinstitute.hellbender.engine.GATKTool;
+import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.tools.walkers.annotator.Annotation;
 import org.broadinstitute.hellbender.tools.walkers.annotator.StandardAnnotation;
 import org.broadinstitute.hellbender.tools.walkers.annotator.allelespecific.AS_StandardAnnotation;
+import org.broadinstitute.hellbender.utils.bigquery.BigQueryUtils;
+import org.broadinstitute.hellbender.utils.bigquery.GATKAvroReader;
+import org.broadinstitute.hellbender.utils.bigquery.StorageAPIAvroReader;
+import org.broadinstitute.hellbender.utils.bigquery.TableReference;
 import org.broadinstitute.hellbender.utils.io.IOUtils;
+import org.broadinstitute.hellbender.utils.localsort.AvroSortingCollectionCodec;
+import org.broadinstitute.hellbender.utils.localsort.EvoquerSortingCollection;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 @CommandLineProgramProperties(
@@ -26,6 +33,9 @@ import java.util.List;
 @DocumentedFeature
 public class ExtractCohort extends GATKTool {
     private static final Logger logger = LogManager.getLogger(ExtractCohort.class);
+    public static final int DEFAULT_LOCAL_SORT_MAX_RECORDS_IN_RAM = 1000000;
+    private VariantContextWriter vcfWriter = null;
+    private ExtractCohortEngine engine;
 
 
     @Argument(
@@ -44,12 +54,26 @@ public class ExtractCohort extends GATKTool {
     private String projectID = null;
 
     @Argument(
+            fullName = "sample-table",
+            doc = "Fully qualified name of a bigquery table containing a single column `sample` that describes the full list of samples to evoque",
+            optional = true
+    )
+    private String sampleTableName = null;
+
+
+    @Argument(
             fullName = "variant-filter-table",
             doc = "Fully qualified name of the filtering table to use for cohort extraction",
             optional = true
     )
     private String filteringFQTableName = null;
 
+    @Argument(
+            fullName = "cohort-extract-table",
+            doc = "Fully qualified name of the table where the cohort data exists (already subsetted)",
+            optional = false
+    )
+    private String cohortTable = null;
 
     @Argument(
             fullName = "print-debug-information",
@@ -69,7 +93,17 @@ public class ExtractCohort extends GATKTool {
             optional = true)
     private double vqsLodINDELThreshold = 99.4;
 
-    private VariantContextWriter vcfWriter = null;
+    @Argument(
+            fullName = "local-sort-max-records-in-ram",
+            doc = "When doing local sort, store at most this many records in memory at once",
+            optional = true
+    )
+    private int localSortMaxRecordsInRam = DEFAULT_LOCAL_SORT_MAX_RECORDS_IN_RAM;
+
+    @Override
+    public boolean requiresReference() {
+        return true;
+    }
 
     @Override
     public boolean useVariantAnnotations() { return true; }
@@ -88,15 +122,32 @@ public class ExtractCohort extends GATKTool {
 //        final VariantAnnotatorEngine annotationEngine = new VariantAnnotatorEngine(makeVariantAnnotations(), null, Collections.emptyList(), false, false);
 
         vcfWriter = createVCFWriter(IOUtils.getPath(outputVcfPathString));
-//        final List<String> sampleNames = loadAllLines(sampleListFile);
-        vcfWriter.writeHeader(evoquerEngine.getHeader());
+        engine = new ExtractCohortEngine(
+                projectID,
+                vcfWriter,
+                reference,
+                sampleTableName,
+                cohortTable,
+                filteringFQTableName,
+                localSortMaxRecordsInRam,
+                printDebugInformation,
+                vqsLodSNPThreshold,
+                vqsLodINDELThreshold,
+                progressMeter);
+        vcfWriter.writeHeader(CommonCode.generateVcfHeader());
     }
 
     @Override
+    // maybe think about creating a BigQuery Row walker?
     public void traverse() {
         progressMeter.setRecordsBetweenTimeChecks(100L);
 
-    }
+        if ( filteringFQTableName == null || filteringFQTableName.equals("") ) {
+            logger.warn("--variant-filter-table is not specified, no filtering of cohort! ");
+        }
+
+        engine.traverse();
+     }
 
     @Override
     protected void onShutdown() {
