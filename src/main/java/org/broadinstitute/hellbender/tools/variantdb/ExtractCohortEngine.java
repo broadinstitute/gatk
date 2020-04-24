e1f4016891afad602bf48f1295fa44ccc6bd263f
Andrea Haessly <ahaessly@broadinstitute.org>
2020-04-24 17:09:29 -0400

    get genotypes working

--- src/main/java/org/broadinstitute/hellbender/tools/variantdb/ExtractCohortEngine.java
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/ExtractCohortEngine.java
@@ -236,7 +236,7 @@ public class ExtractCohortEngine {
         // Note that we remove NON_REF in our variantContextMerger if the GnarlyGenotyper is disabled, but
         // keep NON_REF around if the GnarlyGenotyper is enabled. This is because the GnarlyGenotyper expects
         // the NON_REF allele to still be present, and will give incorrect results if it's not.
-        final VariantContext mergedVC = variantContextMerger.merge(unmergedCalls, new SimpleInterval(contig, (int) start, (int) start), refAllele.getBases()[0], true, false);
+        final VariantContext mergedVC = variantContextMerger.merge(unmergedCalls, new SimpleInterval(contig, (int) start, (int) start), refAllele.getBases()[0], true, false, true);
 
 
         final VariantContext finalVC = filteringTableRef != null ? mergedVC : filterVariants(mergedVC);
