a29f5b98f0fb6a1fec25eaf354c594bd665cb5e6
Samuel Lee <lee.samuel.k@gmail.com>
2020-05-01 14:07:05 -0400

    Added MKL checks to PythonEnvironmentIntegrationTest.

--- src/test/java/org/broadinstitute/hellbender/utils/python/PythonEnvironmentIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/utils/python/PythonEnvironmentIntegrationTest.java
@@ -12,8 +12,8 @@ import java.util.Collections;
 public class PythonEnvironmentIntegrationTest {
     private final static String NL = System.lineSeparator();
 
-    @DataProvider(name="supportedPythonPackages")
-    public Object[][] getSupportedPythonPackages() {
+    @DataProvider(name="dataPackagePresent")
+    public Object[][] getDataPackagePresent() {
         return new Object[][] {
                 // names of base packages that we should be able to import from within the GATK conda environment
                 // NOTE: these must be kept in sync with the versions in gatkcondaenv.yml.template
@@ -32,8 +32,21 @@ public class PythonEnvironmentIntegrationTest {
         };
     }
 
-    @Test(groups = {"python"}, dataProvider="supportedPythonPackages")
-    public void testGATKPythonEnvironmentPackagePresent(final String packageName, final String expectedVersion) {
+    /**
+     * See documentation in scripts/gatkcondaenv.yml.template.
+     */
+    @DataProvider(name="dataPackageMKLEnabled")
+    public Object[][] getDataPackageMKLEnabled() {
+        return new Object[][] {
+                { "numpy",          "numpy.__config__.get_info('blas_mkl_info') != {} and numpy.__config__.get_info('lapack_mkl_info') != {}" },
+                { "theano",         "'-lmkl_rt' in theano.config.blas.ldflags" },
+                { "tensorflow",     "tensorflow.pywrap_tensorflow.IsMklEnabled()" }
+        };
+    }
+
+    @Test(groups = {"python"}, dataProvider="dataPackagePresent")
+    public void testGATKPythonEnvironmentPackagePresent(final String packageName,
+                                                        final String expectedVersion) {
         // Sanity check to ensure that we can import these packages, and that conda is resolving them to
         // the specific version we requested in the conda environment definition.
         final StreamingPythonScriptExecutor<String> streamingPythonExecutor =
@@ -49,19 +62,38 @@ public class PythonEnvironmentIntegrationTest {
         try {
             streamingPythonExecutor.sendSynchronousCommand(String.format("import %s" + NL, packageName));
         } catch (final PythonScriptExecutorException e) {
-            throw new RuntimeException(failedDependencyMessage, e);
+            Assert.fail(failedDependencyMessage);
         }
 
         // for some dependencies, we also validate that the conda environment got the version that we asked for
         if (expectedVersion != null) {
             try {
                 streamingPythonExecutor.sendSynchronousCommand(
-                    String.format("assert(%s.__version__ == '%s')" + NL,
-                            packageName,
-                            expectedVersion));
+                        String.format("assert %s.__version__ == '%s'" + NL,
+                                packageName,
+                                expectedVersion));
             } catch (final PythonScriptExecutorException e) {
-                throw new RuntimeException(failedDependencyMessage, e);
+                Assert.fail(failedDependencyMessage);
             }
+        }
+    }
+
+    /**
+     * See documentation in scripts/gatkcondaenv.yml.template.
+     */
+    @Test(groups = {"python"}, dataProvider="dataPackageMKLEnabled")
+    public void testGATKPythonEnvironmentPackageMKLEnabled(final String packageName,
+                                                           final String assertMKLEnabled) {
+        final StreamingPythonScriptExecutor<String> streamingPythonExecutor =
+                new StreamingPythonScriptExecutor<>(true);
+        Assert.assertNotNull(streamingPythonExecutor);
+        Assert.assertTrue(streamingPythonExecutor.start(Collections.emptyList(), true, null));
+
+        final String failedAssertMKLEnabledMessage = String.format("The installed version of %s does not have MKL enabled.", packageName);
+        try {
+            streamingPythonExecutor.sendSynchronousCommand(String.format("import %s; assert(%s)" + NL, packageName, assertMKLEnabled));
+        } catch (final PythonScriptExecutorException e) {
+            Assert.fail(failedAssertMKLEnabledMessage);
         }
     }
 }
