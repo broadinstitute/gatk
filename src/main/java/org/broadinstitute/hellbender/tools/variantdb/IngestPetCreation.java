e4fe7723a06e6307eb6fd903b2384a9bb7d93673
RoriCremer <6863459+RoriCremer@users.noreply.github.com>
2020-05-08 13:24:28 -0400

    Ingest wip to be added to other var db code (#6582)
    * initial vet creation class
    
    * initial variant walker for jg
    
    * initial pet creation class
    
    add pet creation to walker
    
    update vet schema
    
    add headers enum and method to pet creation
    
    add xsv creation to loop
    
    update vet
    
    fix extra delim bug and show non-ref placeholder
    
    Also fixed `[.....]` strings in the vet and converted a lot of strings to constants plus a couple of TODOs
    
    Fix GT String to output index of alleles instead of allele characters
    
    fix enum bug
    
    add GQ band to drop as an optional param
    
    include end base
    
    code used to push PET and VET to BQ
    
    added ability to pass in what GQ state to interpret as "missing" as well as skeleton for creating the metdata tsv (sample list, what interval list was use, the interval list's md5, and what GQ is signified by "missing".  Also put some util files in a folder under BlahVariantWalkerUtils for now (should get rid of them eventually but nice to have for now)
    
    spacing + adding optional to GQ arguments (whoops)
    
    rename intervalListPath to  intervalListBlob
    
    pass thru sample name
    
    swap out errors thrown
    
    use L for interval list
    
    This doesn't have the level of validation that we need, but works for now
    Ideally the interval list is required to be a picard interval list file
    
    md5 interval list concated string
    
    wip split xsvs by contig
    
    add vet stripping intial changes
    
    review vet trim with Laura
    
    fix overzealous missing bug
    
    laura seal of approval on schema
    
    add back call_DP until we are sure evoq doesn't need it
    
    write chr dirs on the fly
    
    create dirs for chrs w pet and vet dirs inside
    
    add sample id to metadata
    
    semantics for sample id
    
    move position and sample into main method for easier access
    
    add sample id mapping and use it
    
    add enum for chr index
    
    use longs for the chr addition
    
    add array schema and enum
    
    add bool for array
    
    update array options with sample id and chr adjustment
    
    wip to remove chr separation
    
    new sample directory structure
    
    let the rename begin
    
    fixup on walker for arrays
    
    more renames
    
    better comments
    
    fixup on ingest walker
    
    * unrelated conflict--maybe merge mistake?
    
    * cleanup
    
    * swap method for indexed alleles
    
    * add genome specific VET creation
    
    * move mode enum into common code
    
    * wip on adding enums as params

---
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/IngestPetCreation.java
@@ -0,0 +1,145 @@
+package org.broadinstitute.hellbender.tools.variantdb;
+
+import htsjdk.variant.variantcontext.VariantContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class IngestPetCreation {
+
+    /**
+     * Expected headers for the Position Table (PET)
+     */
+    public enum HeaderFieldEnum {
+        position,
+        sample,
+        state,
+    }
+
+    public enum GQStateEnum {
+        VARIANT("v"),
+        STAR("*"),
+        ZERO("0"),
+        TEN("1"),
+        TWENTY("2"),
+        THIRTY("3"),
+        FORTY("4"),
+        FIFTY("5"),
+        SIXTY("6"),
+        MISSING("m"),
+        UNKNOWN("u");
+
+        String value;
+
+        GQStateEnum(String value) {
+            this.value = value;
+        }
+
+    }
+
+    public static List<List<String>> createPositionRows(final long start, final long end, final VariantContext variant, final String sampleId) {
+
+        List<List<String>> rows = new ArrayList<>();
+
+        if (!variant.isReferenceBlock()) {
+            List<String> row = new ArrayList<>();
+            row.add(String.valueOf(start));
+            row.add(sampleId);
+            row.add(GQStateEnum.VARIANT.value);
+            rows.add(row);
+
+            //if variant is variant and has additional positions--must be a deletion: add `*` state
+            for (long i = start + 1 ; i <= end; i++){
+                row = new ArrayList<>();
+                row.add(String.valueOf(i));
+                row.add(sampleId);
+                row.add(GQStateEnum.STAR.value);
+                rows.add(row);
+            }
+        } else {
+            // TODO check in the tool to make sure it's only one sample
+            GQStateEnum state = getGQStateEnum(variant.getGenotype(0).getGQ());
+
+            for (long position = start; position <= end; position++){ // break up ref blocks
+                List<String> row = new ArrayList<>();
+
+                row.add(String.valueOf(position));
+                row.add(sampleId);
+                row.add(state.value);
+                rows.add(row);
+            }
+        }
+
+        return rows;
+    }
+
+    public static List<List<String>> createArrayPositionRows(final long start, final long end, final VariantContext variant, final String sampleName) {
+
+        List<List<String>> rows = new ArrayList<>();
+
+        List<String> row = new ArrayList<>();
+        row.add(String.valueOf(start));
+        row.add(sampleName);
+        row.add(GQStateEnum.UNKNOWN.value);
+        rows.add(row);
+
+        return rows;
+    }
+
+    public static List<List<String>> createSpanDelRows(final long start, final long end, final VariantContext variant, final String sampleName) {
+        if (variant.isReferenceBlock()){
+            throw new IllegalStateException("Cannot create span deletion rows for a reference block");
+        }
+
+        List<List<String>> rows = new ArrayList<>();
+
+        for (long position = start; position <= end; position++){ // break up ref blocks
+            List<String> row = new ArrayList<>();
+
+            row.add(String.valueOf(position));
+            row.add(sampleName);
+            row.add(GQStateEnum.STAR.value);
+            rows.add(row);
+        }
+
+        return rows;
+    }
+
+    public static List<List<String>> createMissingTSV(long start, long end, String sampleName) {
+        List<List<String>> rows = new ArrayList<>();
+
+        for (long position = start; position <= end; position ++){
+            List<String> row = new ArrayList<>();
+            row.add(String.valueOf(position));
+            row.add(sampleName);
+            row.add(GQStateEnum.MISSING.value);
+            rows.add(row);
+        }
+
+        return rows;
+    }
+
+    public static GQStateEnum getGQStateEnum(int GQ){
+        if (GQ < 10) {
+            return GQStateEnum.ZERO;
+        } else if (GQ < 20) {
+            return GQStateEnum.TEN;
+        } else if (GQ < 30) {
+            return GQStateEnum.TWENTY;
+        } else if (GQ < 40) {
+            return GQStateEnum.THIRTY;
+        } else if (GQ < 50) {
+            return GQStateEnum.FORTY;
+        } else if (GQ < 60) {
+            return GQStateEnum.FIFTY;
+        } else {
+            return GQStateEnum.SIXTY;
+        }
+
+    }
+
+    public static List<String> getHeaders() {
+        return Arrays.stream(HeaderFieldEnum.values()).map(String::valueOf).collect(Collectors.toList());
+    }
+}
