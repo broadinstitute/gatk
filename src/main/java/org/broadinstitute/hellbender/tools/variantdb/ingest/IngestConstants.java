8cd1731acb21998a3abf2950658819d927b27272
Andrea Haessly <ahaessly@broadinstitute.org>
2020-06-02 18:32:01 -0400

    major refactor splitting ingest for arrays from exomes/genomes

---
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/ingest/IngestConstants.java
@@ -0,0 +1,10 @@
+package org.broadinstitute.hellbender.tools.variantdb.ingest;
+
+public class IngestConstants {
+
+    public static final char SEPARATOR = '\t';
+    public static final String FILETYPE = ".tsv";
+    public static final String metadataDirectoryName = "metadata";
+    public static final int partitionPerTable = 4000;
+
+}
