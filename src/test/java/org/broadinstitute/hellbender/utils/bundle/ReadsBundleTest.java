005b71dcfbded83744406ffaf8971ddd80adb2ea
Louis Bergelson <louisb@broadinstitute.org>
2020-06-29 13:24:08 -0400

    working

--- src/test/java/org/broadinstitute/hellbender/utils/bundle/ReadsBundleTest.java
+++ src/test/java/org/broadinstitute/hellbender/utils/bundle/ReadsBundleTest.java
@@ -9,27 +9,29 @@ import java.util.Collections;
 
 public class ReadsBundleTest extends BaseTest {
 
+    public static final String EXPECTED_JSON_STRING = "{\n" +
+            "  \"reads\": {\n" +
+            "    \"path\": \"a file\",\n" +
+            "    \"fileType\": \"bam\"\n" +
+            "  },\n" +
+            "  \"index\": {\n" +
+            "    \"path\": \"an index\",\n" +
+            "    \"fileType\": \"bai\"\n" +
+            "  },\n" +
+            "  \"schemaVersion\": \"0.1.0\",\n" +
+            "  \"schemaType\": \"ReadsBundle\"\n" +
+            "}";
+
     @Test
     public void testWrite() {
         final ReadsBundle readsBundle = new ReadsBundle(new BundleFile("a file", "bam"), new BundleFile("an index", "bai"));
         final String s = readsBundle.toJson();
-        Assert.assertEquals(s, "{\n" +
-                "  \"reads\": {\n" +
-                "    \"path\": \"a file\",\n" +
-                "    \"fileType\": \"bam\"\n" +
-                "  },\n" +
-                "  \"index\": {\n" +
-                "    \"path\": \"an index\",\n" +
-                "    \"fileType\": \"bai\"\n" +
-                "  },\n" +
-                "  \"schemaVersion\": \"0.1.0\",\n" +
-                "  \"schemaType\": \"ReadsBundle\"\n" +
-                "}");
+        Assert.assertEquals(s, EXPECTED_JSON_STRING);
     }
 
     @Test
     public void testRead(){
-        final String json = "{\"reads\":{\"path\":\"a file\",\"fileType\":\"bam\"},\"index\":{\"path\":\"an index\",\"fileType\":\"bai\"}}";
+        final String json = EXPECTED_JSON_STRING;
         final ReadsBundle readsBundle1 = ReadsBundle.fromJson(json);
         final GATKPath path = new GATKPath("a file");
         path.setTagAttributes(Collections.singletonMap("type", "bam"));
@@ -40,7 +42,7 @@ public class ReadsBundleTest extends BaseTest {
     public void testReadFromFile(){
         final GATKPath json = getTestPath("reads1.json");
         final ReadsBundle readsBundle1 = ReadsBundle.fromPath(json);
-        Assert.assertEquals(readsBundle1.getReads(), new GATKPath("a file"));
+        Assert.assertEquals(readsBundle1.getReads(), new GATKPath("a file").setTagAttributes(Collections.singletonMap()););
     }
 
 }
