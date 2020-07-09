042e4a4897d2476152cc6dd6d1b6063019bb9409
Andrea Haessly <ahaessly@broadinstitute.org>
2020-07-09 12:42:01 -0400

    update exome ingest

--- src/main/java/org/broadinstitute/hellbender/tools/variantdb/ingest/nextgen/GenomeVetTsvCreator.java
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/ingest/nextgen/GenomeVetTsvCreator.java
@@ -3,6 +3,7 @@ package org.broadinstitute.hellbender.tools.variantdb.ingest.nextgen;
 import htsjdk.variant.variantcontext.VariantContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.broadinstitute.hellbender.tools.variantdb.SchemaUtils;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -14,17 +15,17 @@ import java.util.stream.Collectors;
 public class GenomeVetTsvCreator extends VetTsvCreator {
     static final Logger logger = LogManager.getLogger(GenomeVetTsvCreator.class);
 
-    public GenomeVetTsvCreator(String sampleName, String sampleId, Path sampleDirectoryPath) {
-        super(sampleName, sampleId, sampleDirectoryPath);
+    public GenomeVetTsvCreator(String sampleName, String sampleId, String tableNumberPrefix) {
+        super(sampleName, sampleId, tableNumberPrefix);
     }
 
     @Override
     public List<String> createRow(final long start, final VariantContext variant, final String sampleId) {
         List<String> row = new ArrayList<>();
-        row.add(String.valueOf(start));
+        row.add(String.valueOf(SchemaUtils.encodeLocation(variant.getContig(), variant.getStart())));
         row.add(sampleId);
         for ( final GenomeFieldEnum fieldEnum : GenomeFieldEnum.values() ) {
-            if (!fieldEnum.equals(GenomeFieldEnum.position) && !fieldEnum.equals(GenomeFieldEnum.sample)) {
+            if (!fieldEnum.equals(GenomeFieldEnum.location) && !fieldEnum.equals(GenomeFieldEnum.sample)) {
                 row.add(fieldEnum.getColumnValue(variant));
             }        }
         return row;
