1022cbea7b2480348fcd429145bfec2beaed76c7
Anders Leung <37423859+andersleung@users.noreply.github.com>
2020-07-13 16:12:38 -0400

    WIP Specify 127.0.0.1 in port mapping when running docker

--- scripts/htsgetScripts/launchDocker.sh
+++ scripts/htsgetScripts/launchDocker.sh
@@ -4,7 +4,7 @@ echo "pwd is `pwd`"
 ls
 
 docker pull ga4gh/htsget-ref:1.1.0
-docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://localhost:3000 \
+docker container run -d --name htsget-server -p 127.0.0.1:3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://localhost:3000 \
       -v $WORKING_DIR/gatk/src/test/resources/org/broadinstitute/hellbender/engine:/data ga4gh/htsget-ref:1.1.0 \
       ./htsref -config /data/htsget_config.json
 
