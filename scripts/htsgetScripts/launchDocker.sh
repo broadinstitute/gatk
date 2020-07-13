100b0a478f790b85a5bd931893a57eda0906371c
Anders Leung <37423859+andersleung@users.noreply.github.com>
2020-07-13 14:44:44 -0400

    WIP Try spawning sibling docker container for refserver

--- scripts/htsgetScripts/launchDocker.sh
+++ scripts/htsgetScripts/launchDocker.sh
@@ -4,7 +4,10 @@ echo "pwd is `pwd`"
 ls
 
 docker pull ga4gh/htsget-ref:1.1.0
-docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://localhost:3000 -v $WORKING_DIR/gatk/src/test/resources/org/broadinstitute/hellbender/engine:/data ga4gh/htsget-ref:1.1.0 ./htsref -config /data/htsget_config.json
+docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://localhost:3000 \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v $WORKING_DIR/gatk/src/test/resources/org/broadinstitute/hellbender/engine:/data ga4gh/htsget-ref:1.1.0 \
+      ./htsref -config /data/htsget_config.json
 
 echo "Running docker containers"
 
