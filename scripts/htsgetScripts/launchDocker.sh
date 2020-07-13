c6a36cff02609008641d5fe3e0f839465ec3c152
Anders Leung <37423859+andersleung@users.noreply.github.com>
2020-07-13 16:09:38 -0400

    WIP try 0.0.0.0:3000 instead of 127.0.0.1

--- scripts/htsgetScripts/launchDocker.sh
+++ scripts/htsgetScripts/launchDocker.sh
@@ -5,7 +5,6 @@ ls
 
 docker pull ga4gh/htsget-ref:1.1.0
 docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://localhost:3000 \
-      -v /var/run/docker.sock:/var/run/docker.sock \
       -v $WORKING_DIR/gatk/src/test/resources/org/broadinstitute/hellbender/engine:/data ga4gh/htsget-ref:1.1.0 \
       ./htsref -config /data/htsget_config.json
 
