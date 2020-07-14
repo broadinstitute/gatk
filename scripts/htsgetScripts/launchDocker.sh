f166712844afbc40cae4b2dd6a4846ba0777df1f
Anders Leung <37423859+andersleung@users.noreply.github.com>
2020-07-14 09:15:27 -0400

    WIP configure all IP addresses to 0.0.0.0:3000

--- scripts/htsgetScripts/launchDocker.sh
+++ scripts/htsgetScripts/launchDocker.sh
@@ -1,14 +1,10 @@
 WORKING_DIR=/home/travis/build/broadinstitute
 
-echo "pwd is `pwd`"
-
 docker pull ga4gh/htsget-ref:1.1.0
-docker container run -d --name htsget-server -p 127.0.0.1:3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://0.0.0.0:3000 \
-      -v $WORKING_DIR/gatk/src/test/resources/org/broadinstitute/hellbender/engine:/data ga4gh/htsget-ref:1.1.0 \
+docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://0.0.0.0:3000 \
+      -v $WORKING_DIR/gatk/src/test/resources/org/broadinstitute/hellbender/engine:/data \
+      ga4gh/htsget-ref:1.1.0 \
       ./htsref -config /data/htsget_config.json
-
-echo "Running docker containers"
-
 docker container ls -a
 
 curl http://localhost:3000
