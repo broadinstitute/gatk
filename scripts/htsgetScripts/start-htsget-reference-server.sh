a32a9e391f4d3edc565b8ded67264c4f2939ef5c
Anders Leung <37423859+andersleung@users.noreply.github.com>
2020-08-19 08:59:49 -0400

    Add readme to htsgetScripts, move htsget_config.json

--- scripts/htsgetScripts/start-htsget-reference-server.sh
+++ scripts/htsgetScripts/start-htsget-reference-server.sh
@@ -4,8 +4,9 @@ WORKING_DIR=/home/travis/build/broadinstitute
 docker pull ga4gh/htsget-ref:1.1.0
 docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://127.0.0.1:3000 \
       -v $WORKING_DIR/gatk/src/test/resources/org/broadinstitute/hellbender/engine:/data \
+      -v $WORKING_DIR/scripts/htsgetScripts:/data/scripts \
       ga4gh/htsget-ref:1.1.0 \
-      ./htsref -config /data/htsget_config.json
+      ./htsref -config /data/scripts/htsget_config.json
 docker container ls -a
 
 curl http://localhost:3000
