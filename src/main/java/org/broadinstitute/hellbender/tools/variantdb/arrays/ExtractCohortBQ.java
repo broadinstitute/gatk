0d37846ee2510d070e5ca4226662b2c0c41740b9
Andrea Haessly <ahaessly@broadinstitute.org>
2020-09-18 12:35:02 -0400

    ah update array extract tool (#6827)
    * remove compressed data, change GT to single char

--- src/main/java/org/broadinstitute/hellbender/tools/variantdb/arrays/ExtractCohortBQ.java
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/arrays/ExtractCohortBQ.java
@@ -53,15 +53,15 @@ public class ExtractCohortBQ {
         return SortingCollection.newInstance(GenericRecord.class, sortingCollectionCodec, comparator, localSortMaxRecordsInRam);
     }
 
-    public final static Comparator<GenericRecord> COMPRESSED_PROBE_ID_COMPARATOR = new Comparator<GenericRecord>() {
-        @Override
-        public int compare( GenericRecord o1, GenericRecord o2 ) {
-            final long firstProbeId = new BasicArrayData((Long) o1.get(SchemaUtils.BASIC_ARRAY_DATA_FIELD_NAME)).probeId;
-            final long secondProbeId = new BasicArrayData((Long) o2.get(SchemaUtils.BASIC_ARRAY_DATA_FIELD_NAME)).probeId;
-
-            return Long.compare(firstProbeId, secondProbeId);
-        }
-    };
+//    public final static Comparator<GenericRecord> COMPRESSED_PROBE_ID_COMPARATOR = new Comparator<GenericRecord>() {
+//        @Override
+//        public int compare( GenericRecord o1, GenericRecord o2 ) {
+//            final long firstProbeId = new BasicArrayData((Long) o1.get(SchemaUtils.BASIC_ARRAY_DATA_FIELD_NAME)).probeId;
+//            final long secondProbeId = new BasicArrayData((Long) o2.get(SchemaUtils.BASIC_ARRAY_DATA_FIELD_NAME)).probeId;
+//
+//            return Long.compare(firstProbeId, secondProbeId);
+//        }
+//    };
 
     public final static Comparator<GenericRecord> UNCOMPRESSED_PROBE_ID_COMPARATOR = new Comparator<GenericRecord>() {
         @Override
