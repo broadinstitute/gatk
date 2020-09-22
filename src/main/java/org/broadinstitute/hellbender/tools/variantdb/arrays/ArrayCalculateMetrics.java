004eacf1ebda644bbf8a717494cad7f04d41b755
meganshand <mshand@broadinstitute.org>
2020-09-22 14:28:15 -0400

    updating ArrayCalculateMetrics for new genotype counts table (#6843)

--- src/main/java/org/broadinstitute/hellbender/tools/variantdb/arrays/ArrayCalculateMetrics.java
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/arrays/ArrayCalculateMetrics.java
@@ -44,7 +44,7 @@ public class ArrayCalculateMetrics extends GATKTool {
 
     public enum HeaderFieldEnum {
         probe_id,
-        hwe_pval,
+        excess_het,
         call_rate,
         invariant
     }
