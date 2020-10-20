cbf8fe2a7ab44271b9166cb9b3dd6dc34b6af159
meganshand <mshand@broadinstitute.org>
2020-10-20 13:59:41 -0400

    Moving and testing ingest scripts from variantstore (#6881)
    * Copying files over from variantstore
    
    * some changes
    
    * forgot to turn the test back on
    
    * fixing test
    
    * addressing comments

--- scripts/variantstore_cromwell_tests/run_variantstore_wdl.sh
+++ scripts/variantstore_cromwell_tests/run_variantstore_wdl.sh
@@ -25,15 +25,23 @@ else
 fi
 echo "Docker build done =========="
 echo "Putting the newly built docker image into the json parameters"
-cd $WORKING_DIR/gatk/scripts/
-sed -r "s/__GATK_DOCKER__/broadinstitute\/gatk\:$HASH_TO_USE/g" variantstore_cromwell_tests/import_array_manifest_test.json >$WORKING_DIR/import_array_manifest_test_tmp.json
+CROMWELL_TEST_DIR="${WORKING_DIR}/gatk/scripts/variantstore_cromwell_tests"
+sed -r "s/__GATK_DOCKER__/broadinstitute\/gatk\:$HASH_TO_USE/g" $CROMWELL_TEST_DIR/import_array_manifest_test.json >$WORKING_DIR/import_array_manifest_test_tmp.json
+sed -r "s/__GATK_DOCKER__/broadinstitute\/gatk\:$HASH_TO_USE/g" $CROMWELL_TEST_DIR/import_arrays_test.json >$WORKING_DIR/import_arrays_test_tmp.json
 sed -r "s/__TABLE_NAME__/$UUID/g" $WORKING_DIR/import_array_manifest_test_tmp.json > $WORKING_DIR/import_array_manifest_test_mod.json
-echo "JSON FILE (modified) ======="
+sed -r "s/__UUID__/$UUID/g" $WORKING_DIR/import_arrays_test_tmp.json > $WORKING_DIR/import_arrays_test_mod.json
+echo "MANIFEST JSON FILE (modified) ======="
 cat $WORKING_DIR/import_array_manifest_test_mod.json
+echo "INGEST JSON FILE (modified) ======="
+cat $WORKING_DIR/import_arrays_test_mod.json
 
-sed -r "s|__SERVICE_ACCOUNT__|$GOOGLE_APPLICATION_CREDENTIALS|g" variantstore_cromwell_tests/local-with-gcs.conf >$WORKING_DIR/set_up.conf
+sed -r "s|__SERVICE_ACCOUNT__|$GOOGLE_APPLICATION_CREDENTIALS|g" $CROMWELL_TEST_DIR/local-with-gcs.conf >$WORKING_DIR/set_up.conf
 echo "Updated local_backend.conf with service account"
 
 echo "Running ImportArrayManifest WDL through cromwell"
 ln -fs $WORKING_DIR/gatk/scripts/variantstore_wdl/ImportArrayManifest.wdl
 sudo java -Dconfig.file=$WORKING_DIR/set_up.conf -jar $CROMWELL_JAR run $WORKING_DIR/gatk/scripts/variantstore_wdl/ImportArrayManifest.wdl -i $WORKING_DIR/import_array_manifest_test_mod.json -m $WORKING_DIR/test_import_manifest_wdl.metadata
+
+echo "Running ImportArrays WDL through cromwell"
+ln -fs $WORKING_DIR/gatk/scripts/variantstore_wdl/ImportArrays.wdl
+sudo java -Dconfig.file=$WORKING_DIR/set_up.conf -jar $CROMWELL_JAR run $WORKING_DIR/gatk/scripts/variantstore_wdl/ImportArrays.wdl -i $WORKING_DIR/import_arrays_test_mod.json
