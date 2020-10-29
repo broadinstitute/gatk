4e9adce1575f78106d1af31ca3ae044c8f8c5915
Andrea Haessly <ahaessly@broadinstitute.org>
2020-10-29 11:44:36 -0400

    update for genomes (#6918)
    * update for genomes
    
    * add schema files

--- src/main/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/VetTsvCreator.java
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/VetTsvCreator.java
@@ -13,8 +13,6 @@ import org.broadinstitute.hellbender.utils.tsv.SimpleXSVWriter;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public abstract class VetTsvCreator {
@@ -26,28 +24,23 @@ public abstract class VetTsvCreator {
     private static String VET_FILETYPE_PREFIX = "vet_";
 
 
-    public VetTsvCreator(String sampleName, String sampleId, String tableNumberPrefix) {
+    public VetTsvCreator(String sampleName, String sampleId, String tableNumberPrefix, final File outputDirectory, final List<String>  headers) {
         this.sampleId = sampleId;
         // If the vet directory inside it doesn't exist yet -- create it
         // if the pet & vet tsvs don't exist yet -- create them
         try {
-            final String vetOutputName = VET_FILETYPE_PREFIX + tableNumberPrefix + sampleName  + IngestConstants.FILETYPE;
-            List<String> vetHeader = ExomeVetTsvCreator.getHeaders();
-            vetWriter = new SimpleXSVWriter(Paths.get(vetOutputName), IngestConstants.SEPARATOR);
-            vetWriter.setHeaderLine(vetHeader);
+            final File vetOutputFile = new File(outputDirectory, VET_FILETYPE_PREFIX + tableNumberPrefix + sampleName  + IngestConstants.FILETYPE);
+            vetWriter = new SimpleXSVWriter(vetOutputFile.toPath(), IngestConstants.SEPARATOR);
+            vetWriter.setHeaderLine(headers);
         } catch (final IOException e) {
             throw new UserException("Could not create vet outputs", e);
         }
-
     }
 
-    public abstract List<String> createRow(long start, VariantContext variant, String sampleId);
+    public abstract List<String> createRow(long location, VariantContext variant, String sampleId);
 
     public void apply(VariantContext variant, ReadsContext readsContext, ReferenceContext referenceContext, FeatureContext featureContext) {
         int start = variant.getStart();
-        int end = variant.getEnd();
-        // check to see if this is an array
-        // else, it must be an exome or genome!
         List<String> row = createRow(
                 SchemaUtils.encodeLocation(variant.getContig(), start),
                 variant,
