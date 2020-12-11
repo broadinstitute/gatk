8c5cc5e1c9c22b06ca452b7e9d8def41127e2f2c
Ted Sharpe <tsharpe@broadinstitute.org>
2020-12-11 15:25:20 -0500

    first version custom input format

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/sv/PairedEndAndSplitReadEvidenceCollection.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/sv/PairedEndAndSplitReadEvidenceCollection.java
@@ -1,26 +1,32 @@
 package org.broadinstitute.hellbender.tools.walkers.sv;
 
 import com.google.common.annotations.VisibleForTesting;
+import htsjdk.samtools.Cigar;
 import htsjdk.samtools.CigarElement;
 import htsjdk.samtools.CigarOperator;
 import htsjdk.samtools.SAMSequenceDictionary;
+import htsjdk.samtools.util.BlockCompressedInputStream;
 import htsjdk.samtools.util.BlockCompressedOutputStream;
 import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.barclay.argparser.BetaFeature;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.hellbender.cmdline.programgroups.StructuralVariantDiscoveryProgramGroup;
 import org.broadinstitute.hellbender.engine.FeatureContext;
+import org.broadinstitute.hellbender.engine.GATKPath;
 import org.broadinstitute.hellbender.engine.ReadWalker;
 import org.broadinstitute.hellbender.engine.ReferenceContext;
 import org.broadinstitute.hellbender.engine.filters.ReadFilter;
 import org.broadinstitute.hellbender.engine.filters.ReadFilterLibrary;
 import org.broadinstitute.hellbender.exceptions.GATKException;
 import org.broadinstitute.hellbender.exceptions.UserException;
+import org.broadinstitute.hellbender.utils.Nucleotide;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
 
 import java.io.*;
 import java.util.*;
 import java.util.function.Predicate;
+
+import static htsjdk.samtools.SAMSequenceRecord.UNAVAILABLE_SEQUENCE_INDEX;
 
 /**
  * Creates discordant read pair and split read evidence files for use in the GATK-SV pipeline.
@@ -67,6 +73,10 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
     public static final String PAIRED_END_FILE_ARGUMENT_LONG_NAME = "pe-file";
     public static final String SPLIT_READ_FILE_ARGUMENT_SHORT_NAME = "SR";
     public static final String SPLIT_READ_FILE_ARGUMENT_LONG_NAME = "sr-file";
+    public static final String ALLELE_COUNT_FILE_ARGUMENT_SHORT_NAME = "AC";
+    public static final String ALLELE_COUNT_FILE_ARGUMENT_LONG_NAME = "allele-count-file";
+    public static final String ALLELE_COUNT_LOCI_FILE_ARGUMENT_SHORT_NAME = "AL";
+    public static final String ALLELE_COUNT_LOCI_FILE_ARGUMENT_LONG_NAME = "allele-count-loci-file";
     public static final String SAMPLE_NAME_ARGUMENT_LONG_NAME = "sample-name";
 
     @Argument(shortName = PAIRED_END_FILE_ARGUMENT_SHORT_NAME, fullName = PAIRED_END_FILE_ARGUMENT_LONG_NAME, doc = "Output file for paired end evidence", optional=false)
@@ -74,6 +84,28 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
 
     @Argument(shortName = SPLIT_READ_FILE_ARGUMENT_SHORT_NAME, fullName = SPLIT_READ_FILE_ARGUMENT_LONG_NAME, doc = "Output file for split read evidence", optional=false)
     public String srFile;
+
+    @Argument(shortName = ALLELE_COUNT_FILE_ARGUMENT_SHORT_NAME,
+            fullName = ALLELE_COUNT_FILE_ARGUMENT_LONG_NAME,
+            doc = "Output file for allele counts",
+            optional = true)
+    public String alleleCountFilename;
+
+    @Argument(shortName = ALLELE_COUNT_LOCI_FILE_ARGUMENT_SHORT_NAME,
+            fullName = ALLELE_COUNT_LOCI_FILE_ARGUMENT_LONG_NAME,
+            doc = "Output file for allele counts",
+            optional = true)
+    public String locusFilename;
+
+    @Argument(fullName = "allele-count-min-mapq",
+            doc = "minimum mapping quality for read to be allele-counted",
+            optional = true)
+    public int minMapQ = 30;
+
+    @Argument(fullName = "allele-count-min-baseq",
+            doc = "minimum base call quality for SNP to be allele-counted",
+            optional = true)
+    public int minQ = 20;
 
     @Argument(fullName = SAMPLE_NAME_ARGUMENT_LONG_NAME, doc = "Sample name")
     String sampleName = null;
@@ -85,8 +117,9 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
     int currentDiscordantPosition = -1;
     String currentChrom = null;
 
-    private OutputStreamWriter peWriter;
-    private OutputStreamWriter srWriter;
+    private BufferedWriter peWriter;
+    private BufferedWriter srWriter;
+    private AlleleCounter alleleCounter;
 
     private SAMSequenceDictionary sequenceDictionary;
 
@@ -100,33 +133,20 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
     public void onTraversalStart() {
         super.onTraversalStart();
 
-        try {
-            if (peFile.endsWith(".gz")) {
-                peWriter = new OutputStreamWriter(new BlockCompressedOutputStream(peFile, 6));
-            } else {
-                peWriter = new OutputStreamWriter(new FileOutputStream(new File(peFile)));
-            }
-        } catch (FileNotFoundException e) {
-            throw new UserException("Could not open " + peFile);
-        }
-
-        try {
-            if (srFile.endsWith(".gz")) {
-                srWriter = new OutputStreamWriter(new BlockCompressedOutputStream(srFile, 6));
-            } else {
-                srWriter = new OutputStreamWriter(new FileOutputStream(new File(srFile)));
-            }
-        } catch (FileNotFoundException e) {
-            throw new UserException("Could not open " + srFile);
-        }
+        peWriter = createOutputFile(peFile);
+        srWriter = createOutputFile(srFile);
 
         sequenceDictionary = getBestAvailableSequenceDictionary();
+        if ( locusFilename != null && alleleCountFilename != null ) {
+            alleleCounter =
+                    new AlleleCounter(sequenceDictionary, locusFilename, alleleCountFilename, minMapQ, minQ);
+        }
     }
 
     @Override
     public List<ReadFilter> getDefaultReadFilters() {
         final List<ReadFilter> readFilters = new ArrayList<>(super.getDefaultReadFilters());
-        readFilters.add(ReadFilterLibrary.MATE_UNMAPPED_AND_UNMAPPED_READ_FILTER);
+        readFilters.add(ReadFilterLibrary.MAPPED);
         readFilters.add(ReadFilterLibrary.NOT_DUPLICATE);
         readFilters.add(ReadFilterLibrary.NOT_SECONDARY_ALIGNMENT);
         readFilters.add(ReadFilterLibrary.NOT_SUPPLEMENTARY_ALIGNMENT);
@@ -135,13 +155,37 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
 
     @Override
     public void apply(final GATKRead read, final ReferenceContext referenceContext, final FeatureContext featureContext) {
-        if (isSoftClipped(read)) {
-            countSplitRead(read, splitPosBuffer, srWriter);
-        }
+        if ( read.isPaired() && !read.mateIsUnmapped() && !read.isProperlyPaired() ) {
+            if ( isSoftClipped(read) ) {
+                countSplitRead(read, splitPosBuffer, srWriter);
+            }
 
-        if (! read.isProperlyPaired()) {
             reportDiscordantReadPair(read);
         }
+
+        if ( alleleCounter != null ) {
+            alleleCounter.apply(read);
+        }
+    }
+
+    private static BufferedWriter createOutputFile( final String fileName ) {
+        final GATKPath path = new GATKPath(fileName);
+        if ( fileName.endsWith(".gz") || fileName.endsWith(".bgz") ) {
+            return new BufferedWriter(
+                    new OutputStreamWriter(
+                            new BlockCompressedOutputStream(path.getOutputStream(), path.toPath(), 6)));
+        }
+        return new BufferedWriter(new OutputStreamWriter(path.getOutputStream()));
+    }
+
+    private static BufferedReader openInputFile( final String fileName ) {
+        final GATKPath path = new GATKPath(fileName);
+        if ( fileName.endsWith(".gz") || fileName.endsWith(".bgz") ) {
+            return new BufferedReader(
+                    new InputStreamReader(
+                            new BlockCompressedInputStream(path.getInputStream())));
+        }
+        return new BufferedReader(new InputStreamReader(path.getInputStream()));
     }
 
     private void reportDiscordantReadPair(final GATKRead read) {
@@ -205,7 +249,7 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
      * srWriter if necessary.
      */
     @VisibleForTesting
-    public void countSplitRead(final GATKRead read, final PriorityQueue<SplitPos> splitCounts, final OutputStreamWriter srWriter) {
+    public void countSplitRead(final GATKRead read, final PriorityQueue<SplitPos> splitCounts, final BufferedWriter srWriter) {
         final SplitPos splitPosition = getSplitPosition(read);
         final int readStart = read.getStart();
         if (splitPosition.direction == POSITION.MIDDLE) {
@@ -223,7 +267,7 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
         splitCounts.add(splitPosition);
     }
 
-    private void flushSplitCounts(final Predicate<SplitPos> flushablePosition, final OutputStreamWriter srWriter, final PriorityQueue<SplitPos> splitCounts) {
+    private void flushSplitCounts(final Predicate<SplitPos> flushablePosition, final BufferedWriter srWriter, final PriorityQueue<SplitPos> splitCounts) {
 
         while (splitCounts.size() > 0 && flushablePosition.test(splitCounts.peek())) {
             SplitPos pos = splitCounts.poll();
@@ -262,6 +306,9 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
     public Object onTraversalSuccess() {
         flushSplitCounts(splitPos -> true, srWriter, splitPosBuffer);
         flushDiscordantReadPairs();
+        if ( alleleCounter != null ) {
+            alleleCounter.close();
+        }
         return null;
     }
 
@@ -452,6 +499,242 @@ public class PairedEndAndSplitReadEvidenceCollection extends ReadWalker {
         @Override
         public int compare(final DiscordantRead o1, final DiscordantRead o2) {
             return internalComparator.compare(o1, o2);
+        }
+    }
+
+    @VisibleForTesting
+    final static class AlleleCounter {
+        private final SAMSequenceDictionary dict;
+        private final String locusFilename;
+        private final String outputFilename;
+        private final int minMapQ;
+        private final int minQ;
+        private final BufferedReader reader;
+        private final BufferedWriter writer;
+        private final Deque<LocusCounts> locusCountsQueue;
+
+        public AlleleCounter( final SAMSequenceDictionary dict,
+                              final String locusFilename,
+                              final String outputFilename,
+                              final int minMapQ,
+                              final int minQ ) {
+            this.dict = dict;
+            this.locusFilename = locusFilename;
+            this.outputFilename = outputFilename;
+            this.minMapQ = minMapQ;
+            this.minQ = minQ;
+            reader = openInputFile(locusFilename);
+            writer = createOutputFile(outputFilename);
+            locusCountsQueue = new ArrayDeque<>(100);
+            readLocus();
+        }
+
+        public void apply( final GATKRead read ) {
+            if ( read.getMappingQuality() < minMapQ || locusCountsQueue.isEmpty() ) {
+                return;
+            }
+            // clean queue of LocusCounts that preceed the current read
+            final int contigId = dict.getSequenceIndex(read.getContig());
+            final Locus readStart = new Locus(contigId, read.getStart());
+            while ( locusCountsQueue.getFirst().compareTo(readStart) < 0 ) {
+                writeLocusCounts(locusCountsQueue.removeFirst());
+                if ( locusCountsQueue.isEmpty() ) {
+                    if ( !readLocus() ) {
+                        return;
+                    }
+                }
+            }
+            // make sure that the last LocusCount in the queue happens after the current read
+            //  if such a LocusCount is available
+            final Locus readEnd = new Locus(contigId, read.getEnd());
+            while ( readEnd.compareTo(locusCountsQueue.getLast()) > 0 ) {
+                if ( !readLocus() ) {
+                    break;
+                }
+            }
+            walkReadMatches(read, contigId);
+        }
+
+        public void walkReadMatches( final GATKRead read, final int contigId ) {
+            Locus opStart = new Locus(contigId, read.getStart());
+            int readIdx = 0;
+            final byte[] calls = read.getBasesNoCopy();
+            final byte[] quals = read.getBaseQualitiesNoCopy();
+            for ( final CigarElement cigEle : read.getCigar().getCigarElements() ) {
+                final int eleLen = cigEle.getLength();
+                final CigarOperator cigOp = cigEle.getOperator();
+                if ( cigOp.isAlignment() ) {
+                    final Locus opEnd =
+                            new Locus(opStart.getContigId(), opStart.getPosition() + eleLen);
+                    for ( final LocusCounts locusCounts : locusCountsQueue ) {
+                        if ( locusCounts.compareTo(opEnd) >= 0 ) {
+                            break;
+                        }
+                        if ( opStart.compareTo(locusCounts) <= 0 ) {
+                            final int callIdx =
+                                    readIdx + locusCounts.getPosition() - opStart.getPosition();
+                            if ( quals[callIdx] < minQ ) {
+                                continue;
+                            }
+                            final Nucleotide call = Nucleotide.decode(calls[callIdx]);
+                            if ( call.isStandard() ) {
+                                locusCounts.observe(call.ordinal());
+                            }
+                        }
+                    }
+                }
+                if ( cigOp.consumesReadBases() ) {
+                    readIdx += cigEle.getLength();
+                }
+                if ( cigOp.consumesReferenceBases() ) {
+                    opStart = new Locus(opStart.getContigId(), opStart.getPosition() + eleLen);
+                }
+            }
+        }
+
+        public void close() {
+            while ( !locusCountsQueue.isEmpty() ) {
+                writeLocusCounts(locusCountsQueue.removeFirst());
+            }
+            try {
+                writer.close();
+            } catch ( final IOException ioe ) {
+                throw new UserException("can't close allele count output file " + outputFilename, ioe);
+            }
+            try {
+                reader.close();
+            } catch ( final IOException ioe ) {
+                throw new UserException("can't close allele-count locus input file " + locusFilename);
+            }
+        }
+
+        private boolean readLocus() {
+            final String line;
+            try {
+                line = reader.readLine();
+            } catch ( final IOException ioe ) {
+                throw new UserException("can't read allele-count locus input file " + locusFilename);
+            }
+            if ( line == null ) {
+                return false;
+            }
+
+            final int tabIdx1 = line.indexOf('\t');
+            if ( tabIdx1 == -1 ) {
+                return badLine(line);
+            }
+            final int tabIdx2 = line.indexOf('\t', tabIdx1 + 1);
+            if ( tabIdx2 == -1 ) {
+                return badLine(line);
+            }
+            final int contigId = dict.getSequenceIndex(line.substring(0, tabIdx1));
+            if ( contigId == UNAVAILABLE_SEQUENCE_INDEX ) {
+                return badLine(line);
+            }
+
+            final int position;
+            try {
+                position = Integer.parseInt(line.substring(tabIdx1 + 1, tabIdx2));
+            } catch ( final NumberFormatException nfe ) {
+                return badLine(line);
+            }
+
+            final Nucleotide refCall;
+            try {
+                refCall = Nucleotide.decode(line.substring(tabIdx2 + 1));
+            } catch ( final IllegalArgumentException iae ) {
+                return badLine(line);
+            }
+            if ( !refCall.isStandard() ) {
+                return badLine(line);
+            }
+            locusCountsQueue.add(new LocusCounts(contigId, position, refCall.ordinal()));
+            return true;
+        }
+
+        private boolean badLine( final String line ) {
+            throw new UserException("allele-count locus input file " + locusFilename +
+                    " contains uninterpretable line " + line);
+        }
+
+        private void writeLocusCounts( final LocusCounts locusCounts ) {
+            try {
+                writer.write(dict.getSequence(locusCounts.getContigId()).getContig());
+                writer.write('\t');
+                writer.write(Integer.toString(locusCounts.getPosition()));
+                writer.write('\t');
+                final int refIdx = locusCounts.getRefIdx();
+                writer.write(Integer.toString(locusCounts.getCount(refIdx)));
+                writer.write('\t');
+                final int altIdx = locusCounts.getAltIdx();
+                writer.write(Integer.toString(
+                            altIdx == LocusCounts.NO_ALT_IDX ? 0 : locusCounts.getCount(altIdx)));
+                writer.write('\t');
+                writer.write("ACGT".charAt(refIdx));
+                writer.write('\t');
+                writer.write(altIdx == LocusCounts.NO_ALT_IDX ? 'N' : "ACGT".charAt(altIdx));
+                writer.newLine();
+            } catch ( final IOException ioe ) {
+                throw new UserException("can't write to allele count output file " + outputFilename, ioe);
+            }
+        }
+    }
+
+    @VisibleForTesting
+    static class Locus implements Comparable<Locus> {
+        final int contigId;
+        final int position;
+
+        public Locus( final int contigId, final int position ) {
+            this.contigId = contigId;
+            this.position = position;
+        }
+
+        public int getContigId() { return contigId; }
+        public int getPosition() { return position; }
+
+        @Override public int compareTo( final Locus locus ) {
+            int result = Integer.compare(contigId, locus.contigId);
+            if ( result == 0 ) {
+                result = Integer.compare(position, locus.position);
+            }
+            return result;
+        }
+    }
+
+    @VisibleForTesting
+    final static class LocusCounts extends Locus {
+        public static final int NO_ALT_IDX = -1;
+        private final int[] counts;
+        private final int refIdx;
+
+        public LocusCounts( final int contigId, final int position, final int refIdx ) {
+            super(contigId, position);
+            this.counts = new int[4];
+            this.refIdx = refIdx;
+        }
+
+        public void observe( final int idx ) { counts[idx] += 1; }
+
+        public int getRefIdx() { return refIdx; }
+
+        public int getAltIdx() {
+            int altIdx = NO_ALT_IDX;
+            int altCount = 0;
+            for ( int idx = 0; idx != 4; ++idx ) {
+                if ( idx != refIdx ) {
+                    final int count = counts[idx];
+                    if ( count > altCount ) {
+                        altIdx = idx;
+                        altCount = count;
+                    }
+                }
+            }
+            return altIdx;
+        }
+
+        public int getCount( final int idx ) {
+            return counts[idx];
         }
     }
 }
