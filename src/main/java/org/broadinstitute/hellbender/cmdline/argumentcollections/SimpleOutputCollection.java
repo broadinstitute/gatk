51023d9bdeaf37865fb8dbdeb452468dd203a840
Yossi Farjoun <farjoun@gmail.com>
2021-02-07 22:48:13 -0500

    use standard output value

--- src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/SimpleOutputCollection.java
+++ src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/SimpleOutputCollection.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.hellbender.cmdline.argumentcollections;
 
 import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.engine.GATKPath;
 
 import java.io.IOException;
@@ -12,29 +13,31 @@ import java.nio.file.StandardOpenOption;
  * To Use this class add a @ArgumentCollection variable like so:
  *
  * <code>
- * @ArgumentCollection
- * public final out = new SimpleOutput();
- * </code>
  *
+ * @ArgumentCollection public final out = new SimpleOutput();
+ * </code>
+ * <p>
  * and in the method <code>onTraversalSuccess</code> instead of just outputting to the terminal, also call
  *
  * <code>
- *     out.writeToOutput(value)
+ * out.writeToOutput(value)
  * </code>
- *
+ * <p>
  * where <code>value</code> is the object to be written to the file.
- *
+ * <p>
  * The code will only attempt to write to the output if -o (or --output) has been assigned on the commandline.
- *
  */
 public class SimpleOutputCollection implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Argument(shortName = "o", doc = "Optional output file for the output value.", optional = true)
+    @Argument(fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME,
+            shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
+            doc = "Optional file for the output value.",
+            optional = true)
     public GATKPath output = null;
 
 
-    public void writeToOutput(Object value)  {
+    public void writeToOutput(Object value) {
         if (output != null) {
             try {
                 Files.write(output.toPath(), value.toString().getBytes());
