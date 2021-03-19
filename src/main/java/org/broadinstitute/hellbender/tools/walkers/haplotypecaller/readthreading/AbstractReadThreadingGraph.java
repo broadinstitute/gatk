e83ba49a757654ecf6e8cfd090e152b1de2c353c
Andrea Haessly <ahaessly@broadinstitute.org>
2021-03-19 14:09:25 -0400

    add test, update from PR

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/readthreading/AbstractReadThreadingGraph.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/readthreading/AbstractReadThreadingGraph.java
@@ -594,6 +594,7 @@ public abstract class AbstractReadThreadingGraph extends BaseGraph<MultiDeBruijn
 
         final List<CigarElement> elements = danglingHeadMergeResult.cigar.getCigarElements();
         final CigarElement firstElement = elements.get(0);
+        Utils.validateArg(firstElement.getOperator() == CigarOperator.M, "The first Cigar element must be an M");
 
         final int indexesToMerge = bestPrefixMatchLegacy(danglingHeadMergeResult.referencePathString, danglingHeadMergeResult.danglingPathString, firstElement.getLength());
         if (indexesToMerge <= 0) {
@@ -601,7 +602,7 @@ public abstract class AbstractReadThreadingGraph extends BaseGraph<MultiDeBruijn
         }
 
         // we can't push back the reference path
-        if ( indexesToMerge >= danglingHeadMergeResult.referencePath.size() - 1 ) {
+        if (indexesToMerge >= danglingHeadMergeResult.referencePath.size() - 1) {
             return 0;
         }
 
@@ -611,7 +612,7 @@ public abstract class AbstractReadThreadingGraph extends BaseGraph<MultiDeBruijn
             return 0;
         }
 
-        addEdge(danglingHeadMergeResult.referencePath.get(indexesToMerge), danglingHeadMergeResult.danglingPath.get(indexesToMerge), ((MyEdgeFactory)getEdgeFactory()).createEdge(false, 1));
+        addEdge(danglingHeadMergeResult.referencePath.get(indexesToMerge + 1), danglingHeadMergeResult.danglingPath.get(indexesToMerge), ((MyEdgeFactory) getEdgeFactory()).createEdge(false, 1));
 
         return 1;
     }
