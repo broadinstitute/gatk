c627f8a682af5c90e453db01858dd3cab3d8936d
ndiamant <ndiamant@broadinstitute.org>
2021-03-30 19:55:28 -0400

    added examples to comments

--- ml4h/ml4ht_integration/tensor_map.py
+++ ml4h/ml4ht_integration/tensor_map.py
@@ -14,6 +14,48 @@ class TensorMapSampleGetter:
             tensor_maps_out: List[TensorMap],
             augment: bool = False,
     ):
+        """
+        A SampleGetter that is built from TensorMaps.
+        This allows backwards compatibility with old TensorMaps.
+
+        :param tensor_maps_in: tensor maps to use to get input data for a model
+        :param tensor_maps_out: tensor maps to use to get output data for a model
+        :param augment: whether to apply the TensorMaps' data augmentations.
+                        This should be true for training and false for testing.
+
+        Example preparing TensorMaps for use in ml4h model:
+        ```
+        import torch
+
+        from ml4h.tensormap.ecg import ecg_rest_raw
+        from ml4h.tensormap.ecg import ecg_rest_age
+        from ml4h.ml4ht_integration.tensor_map import TensorMapSampleGetter
+        from ml4h.models.model_factory import block_make_multimodal_multitask_model
+
+        from ml4ht.data.data_loader import numpy_collate_fn, SampleGetterDataset
+
+        model = block_make_multimodal_multitask_model(
+            [ecg_rest_raw],
+            [ecg_rest_age],
+            ...
+        )
+
+        sg = TensorMapSampleGetter(
+            [ecg_rest_raw],
+            [ecg_rest_age],
+            True,
+        )
+        dset = SampleGetterDataset(
+            [1_path_to_hd5.hd5, 2_path_to_hd5.hd5, ...],
+            sg,
+        )
+        data_loader = torch.utils.data.DataLoader(
+            dset, batch_size=128, collate_fn=numpy_collate_fn,
+        )
+
+        model.fit(iter(data_loader))  # train for one epoch
+        ```
+        """
         self.tensor_maps_in = tensor_maps_in
         self.tensor_maps_out = tensor_maps_out
         self.augment = augment
@@ -49,6 +91,35 @@ def tensor_map_from_data_description(
 ) -> TensorMap:
     """
     Allows a DataDescription to be used in the model factory
+    by converting a DataDescription into a TensorMap
+
+    :param data_description: Will be converted into a TensorMap
+    :param interpretation: How the model factory will interpret
+                           the data from the data_description
+    :param shape: The shape of the data from the data_description
+    :param tensor_map_kwargs: keyword arguments passed to TensorMap.__init__
+    :return: A TensorMap created from a DataDescription
+
+    Example using an ECG and a sex data description to build an ml4h model:
+    ```
+    ecg_data_description = ...
+    sex_data_description = ...
+    ecg_tmap = tensor_map_from_data_description(
+        ecg_data_description,
+        Interpretation.CONTINUOUS,
+        shape=(5000, 12),
+    )
+    sex_tmap = tensor_map_from_data_description(
+        sex_data_description,
+        Interpretation.CATEGORICAL,
+        shape=(2,),
+    )
+    model = block_make_multimodal_multitask_model(
+        tensor_maps_in=[ecg_tmap],
+        tensor_maps_out=[sex_tmap],
+        ...
+    )
+    ```
     """
     tmap = TensorMap(
         name=data_description.name,
