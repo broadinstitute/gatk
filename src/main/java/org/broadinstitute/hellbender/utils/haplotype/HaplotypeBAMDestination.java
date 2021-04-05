78e91fa67990de58d776595dded4f15667789bae
Takuto Sato <tsato@broadinstitute.org>
2021-04-05 13:33:10 -0400

    Do not add artificial haplotype read group to bamout when --bam-writer-type NO_HAPLOTYPES (#7141)
    Do not add the artificial haplotype as read group if writer-type is NO_HAPLOTYPES

--- src/main/java/org/broadinstitute/hellbender/utils/haplotype/HaplotypeBAMDestination.java
+++ src/main/java/org/broadinstitute/hellbender/utils/haplotype/HaplotypeBAMDestination.java
@@ -9,6 +9,7 @@ import org.broadinstitute.hellbender.utils.read.GATKRead;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Utility class that allows easy creation of destinations for the HaplotypeBAMWriters
@@ -16,16 +17,17 @@ import java.util.List;
  */
 public abstract class HaplotypeBAMDestination {
     private final SAMFileHeader bamOutputHeader;
-    private final String haplotypeReadGroupID;
+    private final Optional<String> haplotypeReadGroupID;
     private final static String haplotypeSampleTag = "HC";
 
     /**
      * Create a new HaplotypeBAMDestination
      *
      * @param sourceHeader SAMFileHeader used to seed the output SAMFileHeader for this destination.
-     * @param haplotypeReadGroupID read group ID used when writing haplotypes as reads
+     * @param haplotypeReadGroupID read group ID used when writing haplotypes as reads. Empty if
+     *                             {@link HaplotypeBAMWriter.WriterType} == NO_HAPLOTYPES.
      */
-    protected HaplotypeBAMDestination(SAMFileHeader sourceHeader, final String haplotypeReadGroupID) {
+    protected HaplotypeBAMDestination(SAMFileHeader sourceHeader, final Optional<String> haplotypeReadGroupID) {
         Utils.nonNull(sourceHeader, "sourceHeader cannot be null");
         Utils.nonNull(haplotypeReadGroupID, "haplotypeReadGroupID cannot be null");
         this.haplotypeReadGroupID = haplotypeReadGroupID;
@@ -35,13 +37,16 @@ public abstract class HaplotypeBAMDestination {
         bamOutputHeader.setSortOrder(SAMFileHeader.SortOrder.coordinate);
 
         final List<SAMReadGroupRecord> readGroups = new ArrayList<>();
-        readGroups.addAll(sourceHeader.getReadGroups()); // include the original read groups
+        readGroups.addAll(sourceHeader.getReadGroups()); // include the original read groups...
 
         // plus an artificial read group for the haplotypes
-        final SAMReadGroupRecord rgRec = new SAMReadGroupRecord(getHaplotypeReadGroupID());
-        rgRec.setSample(haplotypeSampleTag);
-        rgRec.setSequencingCenter("BI");
-        readGroups.add(rgRec);
+        if (haplotypeReadGroupID.isPresent()){
+            final SAMReadGroupRecord rgRec = new SAMReadGroupRecord(haplotypeReadGroupID.get());
+            rgRec.setSample(haplotypeSampleTag);
+            rgRec.setSequencingCenter("BI");
+            readGroups.add(rgRec);
+        }
+
         bamOutputHeader.setReadGroups(readGroups);
         final List<SAMProgramRecord> programRecords = new ArrayList<>(sourceHeader.getProgramRecords());
         programRecords.add(new SAMProgramRecord("HaplotypeBAMWriter"));
@@ -57,11 +62,13 @@ public abstract class HaplotypeBAMDestination {
 
     /**
      * Get the read group ID that is used by this writer when writing halpotypes as reads.
+     * If {@link HaplotypeBAMWriter.WriterType} is NO_HAPLOTYPES, then returns Optional.empty()
      *
      * @return read group ID
      */
-    public String getHaplotypeReadGroupID() { return haplotypeReadGroupID; }
-
+    public Optional<String> getHaplotypeReadGroupID() {
+        return haplotypeReadGroupID;
+    }
     /**
      * Get the sample tag that is used by this writer when writing halpotypes as reads.
      *
