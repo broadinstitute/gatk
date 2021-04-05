146d010c6601be11af7345ef22d337f2f53e7198
Morgan Taylor <marymorg@broadinstitute.org>
2021-04-05 11:33:35 -0400

    test from avro file for ExtractCohortRecord

--- src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/ExtractCohortRecordUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/ExtractCohortRecordUnitTest.java
@@ -1,122 +1,138 @@
 package org.broadinstitute.hellbender.tools.variantdb.nextgen;
 
-//import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecord;
 import org.broadinstitute.hellbender.GATKBaseTest;
 
-//import org.broadinstitute.hellbender.utils.bigquery.AvroFileReader;
+import org.broadinstitute.hellbender.utils.bigquery.AvroFileReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ExtractCohortRecordUnitTest extends GATKBaseTest {
 
-//    private GenericRecord loadGenericRecord() {
-//         // TODO: read in a real avro file, create a GenericRecord input
-//        final String testAvroFileName =
-//        final AvroFileReader avroFileReader = new AvroFileReader(testAvroFileName);
-//    }
+    private static GenericRecord getSingleRecordFromAvroFile (String inputFileName) {
+        final AvroFileReader avroFileReader = new AvroFileReader(inputFileName);
+        return avroFileReader.next();
+    }
 
-    private final Long location = Long.parseLong("21000008416376");
-    private final String sampleName = "28";
-    private final String state = "v";
-    private final String ref = "G";
-    private final String alt = "C";
-    private final String callGT = "1|1";
-    private final String callGQ = "3";
-    private final String callRGQ = "???RGQ";
-    private final String qualapprox = "19";
-    private final String asQualapprox = "19";
-    private final String callPL = "???PL";
+    // load in test data
+    private static final String testFileDir = "src/test/resources/org/broadinstitute/hellbender/tools/variantdb/nextgen/ExtractCohortRecord/";
+    private static final GenericRecord definedInputGenericRecord = getSingleRecordFromAvroFile(testFileDir + "test_input_defined.avro");
+    private static final GenericRecord nulledInputGenericRecord = getSingleRecordFromAvroFile(testFileDir + "test_input_nulls.avro");
 
-    private ExtractCohortRecord allDefinedRecord = new ExtractCohortRecord(location, sampleName,
-            state, ref, alt, callGT, callGQ, callRGQ, qualapprox, asQualapprox, callPL);
-
-    private ExtractCohortRecord someNullsRecord = new ExtractCohortRecord(location, sampleName,
-            state, ref, alt, null, null, null, null, null, null);
+    // generate records to test
+    private ExtractCohortRecord allDefinedRecord = new ExtractCohortRecord(definedInputGenericRecord);
+    private ExtractCohortRecord someNullsRecord = new ExtractCohortRecord(nulledInputGenericRecord);
 
     @Test
     public void testGetContig() {
-        String expectedContig = "chr21";
+        String expectedContig = "chr15";
         Assert.assertEquals(allDefinedRecord.getContig(), expectedContig);
         Assert.assertEquals(someNullsRecord.getContig(), expectedContig);
     }
 
     @Test
     public void testGetStart() {
-        int expectedStart = 8416376;
-        Assert.assertEquals(allDefinedRecord.getStart(), expectedStart);
-        Assert.assertEquals(someNullsRecord.getStart(), expectedStart);
+        int expectedStartDefined = 28865305;
+        Assert.assertEquals(allDefinedRecord.getStart(), expectedStartDefined);
+
+        int expectedStartNulled = 28865344;
+        Assert.assertEquals(someNullsRecord.getStart(), expectedStartNulled);
     }
 
     @Test
     public void testGetEnd() {
-        int expectedEnd = 8416376;
-        Assert.assertEquals(allDefinedRecord.getEnd(), expectedEnd);
-        Assert.assertEquals(someNullsRecord.getEnd(), expectedEnd);
+        int expectedEndDefined = 28865305;
+        Assert.assertEquals(allDefinedRecord.getEnd(), expectedEndDefined);
+
+
+        int expectedEndNulled = 28865344;
+        Assert.assertEquals(someNullsRecord.getEnd(), expectedEndNulled);
     }
 
     @Test
     public void testGetLocation() {
-        Assert.assertEquals(allDefinedRecord.getLocation(), location);
-        Assert.assertEquals(someNullsRecord.getLocation(), location);
+        Long expectedLocationDefined = Long.parseLong("15000028865305");
+        Assert.assertEquals(allDefinedRecord.getLocation(), expectedLocationDefined);
+
+        Long expectedLocationNulled = Long.parseLong("15000028865344");
+        Assert.assertEquals(someNullsRecord.getLocation(), expectedLocationNulled);
     }
 
     @Test
     public void testGetSampleName() {
+        String sampleName = "SM-GXZVY";
         Assert.assertEquals(allDefinedRecord.getSampleName(), sampleName);
         Assert.assertEquals(someNullsRecord.getSampleName(), sampleName);
     }
 
     @Test
     public void testGetState() {
-        Assert.assertEquals(allDefinedRecord.getState(), state);
-        Assert.assertEquals(someNullsRecord.getState(), state);
+        Assert.assertEquals(allDefinedRecord.getState(), "v");
+
+        Assert.assertEquals(someNullsRecord.getState(), "3");
     }
 
     @Test
     public void testGetRefAllele() {
+        String ref = "CTTT";
         Assert.assertEquals(allDefinedRecord.getRefAllele(), ref);
-        Assert.assertEquals(someNullsRecord.getRefAllele(), ref);
+
+        Assert.assertNull(someNullsRecord.getRefAllele());
     }
 
     @Test
     public void testGetAltAllele() {
+        String alt = "C";
         Assert.assertEquals(allDefinedRecord.getAltAllele(), alt);
-        Assert.assertEquals(someNullsRecord.getAltAllele(), alt);
+
+        Assert.assertNull(someNullsRecord.getAltAllele());
     }
 
     @Test
     public void testGetCallGT() {
+        String callGT = "1/1";
         Assert.assertEquals(allDefinedRecord.getCallGT(), callGT);
+
         Assert.assertNull(someNullsRecord.getCallGT());
     }
 
     @Test
     public void testGetCallGQ() {
+        String callGQ = "3";
         Assert.assertEquals(allDefinedRecord.getCallGQ(), callGQ);
+
         Assert.assertNull(someNullsRecord.getCallGQ());
     }
 
     @Test
     public void testGetCallRGQ() {
+        String callRGQ = "38";
         Assert.assertEquals(allDefinedRecord.getCallRGQ(), callRGQ);
+
         Assert.assertNull(someNullsRecord.getCallRGQ());
     }
 
     @Test
     public void testGetQUALApprox() {
+        String qualapprox = "38";
         Assert.assertEquals(allDefinedRecord.getQUALApprox(), qualapprox);
+
         Assert.assertNull(someNullsRecord.getQUALApprox());
     }
 
     @Test
     public void testGetAsQUALApprox() {
+        String asQualapprox = "38";
         Assert.assertEquals(allDefinedRecord.getAsQUALApprox(), asQualapprox);
+
         Assert.assertNull(someNullsRecord.getAsQUALApprox());
     }
 
     @Test
     public void testGetCallPL() {
+        String callPL = "38,3,0,31,3,28";
         Assert.assertEquals(allDefinedRecord.getCallPL(), callPL);
+
         Assert.assertNull(someNullsRecord.getCallPL());
     }
 }
