60bd7c525b20c500b6d3af5a7849ef25571dc46c
Morgan Taylor <marymorg@broadinstitute.org>
2021-04-09 15:53:46 -0400

    update metadata -> sample_info inputs

--- scripts/variantstore/wdl/extract/run_gvs_tieout_extract.sh
+++ scripts/variantstore/wdl/extract/run_gvs_tieout_extract.sh
@@ -6,6 +6,6 @@ python ngs_cohort_extract.py \
   --fq_temp_table_dataset ${PROJECT}.temp_tables \
   --fq_destination_dataset ${PROJECT}.${DATASET} \
   --destination_table exported_cohort_all_samples \
-  --fq_cohort_sample_names ${PROJECT}.${DATASET}.metadata \
+  --fq_cohort_sample_names ${PROJECT}.${DATASET}.sample_info \
   --query_project ${PROJECT} \
-  --fq_sample_mapping_table ${PROJECT}.${DATASET}.metadata
+  --fq_sample_mapping_table ${PROJECT}.${DATASET}.sample_info
