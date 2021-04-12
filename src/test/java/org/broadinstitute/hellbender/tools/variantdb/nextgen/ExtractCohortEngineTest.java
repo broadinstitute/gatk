cad465e6733e51037d2868a3ff718c615afe17f6
rori <6863459+RoriCremer@users.noreply.github.com>
2021-04-11 22:33:39 -0400

    cleaner path

--- src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/ExtractCohortEngineTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/ExtractCohortEngineTest.java
@@ -1,5 +1,7 @@
 package org.broadinstitute.hellbender.tools.variantdb.nextgen;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.testutils.ArgumentsBuilder;
 import org.broadinstitute.hellbender.testutils.IntegrationTestSpec;
@@ -9,18 +11,20 @@ import java.io.File;
 
 
 public class ExtractCohortEngineTest extends CommandLineProgramTest {
+  private static final Logger logger = LogManager.getLogger(ExtractCohortEngineTest.class);
 
-  private static final String cohortAvroFileName = // is it okay to just grab one?
-      "src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/AnVIL_WGS_100_exported_cohort/000000000000";
-  private static final String sampleFile =
-      "src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/AnVIL_WGS_100_exported_cohort/sample_list";
-  private static final String outputFileName =
-      "src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/outputfile.vcf";
-  private static final String expectedFileName =
-      "src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/AnVIL_WGS_100_exported_cohort/expected.vcf.gz";
+  private final String dirPath = getToolTestDataDir(); // TODO this goes to src/test/resources....
+  // which makes me wonder if I should be putting them someplace else
+  private static final String prefix =
+      "src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/";
+  private static final String cohortAvroFileName = prefix +"AnVIL_WGS_100_exported_cohort/000000000000";
+  private static final String sampleFile = prefix +"AnVIL_WGS_100_exported_cohort/sample_list";
+  private static final String outputFileName = prefix +"outputfile.vcf";
+  private static final String expectedFileName = prefix +"AnVIL_WGS_100_exported_cohort/expected.vcf.gz";
 
   @Test
   public void testFinalVCFfromAvro() throws Exception {
+    logger.info(dirPath); // TODO just to check this. but it looks like it goes to resources
 
     final ArgumentsBuilder args = new ArgumentsBuilder();
     args
@@ -31,8 +35,7 @@ public class ExtractCohortEngineTest extends CommandLineProgramTest {
         .add("O", outputFileName)
         .add("local-sort-max-records-in-ram", 10000000)
         .add("cohort-avro-file-name", cohortAvroFileName)
-        .add("sample-file", sampleFile)
-        .add("project-id", "blah blah");
+        .add("sample-file", sampleFile);
 
 
     runCommandLine(args);
