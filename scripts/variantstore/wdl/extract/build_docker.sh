4ba7b65e887b1dd599ccad7776b7abe54017ccdb
Morgan Taylor <marymorg@broadinstitute.org>
2021-04-13 12:44:36 -0400

    make build_docker script easier to use

--- scripts/variantstore/wdl/extract/build_docker.sh
+++ scripts/variantstore/wdl/extract/build_docker.sh
@@ -1,3 +1,9 @@
+if [ $# -lt 1 ]; then
+    echo "USAGE: ./build_docker.sh [DOCKER_TAG_STRING]"
+    echo " e.g.: ./build_docker.sh mybranch_2021_04_03"
+    exit 1
+fi
+
 INFO=$1
 GCR_TAG="us.gcr.io/broad-dsde-methods/variantstore:${INFO}"
 
@@ -5,4 +11,4 @@ docker build . -t broad-dsde-methods/variantstore:${INFO}
 docker tag broad-dsde-methods/variantstore:${INFO} ${GCR_TAG}
 docker push ${GCR_TAG}
 
-echo "docker pushed to ${GCR_TAG}"
+echo "docker image pushed to \"${GCR_TAG}\""
