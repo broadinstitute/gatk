3ccd7927957421585f2435dd43d0e59d014f332b
Morgan Taylor <marymorg@broadinstitute.org>
2021-04-15 13:12:34 -0400

    add parens to feature_extract.sql expression

--- src/main/resources/org/broadinstitute/hellbender/tools/variantdb/nextgen/feature_extract.sql
+++ src/main/resources/org/broadinstitute/hellbender/tools/variantdb/nextgen/feature_extract.sql
@@ -1,4 +1,5 @@
 CREATE TEMPORARY FUNCTION freq_table(arr ANY TYPE) AS (
+( -- need these extra parentheses to use this query as an expression
   SELECT ARRAY_AGG(entry)
   FROM (
       SELECT STRUCT(x as value, COUNT(1) as freq) as entry
@@ -6,6 +7,7 @@ CREATE TEMPORARY FUNCTION freq_table(arr ANY TYPE) AS (
       GROUP BY x
       ORDER BY x
   )
+)
 );
 
 WITH
