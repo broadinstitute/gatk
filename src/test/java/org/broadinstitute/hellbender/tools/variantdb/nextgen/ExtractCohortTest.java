3c0e6ae0e3383892845424c18cff9ace56517c7a
rori <6863459+RoriCremer@users.noreply.github.com>
2021-04-21 12:46:35 -0400

    rework the output file so it wont be stored permanently after the test has run

--- src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/ExtractCohortTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/ExtractCohortTest.java
@@ -12,15 +12,18 @@ class ExtractCohortTest extends CommandLineProgramTest {
   private final String prefix = getToolTestDataDir();
   private final String cohortAvroFileName = prefix +"chr20_subset_3_samples.avro";
   private final String sampleFile = prefix +"sample_list";
-  private final String outputFileName = prefix +"output.vcf";
 
   @Test
   public void testFinalVCFfromAvro() throws Exception {
-    // To create the expected output file--create a temp table in BQ with the folowing query
-    // and then export it through the GUI as an avro file into GCS.
+    // To create the expectedVCF file (of expected output) --create a temp table in BQ with the following query
+    // and then export it through the BQ GUI as an avro file into GCS.
     // SELECT * FROM `spec-ops-aou.anvil_100_for_testing.exported_cohort_all_samples`
     // where location < 20000000200000 and location >= 20000000100000
     // and (sample_name="HG00405" or sample_name="HG00418" or sample_name="HG00408")
+    final File expectedVCF = getTestFile("expected.vcf");
+
+    // create a temporary file (that will get cleaned up after the test has run) to hold the output data in
+    final File outputVCF = createTempFile("output", "vcf");
 
     final ArgumentsBuilder args = new ArgumentsBuilder();
     args
@@ -28,16 +31,12 @@ class ExtractCohortTest extends CommandLineProgramTest {
         .add("ref-version", 38)
         .add("query-mode", "LOCAL_SORT")
         .add("R", hg38Reference)
-        .add("O", outputFileName)
+        .add("O", outputVCF.getAbsolutePath())
         .add("local-sort-max-records-in-ram", 10000000)
         .add("cohort-avro-file-name", cohortAvroFileName)
         .add("sample-file", sampleFile);
 
     runCommandLine(args);
-
-    final File expectedVCF = getTestFile("expected.vcf");
-    final File outputVCF = getTestFile("output.vcf");
-
     IntegrationTestSpec.assertEqualTextFiles(outputVCF, expectedVCF);
   }
 }
