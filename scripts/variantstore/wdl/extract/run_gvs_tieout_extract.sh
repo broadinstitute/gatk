fd5479699aa775c663f84478750d8df60550be95
Kristian Cibulskis <kcibul@broadinstitute.org>
2021-06-04 14:59:43 -0400

    baseline

--- scripts/variantstore/wdl/extract/run_gvs_tieout_extract.sh
+++ scripts/variantstore/wdl/extract/run_gvs_tieout_extract.sh
@@ -5,7 +5,7 @@ python create_cohort_extract_data_table.py \
   --fq_petvet_dataset ${PROJECT}.${DATASET} \
   --fq_temp_table_dataset ${PROJECT}.temp_tables \
   --fq_destination_dataset ${PROJECT}.${DATASET} \
-  --destination_cohort_table_prefix serial_inserts \
+  --destination_cohort_table_prefix baseline_extract \
   --fq_cohort_sample_names ${PROJECT}.${DATASET}.sample_info \
   --query_project ${PROJECT} \
   --fq_sample_mapping_table ${PROJECT}.${DATASET}.sample_info
