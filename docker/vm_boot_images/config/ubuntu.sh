cb47ae56791e96d4c9edf607682cbd3399f87e20
Samuel Friedman <lucidtronix@gmail.com>
2021-06-11 09:46:46 -0400

    update tf

--- docker/vm_boot_images/config/ubuntu.sh
+++ docker/vm_boot_images/config/ubuntu.sh
@@ -3,4 +3,4 @@
 # Other necessities
 apt-get update
 echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | debconf-set-selections
-apt-get install -y wget unzip curl python-pydot python-pydot-ng graphviz ttf-mscorefonts-installer
+apt-get install -y wget unzip curl python-pydot python-pydot-ng graphviz ttf-mscorefonts-installer git
