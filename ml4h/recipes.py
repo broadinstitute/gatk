fbce40fd109c45e4130eb086109d8e3f246336e2
Samuel Friedman <lucidtronix@gmail.com>
2021-06-24 16:51:40 -0400

    start

--- ml4h/recipes.py
+++ ml4h/recipes.py
@@ -149,7 +149,7 @@ def train_multimodal_multitask(args):
     )
 
     out_path = os.path.join(args.output_folder, args.id + '/')
-    test_paths = []
+    test_paths = None
     test_data, test_labels = big_batch_from_minibatch_generator(generate_test, args.test_steps)
     return _predict_and_evaluate(
         model, test_data, test_labels, args.tensor_maps_in, args.tensor_maps_out, args.tensor_maps_protected,
@@ -171,7 +171,7 @@ def train_block(args):
         decoders[tm].save(f'{args.output_folder}{args.id}/decoder_{tm.name}.h5')
     if merger:
         merger.save(f'{args.output_folder}{args.id}/merger.h5')
-    test_paths = []
+    test_paths = None
     test_data, test_labels = big_batch_from_minibatch_generator(generate_test, args.test_steps)
     performance_metrics = _predict_and_evaluate(
         model, test_data, test_labels, args.tensor_maps_in, args.tensor_maps_out, args.tensor_maps_protected,
