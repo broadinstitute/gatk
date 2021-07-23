92d4c8367dcb4dcfbd24c762f252a953c2e32a36
Samuel Friedman <lucidtronix@gmail.com>
2021-07-23 10:10:44 -0400

    fix it

--- ml4h/tensormap/ukb/ecg.py
+++ ml4h/tensormap/ukb/ecg.py
@@ -406,7 +406,7 @@ ecg_rest_raw_100 = TensorMap(
 )
 ecg_rest_raw_8s = TensorMap(
     'ecg_rest_raw_8s', Interpretation.CONTINUOUS, shape=(4096, 12), path_prefix='ukb_ecg_rest', tensor_from_file=_make_ecg_rest(),
-    channel_map=ECG_REST_LEADS, normalization=Standardize(mean=0, std=100),
+    channel_map=ECG_REST_LEADS, normalization=Standardize(mean=0, std=10),
 )
 ecg_rest_raw_10 = TensorMap(
     'ecg_rest_raw_10', Interpretation.CONTINUOUS, shape=(5000, 12), path_prefix='ukb_ecg_rest', tensor_from_file=_make_ecg_rest(),
