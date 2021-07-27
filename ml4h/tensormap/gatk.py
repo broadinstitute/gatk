67fbaf5fb8e052e7c317d7b807074d9eb34ee7d7
Samuel Friedman <lucidtronix@gmail.com>
2021-07-27 13:56:14 -0400

    start

--- ml4h/tensormap/gatk.py
+++ ml4h/tensormap/gatk.py
@@ -4,12 +4,14 @@ from typing import Dict
 
 from ml4h.TensorMap import TensorMap, Interpretation
 
+DNA_SYMBOLS = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+VARIANT_LABELS = {'NOT_SNP': 0, 'NOT_INDEL': 1, 'SNP': 2, 'INDEL': 3}
 
 def reference_tensor_from_hd5(tm: TensorMap, hd5: h5py.File, dependents: Dict = {}) -> np.ndarray:
     return np.array(hd5['reference'])
 
 
-reference = TensorMap('reference', shape=(128,), tensor_from_file=reference_tensor_from_hd5)
+reference = TensorMap('reference', shape=(128, len(DNA_SYMBOLS)), tensor_from_file=reference_tensor_from_hd5)
 
 
 def read_tensor_from_hd5(tm: TensorMap, hd5: h5py.File, dependents: Dict = {}) -> np.ndarray:
@@ -21,18 +23,18 @@ read_tensor = TensorMap('read_tensor', shape=(128, 128, 15), tensor_from_file=re
 
 def variant_label_from_hd5(tm: TensorMap, hd5: h5py.File, dependents: Dict = {}) -> np.ndarray:
     one_hot = np.zeros(tm.shape, dtype=np.float32)
-    variant_label = str(hd5['variant_label'][()])
+    variant_str = str(hd5['variant_label'][()])
     for channel in tm.channel_map:
-        if channel == variant_label:
+        if channel == variant_str:
             one_hot[tm.channel_map[channel]] = 1.0
     if one_hot.sum() != 1:
-        raise ValueError(f'TensorMap {tm.name} missing or invalid label')
+        raise ValueError(f'TensorMap {tm.name} missing or invalid label: {variant_str}')
     return one_hot
 
-variant_label_channel_map = {'NOT_SNP': 0, 'NOT_INDEL': 1, 'SNP': 2, 'INDEL': 3}
+
 variant_label = TensorMap(
     'variant_label', Interpretation.CATEGORICAL,
-    shape=(len(variant_label_channel_map),),
+    shape=(len(VARIANT_LABELS),),
     tensor_from_file=variant_label_from_hd5,
-    channel_map=variant_label_channel_map
+    channel_map=VARIANT_LABELS
 )
