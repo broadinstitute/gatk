84e29cb42d3642902dff44dea88df0ca39b8c7f0
rori <6863459+RoriCremer@users.noreply.github.com>
2021-08-09 18:14:06 -0400

    take missing case into account

--- scripts/variantstore/wdl/extract/create_variant_annotation_table.py
+++ scripts/variantstore/wdl/extract/create_variant_annotation_table.py
@@ -127,28 +127,32 @@ def check_filtering(variant):
 
 def get_gnomad_subpop(gnomad_obj):
     row = {}
-    max_af = -1
-    max_ac = -1
-    max_an = -1
+    max_af = None
+    max_ac = None
+    max_an = None
     max_subpop = ""
     for gnomad_subpop in gnomad_ordering: # since we cycle through this in order, if there is a tie, we just ignore it because we wouldn't chose it anyway based on order
       subpop_af_key = "".join([gnomad_subpop, "Af"])
       subpop_ac_key = "".join([gnomad_subpop, "Ac"])
       subpop_an_key = "".join([gnomad_subpop, "An"])
-      subpop_af_val = gnomad_obj.get(subpop_af_key)
+      subpop_af_val = gnomad_obj.get(subpop_af_key) # note that these can be null if there is no value in the annotations. They will be null in the VAT
       subpop_ac_val = gnomad_obj.get(subpop_ac_key)
       subpop_an_val = gnomad_obj.get(subpop_an_key)
+      # here we set the subpopulation ac/an/af values
       row["_".join(["gnomad", gnomad_subpop, "an"])] = subpop_an_val
       row["_".join(["gnomad", gnomad_subpop, "ac"])] = subpop_ac_val
       row["_".join(["gnomad", gnomad_subpop, "af"])] = subpop_af_val
-      if subpop_af_val > max_af:
-        max_subpop = gnomad_subpop
-        max_an = subpop_an_val
-        max_ac = subpop_ac_val
+      if subpop_af_val != None and max_af == None: # this will set the first max_af value
         max_af = subpop_af_val
+      if subpop_af_val != None and subpop_af_val > max_af:
+        max_subpop = gnomad_subpop
+        max_ac = subpop_ac_val
+        max_an = subpop_an_val
+        max_af = subpop_af_val
+    # here we set the MAX subpopulation ac/an/af values
     row["gnomad_max_subpop"] = max_subpop
-    row["gnomad_max_an"] = max_an
     row["gnomad_max_ac"] = max_ac
+    row["gnomad_max_an"] = max_an
     row["gnomad_max_af"] = max_af
     return row
 
