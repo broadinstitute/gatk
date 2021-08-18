b841bd8a4c2f80562a11209f999c6d42c7ba1967
rori <6863459+RoriCremer@users.noreply.github.com>
2021-08-17 22:36:34 -0400

    update subpop python script

--- scripts/variantstore/wdl/extract/extract_subpop.py
+++ scripts/variantstore/wdl/extract/extract_subpop.py
@@ -1,33 +1,47 @@
 import csv
 import argparse
 
-subpopulations = [
+expected_subpopulations = [
  "afr",
  "amr",
  "eas",
- "fin",
- "nfr",
- "asj",
+ "eur",
+ "mid",
  "oth",
  "sas"
 ]
 
-def extract_subpopulation(input_path, output_path):
-  with open(input_path, newline='') as tsvin, open(output_path, 'w', newline='') as csvout:
+def extract_subpopulation(input_path):
+  seen_subpopulations = []
+  with open(input_path, newline='') as tsvin:
     tsvin = csv.reader(tsvin, delimiter='\t')
-    csvout = csv.writer(csvout, delimiter='\t')
-
+    next(tsvin) # skip the header
     for row in tsvin:
-      csvout.writerow([row[0], row[4]])
-
+      row_subpopulation = row[4]
+      if row_subpopulation in expected_subpopulations:
+        if row_subpopulation in seen_subpopulations:
+          # open the correct output file
+          output_file = open(row_subpopulation + "_subpopulation.tsv", 'a', newline='')
+          csvout = csv.writer(output_file, delimiter='\t')
+          # add sample name to the subpopulation file
+          csvout.writerow([row[0]])
+        else:
+          seen_subpopulations.append(row_subpopulation)
+          # Create a new file for this subpopulation
+          output_file = open(row_subpopulation + "_subpopulation.tsv", 'w', newline='')
+          csvout = csv.writer(output_file, delimiter='\t')
+          # add sample name to the subpopulation file
+          csvout.writerow([row[0]])
+          print(seen_subpopulations)
+      else:
+        print("WARNING: This list had an unexpected subpopulation", row_subpopulation)
+      ## TODO do I need to close these?
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(allow_abbrev=False, description='Extract subpopulation per sample data out of a callset TSV')
   parser.add_argument('--input_path',type=str, metavar='path', help='path to the original callset TSV', required=True)
-  parser.add_argument('--output_path',type=str, metavar='path', help='path for the output TSV', required=True)
 
   args = parser.parse_args()
 
-  extract_subpopulation(args.input_path,
-                        args.output_path)
+  extract_subpopulation(args.input_path)
 
