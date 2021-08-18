f562d3551ae9045573e67857210f83e55f5f81a2
Rebecca Asch <rasch@broadinstitute.org>
2021-08-18 16:48:10 -0400

    added WDL to parallize loading alt_allele from vet tables

---
+++ scripts/variantstore/wdl/GvsCreateAltAllele.wdl
@@ -0,0 +1,120 @@
+version 1.0
+
+workflow GvsCreateAltAllele {
+  input {
+    String query_project_id
+    String dataset_project_id = query_project_id
+    String dataset_name
+
+    String? service_account_json_path
+    Int? preemptible_tries
+    File? gatk_override
+    String? docker
+  }
+
+  String docker_final = select_first([docker, "us.gcr.io/broad-gatk/gatk:4.1.7.0"])
+
+  call GetVetTableNames {
+    input:
+      query_project_id = query_project_id,
+      dataset_project_id = dataset_project_id,
+      dataset_name = dataset_name,
+      service_account_json_path = service_account_json_path
+  }
+
+  scatter (vet_table in range(length(GetVetTableNames.vet_tables))) {
+    call PopulateAltAlleleTable {
+      input:
+        vet_table_name = vet_table,
+        query_project_id = query_project_id,
+        dataset_project_id = dataset_project_id,
+        dataset_name = dataset_name,
+        service_account_json_path = service_account_json_path
+    }
+  }
+
+  output {
+    Array[String] vet_tables_loaded = PopulateAltAlleleTable.done
+  }
+}
+
+task GetVetTableNames {
+  input {
+    String query_project_id
+    String dataset_project_id
+    String dataset_name
+
+    String? service_account_json_path
+  }
+
+  String has_service_account_file = if (defined(service_account_json_path)) then 'true' else 'false'
+
+  command <<<
+    set -e
+
+    if [ ~{has_service_account_file} = 'true' ]; then
+      gsutil cp ~{service_account_json_path} local.service_account.json
+      gcloud auth activate-service-account --key-file=local.service_account.json
+      gcloud config set project ~{query_project_id}
+    fi
+
+    echo "query_project_id = ~{query_project_id}" > ~/.bigqueryrc
+    bq query --location=US --query_project_id=~{query_project_id} --format=csv --use_legacy_sql=false \
+    "SELECT table_name FROM ~{dataset_project_id}.~{dataset_name}.INFORMATION_SCHEMA.TABLES WHERE table_name LIKE 'vet_%' ORDER BY table_name" > vet_tables.csv
+
+    sed -i 1d vet_tables.csv
+  >>>
+
+  output {
+    Array[String] vet_tables = read_lines("vet_tables.csv")
+  }
+
+  runtime {
+    docker: "gcr.io/google.com/cloudsdktool/cloud-sdk:305.0.0"
+    memory: "3 GB"
+    disks: "local-disk 10 HDD"
+    preemptible: 3
+    cpu: 1
+  }
+}
+
+task PopulateAltAlleleTable {
+  input {
+    String vet_table_name
+    String query_project_id
+    String dataset_project_id
+    String dataset_name
+
+    String? service_account_json_path
+  }
+
+  String has_service_account_file = if (defined(service_account_json_path)) then 'true' else 'false'
+
+  command <<<
+    set -e
+
+    if [ ~{has_service_account_file} = 'true' ]; then
+      gsutil cp ~{service_account_json_path} local.service_account.json
+      gcloud auth activate-service-account --key-file=local.service_account.json
+      SERVICE_ACCOUNT_STANZA="--sa_key_path local.service_account.json "
+    fi
+
+    python3 /app/populate_alt_allele_table.py \
+      --query_project ~{query_project_id} \
+      --vet_table_name ~{vet_table_name} \
+      --fq_dataset ~{dataset_project_id}.~{dataset_name} \
+      $SERVICE_ACCOUNT_STANZA
+  >>>
+
+  output {
+    String done = "~{vet_table_name}"
+  }
+
+  runtime {
+    docker: "us.gcr.io/broad-dsde-methods/variantstore:ah_var_store_20210806"
+    memory: "3 GB"
+    disks: "local-disk 10 HDD"
+    preemptible: 3
+    cpu: 1
+  }
+}
--- scripts/variantstore/wdl/extract/create_alt_allele_table.py
+++ scripts/variantstore/wdl/extract/create_alt_allele_table.py
@@ -1,165 +1,83 @@
 from google.cloud import bigquery
 from google.cloud.bigquery.job import QueryJobConfig
 from google.oauth2 import service_account
+from pathlib import Path
 
 import argparse
 
 client = None
-THREE_QUOTES = "\"\"\""
-alt_allele_temp_function = r"""
-                            CREATE TEMPORARY FUNCTION minimize(ref STRING, allele STRING)
-                            RETURNS STRING 
-                              LANGUAGE js AS """ + THREE_QUOTES + r"""
-                                let done = false
-                                while (!done && ref.length !== 1) {
-                                  if (ref.slice(-1) === allele.slice(-1)) {
-                                    ref = ref.slice(0, -1)
-                                    allele = allele.slice(0,-1)
-                                  } else {
-                                    done = true
-                                  }
-                                }
-                                return ref+','+allele
-                            """ + THREE_QUOTES + ";"
-alt_allele_query_end = r"""
-select location, sample_id, 
-SPLIT(minimize(ref, SPLIT(alt,',')[OFFSET(0)]))[OFFSET(0)] as ref,
-SPLIT(minimize(ref, SPLIT(alt,',')[OFFSET(0)]))[OFFSET(1)] as allele,
-1 as allele_pos, call_GT, call_GQ,
-as_raw_mq,
-cast(SPLIT(as_raw_mq,'|')[OFFSET(1)] as int64) raw_mq,
-as_raw_mqranksum,
-SAFE_cast(SAFE_cast(SPLIT(as_raw_mqranksum,',')[SAFE_OFFSET(0)] as float64) * 10.0 as int64) as raw_mqranksum_x_10, 
-as_qualapprox,
-qualapprox,
-cast(SPLIT(as_qualapprox,'|')[OFFSET(0)] as int64) as qual,
-as_raw_readposranksum,
-SAFE_cast(SAFE_cast(SPLIT(as_raw_readposranksum,',')[SAFE_OFFSET(0)] as float64) * 10.0 as int64) as raw_readposranksum_x_10,
-as_sb_table,
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(0)],',')[OFFSET(0)] as int64) as sb_ref_plus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(0)],',')[OFFSET(1)] as int64) as sb_ref_minus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(1)],',')[OFFSET(0)] as int64) as sb_alt_plus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(1)],',')[OFFSET(1)] as int64) as sb_alt_minus, 
-call_AD,
-cast(SPLIT(call_AD,',')[OFFSET(0)] as int64) as ref_ad, 
-cast(SPLIT(call_AD,',')[OFFSET(1)] as int64) as ad
-from position1
 
-union all
+def execute_with_retry(label, sql):
+    retry_delay = [30, 60, 90] # 3 retries with incremental backoff
+    start = time.time()
+    while len(retry_delay) > 0:
+        try:
+            query_label = label.replace(" ","-").strip().lower()
 
-select location, sample_id, 
-SPLIT(minimize(ref, SPLIT(alt,',')[OFFSET(0)]))[OFFSET(0)] as ref,
-SPLIT(minimize(ref, SPLIT(alt,',')[OFFSET(0)]))[OFFSET(1)] as allele,
-1 as allele_pos, call_GT, call_GQ,
-as_raw_mq,
-cast(SPLIT(as_raw_mq,'|')[OFFSET(1)] as int64) raw_mq,
-as_raw_mqranksum,
-SAFE_cast(SAFE_cast(SPLIT(as_raw_mqranksum,',')[SAFE_OFFSET(0)] as float64) * 10.0 as int64) as raw_mqranksum_x_10,
-as_qualapprox,
-qualapprox,
-cast(SPLIT(as_qualapprox,'|')[OFFSET(0)] as int64) as qual,
-as_raw_readposranksum,
-SAFE_cast(SAFE_cast(SPLIT(as_raw_readposranksum,',')[SAFE_OFFSET(0)] as float64) * 10.0 as int64) as raw_readposranksum_x_10,
-as_sb_table,
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(0)],',')[OFFSET(0)] as int64) as sb_ref_plus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(0)],',')[OFFSET(1)] as int64) as sb_ref_minus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(1)],',')[OFFSET(0)] as int64) as sb_alt_plus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(1)],',')[OFFSET(1)] as int64) as sb_alt_minus, 
-call_AD,
-cast(SPLIT(call_AD,',')[OFFSET(0)] as int64) as ref_ad, 
-cast(SPLIT(call_AD,',')[OFFSET(1)] as int64) as ad
-from position2
+            existing_labels = client._default_query_job_config.labels
+            job_labels = existing_labels
+            job_labels["gvs_query_name"] = query_label
+            job_config = bigquery.QueryJobConfig(labels=job_labels)
+            query = client.query(sql, job_config=job_config)
 
-union all
+            print(f"STARTING - {label}")
+            JOB_IDS.add((label, query.job_id))
+            results = query.result()
+            print(f"COMPLETED ({time.time() - start} s, {3-len(retry_delay)} retries) - {label}")
+            return results
+        except Exception as err:
+            # if there are no retries left... raise
+            if (len(retry_delay) == 0):
+                raise err
+            else:
+                t = retry_delay.pop(0)
+                print(f"Error {err} running query {label}, sleeping for {t}")
+                time.sleep(t)
 
-select location, sample_id,
-SPLIT(minimize(ref, SPLIT(alt,',')[OFFSET(1)]))[OFFSET(0)] as ref,
-SPLIT(minimize(ref, SPLIT(alt,',')[OFFSET(1)]))[OFFSET(1)] as allele,
-2 as allele_pos, call_GT, call_GQ,
-as_raw_mq,
-cast(SPLIT(as_raw_mq,'|')[OFFSET(2)] as int64) raw_mq,
-as_raw_mqranksum,
-SAFE_cast(SAFE_cast(SPLIT(as_raw_mqranksum,',')[SAFE_OFFSET(1)] as float64) * 10.0 as int64) as raw_mqranksum_x_10,
-as_qualapprox,
-qualapprox,
-cast(SPLIT(as_qualapprox,'|')[OFFSET(1)] as int64) as qual,
-as_raw_readposranksum,
-SAFE_cast(SAFE_cast(SPLIT(as_raw_readposranksum,',')[SAFE_OFFSET(1)] as float64) * 10.0 as int64) as raw_readposranksum_x_10,
-as_sb_table,
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(0)],',')[OFFSET(0)] as int64) as sb_ref_plus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(0)],',')[OFFSET(1)] as int64) as sb_ref_minus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(2)],',')[OFFSET(0)] as int64) as sb_alt_plus, 
-cast(SPLIT(SPLIT(as_sb_table,'|')[OFFSET(2)],',')[OFFSET(1)] as int64) as sb_alt_minus, 
-call_AD,
-cast(SPLIT(call_AD,',')[OFFSET(0)] as int64) as ref_ad, 
-cast(SPLIT(call_AD,',')[OFFSET(2)] as int64) as ad
-from position2;
-"""
+def make_alt_allele_table(query_project, vet_table_name, fq_dataset, sa_key_path):
+    global client
+    default_config = QueryJobConfig(priority="INTERACTIVE", use_query_cache=True)
 
-def get_vet_table_names(fq_pet_vet_dataset):
-  sql = f"SELECT table_name FROM {fq_pet_vet_dataset}.INFORMATION_SCHEMA.TABLES WHERE table_name LIKE 'vet_%' ORDER BY table_name"
+    if sa_key_path:
+        credentials = service_account.Credentials.from_service_account_file(
+            sa_key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
+        )
 
-  query = client.query(sql)
-  results = query.result()
-  return results
+        client = bigquery.Client(credentials=credentials,
+                                 project=query_project,
+                                 default_query_job_config=default_config)
+    else:
+        client = bigquery.Client(project=query_project,
+                                 default_query_job_config=default_config)
 
+    alt_allele_temp_function = Path('alt_allele_temp_function.sql').read_text()
+    alt_allele_positions = Path('alt_allele_positions.sql').read_text()
+    first = True if vet_table_name == "vet_001" else False
 
-def make_alt_allele_table(fq_pet_vet_dataset,
-                          query_project,
-                          sa_key_path
-                          ):
-  global client
-  default_config = QueryJobConfig(priority="INTERACTIVE", use_query_cache=True)
-
-  if sa_key_path:
-    credentials = service_account.Credentials.from_service_account_file(
-      sa_key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
-    )
-
-    client = bigquery.Client(credentials=credentials,
-                             project=query_project,
-                             default_query_job_config=default_config)
-  else:
-    client = bigquery.Client(project=query_project,
-                             default_query_job_config=default_config)
-
-  vet_table_names = get_vet_table_names(fq_pet_vet_dataset)
-
-  first = True
-  for vet_table in vet_table_names:
-    query_beginning = f"CREATE OR REPLACE TABLE {fq_pet_vet_dataset}.alt_allele \
-                        PARTITION BY \
-                        RANGE_BUCKET(location, GENERATE_ARRAY(0, 25000000000000, 1000000000000)) \
-                        CLUSTER BY location, sample_id \
-                        AS \n"
-    if (not first):
-      query_beginning = f"INSERT INTO {fq_pet_vet_dataset}.alt_allele \m"
-      first = False
-    fq_vet_table = f"{fq_pet_vet_dataset}.{vet_table.table_name}"
+    query_beginning = f"CREATE OR REPLACE TABLE {fq_dataset}.alt_allele PARTITION BY \
+                    RANGE_BUCKET(location, GENERATE_ARRAY(0, 25000000000000, 1000000000000)) \
+                    CLUSTER BY location, sample_id AS \n"
+    if not first:
+        query_beginning = f"INSERT INTO {fq_dataset}.alt_allele \m"
+    fq_vet_table = f"~{fq_dataset}.{vet_table_name}"
     query_with = f"""WITH 
-                      position1 as (select * from {fq_vet_table} 
-                       WHERE call_GT IN ('0/1', '1/0', '1/1', '0|1', '1|0', '1|1', '0/2', '0|2','2/0', '2|0')), 
-                      position2 as (select * from {fq_vet_table} 
-                       WHERE call_GT IN ('1/2', '1|2', '2/1', '2|1'))
-                  """
+                  position1 as (select * from {fq_vet_table} WHERE call_GT IN ('0/1', '1/0', '1/1', '0|1', '1|0', '1|1', '0/2', '0|2','2/0', '2|0')), 
+                  position2 as (select * from {fq_vet_table} WHERE call_GT IN ('1/2', '1|2', '2/1', '2|1'))"""
 
-    sql = alt_allele_temp_function + query_beginning + query_with + alt_allele_query_end
-    query = client.query(sql)
-    results = query.result()
-    print(f"data loaded into alt_allele from {vet_table.table_name}\n")
-
+    sql = alt_allele_temp_function + query_beginning + query_with + alt_allele_positions
+    result = execute_with_retry(f"into alt allele from {vet_table_name}", sql)
+    return result
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(allow_abbrev=False, description='Generate an alt_allele table for the BigQuery Variant Store ')
+    parser = argparse.ArgumentParser(allow_abbrev=False, description='Populate an alt_allele table for the BigQuery Variant Store')
 
-  parser.add_argument('--fq_petvet_dataset',type=str, help='project.dataset location of pet/vet data', required=True)
-  parser.add_argument('--query_project',type=str, help='Google project where query should be executed', required=True)
-  parser.add_argument('--sa_key_path',type=str, help='Path to json key file for SA', required=False)
+    parser.add_argument('--query_project',type=str, help='Google project where query should be executed', required=True)
+    parser.add_argument('--vet_table_name',type=str, help='vet table name to ingest', required=True)
+    parser.add_argument('--fq_dataset',type=str, help='project and dataset for data', required=True)
+    parser.add_argument('--sa_key_path',type=str, help='Path to json key file for SA', required=False)
 
 
-  # Execute the parse_args() method
-  args = parser.parse_args()
+    # Execute the parse_args() method
+    args = parser.parse_args()
 
-  make_alt_allele_table(args.fq_petvet_dataset,
-                     args.query_project,
-                     args.sa_key_path)
+    make_alt_allele_table(args.query_project, args.vet_table_name, args.fq_dataset, args.sa_key_path)
