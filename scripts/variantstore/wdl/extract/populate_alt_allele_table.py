d1d0a360c3cbefe3a5e91306b00807dafc56330e
Bec Asch <rsasch@users.noreply.github.com>
2021-09-17 17:05:47 -0400

    added job ID to alt_allele population call output [VS-194] (#7473)

--- scripts/variantstore/wdl/extract/populate_alt_allele_table.py
+++ scripts/variantstore/wdl/extract/populate_alt_allele_table.py
@@ -16,16 +16,17 @@ def execute_with_retry(label, sql):
     while len(retry_delay) > 0:
         try:
             query_label = label.replace(" ","-").strip().lower()
-
             existing_labels = client._default_query_job_config.labels
             job_labels = existing_labels
             job_labels["gvs_query_name"] = query_label
             job_config = bigquery.QueryJobConfig(labels=job_labels)
             query = client.query(sql, job_config=job_config)
-
-            print(f"STARTING - {label}")
+            print(f"STARTING - {label} (jobid: {query.job_id})")
             results = query.result()
-            print(f"COMPLETED ({time.time() - start} s, {3-len(retry_delay)} retries) - {label}")
+            job = client.get_job(query.job_id)
+            mb_billed = int(0 if job.total_bytes_billed is None else job.total_bytes_billed)/(1024 * 1024)
+            print(f"COMPLETED ({time.time() - start} seconds, {3-len(retry_delay)} retries, {mb_billed} MBs) - {label}")
+
             return results
         except Exception as err:
             # if there are no retries left... raise
