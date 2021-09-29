e87b869a4fbf2f73f6ce9705deab4aa3e8b8ab81
Rebecca Asch <rasch@broadinstitute.org>
2021-09-29 10:29:32 -0400

    group re-tryable errors into one block

--- scripts/variantstore/wdl/extract/utils.py
+++ scripts/variantstore/wdl/extract/utils.py
@@ -36,14 +36,8 @@ def execute_with_retry(client, label, sql):
                 f"COMPLETED ({time.time() - start} seconds, {3 - len(retry_delay)} retries, {mb_billed} MBs) - {label}")
 
             return results
-        except google.api_core.exceptions.ServiceUnavailable as err:
-            if len(retry_delay) > 0:
-                t = retry_delay.pop(0)
-                print(f"Error {err} running query {label}, sleeping for {t}")
-                time.sleep(t)
-            else:
-                raise err
-        except google.api_core.exceptions.BadRequest as err:
+        except (google.api_core.exceptions.ServiceUnavailable,
+                google.api_core.exceptions.BadRequest) as err:
             if len(retry_delay) > 0:
                 t = retry_delay.pop(0)
                 print(f"Error {err} running query {label}, sleeping for {t}")
