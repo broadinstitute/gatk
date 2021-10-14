a68c05b4000e5d154714a19472e76bead781abc8
Samuel Lee <lee.samuel.k@gmail.com>
2021-10-14 07:15:12 -0400

    1kgp test

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/VariantRecalibratorIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/VariantRecalibratorIntegrationTest.java
@@ -28,6 +28,45 @@ import java.util.stream.Stream;
  */
 public class VariantRecalibratorIntegrationTest extends CommandLineProgramTest {
 
+    @Test
+    public void test1kgp50Exomes() {
+        final String[] arguments = {
+                "-V", "/home/slee/working/vqsr/1kgp-50-exomes/1kgp-50-exomes.sites_only.vcf.gz",
+                "-O", "/home/slee/working/vqsr/1kgp-50-exomes/1kgp-50-exomes.dropin.nojitter.snps.recal",
+                "--tranches-file", "/home/slee/working/vqsr/1kgp-50-exomes/1kgp-50-exomes.dropin.nojitter.snps.tranches",
+                "--output-model", "/home/slee/working/vqsr/1kgp-50-exomes/1kgp-50-exomes.dropin.nojitter.snps.model.report",
+                "--trust-all-polymorphic",
+                "--tranche"," 100.0",
+                "-tranche", "99.95",
+                "-tranche", "99.9",
+                "-tranche", "99.8",
+                "-tranche", "99.7",
+                "-tranche", "99.6",
+                "-tranche", "99.5",
+                "-tranche", "99.4",
+                "-tranche", "99.3",
+                "-tranche", "99.0",
+                "-tranche", "98.0",
+                "-tranche", "97.0",
+                "-tranche", "90.0",
+                "-an", "FS",
+                "-an", "ReadPosRankSum",
+                "-an", "MQRankSum",
+                "-an", "MQ",
+                "-an", "QD",
+                "-an", "SOR",
+                "-mode", "SNP",
+                "--sample-every-Nth-variant", "1",
+                "--max-gaussians", "6",
+                "--resource:hapmap,known=false,training=true,truth=true,prior=15", "/mnt/4AB658D7B658C4DB/working/ref/hapmap_3.3.hg38.vcf.gz",
+                "--resource:omni,known=false,training=true,truth=true,prior=12", "/mnt/4AB658D7B658C4DB/working/ref/1000G_omni2.5.hg38.vcf.gz",
+                "--resource:1000G,known=false,training=true,truth=false,prior=10", "/mnt/4AB658D7B658C4DB/working/ref/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
+//                "--resource:dbsnp,known=true,training=false,truth=false,prior=7", "/mnt/4AB658D7B658C4DB/working/ref/Homo_sapiens_assembly38.dbsnp138.vcf.gz",
+                "--verbosity", "DEBUG"
+        };
+        runCommandLine(arguments);
+    }
+
     private final String[] VQSRSNPParamsWithResources =
         new String[] {
             "--variant",
