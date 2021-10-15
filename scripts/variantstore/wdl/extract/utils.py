3ae91118f5efa9c06a2197d2fc4ddda59b2b1652
Andrea Haessly <ahaessly@broadinstitute.org>
2021-10-15 12:04:33 -0400

    refactor query for aync ablility

--- scripts/variantstore/wdl/extract/utils.py
+++ scripts/variantstore/wdl/extract/utils.py
@@ -23,24 +23,8 @@ def execute_with_retry(client, label, sql, parallelize = False):
     start = time.time()
     while len(retry_delay) >= 0:
         try:
-            query_label = label.replace(" ", "-").strip().lower()
-            existing_labels = client._default_query_job_config.labels
-            job_labels = existing_labels
-            job_labels["gvs_query_name"] = query_label
-            job_config = bigquery.QueryJobConfig(labels=job_labels)
-            query = client.query(sql, job_config=job_config)
-            print(f"STARTING - {label} (jobid: {query.job_id})")
-            if parallelize:
-                return query
-            else:
-                results = query.result()
-                job = client.get_job(query.job_id)
-                mb_billed = int(0 if job.total_bytes_billed is None else job.total_bytes_billed) / (
-                            1024 * 1024)
-                print(
-                    f"COMPLETED ({time.time() - start} seconds, {3 - len(retry_delay)} retries, {mb_billed} MBs) - {label}")
-
-                return results
+            query = execute_query(client, label, sql)
+            get_query_results(query, client, start, retry_delay)
         except (google.api_core.exceptions.InternalServerError,
                 google.api_core.exceptions.TooManyRequests,
                 google.api_core.exceptions.ServiceUnavailable) as err:
@@ -52,6 +36,29 @@ def execute_with_retry(client, label, sql, parallelize = False):
                 raise err
         except Exception:
             raise
+
+
+def execute_query(client, label, sql):
+    query_label = label.replace(" ", "-").strip().lower()
+    existing_labels = client._default_query_job_config.labels
+    job_labels = existing_labels
+    job_labels["gvs_query_name"] = query_label
+    job_config = bigquery.QueryJobConfig(labels=job_labels)
+    query = client.query(sql, job_config=job_config)
+    print(f"STARTING - {label} (jobid: {query.job_id})")
+
+    return query
+
+
+def get_query_results(query, client, start, retry_delay):
+    results = query.result()
+    job = client.get_job(query.job_id)
+    mb_billed = int(0 if job.total_bytes_billed is None else job.total_bytes_billed) / (
+                1024 * 1024)
+    print(
+        f"COMPLETED ({time.time() - start} seconds, {3 - len(retry_delay)} retries, {mb_billed} MBs) - {label}")
+
+    return results
 
 
 def utf8len(s):
