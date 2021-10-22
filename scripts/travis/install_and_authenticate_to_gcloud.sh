3a0747b6d22cadac71f13858c8b1b084b9a57099
Louis Bergelson <louisb@broadinstitute.org>
2021-10-22 11:39:11 -0400

    Rotating travis secrets part 1
    * Switch travis variable name from DOCKER_SERVICE_PASS -> DOCKER_SERVICE_TOKEN for clarity
    * Replace gcloud encrypted key

--- scripts/travis/install_and_authenticate_to_gcloud.sh
+++ scripts/travis/install_and_authenticate_to_gcloud.sh
@@ -3,7 +3,7 @@
 # This is expected to be run from the travis root directory.
 
 export BOTO_CONFIG=/dev/null; # see for more information https://github.com/broadinstitute/gatk/pull/3350
-openssl aes-256-cbc -K $encrypted_703d76169d63_key -iv $encrypted_703d76169d63_iv -in resources_for_CI/servicekey.json.enc -out servicekey.json -d;
+openssl aes-256-cbc -K $encrypted_c51214b7dd65_key -iv $encrypted_c51214b7dd65_iv -in resources_for_CI/servicekey.json.enc -out servicekey.json -d
 scripts/travis/install_gcloud.sh;
 $GCLOUD_HOME/gcloud components update --quiet;
 if [[ $TEST_TYPE == cloud ]]; then
