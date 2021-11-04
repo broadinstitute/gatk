f05d966e97d3cfda0c329c0a004c7b9414a343fe
Andrea Haessly <ahaessly@broadinstitute.org>
2021-11-04 11:11:05 -0400

    change class names

--- src/main/java/org/broadinstitute/hellbender/utils/bigquery/PendingBQWriter.java
+++ src/main/java/org/broadinstitute/hellbender/utils/bigquery/PendingBQWriter.java
@@ -1,17 +1,8 @@
-package org.broadinstitute.hellbender.tools.gvs.ingest;
+package org.broadinstitute.hellbender.utils.bigquery;
 
-import com.google.api.core.ApiFuture;
 import com.google.cloud.bigquery.storage.v1beta2.*;
-import com.google.protobuf.Descriptors;
-import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 public class PendingBQWriter extends CommittedBQWriter {
     static final Logger logger = LogManager.getLogger(PendingBQWriter.class);
