56f2e0e849dc7eee7aa7325277c0b0e74fd35133
James <emeryj@broadinstitute.org>
2021-11-17 15:45:31 -0500

    fixed tests

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/coverage/CoveredIntervalFinder.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/coverage/CoveredIntervalFinder.java
@@ -3,8 +3,10 @@ package org.broadinstitute.hellbender.tools.walkers.coverage;
 import htsjdk.samtools.util.Locatable;
 import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
+import org.broadinstitute.barclay.argparser.ExperimentalFeature;
 import org.broadinstitute.barclay.help.DocumentedFeature;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.cmdline.programgroups.CoverageAnalysisProgramGroup;
 import org.broadinstitute.hellbender.engine.*;
 import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.utils.tsv.SimpleXSVWriter;
@@ -16,26 +18,31 @@ import java.util.Arrays;
 
 @DocumentedFeature
 @CommandLineProgramProperties(
-        oneLineSummary = "Walk through loci and count coverage.",
-        summary = "Walk bam and look at coverage by position.",
-        programGroup = ReferenceProgramGroup.class
+        oneLineSummary = "Walk through loci and find regions of sufficient coverage.",
+        summary = "Walk bam and find intervals of sufficient coverage.",
+        programGroup = CoverageAnalysisProgramGroup.class
 )
-
+@ExperimentalFeature
+/**
+ * A simple tool intended for generating subsets of the reference that correspond to a specified depth. Currently
+ * the tool will output a bed file where each line corresponds to an interval of contiguous coverage >= minimum-depth..
+ */
 public class CoveredIntervalFinder extends LocusWalker {
 
     @Argument(fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME, shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME, doc = "Output bed file", common = false)
     private GATKPath outputFile = null;
 
-    @Argument(fullName = "minimum-depth", doc = "Depth of non-filtered reads to consider sufficient to include in the interval list", optional = true)
+    @Argument(fullName = "minimum-depth", doc = "Depth of non-filtered reads to consider sufficient to include in the interval list", optional = false)
     public int minimumCoverage = 1;
 
-    SimpleXSVWriter outputWriter = null;
+
+    // Writer for the bed file output TODO when we have a class for writing bed files this should be updated
+    private SimpleXSVWriter outputWriter = null;
 
 
     //traversal records
-    Locatable previousLoc = null;
-    int startPosition = -1;
-
+    private Locatable previousLoc = null;
+    private int startPosition = -1;
 
 
     @Override
