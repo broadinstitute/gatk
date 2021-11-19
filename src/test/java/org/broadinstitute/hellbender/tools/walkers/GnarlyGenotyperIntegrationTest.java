f06971ac0824ad7da38b878334df377c30457a23
samuelklee <samuelklee@users.noreply.github.com>
2021-11-18 23:01:36 -0500

    Removed undocumented mid-p correction to p-values in exact test of Hardy-Weinberg equilibrium and updated corresponding tests. (#7394)
    We now report the same value as ExcHet in bcftools. Note that previous values of 3.0103 (corresponding to mid-p values of 0.5) will now be 0.0000.
    
    * Removed undocumented mid-p correction to p-values in exact test of Hardy-Weinberg equilibrium and updated corresponding unit tests.
    
    * Updated expected ExcessHet values in integration test resources and added an update toggle to GnarlyGenotyperIntegrationTest. (Then reverted changes to some test resources in a rebase.)
    
    * Re-updated resources for GenotypeGVCFsIntegrationTest and CombineGVCFsIntegrationTest to resolve rebase conflicts; uncommented tests and updated resources for GnarlyGenotyperIntegrationTest.
    
    * Updated GATK version in ExcessHet documentation.
    
    * Updated resources for HaplotypeCallerIntegration test to resolve rebase conflicts.
    
    * Re-updated GATK version in ExcessHet documentation and reverted removal of code in ReducibleAnnotation stubs.

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/GnarlyGenotyperIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/GnarlyGenotyperIntegrationTest.java
@@ -33,20 +33,33 @@ import static org.broadinstitute.hellbender.testutils.VariantContextTestUtils.ge
  * Created by gauthier on 3/9/18.
  */
 public class GnarlyGenotyperIntegrationTest extends CommandLineProgramTest {
+
+    // If true, update the expected outputs in tests that assert an exact match vs. prior output,
+    // instead of actually running the tests. Can be used with "./gradlew test -Dtest.single=GnarlyGenotyperIntegrationTest"
+    // to update all of the exact-match tests at once. After you do this, you should look at the
+    // diffs in the new expected outputs in git to confirm that they are consistent with expectations.
+    public static final boolean UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS = false;
+
     private static final List<String> NO_EXTRA_ARGS = Collections.emptyList();
+
+    /*
+     * Make sure that someone didn't leave the UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS toggle turned on
+     */
+    @Test
+    public void assertThatExpectedOutputUpdateToggleIsDisabled() {
+        Assert.assertFalse(UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS, "The toggle to update expected outputs should not be left enabled");
+    }
 
     @DataProvider(name="VCFdata")
     public Object[][] getVCFdata() {
         return new Object[][]{
                 // Simple Test, spanning deletions; standard calling confidence
                 //No variants outside requested intervals; no SNPs with QUAL < 60, no INDELs with QUAL < 69?; has star alleles after deletion at chr20:263497; has AC, AF, AN, DP, ExcessHet, FS, MQ, (MQRankSum), (ReadPosRankSum), SOR, QD; has called genotypes
- /*               {new File[]{getTestFile("sample1.vcf"), getTestFile("sample2.vcf"), getTestFile("sample3.vcf"), getTestFile("sample4.vcf"), getTestFile("sample5.vcf")},
+                {new File[]{getTestFile("sample1.vcf"), getTestFile("sample2.vcf"), getTestFile("sample3.vcf"), getTestFile("sample4.vcf"), getTestFile("sample5.vcf")},
                          getTestFile("fiveSampleTest.vcf"), null, Arrays.asList(new SimpleInterval("chr20", 251370, 252000), new SimpleInterval("chr20", 263000, 265600)), Arrays.asList("--merge-input-intervals", "--only-output-calls-starting-in-intervals"), b38_reference_20_21},
                 // Same as above, but with GenomicsDB using BCF2Codec for interchange
                 {new File[]{getTestFile("sample1.vcf"), getTestFile("sample2.vcf"), getTestFile("sample3.vcf"), getTestFile("sample4.vcf"), getTestFile("sample5.vcf")},
                          getTestFile("fiveSampleTest.vcf"), null, Arrays.asList(new SimpleInterval("chr20", 251370, 252000), new SimpleInterval("chr20", 263000, 265600)), Arrays.asList("--merge-input-intervals", "--only-output-calls-starting-in-intervals", "--genomicsdb-use-bcf-codec"), b38_reference_20_21},
-
-
                 //lower calling confidence
                 //same as above except (different intervals and) with SNPs with 40 < QUAL < 60 and INDELs with 49 < QUAL < 69
                 {new File[]{getTestFile("sample1.vcf"), getTestFile("sample2.vcf"), getTestFile("sample3.vcf"), getTestFile("sample4.vcf"), getTestFile("sample5.vcf")},
@@ -54,7 +67,6 @@ public class GnarlyGenotyperIntegrationTest extends CommandLineProgramTest {
                 // Same as above, but with GenomicsDB using BCF2Codec for interchange
                 {new File[]{getTestFile("sample1.vcf"), getTestFile("sample2.vcf"), getTestFile("sample3.vcf"), getTestFile("sample4.vcf"), getTestFile("sample5.vcf")},
                          getTestFile("fiveSampleTest.lowerCallThreshold.vcf"), null, Arrays.asList(new SimpleInterval("chr20", 250865, 348163)), Arrays.asList("-stand-call-conf 10", "--genomicsdb-use-bcf-codec"), b38_reference_20_21},
-*/
                 //using latest reblocking output with allele-specific annotations
                 //has all of the above plus AS_AltDP, AS_FS, AS_MQ, AS_MQRankSum, AS_QD, AS_ReadPosRankSum
                 {new File[]{new File(getToolTestDataDir() + "/../variantutils/ReblockGVCF/expected.NA12878.AS.chr20snippet.reblocked.g.vcf"),
@@ -64,7 +76,6 @@ public class GnarlyGenotyperIntegrationTest extends CommandLineProgramTest {
                 {new File[]{new File(getToolTestDataDir() + "/../variantutils/ReblockGVCF/expected.NA12878.AS.chr20snippet.reblocked.g.vcf"),
                         new File(getToolTestDataDir() + "/../variantutils/ReblockGVCF/expected.NA12892.AS.chr20snippet.reblocked.g.vcf")},
                         getTestFile("twoSampleAS.vcf"), getTestFile("twoSampleASDB.vcf"), Arrays.asList(new SimpleInterval("20")), Arrays.asList("--genomicsdb-use-bcf-codec"), b37_reference_20_21},
-
                 //using legacy reblocking data with no raw GT count values
                 //has ExcessHet, calculated from genotypes
                 {new File[]{new File(getToolTestDataDir() + "noGTCount.sample1.chr20snippet.vcf"),
@@ -102,15 +113,21 @@ public class GnarlyGenotyperIntegrationTest extends CommandLineProgramTest {
         final File output = createTempFile("GnarlyGenotyper", ".vcf");
         final File outputDatabase = createTempFile("GnarlyGenotyper.annotationDatabase", ".vcf");
 
-        runTool(genomicsDBUri, intervals, reference, output, outputDatabase, additionalArguments);
+        final File outputResolved = UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS ? expected : output;
+        final File outputDatabaseResolved = UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS ? expectedDb : outputDatabase;
 
-        final List<VariantContext> expectedVC = getVariantContexts(expected);
-        final List<VariantContext> actualVC = getVariantContexts(output);
-        assertEqualVariants(actualVC, expectedVC);
-        if (expectedDb != null) {
-            final List<VariantContext> expectedDB = getVariantContexts(expectedDb);
-            final List<VariantContext> actualDB = getVariantContexts(outputDatabase);
-            assertEqualVariants(actualDB, expectedDB);
+        runTool(genomicsDBUri, intervals, reference, outputResolved, outputDatabaseResolved, additionalArguments);
+
+        // Test for an exact match against past results
+        if (!UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS) {
+            final List<VariantContext> expectedVC = getVariantContexts(expected);
+            final List<VariantContext> actualVC = getVariantContexts(output);
+            assertEqualVariants(actualVC, expectedVC);
+            if (expectedDb != null) {
+                final List<VariantContext> expectedDB = getVariantContexts(expectedDb);
+                final List<VariantContext> actualDB = getVariantContexts(outputDatabase);
+                assertEqualVariants(actualDB, expectedDB);
+            }
         }
     }
 
@@ -119,10 +136,14 @@ public class GnarlyGenotyperIntegrationTest extends CommandLineProgramTest {
         final ArgumentsBuilder args = new ArgumentsBuilder();
         args.addReference(new File(reference))
         .add("V", input)
-        .add("output-db", outputDatabase.getAbsolutePath())
         .add(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
         args.addOutput(output);
         intervals.forEach(args::addInterval);
+
+        if (outputDatabase != null) {
+            // to avoid null arguments when updating test resources using UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS = true
+            args.add("output-db", outputDatabase.getAbsolutePath());
+        }
 
         additionalArguments.forEach(args::addRaw);
 
@@ -135,24 +156,29 @@ public class GnarlyGenotyperIntegrationTest extends CommandLineProgramTest {
         final File output = createTempFile("GnarlyGenotyper", ".vcf");
         final File expected = new File(getToolTestDataDir() + "expected.hailOutput.chr20snippet.sites_only.vcf");
 
+        final String outputPath = UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS ? expected.getAbsolutePath() : output.getAbsolutePath();
+
         final ArgumentsBuilder args = new ArgumentsBuilder();
         args.addReference(new File(hg38Reference))
                 .add("V", input)
                 .add("L", "chr20:10000000-10030000")
                 .add("only-output-calls-starting-in-intervals", true)
                 .add("keep-all-sites", true)
-                .addOutput(output)
+                .addOutput(outputPath)
                 .add(StandardArgumentDefinitions.ADD_OUTPUT_VCF_COMMANDLINE, "false");
         runCommandLine(args);
 
-        //should have same variants as input with low QUAL variants marked with a monomorphic filter
-        //QUAL column filled in, retains SB_TABLE, MQ_DP (deprecated), VarDP
-        //does not expect ExcessHet because legacy input has neither genotypes nor GT counts
-        try (final FeatureDataSource<VariantContext> actualVcs = new FeatureDataSource<>(output);
-             final FeatureDataSource<VariantContext> expectedVcs = new FeatureDataSource<>(expected)) {
-            GATKBaseTest.assertCondition(actualVcs, expectedVcs,
-                    (a, e) -> VariantContextTestUtils.assertVariantContextsAreEqual(a, e,
-                            Collections.emptyList(), Collections.emptyList()));
+        // Test for an exact match against past results
+        if (!UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS) {
+            //should have same variants as input with low QUAL variants marked with a monomorphic filter
+            //QUAL column filled in, retains SB_TABLE, MQ_DP (deprecated), VarDP
+            //does not expect ExcessHet because legacy input has neither genotypes nor GT counts
+            try (final FeatureDataSource<VariantContext> actualVcs = new FeatureDataSource<>(output);
+                 final FeatureDataSource<VariantContext> expectedVcs = new FeatureDataSource<>(expected)) {
+                GATKBaseTest.assertCondition(actualVcs, expectedVcs,
+                        (a, e) -> VariantContextTestUtils.assertVariantContextsAreEqual(a, e,
+                                Collections.emptyList(), Collections.emptyList()));
+            }
         }
     }
 
