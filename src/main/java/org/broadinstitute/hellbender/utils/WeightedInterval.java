1e2f4be125d689d808837c2d885af18e1716ef83
Kristian Cibulskis <kcibul@broadinstitute.org>
2022-01-03 16:44:25 -0500

    placeholder

---
+++ src/main/java/org/broadinstitute/hellbender/utils/WeightedInterval.java
@@ -0,0 +1,23 @@
+package org.broadinstitute.hellbender.utils;
+
+import htsjdk.samtools.util.Interval;
+import htsjdk.samtools.util.Locatable;
+
+public class WeightedInterval extends Interval {
+    float weight;
+
+    public WeightedInterval(String sequence, int start, int end, float weight) {
+        super(sequence, start, end);
+        this.weight = weight;
+    }
+
+    public WeightedInterval(Locatable locatable, float weight) {
+        super(locatable);
+        this.weight = weight;
+    }
+
+    public WeightedInterval(String sequence, int start, int end, boolean negative, String name, float weight) {
+        super(sequence, start, end, negative, name);
+        this.weight = weight;
+    }
+}
