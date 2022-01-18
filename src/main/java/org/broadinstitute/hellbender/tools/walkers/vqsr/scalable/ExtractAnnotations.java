bfd2b1325faa3b75b75f20019682fab5fa79fa4e
Samuel Lee <lee.samuel.k@gmail.com>
2022-01-18 17:32:24 -0500

    first cut of training + truth extract; some cleanup of VQSR vestiges, but still a lot left

---
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ExtractAnnotations.java
@@ -0,0 +1,219 @@
+package org.broadinstitute.hellbender.tools.walkers.vqsr.scalable;
+
+import htsjdk.variant.variantcontext.Allele;
+import htsjdk.variant.variantcontext.VariantContext;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.broadinstitute.barclay.argparser.Advanced;
+import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.barclay.argparser.CommandLineException;
+import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
+import org.broadinstitute.barclay.help.DocumentedFeature;
+import org.broadinstitute.hdf5.HDF5File;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.engine.FeatureContext;
+import org.broadinstitute.hellbender.engine.FeatureInput;
+import org.broadinstitute.hellbender.engine.MultiVariantWalker;
+import org.broadinstitute.hellbender.engine.ReadsContext;
+import org.broadinstitute.hellbender.engine.ReferenceContext;
+import org.broadinstitute.hellbender.exceptions.GATKException;
+import org.broadinstitute.hellbender.exceptions.UserException;
+import org.broadinstitute.hellbender.utils.io.IOUtils;
+import org.broadinstitute.hellbender.utils.variant.GATKVCFConstants;
+import picard.cmdline.programgroups.VariantFilteringProgramGroup;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * TODO
+ */
+@CommandLineProgramProperties(
+        // TODO
+        summary = "",
+        oneLineSummary = "",
+        programGroup = VariantFilteringProgramGroup.class
+)
+@DocumentedFeature
+public class ExtractAnnotations extends MultiVariantWalker {
+
+    /**
+     * Any set of VCF files to use as lists of training or truth sites.
+     * Training - The program builds the model using input variants that overlap with these training sites.
+     * Truth - The program uses these truth sites to determine where to set the cutoff in sensitivity.
+     */
+    @Argument(
+            fullName = StandardArgumentDefinitions.RESOURCE_LONG_NAME,
+            doc = "") // TODO
+    private List<FeatureInput<VariantContext>> resource = new ArrayList<>();
+
+    @Argument(
+            fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME,
+            shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
+            doc = "Output annotations HDF5 file.")
+    private File outputAnnotationsHDF5File;
+
+    @Argument(
+            fullName = "mode",
+            shortName = "mode",
+            doc = "Variant type to extract")
+    public VariantTypeMode mode = VariantTypeMode.SNP;
+
+    /**
+     * Extract per-allele annotations.
+     * Annotations should be specified using their full names with AS_ prefix.
+     * Non-allele-specific (scalar) annotations will be applied to all alleles.
+     */
+    @Argument(
+            fullName = "use-allele-specific-annotations",
+            shortName = "AS",
+            doc = "If specified, attempt to use the allele-specific versions of the specified annotations.",
+            optional = true)
+    public boolean useASannotations = false;
+
+    /**
+     * See the input VCF file's INFO field for a list of all available annotations.
+     */
+    @Argument(
+            fullName = "use-annotation",
+            shortName = "an",
+            doc = "The names of the annotations to extract.",
+            minElements = 1)
+    private List<String> useAnnotations = new ArrayList<>();
+
+    @Argument(
+            fullName = "ignore-filter",
+            doc = "If specified, use variants marked as filtered by the specified filter name in the input VCF file.",
+            optional = true)
+    private List<String> ignoreInputFilters = new ArrayList<>();
+
+    @Argument(
+            fullName = "ignore-all-filters",
+            doc = "If specified, the variant recalibrator will ignore all input filters. Useful to rerun the VQSR from a filtered output file.",
+            optional = true)
+    private boolean ignoreAllFilters = false;
+
+    @Advanced
+    @Argument(
+            fullName = "trust-all-polymorphic",
+            doc = "Trust that all the input training sets unfiltered records contain only polymorphic sites to decrease runtime.",
+            optional = true)
+    private boolean trustAllPolymorphic = false;
+
+    private VariantDataManager dataManager;
+    private final Set<String> ignoreInputFilterSet = new TreeSet<>();
+    private final List<ImmutablePair<VariantContext, FeatureContext>> variantsAtLocus = new ArrayList<>(10);
+
+    @Override
+    public void onTraversalStart() {
+
+        // fail early if we cannot create the output file
+        if ((outputAnnotationsHDF5File.exists() && !outputAnnotationsHDF5File.canWrite()) ||
+                (!outputAnnotationsHDF5File.exists() && !outputAnnotationsHDF5File.getAbsoluteFile().getParentFile().canWrite())) {
+            throw new UserException(String.format("Cannot create output annotations HDF5 file at %s.", outputAnnotationsHDF5File));
+        }
+
+        dataManager = new VariantDataManager(new ArrayList<>(useAnnotations), useASannotations, trustAllPolymorphic);
+
+        if (ignoreInputFilters != null) {
+            ignoreInputFilterSet.addAll(ignoreInputFilters);
+        }
+
+        for (final FeatureInput<VariantContext> variantFeature : resource) {
+            dataManager.addVariantSet(new VariantSet(variantFeature));
+        }
+
+        if (!dataManager.checkHasTrainingSet()) {
+            throw new CommandLineException(
+                    "No training set found! Please provide sets of known polymorphic loci marked with the training=true feature input tag. " +
+                            "For example, --resource:hapmap,training=true,truth=true hapmapFile.vcf");
+        }
+
+        if (!dataManager.checkHasTruthSet()) {
+            throw new CommandLineException(
+                    "No truth set found! Please provide sets of known polymorphic loci marked with the truth=true feature input tag. " +
+                            "For example, --resource:hapmap,training=true,truth=true hapmapFile.vcf");
+        }
+    }
+
+    @Override
+    public void apply(final VariantContext vc,
+                      final ReadsContext readsContext,
+                      final ReferenceContext ref,
+                      final FeatureContext featureContext) {
+        // Queue up all variant/featureContext pairs that share a start locus, and defer
+        // processing until the start-position or contig changes. In practice this will
+        // rarely queue up more than a single variant at a time.
+        if (variantLocusChanged(vc)) {
+            consumeQueuedVariants();
+        }
+        variantsAtLocus.add(new ImmutablePair<>(vc, featureContext));
+    }
+
+    // Check to see if the start locus for this variant is different from the
+    // ones in the queue.
+    private boolean variantLocusChanged(final VariantContext nextVC) {
+        if (variantsAtLocus.isEmpty()) {
+            return false;
+        } else {
+            final ImmutablePair<VariantContext, FeatureContext> previous = variantsAtLocus.get(0);
+            return (nextVC.getStart() != previous.left.getStart() || !nextVC.getContig().equals(previous.left.getContig()));
+        }
+    }
+
+    private void consumeQueuedVariants() {
+        variantsAtLocus.forEach(v -> addVariantDatum(v.left, v.right));
+        variantsAtLocus.clear();
+    }
+
+    // TODO check presence in training/truth sets outside of addDatum method
+    private void addVariantDatum(final VariantContext vc,
+                                 final FeatureContext context) {
+        if (vc != null && (ignoreAllFilters || vc.isNotFiltered() || ignoreInputFilterSet.containsAll(vc.getFilters()))) {
+            if (VariantDataManager.checkVariationClass(vc, mode) && !useASannotations) {
+                dataManager.addDatum(context, vc, null, null);
+            } else if (useASannotations) {
+                for (final Allele allele : vc.getAlternateAlleles()) {
+                    if (!GATKVCFConstants.isSpanningDeletion(allele) && VariantDataManager.checkVariationClass(vc, allele, mode)) {
+                        //note that this may not be the minimal representation for the ref and alt allele
+                        dataManager.addDatum(context, vc, vc.getReference(), allele);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public Object onTraversalSuccess() {
+
+        consumeQueuedVariants(); // finish processing any queued variants
+
+        if (dataManager.getData().isEmpty()) {
+            throw new GATKException("None of the specified input variants were present in the resource VCFs.");
+        }
+
+        writeAnnotationsHDF5(outputAnnotationsHDF5File);
+
+        logger.info(String.format("Annotations written to %s.", outputAnnotationsHDF5File.getAbsolutePath()));
+
+        return true;
+    }
+
+    private void writeAnnotationsHDF5(final File file) {
+        try (final HDF5File hdf5File = new HDF5File(file, HDF5File.OpenMode.CREATE)) { // TODO allow appending
+            IOUtils.canReadFile(hdf5File.getFile());
+
+            hdf5File.makeStringArray("/data/annotation_names", dataManager.getAnnotationKeys().toArray(new String[0]));
+            hdf5File.makeDoubleMatrix("/data/annotations", dataManager.getData().stream().map(vd -> vd.annotations).toArray(double[][]::new));
+            hdf5File.makeDoubleArray("/data/is_training", dataManager.getData().stream().mapToDouble(vd -> vd.atTrainingSite ? 1 : 0).toArray());
+            hdf5File.makeDoubleArray("/data/is_truth", dataManager.getData().stream().mapToDouble(vd -> vd.atTruthSite ? 1 : 0).toArray());
+
+            logger.info(String.format("Annotations written to %s.", file.getAbsolutePath()));
+        } catch (final RuntimeException exception) {
+            throw new GATKException(String.format("Exception encountered during writing of annotations (%s). Output file at %s may be in a bad state.",
+                    exception, file.getAbsolutePath()));
+        }
+    }
+}
---
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ExtractAnnotationsIntegrationTest.java
@@ -0,0 +1,30 @@
+package org.broadinstitute.hellbender.tools.walkers.vqsr.scalable;
+
+import org.broadinstitute.hellbender.CommandLineProgramTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public final class ExtractAnnotationsIntegrationTest extends CommandLineProgramTest {
+    @Test
+    public void test1kgp50Exomes() {
+        final String[] arguments = {
+                "-L", "chr1",
+                "-V", "/home/slee/working/vqsr/1kgp-50-exomes/resources/1kgp-50-exomes.sites_only.vcf.gz",
+                "-O", "/home/slee/working/vqsr/scalable/extract-test/test.annot.hdf5",
+                "-an", "FS",
+                "-an", "ReadPosRankSum",
+                "-an", "MQRankSum",
+                "-an", "QD",
+                "-an", "SOR",
+                "-an", "MQ",
+                "--trust-all-polymorphic",
+                "-mode", "SNP",
+                "--resource:hapmap,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/hapmap_3.3.hg38.vcf.gz",
+                "--resource:omni,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/1000G_omni2.5.hg38.vcf.gz",
+                "--resource:1000G,training=true,truth=false", "/mnt/4AB658D7B658C4DB/working/ref/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
+                "--verbosity", "DEBUG"
+        };
+        runCommandLine(arguments);
+    }
+}
