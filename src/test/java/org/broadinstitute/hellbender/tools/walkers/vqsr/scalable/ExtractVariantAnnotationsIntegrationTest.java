36af48d988ce14fb5d3c681fa34aaeb7ed75c98a
Samuel Lee <lee.samuel.k@gmail.com>
2022-01-19 07:56:54 -0500

    indel tests

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ExtractVariantAnnotationsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ExtractVariantAnnotationsIntegrationTest.java
@@ -7,11 +7,11 @@ import static org.testng.Assert.*;
 
 public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineProgramTest {
     @Test
-    public void test1kgp50Exomes() {
+    public void test1kgp50ExomesSNP() {
         final String[] arguments = {
                 "-L", "chr1",
                 "-V", "/home/slee/working/vqsr/1kgp-50-exomes/resources/1kgp-50-exomes.sites_only.vcf.gz",
-                "-O", "/home/slee/working/vqsr/scalable/extract-test/test.annot.hdf5",
+                "-O", "/home/slee/working/vqsr/scalable/extract-test/test.snp.annot.hdf5",
                 "-an", "FS",
                 "-an", "ReadPosRankSum",
                 "-an", "MQRankSum",
@@ -23,6 +23,26 @@ public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineP
                 "--resource:hapmap,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/hapmap_3.3.hg38.vcf.gz",
                 "--resource:omni,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/1000G_omni2.5.hg38.vcf.gz",
                 "--resource:1000G,training=true,truth=false", "/mnt/4AB658D7B658C4DB/working/ref/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
+                "--verbosity", "DEBUG"
+        };
+        runCommandLine(arguments);
+    }
+
+    @Test
+    public void test1kgp50ExomesIndel() {
+        final String[] arguments = {
+                "-L", "chr1",
+                "-V", "/home/slee/working/vqsr/1kgp-50-exomes/resources/1kgp-50-exomes.sites_only.vcf.gz",
+                "-O", "/home/slee/working/vqsr/scalable/extract-test/test.indel.annot.hdf5",
+                "-an", "FS",
+                "-an", "ReadPosRankSum",
+                "-an", "MQRankSum",
+                "-an", "QD",
+                "-an", "SOR",
+                "-an", "MQ",
+                "--trust-all-polymorphic",
+                "-mode", "INDEL",
+                "--resource:mills,known=false,training=true,truth=true,prior=12", "/mnt/4AB658D7B658C4DB/working/ref/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz",
                 "--verbosity", "DEBUG"
         };
         runCommandLine(arguments);
--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/TrainVariantAnnotationModelIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/TrainVariantAnnotationModelIntegrationTest.java
@@ -8,10 +8,22 @@ import static org.testng.Assert.*;
 public class TrainVariantAnnotationModelIntegrationTest extends CommandLineProgramIntegrationTest {
 
     @Test
-    public void test1kgp50Exomes() {
+    public void test1kgp50ExomesSNP() {
         final String[] arguments = {
-                "--annotations-hdf5", "/home/slee/working/vqsr/scalable/extract-test/test.annot.hdf5",
-                "-O", "/home/slee/working/vqsr/scalable/train-test/test",
+                "--annotations-hdf5", "/home/slee/working/vqsr/scalable/extract-test/test.snp.annot.hdf5",
+                "-O", "/home/slee/working/vqsr/scalable/train-test/test.snp",
+                "--python-script", "/home/slee/working/vqsr/scalable/train-test/isolation-forest.py",
+                "--hyperparameters-json", "/home/slee/working/vqsr/scalable/train-test/hyperparameters.json",
+                "--verbosity", "DEBUG"
+        };
+        runCommandLine(arguments);
+    }
+
+    @Test
+    public void test1kgp50ExomesIndel() {
+        final String[] arguments = {
+                "--annotations-hdf5", "/home/slee/working/vqsr/scalable/extract-test/test.indel.annot.hdf5",
+                "-O", "/home/slee/working/vqsr/scalable/train-test/test.indel",
                 "--python-script", "/home/slee/working/vqsr/scalable/train-test/isolation-forest.py",
                 "--hyperparameters-json", "/home/slee/working/vqsr/scalable/train-test/hyperparameters.json",
                 "--verbosity", "DEBUG"
