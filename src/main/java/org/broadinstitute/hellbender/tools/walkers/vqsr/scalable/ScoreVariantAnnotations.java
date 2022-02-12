78c525a77481ed4d0d5f94b1ba652d56a22fd912
Samuel Lee <lee.samuel.k@gmail.com>
2022-02-11 22:04:20 -0500

    refactored extract to simplify SNP/INDEL and AS modes

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ScoreVariantAnnotations.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ScoreVariantAnnotations.java
@@ -1,5 +1,11 @@
 //package org.broadinstitute.hellbender.tools.walkers.vqsr.scalable;
 //
+//import htsjdk.samtools.SAMSequenceDictionary;
+//import htsjdk.variant.variantcontext.writer.VariantContextWriter;
+//import htsjdk.variant.vcf.VCFConstants;
+//import htsjdk.variant.vcf.VCFHeader;
+//import htsjdk.variant.vcf.VCFHeaderLine;
+//import htsjdk.variant.vcf.VCFStandardHeaderLines;
 //import org.apache.commons.lang3.tuple.Pair;
 //import org.broadinstitute.barclay.argparser.Argument;
 //import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
@@ -12,6 +18,9 @@
 //import org.broadinstitute.hellbender.utils.io.IOUtils;
 //import org.broadinstitute.hellbender.utils.python.PythonScriptExecutor;
 //import org.broadinstitute.hellbender.utils.runtime.ProcessOutput;
+//import org.broadinstitute.hellbender.utils.variant.GATKVCFConstants;
+//import org.broadinstitute.hellbender.utils.variant.GATKVCFHeaderLines;
+//import org.broadinstitute.hellbender.utils.variant.VcfUtils;
 //import picard.cmdline.programgroups.VariantFilteringProgramGroup;
 //
 //import java.io.File;
@@ -20,6 +29,7 @@
 //import java.util.Arrays;
 //import java.util.Collections;
 //import java.util.List;
+//import java.util.Set;
 //
 ///**
 // * TODO
@@ -32,6 +42,8 @@
 //)
 //@DocumentedFeature
 //public class ScoreVariantAnnotations extends VariantLabeledAnnotationsWalker {
+//
+//    private static final String SCORE_KEY = GATKVCFConstants.VQS_LOD_KEY;
 //
 //    private static final String SCORER_PKL_SUFFIX = ".scorer.pkl";
 //    private static final String SCORER_SER_SUFFIX = ".scorer.ser";
@@ -49,15 +61,12 @@
 //
 //    private File inputScorerPklFile;
 //    private File outputScoresFile;
+//    private File outputVCFFile;
+//    private VariantContextWriter vcfWriter;
 //
 //    @Override
 //    public boolean isExtractVariantsNotOverlappingResources() {
 //        return true;
-//    }
-//
-//    @Override
-//    public String getVCFSuffix() {
-//        return RECALIBRATION_VCF_SUFFIX;
 //    }
 //
 //    @Override
@@ -78,6 +87,7 @@
 //        }
 //
 //        outputScoresFile = new File(outputPrefix + SCORES_HDF5_SUFFIX);
+//        outputVCFFile = new File(outputPrefix + ".vcf"); // TODO add -O VCF output
 //
 //        for (final File outputFile : Collections.singletonList(outputScoresFile)) {
 //            if ((outputFile.exists() && !outputFile.canWrite()) ||
@@ -85,15 +95,15 @@
 //                throw new UserException(String.format("Cannot create output file at %s.", outputFile));
 //            }
 //        }
+//
+//        vcfWriter = createVCFWriter(outputVCFFile);
+//        vcfWriter.writeHeader(constructVCFHeader());
 //    }
 //
 //    @Override
-//    public Object onTraversalSuccess() {
+//    public void afterOnTraversalSuccess() {
 //
 //        logger.info(String.format("Extracted annotations for %s total variants.", dataBatch.getFlattenedData().size()));
-//
-//        logger.info("Writing annotations...");
-//        writeAnnotationsHDF5();
 //
 //        logger.info("Scoring...");
 //        final double[] scores;
@@ -143,9 +153,9 @@
 //        logger.info("Writing VCF...");
 //        writeVCF(false, false,true);
 //
-//        logger.info(String.format("%s complete.", getClass().getSimpleName()));
+//        vcfWriter.close();
 //
-//        return null;
+//        logger.info(String.format("%s complete.", getClass().getSimpleName()));
 //    }
 //
 //    private static List<String> composePythonArguments(final File rawAnnotationsFile,
@@ -159,5 +169,26 @@
 //        } catch (final IOException e) {
 //            throw new UserException.BadInput(String.format("Encountered exception resolving canonical file paths: %s", e));
 //        }
+//    }
+//
+//    private VCFHeader constructVCFHeader() {
+//        //TODO: this should be refactored/consolidated as part of
+//        // https://github.com/broadinstitute/gatk/issues/2112
+//        // https://github.com/broadinstitute/gatk/issues/121,
+//        // https://github.com/broadinstitute/gatk/issues/1116 and
+//        // Initialize VCF header lines
+//        Set<VCFHeaderLine> hInfo = getDefaultToolVCFHeaderLines();
+//        hInfo.add(VCFStandardHeaderLines.getInfoLine(VCFConstants.END_KEY));
+//        hInfo.add(GATKVCFHeaderLines.getInfoLine(SCORE_KEY));
+//        hInfo.add(GATKVCFHeaderLines.getInfoLine(GATKVCFConstants.POSITIVE_LABEL_KEY));
+//        hInfo.add(GATKVCFHeaderLines.getFilterLine(VCFConstants.PASSES_FILTERS_v4));
+//        final SAMSequenceDictionary sequenceDictionary = getBestAvailableSequenceDictionary();
+//        if (hasReference()) {
+//            hInfo = VcfUtils.updateHeaderContigLines(
+//                    hInfo, referenceArguments.getReferencePath(), sequenceDictionary, true);
+//        } else if (null != sequenceDictionary) {
+//            hInfo = VcfUtils.updateHeaderContigLines(hInfo, null, sequenceDictionary, true);
+//        }
+//        return new VCFHeader(hInfo);
 //    }
 //}
--- src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/VariantLabeledAnnotationsData.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/VariantLabeledAnnotationsData.java
@@ -1,23 +1,16 @@
 package org.broadinstitute.hellbender.tools.walkers.vqsr.scalable;
 
 import com.google.common.collect.ImmutableList;
-import htsjdk.samtools.SAMSequenceDictionary;
 import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.VariantContext;
-import htsjdk.variant.variantcontext.VariantContextBuilder;
-import htsjdk.variant.variantcontext.writer.VariantContextWriter;
-import htsjdk.variant.vcf.VCFConstants;
-import org.apache.commons.lang3.tuple.Triple;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.broadinstitute.hdf5.HDF5File;
 import org.broadinstitute.hdf5.HDF5LibException;
 import org.broadinstitute.hellbender.exceptions.GATKException;
 import org.broadinstitute.hellbender.tools.copynumber.utils.HDF5Utils;
-import org.broadinstitute.hellbender.utils.IntervalUtils;
 import org.broadinstitute.hellbender.utils.Utils;
 import org.broadinstitute.hellbender.utils.io.IOUtils;
-import org.broadinstitute.hellbender.utils.variant.GATKVCFConstants;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -37,8 +30,6 @@ import java.util.stream.IntStream;
 final class VariantLabeledAnnotationsData {
     private static final Logger logger = LogManager.getLogger(VariantLabeledAnnotationsData.class);
 
-    private static final int INITIAL_SIZE = 1000000;
-
     // TODO make labels enum?
     static final String TRAINING_LABEL = "training";
     static final String TRUTH_LABEL = "truth";
@@ -47,11 +38,13 @@ final class VariantLabeledAnnotationsData {
     final List<String> sortedLabels;
 
     private final List<VariantContext> variantContexts;
-    private final List<List<AlleleLabeledAnnotationsDatum>> data;
+    private final List<List<LabeledAnnotationsDatum>> data;
+    private final boolean useASAnnotations;
 
     VariantLabeledAnnotationsData(final Collection<String> annotationKeys,
                                   final Collection<String> labels,
-                                  final int initialSize) {
+                                  final int initialSize,
+                                  final boolean useASAnnotations) {
         this.variantContexts = new ArrayList<>(initialSize);
         this.data = new ArrayList<>(initialSize);
         this.sortedAnnotationKeys = ImmutableList.copyOf(annotationKeys.stream().distinct().sorted().collect(Collectors.toList()));
@@ -62,19 +55,15 @@ final class VariantLabeledAnnotationsData {
         if (sortedLabels.size() != labels.size()) {
             logger.warn(String.format("Ignoring duplicate labels: %s.", Utils.getDuplicatedItems(labels)));
         }
+        this.useASAnnotations = useASAnnotations;
     }
 
     public List<VariantContext> getVariantContexts() {
         return Collections.unmodifiableList(variantContexts);
     }
 
-    public List<List<AlleleLabeledAnnotationsDatum>> getData() {
+    public List<List<LabeledAnnotationsDatum>> getData() {
         return Collections.unmodifiableList(data);
-    }
-
-    VariantLabeledAnnotationsData(final Collection<String> annotationKeys,
-                                  final Collection<String> labels) {
-        this(annotationKeys, labels, INITIAL_SIZE);
     }
 
     int size() {
@@ -87,39 +76,60 @@ final class VariantLabeledAnnotationsData {
     }
 
     void add(final VariantContext vc,
-             final List<Allele> altAlleles,
-             final List<VariantType> variantTypesPerAltAllele,
-             final List<Set<String>> labelsPerAltAllele) {
+             final List<Allele> altAllelesPerDatum,
+             final List<VariantType> variantTypePerDatum,
+             final List<Set<String>> labelsPerDatum) {
         variantContexts.add(vc);
-        if (altAlleles == null) {
+        if (!useASAnnotations) {
             // if altAllele is null we are not in AS mode
-            data.add(Collections.singletonList(new AlleleLabeledAnnotationsDatum(vc,
-                    null, variantTypesPerAltAllele.get(0), labelsPerAltAllele.get(0), sortedAnnotationKeys)));
+            data.add(Collections.singletonList(new LabeledAnnotationsDatum(vc,
+                    altAllelesPerDatum,
+                    variantTypePerDatum.get(0),
+                    labelsPerDatum.get(0),
+                    sortedAnnotationKeys,
+                    useASAnnotations)));
         } else {
             // AS mode
-            data.add(IntStream.range(0, altAlleles.size()).boxed()
-                    .map(i -> new AlleleLabeledAnnotationsDatum(vc,
-                            altAlleles.get(i), variantTypesPerAltAllele.get(i), labelsPerAltAllele.get(i), sortedAnnotationKeys))
+            data.add(IntStream.range(0, altAllelesPerDatum.size()).boxed()
+                    .map(i -> new LabeledAnnotationsDatum(vc,
+                            Collections.singletonList(altAllelesPerDatum.get(i)),
+                            variantTypePerDatum.get(i),
+                            labelsPerDatum.get(i),
+                            sortedAnnotationKeys,
+                            useASAnnotations))
                     .collect(Collectors.toList()));
         }
-    }
-
-    List<String> getSortedAnnotationKeys() {
-        return sortedAnnotationKeys;
     }
 
     void writeLabeledAnnotationsBatchToHDF5(final File outputFile,
                                             final int batchIndex) {
         // TODO validate
-        try (final HDF5File outputHDF5File = new HDF5File(outputFile, HDF5File.OpenMode.CREATE)) {
+        try (final HDF5File outputHDF5File = new HDF5File(outputFile, batchIndex == 0 ? HDF5File.OpenMode.CREATE : HDF5File.OpenMode.READ_WRITE)) {
             IOUtils.canReadFile(outputHDF5File.getFile());
 
+            outputHDF5File.makeStringArray(String.format("/data/chrom/chunk_%d", batchIndex),
+                    data.stream().flatMap(List::stream).map(datum -> datum.loc.getContig()).toArray(String[]::new));
+            outputHDF5File.makeDoubleArray(String.format("/data/pos/chunk_%d", batchIndex),
+                    data.stream().flatMap(List::stream).mapToDouble(datum -> datum.loc.getStart()).toArray());
+            outputHDF5File.makeStringArray(String.format("/data/ref/chunk_%d", batchIndex),
+                    data.stream().flatMap(List::stream).map(datum -> datum.refAllele.getDisplayString()).toArray(String[]::new));
+            if (!useASAnnotations) {
+                outputHDF5File.makeStringArray(String.format("/data/alt/chunk_%d", batchIndex),
+                        data.stream().flatMap(List::stream)
+                                .map(datum -> datum.altAlleles.stream().map(Allele::getDisplayString).collect(Collectors.joining(",")))
+                                .toArray(String[]::new));
+            } else {
+                outputHDF5File.makeStringArray(String.format("/data/alt/chunk_%d", batchIndex),
+                        data.stream().flatMap(List::stream).map(datum -> datum.altAlleles.get(0).getDisplayString()).toArray(String[]::new));
+            }
             outputHDF5File.makeStringArray("/data/annotation_names", sortedAnnotationKeys.toArray(new String[0]));
             outputHDF5File.makeDoubleMatrix(String.format("/data/annotations/chunk_%d", batchIndex),
-                    data.stream().flatMap(List::stream).map(vd -> vd.annotations).toArray(double[][]::new));
+                    data.stream().flatMap(List::stream).map(datum -> datum.annotations).toArray(double[][]::new));
+            outputHDF5File.makeDoubleArray(String.format("/data/is_snp/chunk_%d", batchIndex),
+                    data.stream().flatMap(List::stream).mapToDouble(datum -> datum.variantType == VariantType.SNP ? 1 : 0).toArray());
             for (final String label : sortedLabels) {
                 outputHDF5File.makeDoubleArray(String.format("/data/is_%s/chunk_%d", label, batchIndex),
-                        data.stream().flatMap(List::stream).mapToDouble(vd -> vd.labels.contains(label) ? 1 : 0).toArray());
+                        data.stream().flatMap(List::stream).mapToDouble(datum -> datum.labels.contains(label) ? 1 : 0).toArray());
             }
         } catch (final HDF5LibException exception) {
             throw new GATKException(String.format("Exception encountered during writing of labels and annotations to (chunk_%d) (%s). Output file at %s may be in a bad state.",
@@ -172,19 +182,17 @@ final class VariantLabeledAnnotationsData {
 //                         final boolean writeAlleles,
 //                         final boolean writeTrainingOnly,
 //                         final boolean writeScores) {
+//        // TODO validate
 //
-//
-//        final List<AlleleLabeledAnnotationsDatum> data = this.dataBatch.getFlattenedData();
-//
+////        TODO shouldn't this already be sorted?
 //        // we need to sort in coordinate order in order to produce a valid VCF
-//        final SAMSequenceDictionary sequenceDictionary = getBestAvailableSequenceDictionary();
-//        data.sort((vd1, vd2) -> IntervalUtils.compareLocatables(vd1.loc, vd2.loc, sequenceDictionary));
+////        final SAMSequenceDictionary sequenceDictionary = getBestAvailableSequenceDictionary();
+////        dataBatch.sort((vd1, vd2) -> IntervalUtils.compareLocatables(vd1.loc, vd2.loc, sequenceDictionary));
 //
-//        // create dummy alleles to be used
-//        List<Allele> alleles = Arrays.asList(Allele.create("N", true), Allele.create(DUMMY_ALLELE, false));
+//        List<Allele> alleles;
 //
-//        for (int i = 0; i < data.size(); i++) {
-//            final AlleleLabeledAnnotationsDatum datum = data.get(i);
+//        for (int i = 0; i < size(); i++) {
+//            final AlleleLabeledAnnotationsDatum datum = dataBatch.get(i);
 //            if (writeTrainingOnly && !datum.labels.contains(VariantLabeledAnnotationsData.TRAINING_LABEL)) {
 //                continue;
 //            }
