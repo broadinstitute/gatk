365af18eb323b4618974b87d89878bc2ded44c9a
Samuel Lee <lee.samuel.k@gmail.com>
2022-03-04 11:41:24 -0500

    bug

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ExtractVariantAnnotationsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ExtractVariantAnnotationsIntegrationTest.java
@@ -231,6 +231,31 @@ public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineP
     }
 
     @Test
+    public void testJbxAllUnlabeled() {
+        final String[] arguments = {
+                "-V", "/home/slee/working/vqsr/scalable/jbx/resources/Test50Callset.annoated_pids.sites-only.vcf.gz",
+                "-O", "/home/slee/working/vqsr/scalable/jbx/Test50Callset.all-unlabeled.extract",
+                "--maximum-number-of-unlabeled-variants", "10000000",
+                "-A", "FS",
+                "-A", "ReadPosRankSum",
+                "-A", "MQRankSum",
+                "-A", "QD",
+                "-A", "SOR",
+                "-A", "MQ",
+                "-A", "COMBINED_TREE_SCORE",
+                "--trust-all-polymorphic",
+                "--mode", "SNP",
+                "--mode", "INDEL",
+                "--resource:hapmap,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/hapmap_3.3.hg38.vcf.gz",
+                "--resource:omni,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/1000G_omni2.5.hg38.vcf.gz",
+                "--resource:1000G,training=true,truth=false", "/mnt/4AB658D7B658C4DB/working/ref/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
+                "--resource:mills,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz",
+                "--verbosity", "INFO"
+        };
+        runCommandLine(arguments);
+    }
+
+    @Test
     public void testJbxSNP() {
         final String[] arguments = {
                 "-V", "/home/slee/working/vqsr/scalable/jbx/resources/Test50Callset.annoated_pids.sites-only.vcf.gz",
