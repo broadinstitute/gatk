e27538b2bd9054f7079c0eed723ecde9d21d5146
Takuto Sato <tsato@broadinstitute.org>
2022-03-22 11:55:16 -0400

    eh

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/qc/ClipReadsForRSEMIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/qc/ClipReadsForRSEMIntegrationTest.java
@@ -16,10 +16,17 @@ public class ClipReadsForRSEMIntegrationTest extends CommandLineProgramTest {
         //final String bam = home + "SM-LLM7I_Kapa_SM-LQZYG_test_1.bam";
         //final String bam = home + "SM-KYN26_Kapa_SM-LQZYH_test.bam";
         //final String output = home + "SM-LLM7I_Kapa_SM-LQZYG_test_1_out.bam";
-        final String bam = home + "test/SM-HESUP_SSIV_SM-LQZYY_genome_test.bam";
-        final String output = home + "test/SM-HESUP_SSIV_SM-LQZYY_genome_test_out.bam";
+
+        // Case 3
+        // final String bam = home + "test/SM-HESUP_SSIV_SM-LQZYY_genome_test.bam";
+        // final String output = home + "test/SM-HESUP_SSIV_SM-LQZYY_genome_test_out.bam";
+        // 2101:10221:8484 has 2S104M40S and 43S103M, which would be clipped as is.
+
         // final String bam = home + "test/SM-HESUP_SSIV_SM-LQZYY_transcriptome_test.bam";
         // final String output = home + "test/SM-HESUP_SSIV_SM-LQZYY_transcriptome_test_out.bam";
+
+        final String bam = home + "test/SM-LVFDN_15_Min_Low_Mid_transcriptome.duplicate_marked.bam";
+        final String output = home + "test/SM-LVFDN_15_Min_Low_Mid_transcriptome_out.bam";
 
         args.addInput(bam);
         args.addOutput(output);
