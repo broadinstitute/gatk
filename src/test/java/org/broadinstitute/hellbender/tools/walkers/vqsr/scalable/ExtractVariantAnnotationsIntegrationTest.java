7bd6d55aefe0eaa68d34418820ab2e0b7e291c88
Samuel Lee <lee.samuel.k@gmail.com>
2022-03-24 16:55:15 -0400

    more test stuff

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ExtractVariantAnnotationsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/ExtractVariantAnnotationsIntegrationTest.java
@@ -2,6 +2,7 @@ package org.broadinstitute.hellbender.tools.walkers.vqsr.scalable;
 
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.tuple.Pair;
+import org.broadinstitute.hdf5.HDF5File;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.testutils.ArgumentsBuilder;
@@ -26,6 +27,20 @@ import java.util.stream.Collectors;
  */
 public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineProgramTest {
 
+    // If true, update the expected outputs in tests that assert an exact match vs. prior output,
+    // instead of actually running the tests. Can be used with "./gradlew test -Dtest.single=ExtractVariantAnnotationsIntegrationTest"
+    // to update all of the exact-match tests at once. After you do this, you should look at the
+    // diffs in the new expected outputs in git to confirm that they are consistent with expectations.
+    public static final boolean UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS = false;
+
+    /**
+     * Make sure that someone didn't leave the UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS toggle turned on.
+     */
+    @Test
+    public void assertThatExpectedOutputUpdateToggleIsDisabled() {
+        Assert.assertFalse(UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS, "The toggle to update expected outputs should not be left enabled.");
+    }
+
     private static final List<String> NON_ALLELE_SPECIFIC_ANNOTATIONS = Arrays.asList(
             "DP", "FS", "MQ", "MQRankSum", "QD", "ReadPosRankSum", "SOR");
 
@@ -34,11 +49,13 @@ public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineP
 
     private static final File TEST_FILES_DIR = new File(largeFileTestDir,
             "org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/");
-    private static final File INPUT_VCF = new File(TEST_FILES_DIR, "stroke_vqsr_magic_as.chr1.1-10M.vcf.gz");
-    private static final File SNP_TRAINING_VCF = new File(TEST_FILES_DIR, "1000G_omni2.5.hg38.chr1.1-5M.vcf.gz");
-    private static final File SNP_TRUTH_VCF = new File(TEST_FILES_DIR, "1000G_omni2.5.hg38.chr1.5M-10M.vcf.gz");
-    private static final File INDEL_TRAINING_VCF = new File(TEST_FILES_DIR, "Mills_and_1000G_gold_standard.indels.hg38.chr1.1-5M.vcf.gz");
-    private static final File INDEL_TRUTH_VCF = new File(TEST_FILES_DIR, "Mills_and_1000G_gold_standard.indels.hg38.chr1.5M-10M.vcf.gz");
+    private static final File EXPECTED_TEST_FILES_DIR = new File(largeFileTestDir,
+            "org/broadinstitute/hellbender/tools/walkers/vqsr/scalable/expected/extract");
+    private static final File INPUT_VCF = new File(TEST_FILES_DIR, "input/stroke_vqsr_magic_as.chr1.1-10M.vcf.gz");
+    private static final File SNP_TRAINING_VCF = new File(TEST_FILES_DIR, "resources/1000G_omni2.5.hg38.chr1.1-5M.vcf.gz");
+    private static final File SNP_TRUTH_VCF = new File(TEST_FILES_DIR, "resources/1000G_omni2.5.hg38.chr1.5M-10M.vcf.gz");
+    private static final File INDEL_TRAINING_VCF = new File(TEST_FILES_DIR, "resources/Mills_and_1000G_gold_standard.indels.hg38.chr1.1-5M.vcf.gz");
+    private static final File INDEL_TRUTH_VCF = new File(TEST_FILES_DIR, "resources/Mills_and_1000G_gold_standard.indels.hg38.chr1.5M-10M.vcf.gz");
     private static final int MAXIMUM_NUMBER_OF_UNLABELED_VARIANTS = 100;
 
     @DataProvider(name = "dataValidInputs")
@@ -46,12 +63,6 @@ public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineP
         final Supplier<ArgumentsBuilder> baseArgsBuilderSupplier = () -> {
             final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
             argsBuilder.addVCF(INPUT_VCF);
-
-            final File outputDir = createTempDir("testDir");
-            final String outputPrefix = outputDir + "/test";
-            argsBuilder.addOutput(outputPrefix);
-
-            argsBuilder.add(StandardArgumentDefinitions.VERBOSITY_NAME, "INFO");
             return argsBuilder;
         };
 
@@ -95,8 +106,6 @@ public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineP
                         Pair.of("positive", Function.identity()),
                         Pair.of("positiveUnlabeled", addMaximumNumberOfUnlabeledVariants)));
 
-        // TODO add unlabeled
-
         return testConfigurations.stream()
                 .map(tagAndAddFunctionPairs -> new Object[]{
                         tagAndAddFunctionPairs.stream().map(Pair::getLeft).collect(Collectors.joining(".")),
@@ -109,7 +118,82 @@ public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineP
     @Test(dataProvider = "dataValidInputs")
     public void testValidInputs(final String tag,
                                 final ArgumentsBuilder argsBuilder) {
+        final File outputDir = UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS ? EXPECTED_TEST_FILES_DIR : createTempDir("extract");
+        final String outputPrefix = String.format("%s/%s", outputDir, tag);
+        argsBuilder.addOutput(outputPrefix);
+        argsBuilder.add(StandardArgumentDefinitions.VERBOSITY_NAME, "INFO");
         runCommandLine(argsBuilder);
+
+        if (!UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS) {
+            assertOutputs(tag, outputPrefix);
+        }
+    }
+
+    private static void assertOutputs(final String tag,
+                                      final String outputPrefix) {
+
+    }
+
+//    private static void assertAnnotationFilesEqual(final File annotationsFile1,
+//                                                   final File annotationsFile2) {
+//        try (final HDF5File annotationsHDF5File1 = new HDF5File(annotationsFile1, HDF5File.OpenMode.READ_ONLY);
+//             final HDF5File annotationsHDF5File2 = new HDF5File(annotationsFile2, HDF5File.OpenMode.READ_ONLY)) {
+//            Assert.assertEquals(
+//                    annotationsHDF5File1.readStringArray("/annotations/names"),
+//                    annotationsHDF5File2.readStringArray("/annotations/names"));
+//            for (final String label : Arrays.asList("training", "truth")) {
+//                Assert.assertEquals(
+//                        LabeledVariantAnnotationsData.readChunkedDoubleArray(annotationsHDF5File1, String.format("/labels/%s", label)),
+//                        LabeledVariantAnnotationsData.readChunkedDoubleArray(annotationsHDF5File2, String.format("/labels/%s", label)));
+//            }
+//            Assert.assertEquals(
+//                    HDF5Utils.readChunkedDoubleMatrix(annotationsHDF5File1, "/annotations"),
+//                    HDF5Utils.readChunkedDoubleMatrix(annotationsHDF5File2, "/annotations"));
+//        } catch (final HDF5LibException exception) {
+//            Assert.fail("Exception encountered during reading of annotations:", exception);
+//        }
+//    }
+
+    @Test
+    public void test1kgp50ExomesAllUnlabeled() throws IOException {
+        final String[] arguments = {
+                "-L", "chr1",
+                "-V", "/home/slee/working/vqsr/1kgp-50-exomes/resources/1kgp-50-exomes.sites_only.vcf.gz",
+                "-O", "/home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled",
+                "--maximum-number-of-unlabeled-variants", "10000",
+                "-A", "FS",
+                "-A", "ReadPosRankSum",
+                "-A", "MQRankSum",
+                "-A", "QD",
+                "-A", "SOR",
+                "-A", "MQ",
+                "--mode", "SNP",
+                "--mode", "INDEL",
+                "--resource:hapmap,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/hapmap_3.3.hg38.vcf.gz",
+                "--resource:omni,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/1000G_omni2.5.hg38.vcf.gz",
+                "--resource:1000G,training=true,truth=false", "/mnt/4AB658D7B658C4DB/working/ref/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
+                "--resource:mills,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz",
+                "--verbosity", "INFO"
+        };
+        runCommandLine(arguments);
+
+        runSystemCommand("h5diff /home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled.annot.hdf5 /home/slee/working/vqsr/scalable/extract-test/expected/test.all-unlabeled.annot.hdf5");
+        runSystemCommand("h5diff /home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled.unlabeled.annot.hdf5 /home/slee/working/vqsr/scalable/extract-test/expected/test.all-unlabeled.unlabeled.annot.hdf5");
+        runSystemCommand("diff /home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled.vcf.gz /home/slee/working/vqsr/scalable/extract-test/expected/test.all-unlabeled.vcf.gz");
+        runSystemCommand("diff /home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled.vcf.gz.tbi /home/slee/working/vqsr/scalable/extract-test/expected/test.all-unlabeled.vcf.gz.tbi");
+    }
+
+    private static void runSystemCommand(final String command) {
+        try {
+            final Process process = Runtime.getRuntime().exec(command);
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while (reader.readLine() != null) {
+                Assert.fail(command);
+            }
+            reader.close();
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -134,53 +218,6 @@ public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineP
                 "--verbosity", "INFO"
         };
         runCommandLine(arguments);
-    }
-
-    @Test
-    public void test1kgp50ExomesAllUnlabeled() throws IOException {
-        final String[] arguments = {
-                "-L", "chr1",
-                "-V", "/home/slee/working/vqsr/1kgp-50-exomes/resources/1kgp-50-exomes.sites_only.vcf.gz",
-                "-O", "/home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled",
-                "--maximum-number-of-unlabeled-variants", "10000",
-                "-A", "FS",
-                "-A", "ReadPosRankSum",
-                "-A", "MQRankSum",
-                "-A", "QD",
-                "-A", "SOR",
-                "-A", "MQ",
-                "--trust-all-polymorphic",
-                "--mode", "SNP",
-                "--mode", "INDEL",
-                "--resource:hapmap,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/hapmap_3.3.hg38.vcf.gz",
-                "--resource:omni,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/1000G_omni2.5.hg38.vcf.gz",
-                "--resource:1000G,training=true,truth=false", "/mnt/4AB658D7B658C4DB/working/ref/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
-                "--resource:mills,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz",
-                "--verbosity", "INFO"
-        };
-        runCommandLine(arguments);
-
-        runSystemCommand("h5diff /home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled.annot.hdf5 /home/slee/working/vqsr/scalable/extract-test/expected/test.all-unlabeled.annot.hdf5");
-        runSystemCommand("h5diff /home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled.unlabeled.annot.hdf5 /home/slee/working/vqsr/scalable/extract-test/expected/test.all-unlabeled.unlabeled.annot.hdf5");
-        runSystemCommand("diff /home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled.unlabeled.vcf.gz /home/slee/working/vqsr/scalable/extract-test/expected/test.all-unlabeled.unlabeled.vcf.gz");
-        runSystemCommand("diff /home/slee/working/vqsr/scalable/extract-test/test.all-unlabeled.unlabeled.vcf.gz.tbi /home/slee/working/vqsr/scalable/extract-test/expected/test.all-unlabeled.unlabeled.vcf.gz.tbi");
-    }
-
-    private static void runSystemCommand(final String command) {
-        try {
-            Process process = Runtime.getRuntime().exec(command);
-
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
-            while (reader.readLine() != null) {
-                Assert.fail(command);
-            }
-
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Test
@@ -265,75 +302,6 @@ public final class ExtractVariantAnnotationsIntegrationTest extends CommandLineP
         };
         runCommandLine(arguments);
     }
-
-//    @Test
-//    public void test1kgp50ExomesSNPExactMatch() {
-//        final File outputDir = createTempDir("extract-test");
-//        final String[] arguments = {
-//                "-L", "chr1",
-//                "-V", "/home/slee/working/vqsr/1kgp-50-exomes/resources/1kgp-50-exomes.sites_only.vcf.gz",
-//                "-O", new File(outputDir, "test.snp").getAbsolutePath(),
-//                "-A", "FS",
-//                "-A", "ReadPosRankSum",
-//                "-A", "MQRankSum",
-//                "-A", "QD",
-//                "-A", "SOR",
-//                "-A", "MQ",
-//                "--trust-all-polymorphic",
-//                "-mode", "SNP",
-//                "--resource:hapmap,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/hapmap_3.3.hg38.vcf.gz",
-//                "--resource:omni,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/1000G_omni2.5.hg38.vcf.gz",
-//                "--resource:1000G,training=true,truth=false", "/mnt/4AB658D7B658C4DB/working/ref/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
-//                "--verbosity", "INFO"
-//        };
-//        runCommandLine(arguments);
-//        assertAnnotationFilesEqual(new File(outputDir, "test.snp.annot.hdf5"), new File("/home/slee/working/vqsr/scalable/extract-exact-match", "test.snp.annot.hdf5"));
-//    }
-//
-//    @Test
-//    public void testSNPASExactMatch() {
-//        final File outputDir = createTempDir("extract-test");
-//        final String[] arguments = {
-//                "-L", "chr1",
-//                "-V", largeFileTestDir + "VQSR/chr1snippet.doctoredMQ.sites_only.vcf.gz",
-//                "-O", new File(outputDir, "test.snp.as").getAbsolutePath(),
-//                "--use-allele-specific-annotations",
-//                "-A", "AS_FS",
-//                "-A", "AS_ReadPosRankSum",
-//                "-A", "AS_MQRankSum",
-//                "-A", "AS_QD",
-//                "-A", "AS_SOR",
-//                "-A", "AS_MQ",
-//                "--trust-all-polymorphic",
-//                "--mode", "SNP",
-//                "--resource:hapmap,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/hapmap_3.3.hg38.vcf.gz",
-//                "--resource:omni,training=true,truth=true", "/mnt/4AB658D7B658C4DB/working/ref/1000G_omni2.5.hg38.vcf.gz",
-//                "--resource:1000G,training=true,truth=false", "/mnt/4AB658D7B658C4DB/working/ref/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
-//                "--verbosity", "INFO"
-//        };
-//        runCommandLine(arguments);
-//        assertAnnotationFilesEqual(new File(outputDir, "test.snp.as.annot.hdf5"), new File("/home/slee/working/vqsr/scalable/extract-exact-match", "test.snp.as.annot.hdf5"));
-//    }
-//
-//    private static void assertAnnotationFilesEqual(final File annotationsFile1,
-//                                                   final File annotationsFile2) {
-//        try (final HDF5File annotationsHDF5File1 = new HDF5File(annotationsFile1, HDF5File.OpenMode.READ_ONLY);
-//             final HDF5File annotationsHDF5File2 = new HDF5File(annotationsFile2, HDF5File.OpenMode.READ_ONLY)) {
-//            Assert.assertEquals(
-//                    annotationsHDF5File1.readStringArray("/annotations/names"),
-//                    annotationsHDF5File2.readStringArray("/annotations/names"));
-//            for (final String label : Arrays.asList("training", "truth")) {
-//                Assert.assertEquals(
-//                        LabeledVariantAnnotationsData.readChunkedDoubleArray(annotationsHDF5File1, String.format("/labels/%s", label)),
-//                        LabeledVariantAnnotationsData.readChunkedDoubleArray(annotationsHDF5File2, String.format("/labels/%s", label)));
-//            }
-//            Assert.assertEquals(
-//                    HDF5Utils.readChunkedDoubleMatrix(annotationsHDF5File1, "/annotations"),
-//                    HDF5Utils.readChunkedDoubleMatrix(annotationsHDF5File2, "/annotations"));
-//        } catch (final HDF5LibException exception) {
-//            Assert.fail("Exception encountered during reading of annotations:", exception);
-//        }
-//    }
 
     @Test
     public void testJbxAll() {
