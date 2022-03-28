dae8cd61490612987025e56e69402e1b9cd6545c
Takuto Sato <tsato@broadinstitute.org>
2022-03-28 15:41:05 -0400

    test written

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/qc/RecoverReadTagsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/qc/RecoverReadTagsIntegrationTest.java
@@ -2,8 +2,10 @@ package org.broadinstitute.hellbender.tools.walkers.qc;
 
 import htsjdk.samtools.SAMFileHeader;
 import htsjdk.samtools.util.PeekableIterator;
+import htsjdk.samtools.util.RuntimeIOException;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.engine.ReadsPathDataSource;
+import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.testutils.ArgumentsBuilder;
 import org.broadinstitute.hellbender.tools.walkers.mutect.M2TestingUtils;
 import org.broadinstitute.hellbender.utils.BaseUtils;
@@ -23,14 +25,14 @@ import java.util.List;
 public class RecoverReadTagsIntegrationTest extends CommandLineProgramTest {
 
     @Test
-    public void test2() throws IOException {
-        final File alignedSAMFile = File.createTempFile("sam1", ".bam");
-        final File unmappedSAMFile = File.createTempFile("sam2", ".bam");
+    public void test2() {
+        final File alignedSAMFile = createTempFile("sam1", ".bam");
+        final File unmappedSAMFile = createTempFile("sam2", ".bam");
 
         final SAMFileHeader samHeader = M2TestingUtils.createSamHeader("sample");
-        final SAMFileGATKReadWriter writer = new SAMFileGATKReadWriter(ReadUtils.createCommonSAMWriter(alignedSAMFile, null, samHeader, true, true, false));
-        final SAMFileGATKReadWriter unmappedWriter = new SAMFileGATKReadWriter(ReadUtils.createCommonSAMWriter(unmappedSAMFile, null, samHeader, true, true, false));
-
+        samHeader.setSortOrder(SAMFileHeader.SortOrder.queryname);
+        // Often an unaligned bam does not have the SortOrder field populated.
+        final SAMFileHeader unalignedHeader = M2TestingUtils.createSamHeader("sample");
 
         final List<String> umis = Arrays.asList("TCC-ATG", "CTA-GGA", "CCT-AGC", "GCA-AAA");
         final int readLength = 30;
@@ -39,47 +41,39 @@ public class RecoverReadTagsIntegrationTest extends CommandLineProgramTest {
         Arrays.fill(bases, (byte)'A');
         Arrays.fill(quals, (byte)30);
 
-        for (int i = 0; i < umis.size(); i++){
-            final GATKRead unmappedRead = ArtificialReadUtils.createArtificialUnmappedRead(samHeader, bases, quals);
-            unmappedRead.setName("read" + i);
-            unmappedRead.setAttribute("RX", umis.get(i));
-            unmappedWriter.addRead(unmappedRead);
+        try (final SAMFileGATKReadWriter alignedSAMWriter = new SAMFileGATKReadWriter(ReadUtils.createCommonSAMWriter(alignedSAMFile, null, samHeader, true, false, false));
+             final SAMFileGATKReadWriter unmappedWriter = new SAMFileGATKReadWriter(ReadUtils.createCommonSAMWriter(unmappedSAMFile, null, unalignedHeader, true, false, false))) {
 
-//            read.setReadGroup(DEFAULT_READ_GROUP_NAME);
-//            read.setMappingQuality(DEFAULT_MAPQ);
-//            read.setIsFirstOfPair();
-//            read.setIsReverseStrand(i % 2 == 0);
-        }
-
-        for (int i = 0; i < umis.size(); i++){
-            final GATKRead alignedRead = ArtificialReadUtils.createArtificialRead(samHeader, bases, quals, "30M");
-            alignedRead.setName("read" + i);
-
-            // Simulated the case where an aligner drops a read
-            if (i == 1){
-                continue;
+            for (int i = 0; i < umis.size(); i++) {
+                final GATKRead unmappedRead = ArtificialReadUtils.createArtificialUnmappedRead(samHeader, bases, quals);
+                unmappedRead.setName("read" + i);
+                unmappedRead.setAttribute("RX", umis.get(i));
+                unmappedWriter.addRead(unmappedRead);
             }
 
-            writer.addRead(alignedRead);
+            for (int i = 0; i < umis.size(); i++) {
+                final GATKRead alignedRead = ArtificialReadUtils.createArtificialRead(samHeader, bases, quals, "30M");
+                alignedRead.setName("read" + i);
 
-//            read.setReadGroup(DEFAULT_READ_GROUP_NAME);
-//            read.setMappingQuality(DEFAULT_MAPQ);
-//            read.setIsFirstOfPair();
-//            read.setIsReverseStrand(i % 2 == 0);
+                // Simulated the case where an aligner drops a read
+                if (i == 1) {
+                    continue;
+                }
+
+                alignedSAMWriter.addRead(alignedRead);
+            }
+
+        } catch (RuntimeIOException e){
+            throw new UserException("Failed to open SAM writers", e);
         }
 
-        // sato: just do tr with resources...
-        writer.close();
-        unmappedWriter.close();
-
-        final File outputFile = File.createTempFile("output", ".bam");
+        final File outputFile = createTempFile("output", ".bam");
         final ArgumentsBuilder args = new ArgumentsBuilder()
                 .add("I", alignedSAMFile.getAbsolutePath())
                 .add("unmapped-sam", unmappedSAMFile.getAbsolutePath())
                 .add("O", outputFile.getAbsolutePath());
         runCommandLine(args, RecoverReadTags.class.getSimpleName());
 
-        int d = 3;
         final ReadsPathDataSource outputReadSource = new ReadsPathDataSource(outputFile.toPath());
         final Iterator<GATKRead> outputSamIterator = outputReadSource.iterator();
 
@@ -88,9 +82,9 @@ public class RecoverReadTagsIntegrationTest extends CommandLineProgramTest {
             if (i == 1){
                 continue;
             }
+
             final GATKRead outputRead = outputSamIterator.next();
             Assert.assertEquals(outputRead.getName(), "read" + i);
-            outputRead.setName("read" + i);
             Assert.assertEquals(outputRead.getAttributeAsString("RX"), umis.get(i));
         }
     }
