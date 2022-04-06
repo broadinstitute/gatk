e3f6ab495bc6a505f55a4797efb02541e5020580
David Roazen <droazen@broadinstitute.org>
2022-04-06 15:50:25 -0400

    Revert "debug tests"
    This reverts commit 503811c38c34664dd9b17d8f543b46e744a4f248.

--- src/test/java/org/broadinstitute/hellbender/engine/RequesterPaysIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/engine/RequesterPaysIntegrationTest.java
@@ -55,7 +55,6 @@ public class RequesterPaysIntegrationTest extends CommandLineProgramTest {
                                                   String intervalBase, boolean requiresRequesterPays) throws IOException {
         final ArgumentsBuilder args = new ArgumentsBuilder();
         final File output = IOUtils.createTempFile("out", ".txt");
-        System.out.println("GCP TEST PROJECT: " + getGCPTestProject());
         args.addReference(referenceBase + "hg19mini.fasta")
                         .addInput(bamBase + "reads_data_source_test1.bam"  )
                         .addVCF(vcfBase + "example_variants_withSequenceDict.vcf")
