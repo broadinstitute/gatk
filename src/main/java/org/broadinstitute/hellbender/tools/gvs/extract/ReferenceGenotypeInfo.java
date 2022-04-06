a3f0bfc19f6ea2509e8519a7a5bc3d10ffa5d449
Kristian Cibulskis <kcibul@broadinstitute.org>
2022-04-06 10:03:57 -0400

    refactoring

--- src/main/java/org/broadinstitute/hellbender/tools/gvs/extract/ReferenceGenotypeInfo.java
+++ src/main/java/org/broadinstitute/hellbender/tools/gvs/extract/ReferenceGenotypeInfo.java
@@ -1,2 +1,19 @@
-package org.broadinstitute.hellbender.tools.gvs.extract;public class ReferenceGenotypeInfo {
+package org.broadinstitute.hellbender.tools.gvs.extract;
+
+public class ReferenceGenotypeInfo {
+    private String sampleName;
+    private int GQ;
+
+    public ReferenceGenotypeInfo(String sampleName, int GQ) {
+        this.sampleName = sampleName;
+        this.GQ = GQ;
+    }
+
+    public String getSampleName() {
+        return sampleName;
+    }
+
+    public int getGQ() {
+        return GQ;
+    }
 }
