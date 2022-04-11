6e2f5ac9aed5bd9d5657a38f75e8cd853aef81ca
Laura Gauthier <gauthier@broadinstitute.org>
2022-04-11 19:43:47 -0400

    Confirmed new test depends on check that was removed Added documentation for regression test

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/GenotypeGVCFsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/GenotypeGVCFsIntegrationTest.java
@@ -156,10 +156,10 @@ public class GenotypeGVCFsIntegrationTest extends CommandLineProgramTest {
                 {getTestFile( "combined.single.sample.pipeline.gatk3.vcf"),
                         getTestFile( "expected/includeLowQualSites.vcf"),
                         Arrays.asList( " --" + GenotypeGVCFs.ALL_SITES_LONG_NAME + " -L 20:10,012,730-10,012,740"),
-                        b37_reference_20_21},
+                        b37_reference_20_21}
 
                 //23 highly multi-allelic sites across 54 1000G exomes to test allele subsetting and QUAL calculation
-                //plus one 10-allele WGS variant that's all hom-ref with one GT that has unnormalized PLs
+                //plus one 10-allele WGS variant that's all hom-ref with one GT that has unnormalized PLs from some sort of GenomicsDB corner case
                 {getTestFile("multiallelicQualRegression.vcf "),
                         getTestFile("multiallelicQualRegression.expected.vcf"),
                         NO_EXTRA_ARGS, hg38Reference}
