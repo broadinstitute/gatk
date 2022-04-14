87d9c0d0dcb66d31774e490d389cd6858fc772ec
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-04-14 18:48:49 -0400

    Retry 'UNAVAILABLE' status when uploading to BigQuery [VS-261]

--- src/main/java/org/broadinstitute/hellbender/utils/bigquery/CommittedBQWriter.java
+++ src/main/java/org/broadinstitute/hellbender/utils/bigquery/CommittedBQWriter.java
@@ -1,17 +1,23 @@
 package org.broadinstitute.hellbender.utils.bigquery;
 
+import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.bigquery.storage.v1beta2.*;
+import com.google.common.collect.ImmutableSet;
 import com.google.protobuf.Descriptors;
-import io.grpc.Status;
+import io.grpc.Status.Code;
 import io.grpc.StatusRuntimeException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.broadinstitute.hellbender.exceptions.GATKException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+
+import static io.grpc.Status.Code.*;
+
 
 public class CommittedBQWriter implements AutoCloseable {
     static final Logger logger = LogManager.getLogger(CommittedBQWriter.class);
@@ -20,34 +26,28 @@ public class CommittedBQWriter implements AutoCloseable {
     protected WriteStream writeStream;
     protected JsonStreamWriter writer;
     protected TableName parentTable;
-    protected int MAX_RETRIES = 3;
-    protected WriteStream.Type steamType;
-    protected int BATCH_SIZE = 10000;
+    protected WriteStream.Type streamType;
+    protected int maxRetries = 3;
+    protected int batchSize = 10000;
     protected JSONArray jsonArr = new JSONArray();
 
-
-    public void setMaxRetries(int maxRetries) {
-        MAX_RETRIES = maxRetries;
-    }
-
-    public void setBatchSize(int batchSize) {
-        BATCH_SIZE = batchSize;
-    }
-
-    public CommittedBQWriter(String projectId, String datasetName, String tableName) {
-        this(projectId, datasetName, tableName, WriteStream.Type.COMMITTED);
-    }
+    private final ExponentialBackOff backoff = new ExponentialBackOff.Builder().
+            setInitialIntervalMillis(2000).
+            setMaxIntervalMillis(30000).
+            setMultiplier(2).
+            setRandomizationFactor(0.5).
+            build();
 
     protected CommittedBQWriter(String projectId, String datasetName, String tableName, WriteStream.Type type) {
         this.parentTable = TableName.of(projectId, datasetName, tableName);
-        this.steamType = type;
+        this.streamType = type;
     }
 
     protected void createStream() throws Descriptors.DescriptorValidationException, InterruptedException, IOException {
         if (bqWriteClient == null) {
             bqWriteClient = BigQueryWriteClient.create();
         }
-        WriteStream writeStreamConfig = WriteStream.newBuilder().setType(steamType).build();
+        WriteStream writeStreamConfig = WriteStream.newBuilder().setType(streamType).build();
         CreateWriteStreamRequest createWriteStreamRequest =
                 CreateWriteStreamRequest.newBuilder()
                         .setParent(parentTable.toString())
@@ -57,44 +57,48 @@ public class CommittedBQWriter implements AutoCloseable {
         writer = JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
     }
 
-    public AppendRowsResponse addJsonRow(JSONObject row) throws Descriptors.DescriptorValidationException, ExecutionException, InterruptedException, IOException {
-        AppendRowsResponse response = null;
+    public void addJsonRow(JSONObject row) throws Descriptors.DescriptorValidationException, ExecutionException, InterruptedException, IOException {
         if (writer == null) {
             createStream();
         }
         jsonArr.put(row);
 
-        if (jsonArr.length() >= BATCH_SIZE) {
-            response = writeJsonArray();
+        if (jsonArr.length() >= batchSize) {
+            writeJsonArray();
         }
-        return response;
     }
 
-    protected AppendRowsResponse writeJsonArray() throws Descriptors.DescriptorValidationException, ExecutionException, InterruptedException, IOException {
-        return writeJsonArray(0);
+    protected void writeJsonArray() throws Descriptors.DescriptorValidationException, ExecutionException, InterruptedException, IOException {
+        writeJsonArray(0);
     }
 
     protected AppendRowsResponse writeJsonArray(int retryCount) throws Descriptors.DescriptorValidationException, ExecutionException, InterruptedException, IOException {
-        AppendRowsResponse response = null;
+        AppendRowsResponse response;
         try {
             ApiFuture<AppendRowsResponse> future = writer.append(jsonArr);
             response = future.get();
             jsonArr = new JSONArray();
         } catch (StatusRuntimeException ex) {
-            Status status = ex.getStatus();
-            if (status == Status.ABORTED || status == Status.INTERNAL || status == Status.CANCELLED) {
-                logger.warn("Caught exception " + ex + "\n Retrying. " + (MAX_RETRIES - retryCount - 1) + " retries remaining.");
-            }
-            if (retryCount < MAX_RETRIES) {
+            Code code = ex.getStatus().getCode();
+            if (ImmutableSet.of(ABORTED, CANCELLED, INTERNAL, UNAVAILABLE).contains(code)) {
+                if (retryCount >= maxRetries) {
+                    throw new GATKException("Caught exception writing to BigQuery and " + maxRetries + " write retries are exhausted", ex);
+                }
+
+                String message = "Caught exception writing to BigQuery, " + (maxRetries - retryCount - 1) + " retries remaining.";
+                System.err.println(message);
+                // .error during validation as I am suspicious about not seeing output
+                logger.error(message, ex);
+                long backOffMillis = backoff.nextBackOffMillis();
+                Thread.sleep(backOffMillis);
                 createStream();
                 response = writeJsonArray(retryCount + 1);
             } else {
                 throw ex;
             }
         }
-    return response;
+        return response;
     }
-
 
     public void close() {
         if (writer != null) {
