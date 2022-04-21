7008f640ec0fd2504eaf7e2e69988ca861ba54c9
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-04-21 05:56:45 -0400

    cleanup

--- src/main/java/org/broadinstitute/hellbender/utils/bigquery/CommittedBQWriter.java
+++ src/main/java/org/broadinstitute/hellbender/utils/bigquery/CommittedBQWriter.java
@@ -3,7 +3,6 @@ package org.broadinstitute.hellbender.utils.bigquery;
 import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.bigquery.storage.v1beta2.*;
-import com.google.common.collect.ImmutableSet;
 import com.google.protobuf.Descriptors;
 import io.grpc.Status.Code;
 import io.grpc.StatusRuntimeException;
@@ -15,8 +14,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
-
-import static io.grpc.Status.Code.*;
 
 
 public class CommittedBQWriter implements AutoCloseable {
@@ -73,8 +70,7 @@ public class CommittedBQWriter implements AutoCloseable {
     }
 
     private StatusRuntimeException findCausalStatusRuntimeException(Throwable t) {
-        if (t == null) return null;
-        if (t instanceof StatusRuntimeException) {
+        if (t == null || t instanceof StatusRuntimeException) {
             return (StatusRuntimeException) t;
         }
         return findCausalStatusRuntimeException(t.getCause());
@@ -96,49 +92,57 @@ public class CommittedBQWriter implements AutoCloseable {
             Code code = se.getStatus().getCode();
             // Google BigQuery write API error handling
             // https://cloud.google.com/bigquery/docs/write-api#error_handling
-            // The comments in this if/else are nearly all quotations from the reference documentation linked above.
-            if (ALREADY_EXISTS == code) {
-                // ALREADY_EXISTS: The row was already written. This error can happen when you provide stream offsets.
-                // It indicates that a duplicate record was detected. You can safely ignore this error.
+            // The comments in the case matching below are nearly all quotations from the documentation linked above.
+            switch (code) {
+                case ALREADY_EXISTS:
+                    // ALREADY_EXISTS: The row was already written. This error can happen when you provide stream offsets.
+                    // It indicates that a duplicate record was detected. You can safely ignore this error.
 
-                // We don't expect to see this, but to "ignore" we should not retry sending the same `jsonArr`.
-                jsonArr = new JSONArray();
-                return null;
-            } else if (ImmutableSet.of(INVALID_ARGUMENT, NOT_FOUND, OUT_OF_RANGE, PERMISSION_DENIED).contains(code)) {
-                // These are all not retryable:
-                //
-                // INVALID_ARGUMENT: Invalid argument. This error is an application error.
-                //
-                // NOT_FOUND: The stream or table was not found.
-                //
-                // OUT_OF_RANGE. The offset is beyond the current write offset. This error can happen if you provide
-                // stream offsets and a previous write operation failed. In that case, you can retry from the last
-                // successful write. This error can also happen if the application sets the wrong offset value.
-                //
-                // PERMISSION_DENIED. The application does not have permission to write to this table.
-                throw e;
-            } else {
-                if (retryCount >= maxRetries) {
-                    throw new GATKException("Caught exception writing to BigQuery and " + maxRetries + " write retries are exhausted", e);
-                }
-                logger.warn("Caught exception writing to BigQuery, " + (maxRetries - retryCount - 1) + " retries remaining.", e);
-                long backOffMillis = backoff.nextBackOffMillis();
-                Thread.sleep(backOffMillis);
+                    // We don't expect to see this, but to "ignore" we should not retry sending the same `jsonArr`.
+                    jsonArr = new JSONArray();
+                    return null;
+                case INVALID_ARGUMENT:
+                case NOT_FOUND:
+                case OUT_OF_RANGE:
+                case PERMISSION_DENIED:
+                    // Do not retry these:
+                    //
+                    // INVALID_ARGUMENT: Invalid argument. This error is an application error.
+                    //
+                    // NOT_FOUND: The stream or table was not found.
+                    //
+                    // OUT_OF_RANGE. The offset is beyond the current write offset. This error can happen if you provide
+                    // stream offsets and a previous write operation failed. In that case, you can retry from the last
+                    // successful write. This error can also happen if the application sets the wrong offset value.
+                    //
+                    // PERMISSION_DENIED. The application does not have permission to write to this table.
+                    throw e;
+                default:
+                    if (retryCount >= maxRetries) {
+                        throw new GATKException("Caught exception writing to BigQuery and " + maxRetries + " write retries are exhausted", e);
+                    }
+                    logger.warn("Caught exception writing to BigQuery, " + (maxRetries - retryCount - 1) + " retries remaining.", e);
+                    long backOffMillis = backoff.nextBackOffMillis();
+                    Thread.sleep(backOffMillis);
 
-                //noinspection StatementWithEmptyBody
-                if (ImmutableSet.of(INTERNAL, CANCELLED, ABORTED).contains(code)) {
-                    // INTERNAL, CANCELLED, or ABORTED: The operation could not be completed. You can safely retry the operation.
-                } else {
-                    // If you receive an error that's not listed above, then try to open a new connection by closing the
-                    // writer object and creating a new instance.
-                    writer.close();
-                    createStream();
-                }
-                return writeJsonArray(retryCount + 1);
+                    switch (code) {
+                        case INTERNAL:
+                        case CANCELLED:
+                        case ABORTED:
+                            // INTERNAL, CANCELLED, or ABORTED: The operation could not be completed. You can safely retry the operation.
+                            // no extra work required here, break out to the recursion.
+                            break;
+                        default:
+                            // If you receive an error that's not listed above, then try to open a new connection by closing the
+                            // writer object and creating a new instance.
+                            writer.close();
+                            createStream();
+                            break;
+                    }
+                    return writeJsonArray(retryCount + 1);
             }
         }
     }
-
 
     public void close() {
         if (writer != null) {
