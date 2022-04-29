46e0476423c2c5db637f9d4edbd567e83721abde
Rori Cremer <6863459+RoriCremer@users.noreply.github.com>
2022-04-28 22:48:40 -0400

    short term add test that needs to be refactored for clinvar correction

--- scripts/variantstore/wdl/extract/create_variant_annotation_table_test.py
+++ scripts/variantstore/wdl/extract/create_variant_annotation_table_test.py
@@ -12,13 +12,12 @@ import unittest
 
 from create_variant_annotation_table import make_annotated_json_row
 
-vat_expected_pathogenic = open('variant_annotation_table_test_files/vat_expected_pathogenic.json')
-vat_test_pathogenic = open('variant_annotation_table_test_files/vat_test_pathogenic.json')
-cysticFibrosisExpectedVAT = json.load(vat_expected_pathogenic)
-cysticFibrosisNirvanaOutput = json.load(vat_test_pathogenic)
-
 class TestMakeAnnotatedJsonRow(unittest.TestCase):
     def test_make_annotated_json_row_success(self):
+        vat_expected_pathogenic = open('variant_annotation_table_test_files/vat_expected_pathogenic.json')
+        vat_test_pathogenic = open('variant_annotation_table_test_files/vat_test_pathogenic.json')
+        cysticFibrosisExpectedVAT = json.load(vat_expected_pathogenic)
+        cysticFibrosisNirvanaOutput = json.load(vat_test_pathogenic)
         actual = make_annotated_json_row(
           row_position=117559590,
           row_ref="ATCT",
@@ -31,8 +30,27 @@ class TestMakeAnnotatedJsonRow(unittest.TestCase):
         cysticFibrosisExpectedVAT["gnomad_nfr_af"] = None
         cysticFibrosisExpectedVAT["gnomad_failed_filter"] = None
         expected = cysticFibrosisExpectedVAT
+        vat_expected_pathogenic.close()
+        vat_test_pathogenic.close()
         self.maxDiff=None
         self.assertEqual(actual, expected)
 
-#likelyBenignPositionObj = "variant_annotation_table_test_files/vat_test_likely_benign.json"
+
+    def test_clinvar_success(self):
+        vat_expected_likely_benign = open('variant_annotation_table_test_files/vat_expected_likely_benign.json')
+        vat_test_likely_benign = open('variant_annotation_table_test_files/vat_test_likely_benign.json')
+        likelyBenignExpectedVAT = json.load(vat_expected_likely_benign)
+        likelyBenignNirvanaOutput = json.load(vat_test_likely_benign)
+        actual = make_annotated_json_row(
+          row_position=5226567,
+          row_ref="A",
+          row_alt="C",
+          variant_line=likelyBenignNirvanaOutput,
+          transcript_line=None)
+        likelyBenignExpectedVAT["dbsnp_rsid"] = None
+        expected = likelyBenignExpectedVAT
+        vat_expected_likely_benign.close()
+        vat_test_likely_benign.close()
+        self.maxDiff=None
+        self.assertEqual(actual, expected)
 
