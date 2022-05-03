4d9e8c4b8cbfddfb26e8ac7522a56e2901aa3254
Rori Cremer <6863459+RoriCremer@users.noreply.github.com>
2022-05-03 11:08:48 -0400

    run tests inside docker container

--- scripts/variantstore/wdl/extract/build_docker.sh
+++ scripts/variantstore/wdl/extract/build_docker.sh
@@ -4,8 +4,18 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
+INFO=$1
+BASE_REPO="broad-dsde-methods/variantstore"
+REPO_WITH_TAG="${BASE_REPO}:${INFO}"
+GCR_TAG="us.gcr.io/${REPO_WITH_TAG}"
+
+docker build . -t ${REPO_WITH_TAG}
+
+echo ${REPO_WITH_TAG}
+
 # Test that the VAT python code has not been broken
-python -m unittest test_create_variant_annotation_table.py  > /dev/null
+docker run -v $PWD:/in  -t ${REPO_WITH_TAG} bash -c "cd /in; python3 -m unittest test_create_variant_annotation_table.py"
+
 VAT_TEST_RESULTS=$?
 if [ $VAT_TEST_RESULTS -ne 0 ]; then
     echo "TestMakeAnnotatedJsonRow python test has failed"
@@ -13,12 +23,6 @@ if [ $VAT_TEST_RESULTS -ne 0 ]; then
 fi
 
 
-INFO=$1
-BASE_REPO="broad-dsde-methods/variantstore"
-REPO_WITH_TAG="${BASE_REPO}:${INFO}"
-GCR_TAG="us.gcr.io/${REPO_WITH_TAG}"
-
-docker build . -t ${REPO_WITH_TAG}
 docker tag ${REPO_WITH_TAG} ${GCR_TAG}
 docker push ${GCR_TAG}
 
