b2507a1b10edc62ecab4bb34e0f37391bfc6a3b0
Orli Cohen <ocohen@broadinstitute.org>
2022-07-08 17:12:28 -0400

    prelim unit tests

--- src/test/java/org/broadinstitute/hellbender/tools/reference/CompareReferencesIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/reference/CompareReferencesIntegrationTest.java
@@ -3,6 +3,7 @@ package org.broadinstitute.hellbender.tools.reference;
 import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.testutils.IntegrationTestSpec;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -38,18 +39,36 @@ public class CompareReferencesIntegrationTest extends CommandLineProgramTest {
         IntegrationTestSpec.assertEqualTextFiles(output, expectedOutput);
     }
 
-    @Test
-    public void testCompareReferencesMissingValueUseDictMD5() throws IOException{
-        final File ref1 = new File(getToolTestDataDir() + "hg19mini.fasta");
-        final File ref2 = new File(getToolTestDataDir() + "hg19mini_chr2snp.fasta");
-        final File output = createTempFile("testCompareReferencesUseDictMD5", ".table");
-        final File expectedOutput = new File(getToolTestDataDir(), "expected.testCompareReferencesMissingValue.table");
+    @DataProvider(name = "md5ArgumentData")
+    public Object[][] md5ArgumentData() {
+        return new Object[][]{
+                // ref1, ref2, expected output, md5 calculation mode
+                new Object[]{ new File(getToolTestDataDir() + "hg19mini.fasta"),
+                        new File(getToolTestDataDir() + "hg19mini_chr2snp.fasta"),
+                        new File(getToolTestDataDir(), "expected.testCompareReferencesMissingValue.table"),
+                        "USE_DICT"
+                },
+                new Object[]{ new File(getToolTestDataDir() + "hg19mini_missingmd5.fasta"),
+                        new File(getToolTestDataDir() + "hg19mini_chr2snp.fasta"),
+                        new File(getToolTestDataDir(), "expected.testCompareReferencesRecalculateIfMissing.table"),
+                        "RECALCULATE_IF_MISSING"
+                },
+                new Object[]{ new File(getToolTestDataDir() + "hg19mini_missingmd5.fasta"),
+                        new File(getToolTestDataDir() + "hg19mini_chr2snp.fasta"),
+                        new File(getToolTestDataDir(), "expected.testCompareReferencesAlwaysRecalculateMD5.table"),
+                        "ALWAYS_RECALCULATE"
+                },
+        };
+    }
 
+    @Test(dataProvider = "md5ArgumentData")
+    public void testCompareReferencesMD5CalculationModes(File ref1, File ref2, File expected, String md5Mode) throws IOException {
+        final File output = createTempFile("testCompareReferencesMD5CalculationModes", ".table");
         final String[] args = new String[] {"-R", ref1.getAbsolutePath() , "-refcomp", ref2.getAbsolutePath(),
-                "-O", output.getAbsolutePath(), "--md5-calculation-mode", "USE_DICT"};
+                "-O", output.getAbsolutePath(), "--md5-calculation-mode", md5Mode};
         runCommandLine(args);
 
-        IntegrationTestSpec.assertEqualTextFiles(output, expectedOutput);
+        IntegrationTestSpec.assertEqualTextFiles(output, expected);
     }
 
     @Test(expectedExceptions = UserException.BadInput.class)
@@ -61,34 +80,6 @@ public class CompareReferencesIntegrationTest extends CommandLineProgramTest {
         final String[] args = new String[] {"-R", ref1.getAbsolutePath() , "-refcomp", ref2.getAbsolutePath(),
                 "-O", output.getAbsolutePath(), "--md5-calculation-mode", "USE_DICT"};
         runCommandLine(args);
-    }
-
-    @Test
-    public void testCompareReferencesMissingValueRecalculateIfMissingMD5() throws IOException{
-        final File ref1 = new File(getToolTestDataDir() + "hg19mini_missingmd5.fasta");
-        final File ref2 = new File(getToolTestDataDir() + "hg19mini_chr2snp.fasta");
-        final File output = createTempFile("testCompareReferencesRecalculateIfMissingMD5", ".table");
-        final File expectedOutput = new File(getToolTestDataDir(), "expected.testCompareReferencesRecalculateIfMissing.table");
-
-        final String[] args = new String[] {"-R", ref1.getAbsolutePath() , "-refcomp", ref2.getAbsolutePath(),
-                "-O", output.getAbsolutePath(), "--md5-calculation-mode", "RECALCULATE_IF_MISSING"};
-        runCommandLine(args);
-
-        IntegrationTestSpec.assertEqualTextFiles(output, expectedOutput);
-    }
-
-    @Test
-    public void testCompareReferencesMissingValueAlwaysRecalculateMD5() throws IOException{
-        final File ref1 = new File(getToolTestDataDir() + "hg19mini_missingmd5.fasta");
-        final File ref2 = new File(getToolTestDataDir() + "hg19mini_chr2snp.fasta");
-        final File output = createTempFile("testCompareReferencesAlwaysRecalculateMD5", ".table");
-        final File expectedOutput = new File(getToolTestDataDir(), "expected.testCompareReferencesAlwaysRecalculateMD5.table");
-
-        final String[] args = new String[] {"-R", ref1.getAbsolutePath() , "-refcomp", ref2.getAbsolutePath(),
-                "-O", output.getAbsolutePath(), "--md5-calculation-mode", "ALWAYS_RECALCULATE"};
-        runCommandLine(args);
-
-        IntegrationTestSpec.assertEqualTextFiles(output, expectedOutput);
     }
 
     @Test
