0b4df84ca26b7ea6c711281317b1297fedf6abaf
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-08-22 19:44:11 -0400

    oops

--- scripts/variantstore/wdl/extract/populate_alt_allele_table.py
+++ scripts/variantstore/wdl/extract/populate_alt_allele_table.py
@@ -35,18 +35,14 @@ def populate_alt_allele_table(call_set_identifier, query_project, vet_table_name
         position1 AS (
             SELECT * FROM `{fq_vet_table}` WHERE
                 call_GT IN ('0/1', '1/0', '1/1', '0|1', '1|0', '1|1', '0/2', '0|2','2/0', '2|0') AND
-                sample_id IN (
-                    SELECT sample_id from {fq_sample_info} WHERE
-                        (withdrawn is NULL {cutoff_condition}) AND sample_id > {max_sample_id}
-                )
-        )
+                sample_id > {max_sample_id} AND
+                sample_id IN (SELECT sample_id from {fq_sample_info} WHERE (withdrawn is NULL {cutoff_condition}))
+        ),
         position2 AS (
             SELECT * FROM `{fq_vet_table}` WHERE
                 call_GT IN ('1/2', '1|2', '2/1', '2|1') AND
-                sample_id IN (
-                    SELECT sample_id from {fq_sample_info} WHERE
-                        (withdrawn is NULL {cutoff_condition}) AND sample_id > {max_sample_id}
-                )
+                sample_id > {max_sample_id} AND
+                sample_id IN (SELECT sample_id from {fq_sample_info} WHERE (withdrawn is NULL {cutoff_condition}))
         )
     """
 
