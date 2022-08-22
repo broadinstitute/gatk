9918234201b17304c31f142d1e404c608ecf4e71
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-08-22 16:30:08 -0400

    wip

--- scripts/variantstore/wdl/extract/populate_alt_allele_table.py
+++ scripts/variantstore/wdl/extract/populate_alt_allele_table.py
@@ -21,7 +21,6 @@ def populate_alt_allele_table(call_set_identifier, query_project, vet_table_name
                              default_query_job_config=default_config)
 
     cutoff_condition = "OR withdrawn > '{withdrawn_cutoff_date}'" if withdrawn_cutoff_date else ""
-    withdrawn_clause = f"(withdrawn IS NULL {cutoff_condition})"
 
     os.chdir(os.path.dirname(__file__))
     alt_allele_temp_function = Path('alt_allele_temp_function.sql').read_text()
@@ -34,17 +33,21 @@ def populate_alt_allele_table(call_set_identifier, query_project, vet_table_name
     INSERT INTO `{fq_dataset}.alt_allele`
     WITH
         position1 AS (
-            SELECT * FROM `{fq_vet_table}` WHERE call_GT IN (
-                '0/1', '1/0', '1/1', '0|1', '1|0', '1|1', '0/2', '0|2','2/0', '2|0') AND
+            SELECT * FROM `{fq_vet_table}` WHERE
+                call_GT IN ('0/1', '1/0', '1/1', '0|1', '1|0', '1|1', '0/2', '0|2','2/0', '2|0') AND
                 sample_id IN (
-                    SELECT sample_id from {fq_sample_info} WHERE {withdrawn_clause} AND sample_id > {max_sample_id})
-            )
+                    SELECT sample_id from {fq_sample_info} WHERE
+                        (withdrawn is NULL {cutoff_condition}) AND sample_id > {max_sample_id}
+                )
+        )
         position2 AS (
-            SELECT * FROM `{fq_vet_table}` WHERE call_GT IN (
-                '1/2', '1|2', '2/1', '2|1') AND
+            SELECT * FROM `{fq_vet_table}` WHERE
+                call_GT IN ('1/2', '1|2', '2/1', '2|1') AND
                 sample_id IN (
-                    SELECT sample_id from {fq_sample_info} WHERE {withdrawn_clause} AND sample_id > {max_sample_id})
-            )
+                    SELECT sample_id from {fq_sample_info} WHERE
+                        (withdrawn is NULL {cutoff_condition}) AND sample_id > {max_sample_id}
+                )
+        )
     """
 
     sql = alt_allele_temp_function + query_with + alt_allele_positions
