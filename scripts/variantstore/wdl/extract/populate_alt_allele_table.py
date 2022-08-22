f548ec7562b0d17dd44846161217a2d677475bf5
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-08-22 11:55:35 -0400

    linty fresh

--- scripts/variantstore/wdl/extract/populate_alt_allele_table.py
+++ scripts/variantstore/wdl/extract/populate_alt_allele_table.py
@@ -13,7 +13,8 @@ client = None
 def populate_alt_allele_table(call_set_identifier, query_project, vet_table_name, fq_dataset, max_sample_id):
     global client
     # add labels for DSP Cloud Cost Control Labeling and Reporting to default_config
-    default_config = QueryJobConfig(priority="INTERACTIVE", use_query_cache=True, labels={'service':'gvs','team':'variants','managedby':'create_alt_allele'})
+    default_config = QueryJobConfig(priority="INTERACTIVE", use_query_cache=True,
+                                    labels={'service': 'gvs', 'team': 'variants', 'managedby': 'create_alt_allele'})
 
     client = bigquery.Client(project=query_project,
                              default_query_job_config=default_config)
@@ -29,20 +30,23 @@ def populate_alt_allele_table(call_set_identifier, query_project, vet_table_name
 
     sql = alt_allele_temp_function + query_with + alt_allele_positions
     query_return = utils.execute_with_retry(client, f"into alt allele from {vet_table_name}", sql)
-    utils.write_job_stats([{'job': query_return['job'], 'label': query_return['label']}], client, fq_dataset, call_set_identifier, 'CreateAltAlleles', 'PopulateAltAlleleTable', vet_table_name)
+    utils.write_job_stats([{'job': query_return['job'], 'label': query_return['label']}], client, fq_dataset,
+                          call_set_identifier, 'CreateAltAlleles', 'PopulateAltAlleleTable', vet_table_name)
     return query_return['results']
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(allow_abbrev=False, description='Populate an alt_allele table for the BigQuery Variant Store')
 
-    parser.add_argument('--call_set_identifier',type=str, help='callset identifier used to track costs in cost_observability table', default='false')
-    parser.add_argument('--query_project',type=str, help='Google project where query should be executed', required=True)
-    parser.add_argument('--vet_table_name',type=str, help='vet table name to ingest', required=True)
-    parser.add_argument('--fq_dataset',type=str, help='project and dataset for data', required=True)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(allow_abbrev=False,
+                                     description='Populate an alt_allele table for the BigQuery Variant Store')
+
+    parser.add_argument('--call_set_identifier', type=str,
+                        help='callset identifier used to track costs in cost_observability table', default='false')
+    parser.add_argument('--query_project', type=str, help='Google project where query should be executed',
+                        required=True)
+    parser.add_argument('--vet_table_name', type=str, help='vet table name to ingest', required=True)
+    parser.add_argument('--fq_dataset', type=str, help='project and dataset for data', required=True)
     parser.add_argument('--max_sample_id',type=str, help='Maximum value of sample_id already loaded', required=True)
 
-
-    # Execute the parse_args() method
     args = parser.parse_args()
 
     populate_alt_allele_table(args.call_set_identifier,
