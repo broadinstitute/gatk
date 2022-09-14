f49579d5545fe98ce09b226c726c7474350f086d
Chris Norman <cnorman@broadinstitute.org>
2022-09-14 10:00:21 -0400

    Debug cromwell.

--- scripts/vcf_site_level_filtering_cromwell_tests/run_vcf_site_level_filtering_wdl.sh
+++ scripts/vcf_site_level_filtering_cromwell_tests/run_vcf_site_level_filtering_wdl.sh
@@ -35,8 +35,4 @@ echo "=================="
 echo "Running Filtering WDL through cromwell"
 ln -fs $WORKING_DIR/gatk/scripts/vcf_site_level_filtering_wdl/JointVcfFiltering.wdl
 cd $WORKING_DIR/gatk/scripts/vcf_site_level_filtering_wdl/
-grep MemTotal /proc/meminfo
-df -h
-java -jar $CROMWELL_JAR run JointVcfFiltering.wdl -i $WORKING_DIR/vcf_site_level_filtering_travis.json
-grep MemTotal /proc/meminfo
-df -h
+java -DLOG_LEVEL=DEBUG -jar $CROMWELL_JAR run JointVcfFiltering.wdl -i $WORKING_DIR/vcf_site_level_filtering_travis.json
