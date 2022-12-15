ffa5ab030d24d2d612cbab232958e56783cfa9e3
Samuel Lee <lee.samuel.k@gmail.com>
2022-12-15 11:58:45 -0500

    BGMM WDL test

--- scripts/vcf_site_level_filtering_cromwell_tests/run_vcf_site_level_filtering_wdl.sh
+++ scripts/vcf_site_level_filtering_cromwell_tests/run_vcf_site_level_filtering_wdl.sh
@@ -28,8 +28,12 @@ fi
 echo "Docker build done =========="
 
 sed -r "s/__GATK_DOCKER__/broadinstitute\/gatk\:$HASH_TO_USE/g" $CROMWELL_TEST_DIR/vcf_site_level_filtering.json >$WORKING_DIR/vcf_site_level_filtering_mod.json
+sed -r "s/__GATK_DOCKER__/broadinstitute\/gatk\:$HASH_TO_USE/g" $CROMWELL_TEST_DIR/vcf_site_level_filtering_bgmm.json >$WORKING_DIR/vcf_site_level_filtering_bgmm_mod.json
 
 echo "Running Filtering WDL through cromwell"
 
 cat $WORKING_DIR/vcf_site_level_filtering_mod.json
 java -jar $CROMWELL_JAR run $WDL_DIR/JointVcfFiltering.wdl -i $WORKING_DIR/vcf_site_level_filtering_mod.json
+
+cat $WORKING_DIR/vcf_site_level_filtering_bgmm_mod.json
+java -jar $CROMWELL_JAR run $WDL_DIR/JointVcfFiltering.wdl -i $WORKING_DIR/vcf_site_level_filtering_bgmm_mod.json
