a99a825ed4a67b0bf7adb0aaf302b9676a1d615f
gbggrant <ggrant@broadinstitute.org>
2023-01-04 14:28:21 -0500

    Address code review feedback, including storing test files as text (not compressed)

--- scripts/variantstore/wdl/extract/test_create_vt_bqloadjson_from_annotations.py
+++ scripts/variantstore/wdl/extract/test_create_vt_bqloadjson_from_annotations.py
@@ -3,15 +3,11 @@ import unittest
 import copy
 import gzip
 import tempfile
+from pathlib import Path
 
 from create_vt_bqloadjson_from_annotations import make_annotation_json, make_annotated_json_row
 
 dir='variant_annotation_table_test_files/'
-normal_annotated_positions_json=dir + "test_annotated_positions.json.gz"
-expected_output_annotated_positions_bq_json=dir + "expected_test_output_annotated_positions_bq.json.gz"
-
-# An old style (before we separated positions and genes jsons into separate files) - expected to fail
-old_annotated_positions_json=dir + "test_old_annotated_positions.json.gz"
 
 with open(dir + 'vat_expected_pathogenic.json') as vat_expected_pathogenic, open(dir + 'vat_test_pathogenic.json') as vat_test_pathogenic:
     cysticFibrosisExpectedVAT = json.load(vat_expected_pathogenic)
@@ -22,15 +18,24 @@ with open(dir + 'vat_expected_likely_benign.json') as vat_expected_likely_benign
 
 class TestCreateVtBqloadjsonFromAnnotations(unittest.TestCase):
     def test_normal(self):
+        normal_annotated_positions_json=dir + "test_annotated_positions.json"
+        expected_output_annotated_positions_bq_json=dir + "expected_test_output_annotated_positions_bq.json"
+
         output_json = tempfile.NamedTemporaryFile(prefix="test_output_annotated_positions_bq", suffix=".json.gz")
         make_annotation_json(normal_annotated_positions_json, output_json.name)
-        contents = gzip.open(output_json, 'r').read()
-        expected_contents = gzip.open(expected_output_annotated_positions_bq_json, 'r').read()
+        contents = gzip.open(output_json, 'rt').read()
+        expected_contents = Path(expected_output_annotated_positions_bq_json).read_text()
+
+        # contents = Path(output_json).read_text()
+        # expected_contents = Path(expected_output_annotated_positions_bq_json).read_text()
         self.assertEqual(contents, expected_contents)
 
     # This test tries to read an old-style json file and verifies that the program will fail if fed such a file
     @unittest.expectedFailure
     def test_old(self):
+        # An old style (before we separated positions and genes jsons into separate files) - expected to fail
+        old_annotated_positions_json=dir + "test_old_annotated_positions.json"
+
         output_json = tempfile.NamedTemporaryFile(prefix="test_old_output_annotated_positions_bq", suffix=".json.gz")
         make_annotation_json(old_annotated_positions_json, output_json.name)
 
