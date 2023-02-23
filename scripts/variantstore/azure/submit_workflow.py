f7e16a9257097f133eff771e4a7b309fbaee28cd
Miguel Covarrubias <mcovarr@broadinstitute.org>
2023-02-23 18:32:34 -0500

    more

--- scripts/variantstore/azure/submit_workflow.py
+++ scripts/variantstore/azure/submit_workflow.py
@@ -1,5 +1,4 @@
 import argparse
-import sys
 
 import inflection
 import os
@@ -10,7 +9,7 @@ from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.sql import SqlManagementClient
 from azure.mgmt.storage import StorageManagementClient
 from azure.mgmt.subscription import SubscriptionClient
-from azure.storage.blob import BlobClient, BlobServiceClient
+from azure.storage.blob import BlobServiceClient
 
 from pathlib import Path
 from uuid import uuid4
@@ -91,20 +90,20 @@ def get_blob_service_client():
     return BlobServiceClient.from_connection_string(os.getenv('AZURE_CONNECTION_STRING'))
 
 
-def generate_inputs_json(workflow_name, access_token_storage_path, python_script_storage_path,
-                         ammonite_script_storage_path, server_name, database_name):
+def generate_inputs_json():
+    workflow_name = Path(args.workflow).stem
     return f"""
 {{
   "{workflow_name}.utf8_token_file": "{access_token_storage_path}",
   "{workflow_name}.python_script": "{python_script_storage_path}",
   "{workflow_name}.ammonite_script": "{ammonite_script_storage_path}",
-  "{workflow_name}.sql_server": "{server_name}",
-  "{workflow_name}.sql_database": "{database_name}"
+  "{workflow_name}.sql_server": "{args.sql_server}",
+  "{workflow_name}.sql_database": "{args.sql_database}"
 }}
 """
 
 
-def generate_trigger_json(workflow_storage_path, inputs_storage_path):
+def generate_trigger_json():
     """
     Creates a trigger JSON of the form accepted by CromwellOnAzure. This is conceptually similar to the JSON payload
     of a workflow submission POST that would normally go to Cromwell's REST interface.
@@ -120,16 +119,60 @@ def generate_trigger_json(workflow_storage_path, inputs_storage_path):
     """.strip()
 
 
+def stage_input(input_file, blob_address=None):
+    workflow_path = Path(args.workflow)
+    input_path = Path(input_file)
+    with open(input_file, "rb") as input_bytes:
+        # `name` is the filename without leading directory components.
+        # e.g. name for /path/to/Hello.wdl is Hello.wdl
+        # `stem` is the filename without leading directory components and without an extension.
+        # e.g. stem for /path/to/Hello.wdl is Hello
+
+        # `inflection.underscore` snake-cases Pascal-cased workflow names. Not strictly required here but nice.
+        # e.g. "HelloAzure" ==> "hello_azure"
+        if not blob_address:
+            blob_address = f"{inflection.underscore(workflow_path.stem)}/{input_path.name}"
+        blob_client = inputs_client.get_blob_client(blob_address)
+        blob_client.upload_blob(input_bytes, overwrite=True)
+        storage_path = f'/{storage_account.name}/inputs/{blob_address}'
+        return storage_path
+
+
+def stage_trigger_json():
+    workflow_path = Path(args.workflow)
+    # Create the trigger JSON and stage into /<storage account name>/workflows/new.
+    trigger_json = generate_trigger_json()
+    workflows_client = blob_service_client.get_container_client('workflows')
+
+    blob_address = f'new/{workflow_path.stem}-{uuid4()}.json'
+    blob_client = workflows_client.get_blob_client(blob_address)
+    blob_client.upload_blob(bytes(trigger_json, 'utf8'))
+
+    print(f"Trigger JSON staged to /{storage_account.name}/workflows/{blob_address}.")
+
+
+def stage_inputs_json():
+    workflow_path = Path(args.workflow)
+    # Stage the workflow inputs into /<storage container>/inputs/<snake cased workflow name>/<workflow name>.inputs.json
+    blob_address = f"{inflection.underscore(workflow_path.stem)}/{workflow_path.stem}.inputs.json"
+    blob_client = inputs_client.get_blob_client(blob_address)
+    blob_client.upload_blob(bytes(inputs_json, 'utf8'), overwrite=True)
+    inputs_storage_path = f'/{storage_account.name}/inputs/{blob_address}'
+    return inputs_storage_path
+
+
 if __name__ == '__main__':
     description = """
 
-    Cromwell on Azure (CoA) "Hello Azure!" workflow submission script does the following:
+    Cromwell on Azure (CoA) "Hello Azure!" workflow submission script that does the following:
 
     1. Stages the workflow and its `File` inputs (scripts, database access token) to the CoA inputs container.
     2. Generates an inputs JSON corresponding to the inputs file from 1. and stages this to the CoA inputs container.
     3. Generates a trigger JSON for this workflow + inputs and stages this to the CoA workflows container under 'new'.
 
     The script does *not* attempt to poll the submitted workflow for status, this is simple "fire and forget".
+    Observing workflow progress involves poking around the 'workflows' and 'cromwell-executions' containers within the
+    storage account created as part of the Cromwell on Azure deployment.
     """
 
     parser = argparse.ArgumentParser(allow_abbrev=False, description=description)
@@ -151,6 +194,7 @@ if __name__ == '__main__':
     # https://github.com/Azure/azure-sdk-for-python/issues/22822#issuecomment-1024668507
     credentials = DefaultAzureCredential(exclude_shared_token_cache_credential=True)
 
+    # Figure out the particulars of our Cromwell on Azure deployment
     subscription = get_subscription(credentials)
     resource_group = get_resource_group(credentials, subscription, args.resource_group)
     storage_account = get_storage_account(credentials, subscription, resource_group)
@@ -159,60 +203,16 @@ if __name__ == '__main__':
 
     blob_service_client = get_blob_service_client()
     inputs_client = blob_service_client.get_container_client('inputs')
-    # `name` is the filename without leading directory components.
-    # e.g. name for /path/to/Hello.wdl is Hello.wdl
-    # `stem` is the filename without leading directory components and without an extension.
-    # e.g. stem for /path/to/Hello.wdl is Hello
-    workflow_path = Path(args.workflow)
 
-    # Stage the workflow into /<storage container>/inputs/<snake cased workflow name>/<workflow file>.
-    with open(args.workflow, "rb") as workflow:
-        # `inflection.underscore` snake-cases Pascal-cased workflow names. Not strictly required here but nice.
-        # e.g. "HelloAzure" ==> "hello_azure"
-        blob_address = f"{inflection.underscore(workflow_path.stem)}/{workflow_path.name}"
-        blob_client = inputs_client.get_blob_client(blob_address)
-        blob_client.upload_blob(workflow, overwrite=True)
-        workflow_storage_path = f'/{storage_account.name}/inputs/{blob_address}'
+    # Stage the inputs into /<storage container>/inputs/<snake cased workflow name>/<input file>.
+    workflow_storage_path = stage_input(args.workflow)
+    python_script_storage_path = stage_input(args.python_script)
+    ammonite_script_storage_path = stage_input(args.ammonite_script)
+    access_token_storage_path = stage_input(args.utf8_access_token, blob_address=f"{os.environ['USER']}/db_access_token.txt")
 
-    # Stage the Python script into /<storage container>/inputs/<snake cased workflow name>/<Python script>.
-    python_script_path = Path(args.python_script)
-    with open(args.python_script, "rb") as python_script:
-        blob_address = f"{inflection.underscore(workflow_path.stem)}/{python_script_path.name}"
-        blob_client = inputs_client.get_blob_client(blob_address)
-        blob_client.upload_blob(python_script, overwrite=True)
-        python_script_storage_path = f'/{storage_account.name}/inputs/{blob_address}'
+    # Generate the inputs JSON using the values above and stage
+    inputs_json = generate_inputs_json()
+    inputs_storage_path = stage_inputs_json()
 
-    # Stage the Ammonite script into /<storage container>/inputs/<snake cased workflow name>/<Ammonite script>.
-    ammonite_script_path = Path(args.ammonite_script)
-    with open(args.ammonite_script, "rb") as ammonite_script:
-        blob_address = f"{inflection.underscore(workflow_path.stem)}/{ammonite_script_path.name}"
-        blob_client = inputs_client.get_blob_client(blob_address)
-        blob_client.upload_blob(ammonite_script, overwrite=True)
-        ammonite_script_storage_path = f'/{storage_account.name}/inputs/{blob_address}'
-
-    # Stage the access token into /<storage container>/inputs/<user name>/db_access_token.txt
-    with open(args.utf8_access_token, "rb") as token:
-        blob_address = f"{os.environ['USER']}/db_access_token.txt"
-        blob_client = inputs_client.get_blob_client(blob_address)
-        blob_client.upload_blob(token, overwrite=True)
-        access_token_storage_path = f'/{storage_account.name}/inputs/{blob_address}'
-
-    inputs_json = generate_inputs_json(
-        workflow_path.stem, access_token_storage_path, python_script_storage_path, ammonite_script_storage_path,
-        args.sql_server, args.sql_database)
-
-    # Stage the workflow inputs into /<storage container>/inputs/<snake cased workflow name>/<workflow name>.inputs.json
-    blob_address = f"{inflection.underscore(workflow_path.stem)}/{workflow_path.stem}.inputs.json"
-    blob_client = inputs_client.get_blob_client(blob_address)
-    blob_client.upload_blob(bytes(inputs_json, 'utf8'), overwrite=True)
-    inputs_storage_path = f'/{storage_account.name}/inputs/{blob_address}'
-
-    # Create the trigger JSON and stage into /<storage account name>/workflows/new.
-    trigger_json = generate_trigger_json(workflow_storage_path, inputs_storage_path)
-    workflows_client = blob_service_client.get_container_client('workflows')
-
-    blob_address = f'new/{workflow_path.stem}-{uuid4()}.json'
-    blob_client = workflows_client.get_blob_client(blob_address)
-    blob_client.upload_blob(bytes(trigger_json, 'utf8'))
-
-    print(f"Trigger JSON staged to /{storage_account.name}/workflows/{blob_address}.")
+    # Finally stage the trigger JSON that will kick off workflow execution
+    stage_trigger_json()
