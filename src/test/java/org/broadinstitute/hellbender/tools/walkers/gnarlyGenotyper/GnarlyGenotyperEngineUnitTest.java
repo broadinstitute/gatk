19f90bc787d17955ff69fd2397826d5715f4b647
Louis Bergelson <louisb@broadinstitute.org>
2023-03-22 12:59:24 -0400

    Incorporating changes from GVS to existing files
    * Absorbing the changes to core GATK files from the long running GVS work.
    * Based on the vs_834_deletions branch where code was factored to separate the gvs
      specific classes from the shared gatk code
    * There were fairly minimal conflicts between the two except for in the new VQSR package.
      In this case we've taken the master version of those files since the GVS version is out
      of date.

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/gnarlyGenotyper/GnarlyGenotyperEngineUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/gnarlyGenotyper/GnarlyGenotyperEngineUnitTest.java
@@ -31,7 +31,7 @@ public class GnarlyGenotyperEngineUnitTest {
     //use more alts than the maxAltAllelesToOutput for the engine, forcing on-the-fly generation of PL counts not in the cache
     @Test
     public void testLotsOfAlts() {
-        final GnarlyGenotyperEngine engine = new GnarlyGenotyperEngine(false, 4, true);
+        final GnarlyGenotyperEngine engine = new GnarlyGenotyperEngine(false, 4, false, true);
 
         final Genotype g1 = VariantContextTestUtils.makeG("g1", oneInserted, twoInserted, sample1pls);
         final Genotype g2 = VariantContextTestUtils.makeG("g1", Aref, oneInserted, sample2pls);
@@ -45,16 +45,23 @@ public class GnarlyGenotyperEngineUnitTest {
         final int[] rawGenotypeCounts = {0, 1, 1};
 
         final GenotypesContext genotypes = engine.iterateOnGenotypes(vc, Arrays.asList(Aref, oneInserted, twoInserted, threeInserted, fourRepeats),
-                alleleCounts, sbSum, false, rawGenotypeCounts);
+                alleleCounts, sbSum, false, true, rawGenotypeCounts);
 
         Assert.assertTrue(genotypes.get(0).hasPL() && genotypes.get(0).getPL().length == 15);
         Assert.assertTrue(genotypes.get(1).hasPL() && genotypes.get(1).getPL().length == 15);
+
+        // repeat, but this time request that PLs are not returned in the GenotypesContext and verify
+        final GenotypesContext genotypesNoPl = engine.iterateOnGenotypes(vc, Arrays.asList(Aref, oneInserted, twoInserted, threeInserted, fourRepeats),
+                alleleCounts, sbSum, false, false, rawGenotypeCounts);
+
+        Assert.assertFalse(genotypesNoPl.get(0).hasPL());
+        Assert.assertFalse(genotypesNoPl.get(1).hasPL());
     }
 
     //use more alts than the maxAltAllelesToOutput for the engine, forcing on-the-fly generation of GLCalculator not in the cache
     @Test
     public void testGenotypeCallForLotsOfAlts() {
-        final GnarlyGenotyperEngine engine = new GnarlyGenotyperEngine(false, 4, true);
+        final GnarlyGenotyperEngine engine = new GnarlyGenotyperEngine(false, 4, false, true);
 
         final Genotype g1 = VariantContextTestUtils.makeG("g1", oneInserted, twoInserted, sample1pls);
         final Genotype g2 = VariantContextTestUtils.makeG("g1", Aref, oneInserted, sample2pls);
