6e6983eb63788d9d7a80a486529aeaa9b2725049
gbggrant <ggrant@broadinstitute.org>
2023-04-12 14:50:33 -0400

    Cleaned up python errors in the test_summarize_task_monitor_logs.py script.

--- scripts/variantstore/wdl/extract/test_summarize_task_monitor_logs.py
+++ scripts/variantstore/wdl/extract/test_summarize_task_monitor_logs.py
@@ -2,12 +2,15 @@ import unittest
 
 from summarize_task_monitor_logs import parse_monitoring_log_files
 
+
 class TestSummarizeTaskMonitorLogs(unittest.TestCase):
     def test_parse_monitoring_log_files(self):
         import tempfile
 
-        input_files = ['summarize_task_monitor_logs_test_files/call-ScoreVariantAnnotationsINDELs/shard-34/monitoring.log',
-                       'summarize_task_monitor_logs_test_files/call-ScoreVariantAnnotationsINDELs/shard-35/monitoring.log',
+        input_files = ['summarize_task_monitor_logs_test_files/call-ScoreVariantAnnotationsINDELs/shard-34/monitoring'
+                       '.log',
+                       'summarize_task_monitor_logs_test_files/call-ScoreVariantAnnotationsINDELs/shard-35/monitoring'
+                       '.log',
                        'summarize_task_monitor_logs_test_files/call-IndelsVariantRecalibrator/monitoring.log',
                        'summarize_task_monitor_logs_test_files/call-ExtractFilterTask/shard-0/cacheCopy/monitoring.log',
                        'summarize_task_monitor_logs_test_files/call-MergeVCFs/cacheCopy/monitoring.log']
@@ -16,12 +19,13 @@ class TestSummarizeTaskMonitorLogs(unittest.TestCase):
 
             expected_output_file = 'summarize_task_monitor_logs_test_files/expected_monitoring_summary_file.txt'
             with open(actual_output_file.name, 'r') as actual, open(expected_output_file, 'r') as expected:
-                while (True):
+                while True:
                     actual_line = actual.readline().rstrip()
                     expected_line = expected.readline().rstrip()
                     if (actual_line == "") or (expected_line == ""):
                         break
-                    # Need to ignore any diffs in the file path (last element) as it differs on the test running under the docker.
+                    # We Need to ignore any diffs in the file path (last element)
+                    # as it differs on the test running under the docker.
                     actual_line = ",".join(actual_line.split("\t")[0:-1])
                     expected_line = ",".join(expected_line.split("\t")[0:-1])
                     self.assertEqual(actual_line, expected_line)
