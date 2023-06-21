da5f9bba7722cc0cf09c2ab2189ba4d59d9673a8
Miguel Covarrubias <mcovarr@broadinstitute.org>
2023-06-21 18:48:28 -0400

    missed test

--- src/test/java/org/broadinstitute/hellbender/utils/gvs/bigquery/BigQueryUtilsUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/utils/gvs/bigquery/BigQueryUtilsUnitTest.java
@@ -1,4 +1,4 @@
-package org.broadinstitute.hellbender.utils.bigquery;
+package org.broadinstitute.hellbender.utils.gvs.bigquery;
 
 import com.google.cloud.bigquery.FieldValueList;
 import org.apache.avro.generic.GenericRecord;
