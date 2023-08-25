f599a763180ff75aa0508d910dcff7b294b4f18e
Rori Cremer <6863459+RoriCremer@users.noreply.github.com>
2023-08-25 16:08:39 -0400

    add Dan King notes

--- scripts/variantstore/wdl/extract/import_gvs.py
+++ scripts/variantstore/wdl/extract/import_gvs.py
@@ -139,10 +139,24 @@ def import_gvs(refs: 'List[List[str]]',
     use_classic_vqsr : :class:`bool`
         Expect input Avro files to have been generated from VQSR 'Classic' data
 
+    Script workflow:
+    ---------------
+    * Load VQSR data into table.
+    * Convert each 4k sample group into a VDS.
+    * Run the combiner on the VDSes (which does a hierarchical merge).
+    * Annotate with VQSR / filter.
+    * Compute FT genotype annotation.
+    * Remove phase on LGT.
+    * Add GT (computed from LGT).
+    * Write final VDS.
+
     Returns
     -------
     :class:`.VariantDataset`
+
     """
+
+
     from hail.utils.java import info
     vds_paths = []
     assert len(refs) == len(vets)
