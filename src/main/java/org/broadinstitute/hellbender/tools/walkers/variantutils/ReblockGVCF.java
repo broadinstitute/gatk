de371aa9578dad3a6de5a44785db6de07074062c
meganshand <mshand@broadinstitute.org>
2023-08-25 09:07:22 -0400

    Arg to move site level filters to genotype level filters in ReblockGvcfs (#8484)

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/ReblockGVCF.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/ReblockGVCF.java
@@ -115,6 +115,7 @@ public final class ReblockGVCF extends MultiVariantWalker {
     public static final String ALLOW_MISSING_LONG_NAME = "allow-missing-hom-ref-data";
     public static final String KEEP_SITE_FILTERS_LONG_NAME = "keep-site-filters";
     public static final String KEEP_SITE_FILTERS_SHORT_NAME = "keep-filters";
+    public static final String ADD_FILTERS_TO_GENOTYPE = "add-site-filters-to-genotype";
 
     @Argument(fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME, shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
             doc="File to which variants should be written")
@@ -174,6 +175,10 @@ public final class ReblockGVCF extends MultiVariantWalker {
     @Advanced
     @Argument(fullName=KEEP_SITE_FILTERS_LONG_NAME, shortName = KEEP_SITE_FILTERS_SHORT_NAME, doc="Keep site level filters for variants (not ref blocks).")
     private boolean keepFilters = false;
+
+    @Advanced
+    @Argument(fullName= ADD_FILTERS_TO_GENOTYPE, doc="Add site level filters to genotype level. Site level filters removed by default, if they should be kept, use --" + KEEP_SITE_FILTERS_LONG_NAME)
+    private boolean addFiltersToFormatField = false;
 
     //TODO: this will be an argument when posteriors handling is fully implemented in AlleleSubsettingUtils
     protected String posteriorsKey = null;
@@ -271,6 +276,10 @@ public final class ReblockGVCF extends MultiVariantWalker {
             } else {
                 throw new UserException(String.format("%s is not in header of input GVCF but was requested to be kept by %s argument.", annotation, ANNOTATIONS_TO_KEEP_LONG_NAME));
             }
+        }
+
+        if (addFiltersToFormatField) {
+            headerLines.add(VCFStandardHeaderLines.getFormatLine(VCFConstants.GENOTYPE_FILTER_KEY));
         }
 
         //Allele length and Genotype length annotations need to be subset or removed if alleles are dropped so we need to parse the header for annotation count types
@@ -677,8 +686,13 @@ public final class ReblockGVCF extends MultiVariantWalker {
         final Genotype updatedAllelesGenotype = updatedAllelesVC.getGenotype(0);
 
         //remove any AD reads for the non-ref
-        final List<Genotype> genotypesArray = removeNonRefADs(updatedAllelesGenotype, updatedAllelesVC.getAlleleIndex(Allele.NON_REF_ALLELE));
-        builder.genotypes(genotypesArray);
+        final Genotype g = removeNonRefADs(updatedAllelesGenotype, updatedAllelesVC.getAlleleIndex(Allele.NON_REF_ALLELE));
+        if (addFiltersToFormatField) {
+            final List<String> filters = updatedAllelesVC.getFilters().stream().toList();
+            builder.genotypes(new GenotypeBuilder(g).filters(filters).make());
+        } else {
+            builder.genotypes(g);
+        }
 
         composeUpdatedAnnotations(attrMap, doQualApprox, posteriorsKey, variant, annotationEngine, relevantIndices, updatedAllelesVC, annotationsToKeep);
 
@@ -882,9 +896,9 @@ public final class ReblockGVCF extends MultiVariantWalker {
      * Any AD counts for the non-ref allele get propagated to every new allele when GVCFs are merged, so zero them out
      * @param g a genotype that may or may not contain AD
      * @param nonRefInd allele index of the non-ref, -1 if missing
-     * @return  an unmodifiable Genotype array that can be used by a GenotypeBuilder
+     * @return  a Genotype with the non-ref AD zeroed out and new DP, or the original genotype if it doesn't have AD
      */
-    private List<Genotype> removeNonRefADs(final Genotype g, final int nonRefInd) {
+    private Genotype removeNonRefADs(final Genotype g, final int nonRefInd) {
         if (g.hasAD() && nonRefInd != -1) {
             final int[] ad = g.getAD();
             if (ad.length >= nonRefInd && ad[nonRefInd] > 0) { //only initialize a builder if we have to
@@ -897,12 +911,12 @@ public final class ReblockGVCF extends MultiVariantWalker {
                 } else {
                     gb.DP((int) MathUtils.sum(ad));
                 }
-                return Collections.singletonList(gb.make());
+                return gb.make();
             } else {
-                return Collections.singletonList(g);
+                return g;
             }
         } else {
-            return Collections.singletonList(g);
+            return g;
         }
     }
 
