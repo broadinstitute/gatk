4b37b9df533feee2c5032974c370e4c15763de67
Aaron Hatcher <hatcher@broadinstitute.org>
2023-09-22 13:43:32 -0400

    I died a little inside, but I changed all of the variables in close_bed_file_gaps.py to use snake case to mAkE mIgUeL hApPy :-/

--- scripts/variantstore/wdl/extract/close_bed_file_gaps.py
+++ scripts/variantstore/wdl/extract/close_bed_file_gaps.py
@@ -3,16 +3,11 @@ import argparse
 import re
 import math
 
-# put these up here to get the defaults.  But override them later with user input
-binsize_kb = 100
-infile = f"gvs_vet_weights_{binsize_kb}kb.bed"
-outfile = f"gvs_vet_weights_{binsize_kb}kb_gaps_filled.bed"
-
 argParser = argparse.ArgumentParser()
 
-argParser.add_argument("-s", "--bin-size", help="size of the bins in kb", type=int, default=binsize_kb)
-argParser.add_argument("-i", "--infile", help="bed input file", type=str, default=infile)
-argParser.add_argument("-o", "--outfile", help="bed output file", type=str, default=outfile)
+argParser.add_argument("-s", "--bin-size", help="size of the bins in kb", type=int)
+argParser.add_argument("-i", "--infile", help="bed input file", type=str)
+argParser.add_argument("-o", "--outfile", help="bed output file", type=str)
 argParser.add_argument("-l", "--interval-list", help="an interval list we can use to fill in any data on a contig past the end of our samples", type=str)
 
 args = argParser.parse_args()
@@ -21,7 +16,7 @@ binsize_kb = args.bin_size
 infile = args.infile
 outfile = args.outfile
 
-contigSizes = {}
+contig_sizes = {}
 # an interval list is not required.  But if one is supplied, we'll use it to determine how long each contig is so we can
 # pad out our data to that size.  We yank the contig name and length info directly from the lines using simple regexes
 if args.interval_list:
@@ -35,14 +30,14 @@ if args.interval_list:
 				if contig_result and length_result:
 					contig = contig_result.group(1)
 					contig_length = length_result.group(1)
-					contigSizes[contig] = int(contig_length)
+					contig_sizes[contig] = int(contig_length)
 else:
 	print("We do not have an interval list")
 
-lastContig = ""
-lastEnding = 0
-intervalSize = binsize_kb * 1000
-print(f"Interval size is {intervalSize}")
+last_contig = ""
+last_ending = 0
+interval_size = binsize_kb * 1000
+print(f"Interval size is {interval_size}")
 
 with open(infile, newline='') as bedfile, open(outfile, 'w') as output:
 	bedreader = csv.reader(bedfile, delimiter='\t')
@@ -55,57 +50,57 @@ with open(infile, newline='') as bedfile, open(outfile, 'w') as output:
 		start = int(row[1])
 		ending = int(row[2])
 		# Logic to detect gaps in the intervals is only valid WITHIN a contig
-		if contig == lastContig:
+		if contig == last_contig:
 			# the intervals are read as [,) so the final entry in the previous one should
 			# match the first entry in the next one if there are no gaps
-			if start != lastEnding:
-				print(f"detected gap from {contig} {lastEnding} to {start}")
-				sizeOfGap = start - lastEnding
-				numEmptyEntries = sizeOfGap / intervalSize
-				print(f"Will create {numEmptyEntries} 0 weight entries here")
-				for s in range(lastEnding, start, intervalSize):
-					print(f"\t{contig} {s} - {s + intervalSize}")
-					newRow = [contig, str(s), str(s+intervalSize), ".", "0"]
-					output.write("\t".join(newRow) + "\n")
+			if start != last_ending:
+				print(f"detected gap from {contig} {last_ending} to {start}")
+				size_of_gap = start - last_ending
+				num_empty_entries = size_of_gap / interval_size
+				print(f"Will create {num_empty_entries} 0 weight entries here")
+				for s in range(last_ending, start, interval_size):
+					print(f"\t{contig} {s} - {s + interval_size}")
+					new_row = [contig, str(s), str(s + interval_size), ".", "0"]
+					output.write("\t".join(new_row) + "\n")
 		else:
 			# see if there's anything on the end of the last contig to extend out to
-			if lastContig in contigSizes:
+			if last_contig in contig_sizes:
 				# we'll want to round up the listed ending to the next block in case we
 				# haven't reached it yet
-				newEndingBlock = int(math.ceil(contigSizes[lastContig] / float(intervalSize))) * intervalSize
-				print(f"Ending detected for contig: {lastContig} rounded up to block {newEndingBlock}.  Last block written was {lastEnding}")
-				for s in range(lastEnding, newEndingBlock, intervalSize):
-					print(f"\t{lastContig} {s} - {s + intervalSize}")
-					newRow = [lastContig, str(s), str(s+intervalSize), ".", "0"]
-					output.write("\t".join(newRow) + "\n")
+				new_ending_block = int(math.ceil(contig_sizes[last_contig] / float(interval_size))) * interval_size
+				print(f"Ending detected for contig: {last_contig} rounded up to block {new_ending_block}.  Last block written was {last_ending}")
+				for s in range(last_ending, new_ending_block, interval_size):
+					print(f"\t{last_contig} {s} - {s + interval_size}")
+					new_row = [last_contig, str(s), str(s + interval_size), ".", "0"]
+					output.write("\t".join(new_row) + "\n")
 			# make sure we always start at 0 on a new contig and search for gaps
 			print(f"detected gap at beginning of {contig}")
-			lastEnding = 0
-			sizeOfGap = start
-			numEmptyEntries = sizeOfGap / intervalSize
-			print(f"Would create {numEmptyEntries} 0 weight entries here")
-			for s in range(lastEnding, start, intervalSize):
-				print(f"\t{contig} {s} - {s + intervalSize}")
-				newRow = [contig, str(s), str(s+intervalSize), ".", "0"]
-				output.write("\t".join(newRow) + "\n")
+			last_ending = 0
+			size_of_gap = start
+			num_empty_entries = size_of_gap / interval_size
+			print(f"Would create {num_empty_entries} 0 weight entries here")
+			for s in range(last_ending, start, interval_size):
+				print(f"\t{contig} {s} - {s + interval_size}")
+				new_row = [contig, str(s), str(s + interval_size), ".", "0"]
+				output.write("\t".join(new_row) + "\n")
 			
-		lastContig = contig
-		lastEnding = ending
+		last_contig = contig
+		last_ending = ending
 		# now that we've inserted any fake rows necessary to fill gaps, write the current one
 		output.write("\t".join(row) + "\n")
 		
 		
 	#end for loop
 	# check for anything leftover at the end of the last contig
-	if lastContig in contigSizes:
+	if last_contig in contig_sizes:
 		# we'll want to round up the listed ending to the next block in case we
 		# haven't reached it yet
-		newEndingBlock = int(math.ceil(contigSizes[lastContig] / float(intervalSize))) * intervalSize
-		print(f"Ending detected for contig: {lastContig} rounded up to block {newEndingBlock}.  Last block written was {lastEnding}")
-		for s in range(lastEnding, newEndingBlock, intervalSize):
-			print(f"\t{lastContig} {s} - {s + intervalSize}")
-			newRow = [lastContig, str(s), str(s+intervalSize), ".", "0"]
-			output.write("\t".join(newRow) + "\n")
+		new_ending_block = int(math.ceil(contig_sizes[last_contig] / float(interval_size))) * interval_size
+		print(f"Ending detected for contig: {last_contig} rounded up to block {new_ending_block}.  Last block written was {last_ending}")
+		for s in range(last_ending, new_ending_block, interval_size):
+			print(f"\t{last_contig} {s} - {s + interval_size}")
+			new_row = [last_contig, str(s), str(s + interval_size), ".", "0"]
+			output.write("\t".join(new_row) + "\n")
 	print("Processing done")
 
 
