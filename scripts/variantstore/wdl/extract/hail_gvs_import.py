d388fddf83c806bf276406a3a533b207d981b90c
Miguel Covarrubias <mcovarr@users.noreply.github.com>
2023-11-07 12:33:01 -0500

    Apply suggestions from code review
    Co-authored-by: Dan King <daniel.zidan.king@gmail.com>

--- scripts/variantstore/wdl/extract/hail_gvs_import.py
+++ scripts/variantstore/wdl/extract/hail_gvs_import.py
@@ -23,7 +23,7 @@ def create_vds(argsfn, vds_path, references_path, temp_path, use_classic_vqsr):
     rg38.add_sequence(f'{references_path}/Homo_sapiens_assembly38.fasta.gz',
                       f'{references_path}/Homo_sapiens_assembly38.fasta.fai')
 
-    # A full description of the `import_vds` function written by Hail for this process can be found in `import_vds.py`:
+    # A full description of the `import_vds` function written by Hail for this process can be found in `import_gvs.py`:
     # https://github.com/broadinstitute/gatk/blob/ah_var_store/scripts/variantstore/wdl/extract/import_gvs.py
     # Commented out parameters are ones where we are comfortable with the default, but want to make them easily
     # accessible to users.
@@ -48,7 +48,7 @@ def create_vds(argsfn, vds_path, references_path, temp_path, use_classic_vqsr):
 
 def gcs_generate_avro_args(bucket, blob_prefix, key):
     """
-    Generate a list of the Avro arguments for the `import_vds` invocation for the specified key. The datatype should
+    Generate a list of the Avro arguments for the `import_gvs` invocation for the specified key. The datatype should
     match these parameters:
 
     * vets (list of lists, one outer list per GVS superpartition of 4000 samples max)
