9df43e7995690420a59a2f78c784a056e75c58e2
David Benjamin <davidben@broadinstitute.org>
2023-11-17 11:06:44 -0500

    Extend GenotypingModel interface to include the override

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/genotyper/GenotypingModel.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/genotyper/GenotypingModel.java
@@ -4,11 +4,22 @@ import htsjdk.variant.variantcontext.Allele;
 import org.broadinstitute.hellbender.utils.genotyper.AlleleList;
 import org.broadinstitute.hellbender.utils.dragstr.DragstrReferenceAnalyzer;
 
+import java.util.Optional;
+
 /**
  * A wrapping interface between the various versions of genotypers so as to keep them interchangeable.
  */
 public interface GenotypingModel {
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public <A extends Allele> GenotypingLikelihoods<A> calculateLikelihoods(AlleleList<A> genotypingAlleles, GenotypingData<A> data, final byte[] paddedReference, final int offsetForRefIntoEvent, final DragstrReferenceAnalyzer dragstrs);
+    public <A extends Allele> GenotypingLikelihoods<A> calculateLikelihoods(AlleleList<A> genotypingAlleles, GenotypingData<A> data,
+        final byte[] paddedReference, final int offsetForRefIntoEvent, final DragstrReferenceAnalyzer dragstrs);
 
+    default <A extends Allele> GenotypingLikelihoods<A> calculateLikelihoods(AlleleList<A> genotypingAlleles, GenotypingData<A> data,
+        final byte[] paddedReference, final int offsetForRefIntoEvent, final DragstrReferenceAnalyzer dragstrs, Optional<GenotypingLikelihoods<A>> glsOverride) {
+        if (glsOverride.isEmpty()) {
+            return calculateLikelihoods(genotypingAlleles, data, paddedReference, offsetForRefIntoEvent, dragstrs);
+        } else {
+            throw new UnsupportedOperationException("This GenotypingModel class does not implement a GL-override mode.");
+        }
+    }
 }
