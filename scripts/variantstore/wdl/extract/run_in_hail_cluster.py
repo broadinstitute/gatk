f1c379c6afe5e956d22520f29b6028fc94f66901
Rori Cremer <6863459+RoriCremer@users.noreply.github.com>
2024-04-18 10:39:55 -0400

    make standard workers for testing

--- scripts/variantstore/wdl/extract/run_in_hail_cluster.py
+++ scripts/variantstore/wdl/extract/run_in_hail_cluster.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--cluster-name', type=str, required=True, help='Name of the Hail cluster')
     parser.add_argument('--account', type=str, help='GCP account name')
-    parser.add_argument('--worker-machine-type', type=str, required=False, default="n1-highmem-8",
+    parser.add_argument('--worker-machine-type', type=str, required=False, default="n1-standard-8",
                         help='Dataproc cluster worker machine type')
     parser.add_argument('--master-machine-type', type=str, required=False, default="n1-highmem-32",
                         help='Dataproc cluster master machine type')
