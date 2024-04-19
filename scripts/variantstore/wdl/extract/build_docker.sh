7bb9ed8b32060f7fd85a1a5d5f2eb06b0cd85521
Miguel Covarrubias <mcovarr@broadinstitute.org>
2024-04-19 10:14:26 -0400

    GAR

--- scripts/variantstore/wdl/extract/build_docker.sh
+++ scripts/variantstore/wdl/extract/build_docker.sh
@@ -1,3 +1,5 @@
+set -o errexit -o nounset -o pipefail
+
 usage() {
     echo "
 
@@ -6,27 +8,26 @@ USAGE: ./build_docker.sh
 Generate a tag suitable for publication of a Variants Docker image.
 Tags will be of the form <ISO 8601 Date>-alpine-<Docker image ID>.
 
-e.g. 2024-04-18-alpine-f000ba44
+e.g. 2024-04-19-alpine-f000ba44
 "
     exit 1
 }
 
-if [[ $? -ne 0 ]]
+if [[ $# -ne 0 ]]
 then
     usage
 fi
 
 docker build .
 
-TAG=$(python3 ./build_docker_tag.py $*)
-# Take everything after the last slash to recover the Docker image ID from the tag.
+TAG=$(python3 ./build_docker_tag.py)
+# Take everything after the last dash to recover the Docker image ID from the tag.
 IMAGE_ID=${TAG##*-}
 BASE_REPO="broad-dsde-methods/variantstore"
-REPO_WITH_TAG="${BASE_REPO}:${TAG}"
+REPO_WITH_TAG="${BASE_REPO}/variantstore:${TAG}"
 docker tag "${IMAGE_ID}" "${REPO_WITH_TAG}"
-GCR_TAG="us.gcr.io/${REPO_WITH_TAG}"
 
-# Run unit tests before pushing to GCR.
+# Run unit tests before pushing.
 set +o errexit
 fail=0
 for test in test_*.py
@@ -45,7 +46,11 @@ fi
 
 set -o errexit
 
-docker tag "${REPO_WITH_TAG}" "${GCR_TAG}"
-docker push "${GCR_TAG}"
+GAR_TAG="us-central1-docker.pkg.dev/${REPO_WITH_TAG}"
+docker tag "${REPO_WITH_TAG}" "${GAR_TAG}"
 
-echo "Docker image pushed to \"${GCR_TAG}\""
+# Docker must be configured for GAR before pushes will work:
+# gcloud auth configure-docker us-central1-docker.pkg.dev
+docker push "${GAR_TAG}"
+
+echo "Docker image pushed to \"${GAR_TAG}\""
