70b9444a5058e910ffe5a798c0b5fa1ec981e764
Miguel Covarrubias <mcovarr@users.noreply.github.com>
2024-04-19 12:46:07 -0400

    Docker image ID in tags [VS-1340] (#8783)

--- scripts/variantstore/wdl/extract/build_docker.sh
+++ scripts/variantstore/wdl/extract/build_docker.sh
@@ -1,29 +1,33 @@
+set -o errexit -o nounset -o pipefail
+
 usage() {
     echo "
 
 USAGE: ./build_docker.sh
 
 Generate a tag suitable for publication of a Variants Docker image.
-Tags will be of the form <ISO 8601 Date>-alpine-<short git hash>.
+Tags will be of the form <ISO 8601 Date>-alpine-<Docker image ID>.
 
-e.g. 2023-10-10-alpine-f000ba44
+e.g. 2024-04-19-alpine-f000ba44
 "
     exit 1
 }
 
-TAG=$(python3 ./build_docker_tag.py $*)
-if [[ $? -ne 0 ]]
+if [[ $# -ne 0 ]]
 then
     usage
 fi
 
+docker build .
+
+TAG=$(python3 ./build_docker_tag.py)
+# Take everything after the last dash to recover the Docker image ID from the tag.
+IMAGE_ID=${TAG##*-}
 BASE_REPO="broad-dsde-methods/variantstore"
-REPO_WITH_TAG="${BASE_REPO}:${TAG}"
-GCR_TAG="us.gcr.io/${REPO_WITH_TAG}"
+REPO_WITH_TAG="${BASE_REPO}/variantstore:${TAG}"
+docker tag "${IMAGE_ID}" "${REPO_WITH_TAG}"
 
-docker build . -t "${REPO_WITH_TAG}"
-
-# Run unit tests before pushing to GCR.
+# Run unit tests before pushing.
 set +o errexit
 fail=0
 for test in test_*.py
@@ -36,13 +40,17 @@ do
 done
 
 if [ $fail -ne 0 ]; then
-    echo "One or more unit test has failed, exiting."
+    echo "One or more unit tests have failed, exiting."
     exit $fail
 fi
 
 set -o errexit
 
-docker tag "${REPO_WITH_TAG}" "${GCR_TAG}"
-docker push "${GCR_TAG}"
+GAR_TAG="us-central1-docker.pkg.dev/${REPO_WITH_TAG}"
+docker tag "${REPO_WITH_TAG}" "${GAR_TAG}"
 
-echo "docker image pushed to \"${GCR_TAG}\""
+# Docker must be configured for GAR before pushes will work:
+# gcloud auth configure-docker us-central1-docker.pkg.dev
+docker push "${GAR_TAG}"
+
+echo "Docker image pushed to \"${GAR_TAG}\""
