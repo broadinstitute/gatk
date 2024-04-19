d731ae5f297a1c50b3726c6ac2ac1f93a1ec221f
Miguel Covarrubias <mcovarr@users.noreply.github.com>
2024-04-19 12:46:07 -0400

    Docker image ID in tags [VS-1340] (#8783)

--- scripts/variantstore/wdl/extract/test_build_docker_tag.py
+++ scripts/variantstore/wdl/extract/test_build_docker_tag.py
@@ -11,6 +11,6 @@ class TestBuildDockerTag(unittest.TestCase):
     def test_tag(self):
         tag_re = re.compile(r"^20\d{2}-\d{2}-\d{2}-alpine-f00ba4ba5$")
 
-        args = self.argument_parser.parse_args(['--dummy-testing-hash', 'f00ba4ba5'])
+        args = self.argument_parser.parse_args(['--dummy-testing-image-id', 'f00ba4ba5'])
         tag = build_tag(args)
         self.assertTrue(tag_re.match(tag))
