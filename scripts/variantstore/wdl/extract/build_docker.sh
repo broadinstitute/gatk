650447831e90c7417b78123330c64a4a4e254912
Miguel Covarrubias <mcovarr@broadinstitute.org>
2024-04-22 10:27:53 -0400

    Reverting due to permissions issues in GAR... Revert "Docker image ID in tags [VS-1340] (#8783)"
    This reverts commit d731ae5f297a1c50b3726c6ac2ac1f93a1ec221f.

--- scripts/variantstore/wdl/extract/build_docker.sh
+++ scripts/variantstore/wdl/extract/build_docker.sh
@@ -1,33 +1,29 @@
-set -o errexit -o nounset -o pipefail
-
 usage() {
     echo "
 
 USAGE: ./build_docker.sh
 
 Generate a tag suitable for publication of a Variants Docker image.
-Tags will be of the form <ISO 8601 Date>-alpine-<Docker image ID>.
+Tags will be of the form <ISO 8601 Date>-alpine-<short git hash>.
 
-e.g. 2024-04-19-alpine-f000ba44
+e.g. 2023-10-10-alpine-f000ba44
 "
     exit 1
 }
 
-if [[ $# -ne 0 ]]
+TAG=$(python3 ./build_docker_tag.py $*)
+if [[ $? -ne 0 ]]
 then
     usage
 fi
 
-docker build .
-
-TAG=$(python3 ./build_docker_tag.py)
-# Take everything after the last dash to recover the Docker image ID from the tag.
-IMAGE_ID=${TAG##*-}
 BASE_REPO="broad-dsde-methods/variantstore"
-REPO_WITH_TAG="${BASE_REPO}/variantstore:${TAG}"
-docker tag "${IMAGE_ID}" "${REPO_WITH_TAG}"
+REPO_WITH_TAG="${BASE_REPO}:${TAG}"
+GCR_TAG="us.gcr.io/${REPO_WITH_TAG}"
 
-# Run unit tests before pushing.
+docker build . -t "${REPO_WITH_TAG}"
+
+# Run unit tests before pushing to GCR.
 set +o errexit
 fail=0
 for test in test_*.py
@@ -40,17 +36,13 @@ do
 done
 
 if [ $fail -ne 0 ]; then
-    echo "One or more unit tests have failed, exiting."
+    echo "One or more unit test has failed, exiting."
     exit $fail
 fi
 
 set -o errexit
 
-GAR_TAG="us-central1-docker.pkg.dev/${REPO_WITH_TAG}"
-docker tag "${REPO_WITH_TAG}" "${GAR_TAG}"
+docker tag "${REPO_WITH_TAG}" "${GCR_TAG}"
+docker push "${GCR_TAG}"
 
-# Docker must be configured for GAR before pushes will work:
-# gcloud auth configure-docker us-central1-docker.pkg.dev
-docker push "${GAR_TAG}"
-
-echo "Docker image pushed to \"${GAR_TAG}\""
+echo "docker image pushed to \"${GCR_TAG}\""
--- scripts/variantstore/wdl/extract/build_docker_tag.py
+++ scripts/variantstore/wdl/extract/build_docker_tag.py
@@ -8,19 +8,19 @@ def build_tag(args):
     current_date = datetime.now()
     date_string = current_date.strftime('%Y-%m-%d')
 
-    if not args.dummy_testing_image_id:
-        proc = run(["bash", "-c", "docker images --quiet | head -1"], stdout=subprocess.PIPE)
-        docker_image_id = proc.stdout.rstrip().decode('utf-8')
+    if not args.dummy_testing_hash:
+        proc = run(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        git_hash = proc.stdout.rstrip().decode('utf-8')
     else:
         # Do not actually try to run git during testing, the .git directory is not mounted into the container.
-        docker_image_id = args.dummy_testing_image_id
-    return f"{date_string}-alpine-{docker_image_id}"
+        git_hash = args.dummy_testing_hash
+    return f"{date_string}-alpine-{git_hash}"
 
 
 def build_argument_parser():
     parser = argparse.ArgumentParser(allow_abbrev=False, description='Build a tag for Variants Docker image')
-    parser.add_argument('-d', '--dummy-testing-image-id', default=None,
-                        help='Dummy short Docker image ID to return during testing')
+    parser.add_argument('-d', '--dummy-testing-hash', default=None,
+                        help='Dummy short git hash to return during testing')
     return parser
 
 
