b43709a82d8867e8fb58c23fb181beef45ac920f
Chris Norman <cnorman@broadinstitute.org>
2024-05-01 16:43:48 -0400

    8768 file diagnostics.

---
+++ src/main/java/org/broadinstitute/hellbender/tools/CRAMIssue8768Detector.java
@@ -0,0 +1,91 @@
+package org.broadinstitute.hellbender.tools;
+
+import org.broadinstitute.barclay.argparser.*;
+import org.broadinstitute.hellbender.tools.filediagnostics.CRAMIssue8768Analyzer;
+import org.broadinstitute.hellbender.cmdline.CommandLineProgram;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.engine.GATKPath;
+import picard.cmdline.programgroups.OtherProgramGroup;
+
+/**
+ * A diagnostic tool that analyzes a CRAM input file to look for conditions that trigger issue 8768.
+ *
+ * Writes a report to a file indicating whether the file appears to have read base corruption caused by 8768.
+ *
+ * By default, the output report will have a summary of the average mismatch rate for all suspected bad containers
+ * and a few presumed good containers in order to determine if there is a large difference in the base mismatch rate.
+ * To analyze the base mismatch rate for ALL containers, use the "verbose" option.
+ *
+ * Works on files ending in .cram.
+ *
+ * Sample Usage:
+ *
+ * gatk CRAMIssue8768Detector -I input.cram -O output.txt -R reference.fasta
+ */
+@ExperimentalFeature
+@WorkflowProperties
+@CommandLineProgramProperties(
+        summary = "Analyze a CRAM file for issue 8768",
+        oneLineSummary = "Analyze a CRAM file for issue 8768",
+        programGroup = OtherProgramGroup.class
+)
+public class CRAMIssue8768Detector extends CommandLineProgram {
+    // default average mismatch rate threshold above which we consider the file to be corrupt
+    private static final double DEFAULT_MISMATCH_RATE_THRESHOLD = 0.05;
+
+    @Argument(fullName = StandardArgumentDefinitions.INPUT_LONG_NAME,
+            shortName = StandardArgumentDefinitions.INPUT_SHORT_NAME,
+            doc = "Input path to analyze",
+            common = true)
+    @WorkflowInput
+    public GATKPath inputPath;
+
+    @Argument(fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME,
+            shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
+            doc = "Output diagnostics file",
+            common = true)
+    @WorkflowOutput
+    public GATKPath outputPath;
+
+    @Argument(fullName = StandardArgumentDefinitions.REFERENCE_LONG_NAME,
+            shortName = StandardArgumentDefinitions.REFERENCE_SHORT_NAME,
+            doc = "Reference for the CRAM file",
+            common = true)
+    @WorkflowOutput
+    public GATKPath referencePath;
+
+    public static final String MISMATCH_RATE_THRESHOLD_ARG_NAME = "mismatch-rate-threshold";
+    @Argument(fullName = MISMATCH_RATE_THRESHOLD_ARG_NAME,
+            shortName = MISMATCH_RATE_THRESHOLD_ARG_NAME,
+            doc = "Mismatch rate threshold above which we consider the file to be corrupt",
+            optional = true)
+    public double mismatchRateThreshold = DEFAULT_MISMATCH_RATE_THRESHOLD;
+
+    public static final String VERBOSE_ARG_NAME = "verbose";
+    @Argument(fullName = VERBOSE_ARG_NAME,
+            shortName= VERBOSE_ARG_NAME,
+            doc="Calculate and print the mismatch rate for all containers",
+            optional=true)
+    public boolean verbose = false;
+
+    private CRAMIssue8768Analyzer cramAnalyzer;
+
+    @Override
+    protected Object doWork() {
+        cramAnalyzer = new CRAMIssue8768Analyzer(inputPath, outputPath, referencePath, mismatchRateThreshold, verbose);
+        cramAnalyzer.doAnalysis();
+        return 0;
+    }
+
+    @Override
+    protected void onShutdown() {
+        if ( cramAnalyzer != null ) {
+            try {
+                cramAnalyzer.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
---
+++ src/test/java/org/broadinstitute/hellbender/CRAMIssue8768DetectorIntegrationTest.java
@@ -0,0 +1,112 @@
+package org.broadinstitute.hellbender;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.broadinstitute.hellbender.testutils.ArgumentsBuilder;
+import org.broadinstitute.hellbender.testutils.IntegrationTestSpec;
+import org.broadinstitute.hellbender.tools.CRAMIssue8768Detector;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CRAMIssue8768DetectorIntegrationTest extends CommandLineProgramTest {
+
+    @DataProvider(name = "cramAnalysisTestCases")
+    public Object[][] getFileDiagnosticsTestCases() {
+        return new Object[][]{
+                // local file tests
+                {
+                        // test file created by rewriting the htsjdk test file
+                        // src/test/resources/htsjdk/samtools/cram/mitoAlignmentStartTest.cram, using a version
+                        // of GATK that has bug https://github.com/broadinstitute/gatk/issues/8768. the rewrite causes
+                        // the file to have corrupt read bases, so we can use it to test the detector
+                        // 1 bad container
+                        "src/test/resources/filediagnostics/thisFileIsCorruptMito.bug8768.cram",
+                        "src/test/resources/filediagnostics/mitoCorrupt.bug8768.fa",
+                        List.of(Pair.of(CRAMIssue8768Detector.VERBOSE_ARG_NAME, "false")),
+                        "src/test/resources/filediagnostics/thisFileIsCorruptMito.false.bug8768.txt",
+                },
+                {
+                        // test file created by rewriting the htsjdk test file
+                        // src/test/resources/htsjdk/samtools/cram/mitoAlignmentStartTest.cram, using a version
+                        // of GATK that has bug https://github.com/broadinstitute/gatk/issues/8768. the rewrite causes
+                        // the file to have corrupt read bases, so we can use it to test the detector
+                        // 1 bad container
+                        "src/test/resources/filediagnostics/thisFileIsCorruptMito.bug8768.cram",
+                        "src/test/resources/filediagnostics/mitoCorrupt.bug8768.fa",
+                        List.of(Pair.of(CRAMIssue8768Detector.VERBOSE_ARG_NAME, "true")),
+                        "src/test/resources/filediagnostics/thisFileIsCorruptMito.true.bug8768.txt",
+                },
+                {
+                        // 0 bad containers
+                        NA12878_20_21_WGS_cram,
+                        b37_reference_20_21,
+                        List.of(Pair.of(CRAMIssue8768Detector.VERBOSE_ARG_NAME, "false")),
+                        "src/test/resources/filediagnostics/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.false.bug8768.txt",
+                },
+                {
+                        // 0 bad containers
+                        NA12878_20_21_WGS_cram,
+                        b37_reference_20_21,
+                        List.of(Pair.of(CRAMIssue8768Detector.VERBOSE_ARG_NAME, "true")),
+                        "src/test/resources/filediagnostics/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.true.bug8768.txt",
+                },
+                {
+                        // test file created by rewriting the htsjdk test file
+                        // src/test/resources/htsjdk/samtools/cram/mitoAlignmentStartTest.cram using a version
+                        // of GATK that has bug https://github.com/broadinstitute/gatk/issues/8768, along with code
+                        // to force the file to have 2 bad containers by forcing numerous reads to be aligned to
+                        // position 1.
+                        // 2 bad containers
+                        "src/test/resources/filediagnostics/thisFileIsCorruptMito.2BadContainers.bug8768.cram",
+                        "src/test/resources/filediagnostics/mitoCorrupt.bug8768.fa",
+                        List.of(Pair.of(CRAMIssue8768Detector.VERBOSE_ARG_NAME, "false")),
+                        "src/test/resources/filediagnostics/thisFileIsCorruptMito.2BadContainers.false.bug8768.txt",
+                },
+                {
+                        // test file created by rewriting the htsjdk test file
+                        // src/test/resources/htsjdk/samtools/cram/mitoAlignmentStartTest.cram using a version
+                        // of GATK that has bug https://github.com/broadinstitute/gatk/issues/8768, along with code
+                        // to force the file to have 2 bad containers by forcing numerous reads to be aligned to
+                        // position 1.
+                        //   // 3 bad containers
+                    "src/test/resources/filediagnostics/thisFileIsCorruptMito.3BadContainers.bug8768.cram",
+                    "src/test/resources/filediagnostics/mitoCorrupt.bug8768.fa",
+                    List.of(Pair.of(CRAMIssue8768Detector.VERBOSE_ARG_NAME, "false")),
+                    "src/test/resources/filediagnostics/thisFileIsCorruptMito.3BadContainers.false.bug8768.txt",
+                },
+
+                // cloud file test
+                {
+                    // 0 bad containers
+                    "gs://hellbender/test/resources/large/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.cram",
+                    "gs://hellbender/test/resources/large/human_g1k_v37.20.21.fasta",
+                    List.of(Pair.of(CRAMIssue8768Detector.VERBOSE_ARG_NAME, "false")),
+                    "src/test/resources/filediagnostics/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.false.bug8768.cloud.txt",
+                }
+        };
+    }
+
+    @Test(dataProvider = "cramAnalysisTestCases")
+    public void testCramAnalysis(
+            final String inputPath,
+            final String referencePath, // unused for now
+            final List<Pair<String, String>> extraArgs,
+            final String expectedOutputPath) throws IOException {
+        final File outFile = createTempFile("testFileDiagnostics", ".txt");
+        ArgumentsBuilder argBuilder = new ArgumentsBuilder();
+        argBuilder.addInput(inputPath);
+        argBuilder.addOutput(outFile);
+        argBuilder.addReference(Paths.get(referencePath));
+        if (extraArgs != null) {
+            extraArgs.forEach(argPair -> argBuilder.add(argPair.getKey(), argPair.getValue()));
+        }
+        runCommandLine(argBuilder.getArgsList());
+
+        IntegrationTestSpec.assertEqualTextFiles(outFile, new File(expectedOutputPath));
+    }
+
+}
