7087e50b7ed7bd5bb0dad4f8e26b53eaf0aa8d44
Chris Vittal <chris@vittal.dev>
2024-06-24 15:06:48 -0400

    Use struct for sex chromosome ploidy

--- scripts/variantstore/wdl/extract/patch_echo.py
+++ scripts/variantstore/wdl/extract/patch_echo.py
@@ -89,7 +89,6 @@ def patch_vds(
     variant_data = patch_variant_data(vds.variant_data, site, vets)
     reference_data = patch_reference_data(vds.reference_data, ploidy)
 
-
     return hl.vds.VariantDataset(
         reference_data=reference_data, variant_data=variant_data
     ).checkpoint(output_path, overwrite=overwrite)
@@ -179,7 +178,7 @@ def import_vets(
     return hl.read_table(vets_path, _intervals=intervals)
 
 
-def import_ploidy(*avros) -> dict[str, dict[str, int]]:
+def import_ploidy(*avros) -> dict[str, hl.Struct]:
     """
     Parameters
     ----------
@@ -216,9 +215,21 @@ def import_ploidy(*avros) -> dict[str, dict[str, int]]:
     DataFileReader.determine_file_length = original_determine_file_length
 
     hg38 = hl.get_reference("GRCh38")
-    return {
+    xy_contigs = set(hg38.x_contigs + hg38.y_contigs)
+    ploidy_table = {
         contig: ploidy_table[key]
         for contig, key in zip(hg38.contigs, sorted(ploidy_table))
+        if contig in xy_contigs
+    }
+    x_table = ploidy_table["chrX"]
+    y_table = ploidy_table["chrY"]
+    assert set(x_table) == set(y_table)
+
+    return {
+        sample_name: hl.Struct(
+            x_ploidy=x_table[sample_name], y_ploidy=y_table[sample_name]
+        )
+        for sample_name in x_table
     }
 
 
@@ -327,14 +338,14 @@ def patch_reference_data(rd, ploidy) -> hl.MatrixTable:
         table of ploidy information. Keys of outer dict are contigs. Keys of inner dict are sample names.
         Values of inner dict are the ploidy to use for the reference genotype in nonpar regions.
     """
-    rd = rd.annotate_globals(ploidy_data=hl.literal(ploidy))
-    rd = rd.annotate_rows(autosome_or_par=rd.locus.in_autosome_or_par())
+    rd = rd.annotate_cols(ploidy_data=hl.literal(ploidy)[rd.s])
+    rd = rd.annotate_rows(autosome_or_par=rd.locus.in_autosome_or_par(), is_y=rd.locus.contig == 'chrY')
     rd = rd.annotate_entries(
         GT=hl.if_else(
             rd.autosome_or_par,
             hl.call(0, 0),
             hl.rbind(
-                rd.ploidy_data[rd.locus.contig].get(rd.s, 2),
+                hl.if_else(rd.is_y, rd.ploidy_data.y_ploidy, rd.ploidy_data.x_ploidy),
                 lambda ploidy: hl.switch(ploidy)
                 .when(1, hl.call(0))
                 .when(2, hl.call(0, 0))
