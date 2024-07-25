924216b27d55033545cc16ebe8a17330b2246ece
Sana Shah <shahs@catlin.edu>
2024-07-25 15:31:27 -0400

    add: more tests

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/conversion/GtfToBedIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/conversion/GtfToBedIntegrationTest.java
@@ -8,8 +8,11 @@ import org.testng.annotations.Test;
 import java.io.File;
 
 public class GtfToBedIntegrationTest extends CommandLineProgramTest {
-    private static final File input = new File("/Users/shahsana/TestGatk/gencode.v38.chr_patch_hapl_scaff.annotation.gtf");
-    private static final File dictionary = new File("/Users/shahsana/TestGatk/reference.dict");
+    private static final File input = new File(TestUtils.getGencodeGtf());
+    private static final File mapk1 = new File(TestUtils.getMapk1Gtf());
+    private static final File decoys = new File(TestUtils.getDecoysGtf());
+    private static final File dictionary = new File(TestUtils.getReferenceDict());
+
 
     @Test
     public void testGene() {
@@ -22,30 +25,55 @@ public class GtfToBedIntegrationTest extends CommandLineProgramTest {
     }
 
     @Test
-    public void testOneGene(){
-
+    public void testMapk1Gene(){
+        runMapk1(false);
     }
 
     @Test
-    public void testOneTranscript(){
-
+    public void testMapk1Transcript(){
+        runMapk1(true);
     }
 
     @Test
     public void testDecoyGenes(){
-
+        runDecoys(false);
     }
 
     @Test
     public void testDecoyTranscripts(){
-
+        runDecoys(true);
     }
 
 
     private void runGtfToBed(boolean transcript){
-        File outputFile = new File("/Users/shahsana/TestGatk/output.bed");
+        //File outputFile = new File("/Users/shahsana/TestGatk/output.bed");
+        final File outputFile = createTempFile("gtf_to_bed", ".bed");
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder()
                 .add("G", input)
+                .add("D", dictionary)
+                .add(GtfToBed.SORT_BY_TRANSCRIPT_LONG_NAME, transcript)
+                .addOutput(outputFile);
+        runCommandLine(argsBuilder);
+        Assert.assertEquals(1,1);
+    }
+
+    private void runMapk1(boolean transcript){
+        //File outputFile = new File("/Users/shahsana/TestGatk/output.bed");
+        final File outputFile = createTempFile("gtf_to_bed", ".bed");
+        final ArgumentsBuilder argsBuilder = new ArgumentsBuilder()
+                .add("G", mapk1)
+                .add("D", dictionary)
+                .add(GtfToBed.SORT_BY_TRANSCRIPT_LONG_NAME, transcript)
+                .addOutput(outputFile);
+        runCommandLine(argsBuilder);
+        Assert.assertEquals(1,1);
+    }
+
+    private void runDecoys(boolean transcript){
+        //File outputFile = new File("/Users/shahsana/TestGatk/output.bed");
+        final File outputFile = createTempFile("gtf_to_bed", ".bed");
+        final ArgumentsBuilder argsBuilder = new ArgumentsBuilder()
+                .add("G", decoys)
                 .add("D", dictionary)
                 .add(GtfToBed.SORT_BY_TRANSCRIPT_LONG_NAME, transcript)
                 .addOutput(outputFile);
