09b6edb139187c68c0c78331c25312d4d2e2e6ed
Dror Kessler <dror27.kessler@gmail.com>
2024-08-15 11:01:03 +0200

    make CachingIndexedFastaSequenceFile.java thread safe + allow to increasing cache size

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/featuremapping/FlowFeatureMapper.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/featuremapping/FlowFeatureMapper.java
@@ -27,6 +27,7 @@ import org.broadinstitute.hellbender.tools.walkers.haplotypecaller.HaplotypeCall
 import org.broadinstitute.hellbender.tools.walkers.haplotypecaller.ReferenceConfidenceMode;
 import org.broadinstitute.hellbender.utils.SimpleInterval;
 import org.broadinstitute.hellbender.utils.Utils;
+import org.broadinstitute.hellbender.utils.fasta.CachingIndexedFastaSequenceFile;
 import org.broadinstitute.hellbender.utils.haplotype.FlowBasedHaplotype;
 import org.broadinstitute.hellbender.utils.haplotype.Haplotype;
 import org.broadinstitute.hellbender.utils.read.FlowBasedRead;
@@ -90,6 +91,7 @@ import java.util.function.Supplier;
 public final class FlowFeatureMapper extends ThreadedReadWalker {
 
     public static final int CAPACITY1 = 5000;
+    private static final long CACHE_SIZE_FACTOR = 5;
 
     static class CopyAttrInfo {
         public final String name;
@@ -318,6 +320,11 @@ public final class FlowFeatureMapper extends ThreadedReadWalker {
                 }
             });
             writerWorker.start();;
+        }
+
+        // if using threaded walker extend reference cache to avoid thrushing
+        if ( threadedWalker ) {
+            CachingIndexedFastaSequenceFile.requestedCacheSize = CachingIndexedFastaSequenceFile.DEFAULT_CACHE_SIZE * CACHE_SIZE_FACTOR;
         }
     }
 
