7a7a7d09b4fec8a4a8d63692762eeb51e5e47d99
Sana Shah <54493565+sanashah007@users.noreply.github.com>
2024-08-22 12:50:25 -0700

    add: javadoc to GtfInfo

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/conversion/GtfInfo.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/conversion/GtfInfo.java
@@ -2,6 +2,25 @@ package org.broadinstitute.hellbender.tools.walkers.conversion;
 
 import htsjdk.samtools.util.Interval;
 
+/**
+ * A class that represents information extracted from a feature in a GTF file.
+ * The {@code GtfInfo} object encapsulates details about a specific gene or transcript,
+ * including its type, gene name, and interval.
+ *
+ * <p>The {@code GtfInfo.Type} enum specifies whether the features is a
+ * {@code GENE} or a {@code TRANSCRIPT}.</p>
+ *
+ * <p>The interval specifies the feature's contig (chromosome), start position,
+ * and end position, providing the precise location of the gene or transcript
+ * on the genome.</p>
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ *     Interval interval = new Interval("chr1", 1000, 2000);
+ *     GtfInfo gtfInfo = new GtfInfo(interval, GtfInfo.Type.GENE, "MAPK1");
+ * </pre>
+ **/
+
 public class GtfInfo {
 
     public enum Type {
