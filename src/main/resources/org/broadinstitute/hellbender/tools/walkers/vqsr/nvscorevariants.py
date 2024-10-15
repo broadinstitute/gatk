8847960d5fbde8db4025c0e9be437311574b755e
Louis Bergelson <louisb@broadinstitute.org>
2024-10-14 23:04:32 -0400

    fix

--- src/main/resources/org/broadinstitute/hellbender/tools/walkers/vqsr/nvscorevariants.py
+++ src/main/resources/org/broadinstitute/hellbender/tools/walkers/vqsr/nvscorevariants.py
@@ -32,7 +32,7 @@ def get_args():
     parser.add_argument('--seed', type=int, default=724, help='Seed to initialize the random number generator')
     parser.add_argument('--tmp-file', default='tmp.txt', help='The temporary VCF-like file where variants scores will be written')
     parser.add_argument('--output-file', required=True, help='Output VCF file')
-    parser.add_argument('--devices', type=int, default=None, help='Number of GPUs (int) or other accelerator to use')
+    parser.add_argument('--devices', default="auto", help='Number of GPUs (int) or other accelerator to use')
     parser.add_argument('--accelerator', default='auto', help='Type of hardware accelerator to use (auto, cpu, cuda, mps, tpu, etc)')
 
     parser.add_argument('--model-directory', default='models', help='Directory containing model files')
