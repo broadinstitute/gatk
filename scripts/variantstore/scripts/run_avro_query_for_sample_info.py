7c3b0c3a0672b8303833350ffb962a7b5a122320
Rori Cremer <6863459+RoriCremer@users.noreply.github.com>
2025-03-02 21:23:29 -0500

    for now add load_sample_names as a function

--- scripts/variantstore/scripts/run_avro_query_for_sample_info.py
+++ scripts/variantstore/scripts/run_avro_query_for_sample_info.py
@@ -8,6 +8,7 @@ from google.cloud.bigquery.job import QueryJobConfig
 import run_avro_query
 import utils
 
+## from create_ranges_cohort_extract_data_table import load_sample_names ## TODO can I just import this?!?!
 
 def get_number_of_partitions(dataset_name, project_id):
     query_labels_map = {
@@ -31,9 +32,41 @@ def get_number_of_partitions(dataset_name, project_id):
     max_table_num = [row.get('max_table_num') for row in query_result.get('results')]
     return math.ceil(max_table_num[0])
 
+## load_sample_names(sample_names_to_extract, fq_temp_table_dataset) TODO can I just import this?!?!
+
+def load_sample_names(sample_names_to_extract, fq_temp_table_dataset):
+    schema = [bigquery.SchemaField("sample_name", "STRING", mode="REQUIRED")]
+    fq_sample_table = f"{fq_temp_table_dataset}.{EXTRACT_SAMPLE_TABLE}"
+
+    job_labels = client._default_query_job_config.labels
+    job_labels["gvs_query_name"] = "load-sample-names"
+
+    job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.CSV, skip_leading_rows=0, schema=schema,
+                                        labels=job_labels)
+
+    with open(sample_names_to_extract, "rb") as source_file:
+        job = client.load_table_from_file(source_file, fq_sample_table, job_config=job_config)
+
+    job.result()  # Waits for the job to complete.
+
+    # setting the TTL needs to be done as a second API call
+    table = bigquery.Table(fq_sample_table, schema=schema)
+    expiration = datetime.datetime.utcnow() + datetime.timedelta(hours=TEMP_TABLE_TTL_HOURS)
+    table.expires = expiration
+    client.update_table(table, ["expires"])
+
+    return fq_sample_table
+
 
 def construct_sample_info_avro_queries(call_set_identifier, dataset_name, project_id, avro_prefix):
     num_of_tables = get_number_of_partitions(dataset_name, project_id)
+
+   # if we have a file of sample names, load it into a temporary table
+   if sample_names_to_extract:
+       ## TODO note that I haven't defined sample_names_to_extract OR fq_temp_table_dataset
+       fq_sample_name_table = load_sample_names(sample_names_to_extract, fq_temp_table_dataset)
+   else:
+       fq_sample_name_table = f"{project_id}.{dataset_name}.sample_info"
 
     for i in range(1, num_of_tables + 1):
         file_name = f"*.{i:03}.avro"
@@ -44,7 +77,7 @@ def construct_sample_info_avro_queries(call_set_identifier, dataset_name, projec
                 uri='{avro_prefix}/sample_mapping/{file_name}', format='AVRO', compression='SNAPPY', overwrite=true) AS
             SELECT sample_id, sample_name, '40',
             'gs://gcp-public-data--broad-references/hg38/v0/wgs_calling_regions.hg38.noCentromeres.noTelomeres.interval_list' AS intervals_file
-            FROM `{project_id}.{dataset_name}.sample_info`
+            FROM `{fq_sample_name_table}`
             WHERE {id_where_clause} AND is_control = false
             ORDER BY sample_id"""
 
