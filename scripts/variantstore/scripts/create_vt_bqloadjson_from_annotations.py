a41ab2379cb1d5863cec2edfb426891b19041198
George Grant <ggrant@broadinstitute.org>
2025-04-15 13:15:03 -0400

    Update scripts/variantstore/scripts/create_vt_bqloadjson_from_annotations.py
    Co-authored-by: Miguel Covarrubias <mcovarr@users.noreply.github.com>

--- scripts/variantstore/scripts/create_vt_bqloadjson_from_annotations.py
+++ scripts/variantstore/scripts/create_vt_bqloadjson_from_annotations.py
@@ -8,9 +8,6 @@ import logging
 import sys
 from Bio.Seq import reverse_complement
 
-
-
-
 vat_nirvana_positions_dictionary = {
     "position": "position", # required
     "ref_allele": "refAllele", # required
