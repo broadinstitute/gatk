f3ee9d8b7ef3cf899d05e01a263592229348ea5f
Kevin Lydon <klydon@broadinstitute.org>
2025-05-20 12:45:00 -0400

    Modified docker to be staged so we can also build a version without the conda environment (#9168)

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/varianteval/AlleleFrequencyQCIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/varianteval/AlleleFrequencyQCIntegrationTest.java
@@ -1,7 +1,10 @@
 package org.broadinstitute.hellbender.tools.walkers.varianteval;
 
 import org.broadinstitute.hellbender.CommandLineProgramTest;
+import org.broadinstitute.hellbender.exceptions.UserException;
+import org.broadinstitute.hellbender.testutils.EnvironmentTestUtils;
 import org.broadinstitute.hellbender.testutils.IntegrationTestSpec;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -32,6 +35,29 @@ public class AlleleFrequencyQCIntegrationTest extends CommandLineProgramTest {
                 , Arrays.asList(getExpectedFile(name)));
 
         spec.executeTest(name, this);
+    }
+
+    @Test(singleThreaded = true)
+    public void testInGatkLiteDocker() {
+        EnvironmentTestUtils.checkWithGATKDockerPropertySet(() -> {
+            String name = "testAFQCIntegration";
+
+            IntegrationTestSpec spec = new IntegrationTestSpec(
+                    " -R " + b37Reference +
+                            " --eval " + evalVcf +
+                            " --comp " + comparisonVcf +
+                            " -eval:thousand_genomes " + comparisonVcf +
+                            " -L " + comparisonVcf +
+                            " -O " + getExpectedFile(name)
+                    ,0, UserException.NotAvailableInGatkLiteDocker.class);
+
+            try {
+                spec.executeTest(name, this);
+            }
+            catch(final IOException e) {
+                Assert.fail("Failed with IOException: " + e.getMessage()); 
+            }
+        });
     }
 
 }
