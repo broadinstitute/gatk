cb5bce2663c447a90d054f720dca493971c5cf49
Miguel Covarrubias <mcovarr@users.noreply.github.com>
2025-06-02 11:54:20 -0400

    Bypass Terra Data Tables for Foxtrot [VS-1185][VS-1661][VS-1662][VS-1663] (#9193)

--- scripts/variantstore/scripts/data_table_upload.py
+++ scripts/variantstore/scripts/data_table_upload.py
@@ -2,7 +2,7 @@
 
 """
 Can be run in any analysis terminal provided the associated proxy group has auth to write to the target table.
-Note that this is currently hardcoded to write to the Foxtrot scale test workspace.
+Note that this is currently hardcoded to write to the Foxtrot Batch / scale test workspace.
 """
 
 import csv
@@ -16,7 +16,7 @@ INPUT_FILE = "output.csv"
 
 def put_items(batch):
     table.put_rows(
-        table = "samples", # should be 'sample' but that consistently gives cryptic 400 errors
+        table = "sample",
         items = batch,
         workspace = WORKSPACE_NAME,
         workspace_namespace = WORKSPACE_NAMESPACE)
@@ -25,9 +25,13 @@ def put_items(batch):
 i = 0
 batch = []
 with open(INPUT_FILE, 'r') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        batch.append(dict(row))
+    reader = csv.DictReader(f, delimiter='\t')
+    for csv_row in reader:
+        name = csv_row['sample_id']
+        attr_names = ['research_id', 'reblocked_gvcf', 'reblocked_gvcf_index']
+        attrs = {k: csv_row[k] for k in attr_names}
+        row = table.Row(name=name, attributes=attrs)
+        batch.append(row)
         i = i + 1
         if i % UPLOAD_BATCH_SIZE == 0:
             put_items(batch)
