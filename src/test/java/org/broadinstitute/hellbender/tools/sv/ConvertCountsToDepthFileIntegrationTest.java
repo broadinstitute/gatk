bb6d92b2caa5cf96daa0754707fc140065d72a40
Vahid <VJalili@users.noreply.github.com>
2025-07-21 14:13:16 -0400

    Improve integration test.

--- src/test/java/org/broadinstitute/hellbender/tools/sv/ConvertCountsToDepthFileIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/sv/ConvertCountsToDepthFileIntegrationTest.java
@@ -4,6 +4,7 @@ import org.broadinstitute.hellbender.CommandLineProgramTest;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
 import org.broadinstitute.hellbender.testutils.IntegrationTestSpec;
 import org.broadinstitute.hellbender.utils.codecs.DepthEvidenceCodec;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -12,20 +13,42 @@ import java.util.Collections;
 public class ConvertCountsToDepthFileIntegrationTest extends CommandLineProgramTest {
     public static final String converterTestDir = toolsTestDir + "walkers/sv/convertCountsToDepthFile";
 
-    @Test
-    public void testSuccessfullConvert() throws IOException {
-        final String inputFilename = converterTestDir + "/NA12878.counts.tsv";
-        final String expectedOutput = converterTestDir + "/expected_output" + DepthEvidenceCodec.FORMAT_SUFFIX;
+    public record TestCase(
+            String testLabel,
+            String inputFilename,
+            String expectedOutputFilename,
+            String sampleLabel)
+    { }
 
+    @DataProvider
+    public TestCase[] cases()
+    {
+        return new TestCase[]
+            {
+                new TestCase(
+                    "Test successful convert counts to depth file",
+                    converterTestDir + "/NA12878.counts.tsv",
+                    converterTestDir + "/expected_output" + DepthEvidenceCodec.FORMAT_SUFFIX + ".gz",
+                    "NA12878"),
+                new TestCase(
+                    "Test successful read sample name from file header",
+                    converterTestDir + "/NA12878.counts.tsv",
+                    converterTestDir + "/expected_output" + DepthEvidenceCodec.FORMAT_SUFFIX + ".gz",
+                    null)
+            };
+    }
+    @Test(dataProvider="cases")
+    public void testSuccessfulConvert(final TestCase testCase) throws IOException {
         final String args =
-                " --counts-filename " + inputFilename +
-                " --" + StandardArgumentDefinitions.SEQUENCE_DICTIONARY_NAME + " " + FULL_HG38_DICT +
-                " --sample-name test" +
-                " -" + StandardArgumentDefinitions.OUTPUT_SHORT_NAME + " %s";
+            " --" + ConvertCountsToDepthFile.COUNTS_FILENAME_ARG_FULL_NAME + " " + testCase.inputFilename +
+            " --" + StandardArgumentDefinitions.SEQUENCE_DICTIONARY_NAME + " " + FULL_HG38_DICT +
+            ((testCase.inputFilename == null) ?
+                    "" :
+                    (" --" + ConvertCountsToDepthFile.SAMPLE_NAME_ARG_FULL_NAME + " " + testCase.sampleLabel)) +
+            " -" + StandardArgumentDefinitions.OUTPUT_SHORT_NAME + " %s";
 
-        final IntegrationTestSpec testSpec = new IntegrationTestSpec(args, Collections.singletonList(expectedOutput));
-        testSpec.setOutputFileExtension(DepthEvidenceCodec.FORMAT_SUFFIX);
-
-        testSpec.executeTest("test successful convert counts to depth file", this);
+        final IntegrationTestSpec testSpec = new IntegrationTestSpec(args, Collections.singletonList(testCase.expectedOutputFilename));
+        testSpec.setOutputFileExtension(DepthEvidenceCodec.FORMAT_SUFFIX + ".gz");
+        testSpec.executeTest(testCase.testLabel, this);
     }
 }
