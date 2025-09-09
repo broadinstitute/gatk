01decd83f9d582b9b4e5eab87d326e73d4448621
ggrant <ggrant@broadinstitute.org>
2025-09-09 11:03:38 -0400

    Stupid try

--- src/test/java/org/broadinstitute/hellbender/tools/gvs/extract/ExtractCohortToPgenTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/gvs/extract/ExtractCohortToPgenTest.java
@@ -157,6 +157,16 @@ public class ExtractCohortToPgenTest extends CommandLineProgramTest {
     IntegrationTestSpec.assertEqualTextFiles(outputPsam, expectedPsam);
     // Decompress the pvar for validation
     final File decompressedPvar = decompressPvar(outputPvar);
+    System.out.println("Hello");
+    System.err.println("There");
+    try (BufferedReader reader = new BufferedReader(new FileReader(decompressedPvar))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to read decompressed pvar file: ", e);
+    }
     IntegrationTestSpec.assertEqualTextFiles(decompressedPvar, expectedPvar);
   }
 
