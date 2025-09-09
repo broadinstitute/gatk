24f4bbed8ca60a32e4ae748bb9ef437163950cab
Mark Walker <markw@broadinstitute.org>
2025-09-09 12:20:09 -0400

    Update AggregateSVEvidenceTest

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/sv/AggregateSVEvidenceTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/sv/AggregateSVEvidenceTest.java
@@ -6,22 +6,53 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.broadinstitute.hellbender.engine.GatkToolIntegrationTest;
 import org.broadinstitute.hellbender.testutils.ArgumentsBuilder;
 import org.broadinstitute.hellbender.testutils.VariantContextTestUtils;
+import org.broadinstitute.hellbender.tools.spark.sv.utils.GATKSVVCFConstants;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 public class AggregateSVEvidenceTest extends GatkToolIntegrationTest {
 
+    // If true, update the expected outputs in tests that assert an exact or approximate match vs. prior output,
+    // instead of actually running the tests.
+    public static final boolean UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS = false;
+
     public static final String TOOL_TEST_DIR = toolsTestDir + "walkers/sv/AggregatePairedEndAndSplitReadEvidence";
+
+    public static final double FLOAT_TOLERANCE = 1e-4;
 
     public static final int PE_INNER_WINDOW = 50;
     public static final int PE_OUTER_WINDOW = 500;
     public static final int SR_WINDOW = 200;
     public static final int SR_CROSSOVER = 20;
+
+    public static final List<String> attributesWithJitter = Arrays.asList(
+            GATKSVVCFConstants.FIRST_SPLIT_QUALITY_ATTRIBUTE,
+            GATKSVVCFConstants.FIRST_SPLIT_CARRIER_SIGNAL_ATTRIBUTE,
+            GATKSVVCFConstants.SECOND_SPLIT_QUALITY_ATTRIBUTE,
+            GATKSVVCFConstants.SECOND_SPLIT_CARRIER_SIGNAL_ATTRIBUTE,
+            GATKSVVCFConstants.TOTAL_SPLIT_QUALITY_ATTRIBUTE,
+            GATKSVVCFConstants.TOTAL_SPLIT_CARRIER_SIGNAL_ATTRIBUTE,
+            GATKSVVCFConstants.DISCORDANT_PAIR_QUALITY_ATTRIBUTE,
+            GATKSVVCFConstants.DISCORDANT_PAIR_CARRIER_SIGNAL_ATTRIBUTE,
+            GATKSVVCFConstants.PESR_QUALITY_ATTRIBUTE,
+            GATKSVVCFConstants.PESR_CARRIER_SIGNAL_ATTRIBUTE,
+            GATKSVVCFConstants.BAF_HET_RATIO_ATTRIBUTE,
+            GATKSVVCFConstants.BAF_KS_STAT_ATTRIBUTE,
+            GATKSVVCFConstants.BAF_KS_Q_ATTRIBUTE
+    );
+    /**
+     * Make sure that someone didn't leave the {@value UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS} toggle turned on
+     */
+    @Test
+    public void assertThatExpectedOutputUpdateToggleIsDisabled() {
+        Assert.assertFalse(UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS, "The toggle to update expected outputs should not be left enabled");
+    }
 
     @Test
     public void test() {
@@ -29,7 +60,7 @@ public class AggregateSVEvidenceTest extends GatkToolIntegrationTest {
         final File peFile = new File(TOOL_TEST_DIR, "test_hg38.pe.txt.gz");
         final File srFile = new File(TOOL_TEST_DIR, "test_hg38.sr.txt.gz");
         final File coverageFile = new File(TOOL_TEST_DIR, "test_hg38.sample_coverage.tsv");
-        final File outputFile = createTempFile("aggregated", ".vcf.gz");
+        final String outputFile = UPDATE_EXACT_MATCH_EXPECTED_OUTPUTS ? TOOL_TEST_DIR + "/test_hg38.expected.vcf.gz" : createTempFile("aggregated", ".vcf.gz").getAbsolutePath();
 
         final ArgumentsBuilder args = new ArgumentsBuilder()
                 .addVCF(vcfFile)
@@ -47,7 +78,7 @@ public class AggregateSVEvidenceTest extends GatkToolIntegrationTest {
 
         final File expectedFile = new File(TOOL_TEST_DIR, "test_hg38.expected.vcf.gz");
         final Pair<VCFHeader, List<VariantContext>> expected = VariantContextTestUtils.readEntireVCFIntoMemory(expectedFile.getPath());
-        final Pair<VCFHeader, List<VariantContext>> output = VariantContextTestUtils.readEntireVCFIntoMemory(outputFile.getPath());
+        final Pair<VCFHeader, List<VariantContext>> output = VariantContextTestUtils.readEntireVCFIntoMemory(outputFile);
 
         Assert.assertEquals(expected.getValue().size(), output.getValue().size());
         final Iterator<VariantContext> expectedIterator = expected.getValue().iterator();
@@ -55,7 +86,15 @@ public class AggregateSVEvidenceTest extends GatkToolIntegrationTest {
         while (expectedIterator.hasNext()) {
             final VariantContext expectedVariant = expectedIterator.next();
             final VariantContext variant = outputIterator.next();
-            VariantContextTestUtils.assertVariantContextsAreEqual(variant, expectedVariant, Collections.emptyList(), Collections.emptyList());
+            VariantContextTestUtils.assertVariantContextsAreEqual(variant, expectedVariant, Collections.emptyList(), attributesWithJitter);
+            for (final String attribute : attributesWithJitter) {
+                final double expectedValue = expectedVariant.getAttributeAsDouble(attribute, 0);
+                if (Double.isNaN(expectedValue)) {
+                    Assert.assertTrue(Double.isNaN(variant.getAttributeAsDouble(attribute, 0)));
+                } else {
+                    Assert.assertTrue(Math.abs(expectedValue - variant.getAttributeAsDouble(attribute, 0)) < FLOAT_TOLERANCE);
+                }
+            }
         }
     }
 }
