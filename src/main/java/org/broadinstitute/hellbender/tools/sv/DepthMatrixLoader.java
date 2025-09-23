e9bfc01d856b68dddcaa39d645f434d1c6c2de01
Mark Walker <markw@broadinstitute.org>
2025-09-23 17:50:05 -0400

    Finish loader cleanup

--- src/main/java/org/broadinstitute/hellbender/tools/sv/DepthMatrixLoader.java
+++ src/main/java/org/broadinstitute/hellbender/tools/sv/DepthMatrixLoader.java
@@ -2,7 +2,6 @@ package org.broadinstitute.hellbender.tools.sv;
 
 import htsjdk.samtools.SAMSequenceDictionary;
 import org.broadinstitute.hellbender.exceptions.GATKException;
-import org.broadinstitute.hellbender.tools.spark.sv.utils.SVUtils;
 import org.broadinstitute.hellbender.utils.IntervalUtils;
 import org.broadinstitute.hellbender.utils.SimpleInterval;
 import org.broadinstitute.hellbender.engine.FeatureDataSource;
@@ -12,6 +11,8 @@ import picard.sam.util.Pair;
 import java.util.*;
 
 public class DepthMatrixLoader {
+
+    protected static final int MIN_BINS_FOR_TRIMMING = 4;
 
     protected final FeatureDataSource<DepthEvidence> dataSource;
     protected final SAMSequenceDictionary dictionary;
@@ -24,13 +25,14 @@ public class DepthMatrixLoader {
     }
 
     public DepthMatrixLoader(final FeatureDataSource<DepthEvidence> dataSource,
-                             int bins, long largeSizeCutoff, int largeVariantRegionPoints, int largeVariantWindow,
+                             final int bins, final long largeSizeCutoff, final int largeVariantRegionPoints, final int largeVariantWindow,
                              final SAMSequenceDictionary dictionary) {
         Utils.nonNull(dataSource);
         Utils.validateArg(bins > 0, "bins must be greater than zero");
         Utils.validateArg(largeSizeCutoff >= 0, "large size cutoff must be non-negative");
         Utils.validateArg(largeVariantRegionPoints > 0, "large variant region points must be greater than zero");
         Utils.validateArg(largeVariantWindow >= 0, "large variant region window must be non-negative");
+        Utils.nonNull(dictionary);
         this.dataSource = dataSource;
         this.dictionary = dictionary;
         this.bins = bins;
@@ -45,7 +47,6 @@ public class DepthMatrixLoader {
         if (!IntervalUtils.intervalIsOnDictionaryContig(interval, dictionary)) {
             throw new GATKException("Load interval " + interval + " failed to validate against the sequence dictionary");
         }
-        Utils.nonNull(dataSource);
         Utils.nonNull(sampleMedians);
 
         final DepthMatrix rawMatrix;
@@ -56,10 +57,10 @@ public class DepthMatrixLoader {
         }
 
         // Calculate compression and region bounds
-        final CompressionResult compressionResult = calculateCompression(rawMatrix, interval);
+        final CompressionResult compressionResult = calculateCompression(rawMatrix, interval, bins);
 
         // Trim bins from compression
-        final DepthMatrix trimmedMatrix = trimMatrix(rawMatrix, compressionResult);
+        final DepthMatrix trimmedMatrix = trimMatrixBeforeCompression(rawMatrix, compressionResult);
 
         // Set 0 entries to 1
         setZeroesToOnes(trimmedMatrix);
@@ -70,7 +71,7 @@ public class DepthMatrixLoader {
 
         // Normalize by sample depth
         final DepthMatrix normalizedMatrix = normalizeMatrix(compressedMatrix, compressedMedians);
-        return trimMatrix(normalizedMatrix);
+        return trimOuterBins(normalizedMatrix);
     }
 
     private static void setZeroesToOnes(final DepthMatrix matrix) {
@@ -85,7 +86,7 @@ public class DepthMatrixLoader {
         }
     }
 
-    private DepthMatrix trimMatrix(final DepthMatrix matrix, final CompressionResult compressionResult) {
+    private static DepthMatrix trimMatrixBeforeCompression(final DepthMatrix matrix, final CompressionResult compressionResult) {
         if (compressionResult == null) {
             return matrix;
         }
@@ -125,9 +126,9 @@ public class DepthMatrixLoader {
         return new DepthMatrix(newBins, newMatrix);
     }
 
-    private DepthMatrix trimMatrix(final DepthMatrix matrix) {
+    private static DepthMatrix trimOuterBins(final DepthMatrix matrix) {
         final int numBins = matrix.getNumBins();
-        final boolean subset = numBins >= 4;
+        final boolean subset = numBins >= MIN_BINS_FOR_TRIMMING;
         final List<SimpleInterval> newBins = subset ? matrix.getBins().subList(1, numBins - 1) : matrix.getBins();
         final Map<String, double[]> newMatrix = new HashMap<>();
         for (final String sample : matrix.getSampleSet()) {
@@ -138,7 +139,7 @@ public class DepthMatrixLoader {
         return new DepthMatrix(newBins, newMatrix);
     }
 
-    private CompressionResult calculateCompression(final DepthMatrix matrix, final SimpleInterval interval) {
+    private static CompressionResult calculateCompression(final DepthMatrix matrix, final SimpleInterval interval, final int bins) {
         final int start = interval.getStart() - 1; // covert to 0-based to match R code
         final int end = interval.getEnd();
 
@@ -193,7 +194,7 @@ public class DepthMatrixLoader {
         return new CompressionResult(compression, adjustedRegion);
     }
 
-    private DepthMatrix compressMatrix(final DepthMatrix matrix,
+    private static DepthMatrix compressMatrix(final DepthMatrix matrix,
                                        final CompressionResult compressionResult) {
         if (compressionResult == null || compressionResult.compression() <= 1 || matrix.isEmpty()) {
             return matrix;
@@ -211,7 +212,7 @@ public class DepthMatrixLoader {
         return new DepthMatrix(compressedBins, compressedMatrix);
     }
 
-    private Map<String, Double> compressMedians(final Map<String, Double> sampleMedians,
+    private static Map<String, Double> compressMedians(final Map<String, Double> sampleMedians,
                                                 final CompressionResult compressionResult) {
         if (compressionResult == null || compressionResult.compression() <= 1) {
             return sampleMedians;
@@ -260,7 +261,7 @@ public class DepthMatrixLoader {
         return Collections.unmodifiableList(result);
     }
 
-    private List<SimpleInterval> compressBins(final List<SimpleInterval> bins, final List<Pair<Integer, Integer>> indices) {
+    private static List<SimpleInterval> compressBins(final List<SimpleInterval> bins, final List<Pair<Integer, Integer>> indices) {
         final List<SimpleInterval> compressedBins = new ArrayList<>(indices.size());
         for (final Pair<Integer, Integer> pair : indices) {
             compressedBins.add(makeCompressedBin(bins, pair.getLeft(), pair.getRight()));
@@ -268,7 +269,7 @@ public class DepthMatrixLoader {
         return compressedBins;
     }
 
-    private SimpleInterval makeCompressedBin(final List<SimpleInterval> bins, final int startIndex, final int endIndex) {
+    private static SimpleInterval makeCompressedBin(final List<SimpleInterval> bins, final int startIndex, final int endIndex) {
         if (startIndex < 0 || startIndex >= bins.size()) {
             throw new IllegalArgumentException("Invalid start bin index: " + startIndex);
         }
