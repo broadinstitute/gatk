f31bd4d789218f3526bbd5932c4938f5f0d7a57c
Mark Walker <markw@broadinstitute.org>
2025-10-07 12:13:53 -0400

    Update docs; use master sequence dictionary in SVRegionOverlap

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/sv/AggregateDepthEvidence.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/sv/AggregateDepthEvidence.java
@@ -9,6 +9,7 @@ import htsjdk.variant.vcf.*;
 import org.apache.arrow.util.VisibleForTesting;
 import org.apache.commons.io.IOUtils;
 import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.barclay.argparser.BetaFeature;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.argparser.ExperimentalFeature;
 import org.broadinstitute.barclay.help.DocumentedFeature;
@@ -27,13 +28,66 @@ import java.nio.charset.Charset;
 import java.util.*;
 
 /**
- * AggregateDepthEvidence
+ * <p>This tool assesses split read (SR), discordant paired-end (PE), and B-allele frequency (BAF) evidence for structural variants (SVs),
+ * annotating records with statistical metrics that can be used to assess a variant's quality. The input VCF should
+ * contain multiple samples with GT fields populated. Note that this tool only considers carrier status and does not
+ * differentiate heterozygous from homozygous variant genotypes. For read depth evidence metrics, see
+ * the AggregateDepthEvidence tool.</p>
+ *
+ * <p>Detailed methodology can be found in the supplement of <a href="https://doi.org/10.1038/s41586-020-2287-8">Collins et al. 2020</a>.</p>
+ *
+ * <p>Briefly, for each variant the supporting split reads and discordant pairs are counted. Phred-scaled quality scores
+ * (SRQ, PEQ, PESRQ) are then computed based on a Poisson test of the observed median carrier sample signal against
+ * background. The raw fraction of median SR signal attributed to carriers (SRCS, PECS, PESRCS) is also annotated as an additional
+ * metric to assess concordance between detected evidence and genotypes.</p>
+ *
+ * <p>During SR aggregation, breakpoint refinement is performed (SR1POS, SR2POS) by maximizing the quality score
+ * over all positions within a small window around each end of the variant.</p>
+ *
+ * <p>Bi-allelic copy number variants are also assessed using BAF evidence. Deletions are annotated with the ratio of heterozygous
+ * SNPs in carrier samples to in controls (BAF_HET_RATIO). Duplications are assessed by comparing the distribution of
+ * BAFs across SNPs with a Kolmogorov-Smirnov test statistic (BAF_KS_STAT), which is used to compute a quality
+ * score (BAF_KS_Q).</p>
+ *
+ * <h3>Inputs</h3>
+ *
+ * <ul>
+ *     <li>
+ *         SV VCF
+ *     </li>
+ *     <li>
+ *         Depth evidence file
+ *     </li>
+ *     <li>
+ *         Median binned read counts table
+ *     </li>
+ * </ul>
+ *
+ * <h3>Output</h3>
+ *
+ * <ul>
+ *     <li>
+ *         SV VCF
+ *     </li>
+ * </ul>
+ *
+ * <h3>Usage example</h3>
+ *
+ * <pre>
+ *     gatk AggregateDepthEvidence \
+ *      -V input.vcf.gz \
+ *      -O output.vcf.gz \
+ *      --median-coverage median_coverage.tsv \
+ *      --rd-file all_samples.rd.txt.gz
+ * </pre>
+ *
+ * @author Mark Walker &lt;markw@broadinstitute.org&gt;
  */
 @DocumentedFeature
-@ExperimentalFeature
+@BetaFeature
 @CommandLineProgramProperties(
-        summary = "Batch CNV Interval Genotyping - GATK VariantWalker Port of RdTest.R",
-        oneLineSummary = "Batch CNV Interval Genotyping - GATK Java VariantWalker port",
+        summary = "Read depth evidence assessment tool for copy number variants",
+        oneLineSummary = "Read depth evidence assessment tool for copy number variants",
         programGroup = StructuralVariantDiscoveryProgramGroup.class
 )
 public final class AggregateDepthEvidence extends VariantWalker {
@@ -49,7 +103,7 @@ public final class AggregateDepthEvidence extends VariantWalker {
 
     @Argument(
             fullName = DEPTH_EVIDENCE_FILE_PATH_LONG_NAME,
-            doc = "Read counts file ending with " + DepthEvidenceCodec.FORMAT_SUFFIX + "' (may be gzipped)."
+            doc = "Indexed read counts file ending with " + DepthEvidenceCodec.FORMAT_SUFFIX + ".gz"
     )
     public GATKPath evidenceFile;
 
