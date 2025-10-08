da7254bfdda465981e27cae799f1432c9e4fd710
Aaron Hatcher <hatcher@broadinstitute.org>
2025-10-08 09:15:41 -0400

    no more build issues

--- src/main/java/org/broadinstitute/hellbender/utils/gvs/parquet/GvsVariantParquetFileWriter.java
+++ src/main/java/org/broadinstitute/hellbender/utils/gvs/parquet/GvsVariantParquetFileWriter.java
@@ -2,22 +2,23 @@ package org.broadinstitute.hellbender.utils.gvs.parquet;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.parquet.column.ParquetProperties;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.api.WriteSupport;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
-import org.apache.parquet.io.OutputFile;
 import org.apache.parquet.schema.MessageType;
 import org.json.JSONObject;
 
-
 import java.io.IOException;
 
+/**
+ * A wrapper around ParquetWriter that simplifies writing JSON objects to Parquet files.
+ * 
+ * This implementation uses the modern composition pattern where WriteSupport is created
+ * independently and passed to the Builder, rather than creating it inside a deprecated
+ * getWriteSupport(Configuration) method. This approach provides better testability,
+ * flexibility, and follows dependency injection principles.
+ */
 public class GvsVariantParquetFileWriter {
-    /**
-     * Let the top-level class simply create and delegate to the actual writer, so users of it
-     * don't have to worry about details like builders and obscure arguments
-     */
     private ParquetWriter<JSONObject> parquetWriterImpl;
 
     public GvsVariantParquetFileWriter(
@@ -25,9 +26,12 @@ public class GvsVariantParquetFileWriter {
             MessageType schema,
             CompressionCodecName codecName
     ) throws IOException {
-        GvsVariantParquetFileWriter.Builder builder = new Builder(file);
-        this.parquetWriterImpl = builder.withType(schema)
-                .withCompressionCodec(codecName).build();
+        // Use composition pattern: create WriteSupport independently
+        WriteSupport<JSONObject> writeSupport = new GvsVariantWriteSupport(schema);
+        
+        // Use our modern Builder that accepts WriteSupport via constructor
+        Builder builder = new Builder(file, writeSupport);
+        this.parquetWriterImpl = builder.withCompressionCodec(codecName).build();
     }
 
     public void write(JSONObject object) throws IOException {
@@ -38,25 +42,55 @@ public class GvsVariantParquetFileWriter {
         this.parquetWriterImpl.close();
     }
 
+    /**
+     * Modern Builder implementation using composition pattern.
+     * 
+     * The WriteSupport is injected via constructor rather than created inside
+     * getWriteSupport(), which provides better separation of concerns and testability.
+     * 
+     * Note: The getWriteSupport(Configuration) method is deprecated in Parquet 1.13.1
+     * but there's no alternative method available yet. The @SuppressWarnings annotation
+     * is used because:
+     * 1. This is the only way to implement ParquetWriter.Builder in this version
+     * 2. We're using best practices (composition) despite the deprecated signature
+     * 3. The Configuration parameter is ignored as intended by the new design
+     */
     public static class Builder extends ParquetWriter.Builder<JSONObject, Builder> {
-        private MessageType schema = null;
+        private final WriteSupport<JSONObject> writeSupport;
 
-        private Builder(Path file) {
+        /**
+         * Creates a builder with injected WriteSupport (composition pattern).
+         * 
+         * @param file the output file path
+         * @param writeSupport the WriteSupport instance to use (injected dependency)
+         */
+        public Builder(Path file, WriteSupport<JSONObject> writeSupport) {
             super(file);
+            this.writeSupport = writeSupport;
         }
 
-        public Builder withType(MessageType type) {
-            this.schema = type;
-            return this;
-        }
         @Override
         protected Builder self() {
             return this;
         }
 
+        /**
+         * Returns the WriteSupport instance provided via constructor.
+         * 
+         * This method is deprecated but still required to implement ParquetWriter.Builder.
+         * We suppress the deprecation warning because:
+         * - This is the only way to extend ParquetWriter.Builder in Parquet 1.13.1
+         * - We follow modern composition patterns by injecting WriteSupport via constructor
+         * - The Configuration parameter is properly ignored (not used)
+         * - This approach is more testable and flexible than creating WriteSupport here
+         * 
+         * @param configuration ignored parameter (as intended by modern design)
+         * @return the WriteSupport instance provided at construction time
+         */
         @Override
+        @SuppressWarnings("deprecation")
         protected WriteSupport<JSONObject> getWriteSupport(Configuration configuration) {
-            return new GvsVariantWriteSupport(schema);
+            return this.writeSupport;
         }
     }
 }
