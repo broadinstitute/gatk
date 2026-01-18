85038940c7ca40d2dfe6ec9ee402729a990208db
David Benjamin <davidben@broadinstitute.org>
2026-01-18 00:51:18 -0500

    first pass at the meat of Mutect2ActivityProfile.  Probably does not work yet.

--- src/main/java/org/broadinstitute/hellbender/utils/activityprofile/ActivityProfile.java
+++ src/main/java/org/broadinstitute/hellbender/utils/activityprofile/ActivityProfile.java
@@ -355,7 +355,7 @@ public class ActivityProfile {
      * @param maxRegionSize the maximize size of the returned region
      * @return a Pair containing the size of the next region and whether it is active
      */
-    private Pair<Integer, Boolean> findSizeOfRegionAndActivity(final int minRegionSize, final int maxRegionSize) {
+    protected Pair<Integer, Boolean> findSizeOfRegionAndActivity(final int minRegionSize, final int maxRegionSize) {
         Utils.validate(!stateList.isEmpty(), "state list is empty");
         Utils.validateArg(minRegionSize >= 0, "minRegionSize must be >= 0");
         Utils.validateArg(maxRegionSize >= minRegionSize, "Max region size is less than min region size.");
@@ -380,7 +380,7 @@ public class ActivityProfile {
      * @param index a valid offset into the state list
      * @return the isActiveProb of the state at index
      */
-    private double getProb(final int index) {
+    protected double getProb(final int index) {
         Utils.validIndex(index, stateList.size());
 
         return stateList.get(index).isActiveProb();
--- src/main/java/org/broadinstitute/hellbender/utils/activityprofile/Mutect2ActivityProfile.java
+++ src/main/java/org/broadinstitute/hellbender/utils/activityprofile/Mutect2ActivityProfile.java
@@ -1,21 +1,78 @@
 package org.broadinstitute.hellbender.utils.activityprofile;
 
 import htsjdk.samtools.SAMFileHeader;
+import org.apache.commons.lang3.tuple.Pair;
 import org.broadinstitute.hellbender.engine.spark.AssemblyRegionArgumentCollection;
 import org.broadinstitute.hellbender.utils.SimpleInterval;
+import org.broadinstitute.hellbender.utils.Utils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Mutect2ActivityProfile extends ActivityProfile {
+
     public Mutect2ActivityProfile(final AssemblyRegionArgumentCollection args, final SAMFileHeader header) {
+        // note that in this class maxProbPropagationDistance is interpreted as a maximum phasing distance
+        // we don't "propagate" probability like the BandPassActivityFilter
         super(args.maxProbPropagationDistance, args.activeProbThreshold, header);
     }
 
     @Override
     protected Collection<ActivityProfileState> processState(final ActivityProfileState justAddedState) {
         return Collections.singletonList(justAddedState);
+    }
+
+    // if two active loci are separated by this distance or less, they go in the same AssemblyRegion
+    // eg if maxPhasingDistance is 10, sites 1 and 11 are assembled together but 1 and 12 are not.
+    private int maxPhasingDistance() { return getMaxProbPropagationDistance(); }
+
+    /**
+     * Note: when this is called we have already checked readyToPopNextAssemblyRegion().  Therefore, either 1) we are at the
+     * end of an interval and have to form a region without knowing about potential variants past the interval's end or 2)
+     * the state list spans the maxRegionSize PLUS the maxPhasingDistance.
+     *
+     * In either case, we have enough information to make regions up to Math.min(stateList.size(), maxRegionSize) bases.
+     *
+     * Furthermore, any previous region has already checked that it's out of phasing distance with these states.
+
+     */
+    @Override
+    protected Pair<Integer, Boolean> findSizeOfRegionAndActivity(final int minRegionSize, final int maxRegionSize) {
+        // TODO: should this be padded?  The BandPassActivityProfile implicitly pads by propagating
+        // TODO: probability with the Gaussian filter
+        // TODO: I'm especially nervous about indels -- maybe the actual variant is a base or two before the
+        // TODO: active pileup
+        Utils.validate(!stateList.isEmpty(), "This code should only be called when there are states with which to form a region.");
+
+        final int maxSize = Math.min(stateList.size(), maxRegionSize);
+
+        // If inactive, extend up to the first active site or the maximum possible size
+        // If active, extend until we reach an inactive gap longer than the max phasing distance
+        if (getProb(0) < activeProbThreshold) {
+            final int numInactiveStates = IntStream.range(0, maxSize)
+                    .filter(n -> getProb(n) >= activeProbThreshold)
+                    .findFirst().orElse(maxSize);
+
+            return Pair.of(numInactiveStates, false);
+        } else {
+            int lastActiveIdx = 0;
+            int startOfLargestGap = 0;  // the active site right before the longest inactive gap
+            int maxGapLength = 0;
+            for (int idx = 0; idx < maxSize; idx++) {
+                final int gapLength = idx - lastActiveIdx - 1;
+                if (gapLength > maxGapLength) {
+                    startOfLargestGap = lastActiveIdx;
+                    maxGapLength = gapLength;
+                }
+
+                if (idx > lastActiveIdx + maxPhasingDistance()) {
+                    break;
+                } else if (getProb(idx) >= activeProbThreshold) {
+                    lastActiveIdx = idx;
+                }
+            }
+
+            return Pair.of(startOfLargestGap, true);
+        }
     }
 }
