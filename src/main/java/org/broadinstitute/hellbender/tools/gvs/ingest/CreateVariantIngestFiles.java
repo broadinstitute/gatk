e2d666eaca3700dc2b353c109520c6c5c3d49e80
Miguel Covarrubias <mcovarr@broadinstitute.org>
2026-02-26 17:41:55 -0500

    fix parquet schema

--- src/main/java/org/broadinstitute/hellbender/tools/gvs/ingest/CreateVariantIngestFiles.java
+++ src/main/java/org/broadinstitute/hellbender/tools/gvs/ingest/CreateVariantIngestFiles.java
@@ -211,6 +211,14 @@ public final class CreateVariantIngestFiles extends VariantWalker {
             }
             """);
 
+    public final MessageType refRangesCompressedRowSchema = MessageTypeParser
+            .parseMessageType("""
+            message RefRangesRow {
+                required int64 sample_id;
+                required int64 packed_ref_data;
+            }
+            """);
+
     public final MessageType headersRowSchema = MessageTypeParser
             .parseMessageType("""
             message HeaderRow {
@@ -353,7 +361,8 @@ public final class CreateVariantIngestFiles extends VariantWalker {
 
         if (enableReferenceRanges && refRangesRowsExist == Boolean.FALSE) {
             logger.info("Writing reference range data for sample id = {}, name = {}", sampleId, sampleName);
-            refCreator = new RefCreator(sampleIdentifierForOutputFileName, sampleId, tableNumber, seqDictionary, gqStatesToIgnore, outputDir, outputType, enableReferenceRanges, projectID, datasetName, storeCompressedReferences, refRangesRowSchema);
+            MessageType refRangesParquetSchema = storeCompressedReferences ? refRangesCompressedRowSchema : refRangesRowSchema;
+            refCreator = new RefCreator(sampleIdentifierForOutputFileName, sampleId, tableNumber, seqDictionary, gqStatesToIgnore, outputDir, outputType, enableReferenceRanges, projectID, datasetName, storeCompressedReferences, refRangesParquetSchema);
 
             // The ploidy table is really only needed for inferring reference ploidy, as variants store their genotypes
             // directly.  If we're not ingesting references, we can't compute and ingest ploidy either
