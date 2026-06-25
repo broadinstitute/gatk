b804b5c261f31f4aa74eec1fd0110bcab1195f86
David Benjamin <davidben@broadinstitute.org>
2026-06-25 09:00:27 -0400

    wrote the trivial processState override in M2ActivityProfile -- no band pass filter stuff

--- src/main/java/org/broadinstitute/hellbender/utils/activityprofile/Mutect2ActivityProfile.java
+++ src/main/java/org/broadinstitute/hellbender/utils/activityprofile/Mutect2ActivityProfile.java
@@ -3,8 +3,16 @@ package org.broadinstitute.hellbender.utils.activityprofile;
 import htsjdk.samtools.SAMFileHeader;
 import org.broadinstitute.hellbender.engine.spark.AssemblyRegionArgumentCollection;
 
+import java.util.Collection;
+import java.util.Collections;
+
 public class Mutect2ActivityProfile extends ActivityProfile {
     public Mutect2ActivityProfile(final AssemblyRegionArgumentCollection args, final SAMFileHeader header) {
         super(args.maxProbPropagationDistance, args.activeProbThreshold, header);
+    }
+
+    @Override
+    protected Collection<ActivityProfileState> processState(final ActivityProfileState justAddedState) {
+        return Collections.singletonList(justAddedState);
     }
 }
