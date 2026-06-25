240bcf6065fb791194fe83e15b992a83cc3d258c
David Benjamin <davidben@broadinstitute.org>
2026-06-25 08:58:05 -0400

    simplify code for trimming extra states when forceConversion is true

--- src/main/java/org/broadinstitute/hellbender/utils/activityprofile/ActivityProfile.java
+++ src/main/java/org/broadinstitute/hellbender/utils/activityprofile/ActivityProfile.java
@@ -323,8 +323,7 @@ public class ActivityProfile {
     private AssemblyRegion popNextReadyAssemblyRegion( final int assemblyRegionExtension, final int minRegionSize, final int maxRegionSize, final boolean forceConversion ) {
         // If we are flushing the activity profile we need to trim off the excess states so that we don't create regions outside of our current processing interval
         if( forceConversion ) {
-            final List<ActivityProfileState> statesToTrimAway = new ArrayList<>(stateList.subList(getSpan().size(), stateList.size()));
-            stateList.removeAll(statesToTrimAway);
+            stateList.subList(getSpan().size(), stateList.size()).clear();
         }
 
         final ActivityProfileState first = stateList.get(0);
